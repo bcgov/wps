b08c7b6aff743a08781d0100c44db73d9e4fd6b6
DerekRoberts <derek.roberts@gov.bc.ca>
2020-01-28 18:22:40 -0800

    config update

--- .jenkins/.pipeline/lib/config.js
+++ .jenkins/.pipeline/lib/config.js
@@ -5,8 +5,8 @@ const version = '1.0.0'
 const name = 'jenkins'
 
 const phases = {
-  build: {namespace:'auzhsi-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
-  dev: {namespace:'auzhsi-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
+  build: {namespace:'auzhsi-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`, transient:true},
+  dev: {namespace:'auzhsi-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`, transient:true},
   prod: {namespace:'auzhsi-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
 };
 
