5fe5758c3c22315d664f3f1ab23b506e232e46f7
Kyubinhan <kyubinhan11@gmail.com>
2020-02-05 13:30:16 -0800

    Add a basic Hello World React app with ESLint and Husky (#2)
    * Create a React app bootstrapped with Create React App.
    
    * Set up ESlint with TypeScript
    
    * Add Husky for git hooks
    
    * Organize paths with baseURL as src and modify .gitignore a bit
    
    * Add BC Sans font to the body tag

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Hello World!</header>
+    </div>
+  )
+}
+
+export default App
