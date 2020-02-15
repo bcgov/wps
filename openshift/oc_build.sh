189b546fc7d3c3714480ddde6c5ffb959801ea20
Derek Roberts <derek.roberts@gmail.com>
2020-02-14 16:16:41 -0800

    Build now using script

---
+++ openshift/oc_build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+# Halt on errors/unsets, change fail returns, change field separator
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+
+# Parameters and defaults
+#
+PR_NO=${1:-}
+#
+NAME=${APPLICATION_NAME:-wps}
+PROJECT=${PROJECT:-auzhsi-tools}
+PATH_BC=${PATH_BC:-templates/wps.bc.yaml}
+GIT_URL=${GIT_URL:-$(git remote get-url origin)}
+GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+
+
+# Show message if no params
+#
+if [ "${#}" -lt 1 ]
+then
+	echo
+	echo "OC Template Builder"
+	echo
+	echo "Provide a pull request number."
+	echo " './$(basename $0) <PR_NUMBER>'"
+	echo
+	echo "Override variables at runtime.  E.g.:"
+	echo " 'VERBOSE=true GIT_BRANCH=master ./$(basename $0) <...>'"
+	echo
+	exit
+fi
+
+
+# Function for oc command
+ocBuild() {
+    oc -n "${PROJECT}" process -f "${PATH_BC}" -p NAME="${NAME}" -p SUFFIX=pr-"${PR_NO}" \
+        -p GIT_URL="${GIT_URL}" -p GIT_REF="${GIT_BRANCH}"
+}
+TEMPLATE=$(ocBuild)
+echo "Template: ---"
+echo "${TEMPLATE}\n\n"
+echo "${TEMPLATE}" | oc -n "${PROJECT}" apply -f - --dry-run
+set -x
+ocBuild | oc -n "${PROJECT}" apply -f -
