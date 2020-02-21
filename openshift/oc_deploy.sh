d5b57f771ec01650b8cbc47d446c2cde37421010
Derek Roberts <4391600+DerekRoberts@users.noreply.github.com>
2020-02-20 17:17:00 -0800

    Very basic build (#81)
    * Very basic build
    
    * Copy and paste quick run
    
    * Excluded .vscode in .gitignore
    
    * Cleaned up temp build config
    
    * Cleaned up temp deployment config
    
    * Jenkins no longer hard coding commands
    
    * Cut down bc and dc
    
    * Renamed and moved demo templates
    
    * Removed VERSION from bc
    
    * Removed var
    
    * Build now using script
    
    * Script now runs from any path
    
    * Simplified build script
    
    * Deploy script
    
    * Cleaned up script output
    
    * Jenkinsfile deploying with script
    
    * oc_build.sh cleaned up, defaults to dry run
    
    * oc_build.sh - verify oc login
    
    * Improved oc_build.sh output
    
    * Improved oc_build.sh output further
    
    * oc_build.sh overhauled to use commented help in script
    
    * oc_deploy.sh given same treatment as oc_build.sh
    
    * Jenkinsfile now full of stubs
    
    * Auto-formatter

---
+++ openshift/oc_deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#%
+#% OpenShift Deploy Helper
+#%
+#%   Intended for use with a pull request-based pipeline.
+#%
+#% Usage:
+#%
+#%   ${THIS_FILE} [PR_NUMBER] [apply]
+#%
+#% Examples:
+#%
+#%   Provide a PR number. Defaults to is a dry-run.
+#%   ${THIS_FILE} 0
+#%
+#%   Apply when satisfied.
+#%   ${THIS_FILE} 0 apply
+#%
+#%   Override variables at runtime.
+#%   NAME=name PROJECT=project PATH_DC=./dc.yaml ${THIS_FILE} 0 apply
+#%
+
+# Halt on errors, unsets and non-zeros exit (pipe fail); change field separator
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+# Parameters and defaults
+#
+THIS_FILE="./$(basename ${0})"
+PR_NO=${1:-}
+APPLY=${2:-}
+#
+NAME=${APPLICATION_NAME:-wps}
+PROJ_TOOLS=${PROJ_TOOLS:-auzhsi-tools}
+PROJ_DEPLOY=${PROJ_DEPLOY:-auzhsi-dev}
+PATH_DC=${PATH_DC:-$(dirname $0)/templates/wps.dc.yaml}
+
+# Show help if no params
+#
+[ "${#}" -gt 0 ] || {
+	cat ${THIS_FILE} | grep "^#%" | sed -e "s|^#%||g" -e "s|\${THIS_FILE}|${THIS_FILE}|g"
+	exit
+}
+
+# Verify login
+#
+$(oc whoami &>/dev/null) || {
+	echo "Please verify oc login"
+	exit
+}
+
+# Process commands
+#
+OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_DC} -p NAME=${NAME} -p SUFFIX=pr-${PR_NO}"
+OC_APPLY="oc -n ${PROJ_DEPLOY} apply -f -"
+OC_COMMAND="${OC_PROCESS} | ${OC_APPLY}"
+#
+[ "${APPLY}" == "apply" ] || {
+	OC_COMMAND+=" --dry-run"
+	eval "${OC_PROCESS}"
+	echo -e "\n*** This is a dry run.  Use 'apply' to deploy. ***\n"
+}
+eval "${OC_COMMAND}"
+echo -e "\n${OC_COMMAND}\n"
