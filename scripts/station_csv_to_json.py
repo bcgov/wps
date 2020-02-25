1496c6b1ed85e88ed52c1663f5ac4a62d39cdd24
Sybrand Strauss <sybrand.strauss@gmail.com>
2020-02-25 12:35:50 -0800

    Exclude inactive and quick deploy stations (#10)
    - formatting station json nicely
    - exclude stations starting with ZZ (inactive) and ending with QD (quick deploy)
    - ordered result by station name)

--- scripts/station_csv_to_json.py
+++ scripts/station_csv_to_json.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 
 with open('Station_BC.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, dialect=csv.unix_dialect)
@@ -14,8 +15,16 @@ with open('Station_BC.csv', 'r') as csvfile:
     # Keep track of station count for debug purposes.
     station_count = 0
     for row in reader:
-        # We're only interested in active weather stations.
+        # We're only interested in permanent, active weather stations
+        # Active stations are either marked as 'active' in the station_category row.
+        # Some stations are incorrectly labeled 'active', station names that start
+        # with ZZ are not active, and must be skipped.
+        # Quick deploy (temporary) stations are marked QD at the end
+        regex = re.compile('^(ZZ)|(.*QD)$', re.I)
         if row[station_category] == 'active':
+            if regex.match(row[station_name]):
+                print('Skipping {}:{}'.format(row[station_code], row[station_name]))
+                continue
             station_count = station_count + 1
             weather_stations.append(
                 {
@@ -24,9 +33,13 @@ with open('Station_BC.csv', 'r') as csvfile:
                 }
             )
 
+    # Order stations by name.
+    weather_stations.sort(key=lambda station: station['name'])
+
     with open('weather_stations.json', 'w') as json_file:
+        # Dump json with an indent making it more human readable.
         json.dump({
             'weather_stations': weather_stations
-        }, json_file)
+        }, json_file, indent=' ')
 
     print('Station export complete, {} stations exported.'.format(station_count))
