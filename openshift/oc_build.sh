ba570a7474fe518eb725963dc126b3bdc5178fec
DerekRoberts <4391600+DerekRoberts@users.noreply.github.com>
2020-02-25 13:26:22 -0800

    Moved script vars into envars file

--- openshift/oc_build.sh
+++ openshift/oc_build.sh
@@ -17,7 +17,7 @@
 #%   ${THIS_FILE} 0 apply
 #%
 #%   Override variables at runtime.
-#%   GIT_BRANCH=branch PROJECT=project PATH_BC=./bc.yaml ${THIS_FILE} 0 apply
+#%   GIT_BRANCH=branch PROJ_TOOLS=project PATH_BC=./bc.yaml ${THIS_FILE} 0 apply
 #%
 
 # Halt conditions (errors, unsets, non-zero pipes), field separator and verbosity
@@ -26,21 +26,16 @@ set -euo pipefail
 IFS=$'\n\t'
 [ ! "${VERBOSE:-}" == "true" ] || set -x
 
-# Parameters and defaults
+# Parameters and environment vars
 #
-THIS_FILE="$(dirname ${0})/$(basename ${0})"
 PR_NO=${1:-}
 APPLY=${2:-}
-#
-NAME=${APPLICATION_NAME:-wps-web}
-PROJECT=${PROJECT:-auzhsi-tools}
-PATH_BC=${PATH_BC:-$(dirname ${0})/templates/build.bc.yaml}
-GIT_URL=${GIT_URL:-$(git remote get-url origin)}
-GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+source "$(dirname ${0})/envars"
 
 # Show help if no params
 #
 [ "${#}" -gt 0 ] || {
+	THIS_FILE="$(dirname ${0})/$(basename ${0})"
 	cat ${THIS_FILE} | grep "^#%" | sed -e "s|^#%||g" -e "s|\${THIS_FILE}|${THIS_FILE}|g"
 	exit
 }
@@ -54,8 +49,8 @@ $(oc whoami &>/dev/null) || {
 
 # Process commands
 #
-OC_PROCESS="oc -n ${PROJECT} process -f ${PATH_BC} -p NAME=${NAME} -p SUFFIX=pr-${PR_NO} -p GIT_URL=${GIT_URL} -p GIT_BRANCH=${GIT_BRANCH}"
-OC_APPLY="oc -n "${PROJECT}" apply -f -"
+OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_BC} -p NAME=${NAME} -p SUFFIX=pr-${PR_NO} -p GIT_URL=${GIT_URL} -p GIT_BRANCH=${GIT_BRANCH}"
+OC_APPLY="oc -n "${PROJ_TOOLS}" apply -f -"
 OC_COMMAND="${OC_PROCESS} | ${OC_APPLY}"
 #
 [ "${APPLY}" == "apply" ] || {
@@ -68,10 +63,10 @@ eval "${OC_COMMAND}"
 # Follow source to image build (build source, provide artifact to docker)
 #
 [ "${APPLY}" != "apply" ] || {
-	POD_SOURCE=$(oc get bc -n ${PROJECT} -o name -l app=${NAME}-pr-${PR_NO} | grep "source")
-	POD_DOCKER=$(oc get bc -n ${PROJECT} -o name -l app=${NAME}-pr-${PR_NO} | grep "docker")
-	oc logs -n ${PROJECT} --follow ${POD_SOURCE}
-	oc logs -n ${PROJECT} --follow ${POD_DOCKER}
+	POD_SOURCE=$(oc get bc -n ${PROJ_TOOLS} -o name -l app=${NAME}-pr-${PR_NO} | grep "source")
+	POD_DOCKER=$(oc get bc -n ${PROJ_TOOLS} -o name -l app=${NAME}-pr-${PR_NO} | grep "docker")
+	oc logs -n ${PROJ_TOOLS} --follow ${POD_SOURCE}
+	oc logs -n ${PROJ_TOOLS} --follow ${POD_DOCKER}
 }
 
 # Echo command
