975b611cea70cb5d26fbf969f0f60cb927027482
Nkuruba <40212850+Nkuruba@users.noreply.github.com>
2020-02-25 16:44:34 -0800

    Weather map link (#16)
    * Hook up the percentile api to get station percentiles
    
    The display of result is in a rough shape(just JSON format)
    
    * Add unit tests for percentiles Slice
    
    * Added an icon to navigate to weather stations map link
    
    Co-authored-by: Kyubinhan <kyubinhan11@gmail.com>

--- src/features/fwiCalculator/components/StationsDropdown.tsx
+++ src/features/fwiCalculator/components/StationsDropdown.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Autocomplete from '@material-ui/lab/Autocomplete'
-import { TextField } from '@material-ui/core'
+import { TextField, Tooltip } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
+import MapIcon from '@material-ui/icons/Map'
+import IconButton from '@material-ui/core/IconButton'
 import { Station } from 'api/stationAPI'
 import { selectStationsReducer } from 'app/rootReducer'
 
 const useStyles = makeStyles({
   root: {
     width: 600
+  },
+  mapIcon: {
+    padding: 10
+  },
+  wrapper: {
+    display: 'flex'
   }
 })
 
@@ -22,28 +30,47 @@ export const WeatherStationsDropdown = (props: Props) => {
   const classes = useStyles()
   const { stations, error } = useSelector(selectStationsReducer)
 
+  const onMapIconClick = () => {
+    window.open(
+      'https://governmentofbc.maps.arcgis.com/apps/webappviewer/index.html?id=c36baf74b74a46978cf517579a9ee332',
+      '_blank'
+    )
+  }
+
   return (
     <>
-      <Autocomplete
-        className={classes.root}
-        data-testid="weather-station-dropdown"
-        options={stations}
-        getOptionLabel={option => `${option.name} (${option.code})`}
-        onChange={(_, stations) => {
-          props.onStationsChange(stations)
-        }}
-        multiple
-        value={props.stations}
-        renderInput={params => (
-          <TextField
-            {...params}
-            label="Weather Stations"
-            variant="outlined"
-            fullWidth
-            size="small"
-          />
-        )}
-      />
+      <div className={classes.wrapper}>
+        <Autocomplete
+          className={classes.root}
+          data-testid="weather-station-dropdown"
+          options={stations}
+          getOptionLabel={option => `${option.name} (${option.code})`}
+          onChange={(_, stations) => {
+            props.onStationsChange(stations)
+          }}
+          multiple
+          value={props.stations}
+          renderInput={params => (
+            <TextField
+              {...params}
+              label="Weather Stations"
+              variant="outlined"
+              fullWidth
+              size="small"
+            />
+          )}
+        />
+        <Tooltip title="Navigate to Weather Stations Map">
+          <IconButton
+            color="primary"
+            className={classes.mapIcon}
+            aria-label="directions"
+            onClick={onMapIconClick}
+          >
+            <MapIcon />
+          </IconButton>
+        </Tooltip>
+      </div>
       {error && <div>{error} (while fetching weather stations) </div>}
     </>
   )
