f38eae6be5fafe85d40c29e33e7d4ffe05ada789
Kyubin Han <kyubinhan11@gmail.com>
2020-03-05 09:29:48 -0800

    Move the theme config into App component (#28)
    * Move the theme config into App component so that it won't be exported somewhere else
    
    * Make hex values consistent with Uppercase

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import { CssBaseline } from '@material-ui/core'
-import { ThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
-import theme from 'app/theme'
 import { FWICalculatorPage } from 'features/fwiCalculator/FWICalculatorPage'
+
+// Theme documentation: https://material-ui.com/customization/palette/
+// Theme demo: https://material.io/resources/color/#!/?view.left=1&view.right=1&primary.color=003365&secondary.color=FBC02D
+// Do not export this! theme can be accessed within makeStyles & withStyles. Use ErrorMessage.tsx as a reference
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#3E5C93',
+      main: '#003365',
+      dark: '#000C3A'
+    },
+    secondary: {
+      light: '#FFF263',
+      main: '#FBC02D',
+      dark: '#C49000'
+    },
+    success: { main: '#44D77A' },
+    error: { main: '#FF3E34' },
+    warning: { main: '#FE7921' },
+    contrastThreshold: 3,
+    tonalOffset: 0.1
+  },
+  typography: {
+    button: {
+      textTransform: 'none'
+    }
+  }
+})
 
 const App = () => {
   return (
