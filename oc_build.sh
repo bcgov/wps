ac57473b831d0ee32070ec1720860eb8bab08c52
Kyubin Han <kyubinhan11@gmail.com>
2020-03-13 13:28:45 -0700

    Use oc start-build after applying the template
    and remove for loop for following the building process

--- oc_build.sh
+++ oc_build.sh
@@ -27,35 +27,35 @@ OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_BC} -p NAME=${NAME} -p SUFFIX=
 
 # Apply a template (apply or use --dry-run)
 #
-OC_APPLY="oc -n "${PROJ_TOOLS}" apply -f -"
+OC_APPLY="oc -n ${PROJ_TOOLS} apply -f -"
 [ "${APPLY}" ] || OC_APPLY+=" --dry-run"
+
+# Force to start a new build each time (apply or don't run)
+#
+OC_START_BUILD="oc -n ${PROJ_TOOLS} start-build ${NAME}-${SUFFIX} --follow=true"
+[ "${APPLY}" ] || OC_START_BUILD=""
 
 # Execute commands
 #
 eval "${OC_PROCESS}"
 eval "${OC_PROCESS} | ${OC_APPLY}"
+eval "${OC_START_BUILD}"
 
-# Follow builds if deploying (wait condition) and ensure sure they pass successfully
-#
-if [ "${APPLY}" ]; then
-	APP_NAME=${NAME}-${SUFFIX}
-	# Identify buildconfig objects
-	BUILD_PODS=$(oc get bc -n ${PROJ_TOOLS} -o name -l app=${APP_NAME})
-	# Follow building of these objects (creates wait condition, lots of output!)
-	for p in "${BUILD_PODS}"; do
-		oc logs -n ${PROJ_TOOLS} --follow $p
-	done
-	# Get the most recent build version
-	BUILD_LAST=$(oc -n ${PROJ_TOOLS} get bc/${APP_NAME} -o 'jsonpath={.status.lastVersion}')
-	# Command to get the build result
-	BUILD_RESULT=$(oc -n ${PROJ_TOOLS} get build/${APP_NAME}-${BUILD_LAST} -o 'jsonpath={.status.phase}')
-	# Make sure that result is a successful completion
-	if [ "${BUILD_RESULT}" != "Complete" ]; then
-		echo -e "\n*** Build not complete! ***\n"
-		exit 1
-	fi
+# Follow builds if deploying and ensure they pass successfully
+APP_NAME="${NAME}-${SUFFIX}"
+# Get the most recent build version
+BUILD_LAST=$(oc -n ${PROJ_TOOLS} get bc/${APP_NAME} -o 'jsonpath={.status.lastVersion}')
+# Command to get the build result
+BUILD_RESULT=$(oc -n ${PROJ_TOOLS} get build/${APP_NAME}-${BUILD_LAST} -o 'jsonpath={.status.phase}')
+
+# Make sure that result is a successful completion
+if [ "${BUILD_RESULT}" != "Complete" ]; then
+	echo "Build result: ${BUILD_RESULT}"
+	echo -e "\n*** Build not complete! ***\n"
+	exit 1
 fi
 
 # Provide oc command instruction
 #
 display_helper "${OC_PROCESS} | ${OC_APPLY}"
+display_helper "${OC_START_BUILD}"
