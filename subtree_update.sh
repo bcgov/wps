06d848216337a00142cc7416e10534d4c799c087
Derek Roberts <derek.roberts@gmail.com>
2020-03-13 17:02:04 -0700

    Script cleanup

--- subtree_update.sh
+++ subtree_update.sh
@@ -2,7 +2,5 @@
 #
 # Move to repo root and pull subtree updates
 #
-set -x
 pushd $(dirname ${0})/../..
-pwd
 git subtree pull --prefix openshift/scripts git@github.com:bcgov/wps.git master --squash
