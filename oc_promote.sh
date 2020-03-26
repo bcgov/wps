fe1baa31e2ca9d3364cf33ead510a2ee097f875b
Derek Roberts <derek.roberts@gmail.com>
2020-03-26 14:52:48 -0700

    Script changes moving from bash to sh for Coco OC Runner

--- oc_promote.sh
+++ oc_promote.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -l
 #
 source "$(dirname ${0})/common/common"
 
@@ -23,12 +23,12 @@ source "$(dirname ${0})/common/common"
 
 # Delete previous prod label
 #
-OC_IMG_DELETE="oc -n ${PROJ_TOOLS} delete istag ${NAME}:${TAG_PROD}"
+OC_IMG_DELETE="oc -n ${PROJ_TOOLS} delete istag ${NAME_APP}:${TAG_PROD}"
 
 # Full copy imagestream into prod tag (not a pointer)
 #
-SOURCE_IMG="docker-registry.default.svc:5000/${PROJ_TOOLS}/${NAME}:${SUFFIX}"
-OC_IMG_IMPORT="oc -n ${PROJ_TOOLS} import-image ${NAME}:${TAG_PROD} --from=${SOURCE_IMG}"
+SOURCE_IMG="docker-registry.default.svc:5000/${PROJ_TOOLS}/${NAME_APP}:${SUFFIX}"
+OC_IMG_IMPORT="oc -n ${PROJ_TOOLS} import-image ${NAME_APP}:${TAG_PROD} --from=${SOURCE_IMG}"
 
 # Execute commands
 #
