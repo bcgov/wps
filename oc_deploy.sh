7d9a70e3b05ca59b61fd581737e1b1ffbffc226c
Kyubin Han <kyubinhan11@gmail.com>
2020-03-27 14:47:24 -0700

    Script changes for explicit build and deploy without using any internal triggers (#200)
    * Remove cancel-build command in oc_build.sh
    
    * Use oc rollout to deploy apps in oc_deploy.sh

--- oc_deploy.sh
+++ oc_deploy.sh
@@ -21,7 +21,7 @@ source "$(dirname ${0})/common/common"
 #%   ${THIS_FILE} pr-0 apply
 #%
 
-# Target project override for TEST or PROD first deployments
+# Target project override for Dev or Prod deployments
 #
 PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 
@@ -34,11 +34,22 @@ OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_DC} -p NAME=${NAME_APP} -p SUF
 OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
 [ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run"
 
+# Deploy and follow the progress
+# 
+OC_DEPLOY="oc -n ${PROJ_TARGET} rollout latest dc/${NAME_OBJ}"
+OC_LOG="oc -n ${PROJ_TARGET} logs -f dc/${NAME_OBJ}"
+if [ ! "${APPLY}" ]; then
+  OC_DEPLOY="${OC_DEPLOY} --dry-run"
+  OC_LOG=""
+fi
+
 # Execute commands
 #
 eval "${OC_PROCESS}"
 eval "${OC_PROCESS} | ${OC_APPLY}"
+eval "${OC_DEPLOY}"
+eval "${OC_LOG}"
 
 # Provide oc command instruction
 #
-display_helper "${OC_PROCESS} | ${OC_APPLY}"
+display_helper "${OC_PROCESS} | ${OC_APPLY}" "${OC_DEPLOY}" "${OC_LOG}"
