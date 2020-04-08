34dbe1244f54b91f4948cdf4a3e0b3840f50cb75
Derek Roberts <4391600+DerekRoberts@users.noreply.github.com>
2020-04-08 09:35:34 -0700

    Improve Image Promotion (#91)
    * Remove scripts dir, to replace
    
    * Squashed 'openshift/scripts/' content from commit 7d9a70e
    
    git-subtree-dir: openshift/scripts
    git-subtree-split: 7d9a70e3b05ca59b61fd581737e1b1ffbffc226c
    
    * Handle imagestreams per-PR, not in one object
    
    * Promote to distinct PROD imagestream, retage explicitly
    
    * Removed common tag from now separate imagestreams

--- openshift/scripts/oc_promote.sh
+++ openshift/scripts/oc_promote.sh
@@ -21,22 +21,23 @@ source "$(dirname ${0})/common/common"
 #%   ${THIS_FILE} pr-0 apply
 #%
 
-# Delete previous prod label
+# Source and destination
 #
-OC_IMG_DELETE="oc -n ${PROJ_TOOLS} delete istag ${NAME_APP}:${TAG_PROD}"
+IMG_SOURCE="docker-registry.default.svc:5000/${PROJ_TOOLS}/${NAME_APP}-${SUFFIX}:latest"
+IMG_DEST="${NAME_APP}-${TAG_PROD}"
 
-# Full copy imagestream into prod tag (not a pointer)
+# Import to new image and retag, leaving the original tag in
 #
-SOURCE_IMG="docker-registry.default.svc:5000/${PROJ_TOOLS}/${NAME_APP}:${SUFFIX}"
-OC_IMG_IMPORT="oc -n ${PROJ_TOOLS} import-image ${NAME_APP}:${TAG_PROD} --from=${SOURCE_IMG}"
+OC_IMG_IMPORT="oc -n ${PROJ_TOOLS} import-image ${IMG_DEST}:${SUFFIX} --from=${IMG_SOURCE} --confirm"
+OC_IMG_RETAG="oc -n ${PROJ_TOOLS} tag ${IMG_DEST}:${SUFFIX} ${IMG_DEST}:${TAG_PROD}"
 
 # Execute commands
 #
 if [ "${APPLY}" ]; then
-	eval "${OC_IMG_DELETE}"
 	eval "${OC_IMG_IMPORT}"
+	eval "${OC_IMG_RETAG}"
 fi
 
 # Provide oc command instruction
 #
-display_helper "${OC_IMG_DELETE}" "${OC_IMG_IMPORT}"
+display_helper "${OC_IMG_IMPORT}" "${OC_IMG_RETAG}"
