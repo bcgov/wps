07e126f3f6d4d4591988f70a76d31bea6da24b3e
Derek Roberts <4391600+DerekRoberts@users.noreply.github.com>
2020-04-09 14:50:01 -0700

    Update Pipeline Scripts (plus everything that affected) (#35)
    * Removed openshift/scripts for updating
    
    * Squashed 'openshift/scripts/' content from commit 5261836
    
    git-subtree-dir: openshift/scripts
    git-subtree-split: 5261836d76a89ba7cb5914649f9c38e0760c4787
    
    * Updated BC and DC for newly separate DEV and PROD imagestreams
    
    * Remove legacy code from Jenkinsfile via @kyubinhan

--- openshift/scripts/oc_build.sh
+++ openshift/scripts/oc_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -l
 #
 source "$(dirname ${0})/common/common"
 
@@ -23,42 +23,38 @@ source "$(dirname ${0})/common/common"
 
 # Process a template (mostly variable substition)
 #
-OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_BC} -p NAME=${NAME} -p SUFFIX=${SUFFIX} -p GIT_BRANCH=${GIT_BRANCH}"
+OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_BC} -p NAME=${NAME_APP} -p SUFFIX=${SUFFIX} -p GIT_BRANCH=${GIT_BRANCH}"
 
 # Apply a template (apply or use --dry-run)
 #
 OC_APPLY="oc -n ${PROJ_TOOLS} apply -f -"
-[ "${APPLY}" ] || OC_APPLY+=" --dry-run"
+[ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run"
 
-
-# Cancel non complete builds and start a new build (apply or don't run)
+# Start a new build (apply or don't run)
 #
-OC_CANCEL_BUILD="oc -n ${PROJ_TOOLS} cancel-build bc/${NAME}-${SUFFIX}"
-OC_START_BUILD="oc -n ${PROJ_TOOLS} start-build ${NAME}-${SUFFIX} --follow=true"
+OC_START_BUILD="oc -n ${PROJ_TOOLS} start-build ${NAME_OBJ} --follow=true"
+[ "${APPLY}" ] || OC_START_BUILD=""
 
 # Execute commands
 #
 eval "${OC_PROCESS}"
 eval "${OC_PROCESS} | ${OC_APPLY}"
+eval "${OC_START_BUILD}"
+
 if [ "${APPLY}" ]; then
-	eval "${OC_CANCEL_BUILD}" 
-	eval "${OC_START_BUILD}"
-fi
+	# Get the most recent build version
+	BUILD_LAST=$(oc -n ${PROJ_TOOLS} get bc/${NAME_OBJ} -o 'jsonpath={.status.lastVersion}')
+	# Command to get the build result
+	BUILD_RESULT=$(oc -n ${PROJ_TOOLS} get build/${NAME_OBJ}-${BUILD_LAST} -o 'jsonpath={.status.phase}')
 
-# Follow builds if deploying and ensure they pass successfully
-APP_NAME="${NAME}-${SUFFIX}"
-# Get the most recent build version
-BUILD_LAST=$(oc -n ${PROJ_TOOLS} get bc/${APP_NAME} -o 'jsonpath={.status.lastVersion}')
-# Command to get the build result
-BUILD_RESULT=$(oc -n ${PROJ_TOOLS} get build/${APP_NAME}-${BUILD_LAST} -o 'jsonpath={.status.phase}')
-
-# Make sure that result is a successful completion
-if [ "${BUILD_RESULT}" != "Complete" ]; then
-	echo "Build result: ${BUILD_RESULT}"
-	echo -e "\n*** Build not complete! ***\n"
-	exit 1
+	# Make sure that result is a successful completion
+	if [ "${BUILD_RESULT}" != "Complete" ]; then
+		echo "Build result: ${BUILD_RESULT}"
+		echo -e "\n*** Build not complete! ***\n"
+		exit 1
+	fi
 fi
 
 # Provide oc command instruction
 #
-display_helper "${OC_PROCESS} | ${OC_APPLY}" "${OC_CANCEL_BUILD}" "${OC_START_BUILD}"
+display_helper "${OC_PROCESS} | ${OC_APPLY}" "${OC_START_BUILD}"
