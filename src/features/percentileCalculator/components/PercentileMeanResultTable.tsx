70aea0f844d68bd277226f72abbd4f7ef07f2f85
Andrea Williams <54914403+andrea-williams@users.noreply.github.com>
2020-05-05 16:38:17 -0700

    Feature/275 add table header (#114)
    * Add table header to mean results table
    
    * Remove extra ,
    
    * Fix lint/merge errors

--- src/features/percentileCalculator/components/PercentileMeanResultTable.tsx
+++ src/features/percentileCalculator/components/PercentileMeanResultTable.tsx
@@ -7,6 +7,7 @@ import {
   TableCell,
   TableBody
 } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
 import { MeanValues } from 'api/percentileAPI'
 import { FWI_VALUES_DECIMAL } from 'utils/constants'
@@ -15,22 +16,41 @@ interface Props {
   meanValues: MeanValues
 }
 
+const useStyles = makeStyles(theme => ({
+  tableHeader: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.primary.contrastText
+  }
+}))
+
 export const PercentileMeanResultTable = ({ meanValues }: Props) => {
+  const classes = useStyles()
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableBody>
           <TableRow>
+            <TableCell className={classes.tableHeader} align="right" colSpan={2}>
+              Daily Value Percentile
+            </TableCell>
+          </TableRow>
+          <TableRow>
             <TableCell>FFMC mean value</TableCell>
-            <TableCell>{meanValues.ffmc.toFixed(FWI_VALUES_DECIMAL)}</TableCell>
+            <TableCell align="right">
+              {meanValues.ffmc.toFixed(FWI_VALUES_DECIMAL)}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell>BUI mean value</TableCell>
-            <TableCell>{meanValues.bui.toFixed(FWI_VALUES_DECIMAL)}</TableCell>
+            <TableCell align="right">
+              {meanValues.bui.toFixed(FWI_VALUES_DECIMAL)}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell>ISI mean value</TableCell>
-            <TableCell>{meanValues.isi.toFixed(FWI_VALUES_DECIMAL)}</TableCell>
+            <TableCell align="right">
+              {meanValues.isi.toFixed(FWI_VALUES_DECIMAL)}
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
