500cf1bb1564f103d3117d17e30ef22e09fcdd96
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2020-06-19 16:01:26 -0700

    escaped user name and db name

--- patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -8,10 +8,10 @@ set -Eeu
 
 if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]]; then
   echo "Creating user ${APP_USER}"
-  psql "$1" -w -c "create user ${APP_USER} WITH LOGIN ENCRYPTED PASSWORD '${APP_PASSWORD}'"
+  psql "$1" -w -c "create user \"${APP_USER}\" WITH LOGIN ENCRYPTED PASSWORD '${APP_PASSWORD}'"
 
   echo "Creating database ${APP_DATABASE}"
-  psql "$1" -w -c "CREATE DATABASE ${APP_DATABASE} OWNER ${APP_USER} ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
+  psql "$1" -w -c "CREATE DATABASE \"${APP_DATABASE}\" OWNER \"${APP_USER}\" ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
 
   echo "Creating PostGIS extension"
   psql $APP_DATABASE -w -c "CREATE EXTENSION IF NOT EXISTS POSTGIS"
