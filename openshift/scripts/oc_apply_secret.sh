21794c5f016b3e4fe422b84fd156be4f4d7579e0
Sybrand Strauss <sybrand.strauss@gmail.com>
2020-06-25 12:54:05 -0700

    HA DB for each PR. (#86)
    Templates and scripts for adding database for each deployment.

---
+++ openshift/scripts/oc_apply_secret.sh
@@ -0,0 +1,47 @@
+#!/bin/sh -l
+#
+source "$(dirname ${0})/common/common"
+
+#%
+#% OpenShift Deploy Helper
+#%
+#%   Intended for use with a pull request-based pipeline.
+#%   Suffixes incl.: pr-###.
+#%
+#% Usage:
+#%
+#%    ${THIS_FILE} [SUFFIX] [apply]
+#%
+#% Examples:
+#%
+#%   Provide a PR number. Defaults to a dry-run.
+#%   ${THIS_FILE} pr-0
+#%
+#%   Apply when satisfied.
+#%   ${THIS_FILE} pr-0 apply
+#%
+
+
+# Target project override for Dev or Prod deployments
+#
+PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
+
+# Process template
+# Note: A role issue is currenty preventing use of the image if it resides in the tools project.
+OC_PROCESS="oc -n ${PROJ_TARGET} process -f $(dirname ${0})/../templates/secret.config.yaml \
+-p NAME=${NAME_APP} \
+-p SUFFIX=${SUFFIX}"
+
+# Apply template (apply or use --dry-run)
+#
+OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
+[ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run"
+
+# Execute commands
+#
+eval "${OC_PROCESS}"
+eval "${OC_PROCESS} | ${OC_APPLY}"
+
+# Provide oc command instruction
+#
+display_helper "${OC_PROCESS} | ${OC_APPLY}"
