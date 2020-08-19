d5d70c4a9fa80a634e6444256867a928dfd67f7b
Andrea Williams <andrea.williams@gov.bc.ca>
2020-08-19 12:36:53 -0700

    Fixed health_check to output status code instead of true/false

--- openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/health_check.sh
+++ openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/health_check.sh
@@ -5,4 +5,4 @@ set -o pipefail
 # patronictl list outputs status for each of the pods in the Patroni cluster, but we are only interested in
 # the information for the individual pod that is currently running this script (identified by Openshift $(hostname))
 # if the pod is the Leader, there is no Lag. If the pod is a Replica, there will be a "Lag in MB" attribute
-pg_isready -q && patronictl list --format=json | jq -e ".[] | select(.Member == \"$(hostname)\") | .State == \"running\" and (.Role == \"Leader\" or .\"Lag in MB\" == 0)"
+pg_isready -q && patronictl list --format=json | jq -e ".[] | select(.Member == \"$(hostname)\" and .State == \"running\" and (.Role == \"Leader\" or .\"Lag in MB\" == 0))"
