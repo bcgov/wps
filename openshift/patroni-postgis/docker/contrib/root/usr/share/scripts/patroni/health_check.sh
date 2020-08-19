0cc31fce92e941c85519ca0b96b2be29f643a527
Andrea Williams <andrea.williams@gov.bc.ca>
2020-08-19 09:46:02 -0700

    Output health check of individual pod

--- openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/health_check.sh
+++ openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/health_check.sh
@@ -2,4 +2,7 @@
 set -Eeu
 set -o pipefail
 
-pg_isready -q && patronictl list --format=json | jq -e ".[] | 'if .Role == \"Leader\" then select(.Member == \"$(hostname)\" and .State == \"running\") else select(.Member == \"$(hostname)\" and .State == \"running\" and .\"Lag in MB\" == 0) end' "
+# patronictl list outputs status for each of the pods in the Patroni cluster, but we are only interested in
+# the information for the individual pod that is currently running this script (identified by Openshift $(hostname))
+# if the pod is the Leader, there is no Lag. If the pod is a Replica, there will be a "Lag in MB" attribute
+pg_isready -q && patronictl list --format=json | jq -e ".[] | select(.Member == \"$(hostname)\") | .State == \"running\" and (.Role == \"Leader\" or .\"Lag in MB\" == 0)"
