f48aef9f8a9dde03a770c94b4495f5ed91fa453c
Andrea Williams <andrea.williams@gov.bc.ca>
2020-10-08 15:04:02 -0700

    Reduce duplication in EC unit test files

--- api/app/tests/models/test_env_canada_hrdps.py
+++ api/app/tests/models/test_env_canada_hrdps.py
@@ -3,8 +3,6 @@
 import os
 import sys
 import logging
-import datetime
-from datetime import datetime
 import pytest
 import requests
 import shapely.wkt
@@ -13,61 +11,14 @@ from alchemy_mock.mocking import UnifiedAlchemyMagicMock
 from alchemy_mock.compat import mock
 import app.time_utils as time_utils
 import app.db.database
-from app.schemas import WeatherStation, Season
-from app.models import env_canada, machine_learning
+from app.models import env_canada
 from app.db.models import (PredictionModel, ProcessedModelRunUrl, PredictionModelRunTimestamp,
-                           PredictionModelGridSubset, ModelRunGridSubsetPrediction)
-from app.tests.models.crud import get_actuals_left_outer_join_with_predictions
+                           PredictionModelGridSubset)
+from app.tests.models.test_env_canada import MockResponse
 # pylint: disable=unused-argument, redefined-outer-name
 
 
 logger = logging.getLogger(__name__)
-
-
-class MockResponse:
-    """ Mocked out request.Response object """
-
-    def __init__(self, status_code, content=None):
-        self.status_code = status_code
-        self.content = content
-
-
-@pytest.fixture()
-def mock_get_stations(monkeypatch):
-    """ Mocked out listing of weather stations """
-    def mock_get(*args):
-        return [WeatherStation(
-            code=123, name='Test', lat=50.7, long=-120.425, ecodivision_name='Test',
-            core_season=Season(
-                start_month=5, start_day=1, end_month=9, end_day=21)), ]
-    monkeypatch.setattr(env_canada, 'get_stations_synchronously', mock_get)
-
-
-@pytest.fixture()
-def mock_get_processed_file_count(monkeypatch):
-    """ Mocked out get processed file count """
-    def mock_get_count(*args):
-        return 162
-    monkeypatch.setattr(env_canada, 'get_processed_file_count', mock_get_count)
-
-
-@pytest.fixture()
-def mock_get_model_run_predictions_for_grid(monkeypatch):
-    """ Mock out call to DB returning predictions """
-    def mock_get(*args):
-        result = [
-            ModelRunGridSubsetPrediction(
-                tmp_tgl_2=[2, 3, 4, 5],
-                rh_tgl_2=[10, 20, 30, 40],
-                prediction_timestamp=datetime(2020, 10, 10, 18)),
-            ModelRunGridSubsetPrediction(
-                tmp_tgl_2=[1, 2, 3, 4],
-                rh_tgl_2=[20, 30, 40, 50],
-                prediction_timestamp=datetime(2020, 10, 10, 21))
-        ]
-        return result
-    monkeypatch.setattr(
-        env_canada, 'get_model_run_predictions_for_grid', mock_get)
 
 
 @ pytest.fixture()
@@ -134,15 +85,6 @@ def mock_download(monkeypatch):
     monkeypatch.setattr(requests, 'get', mock_requests_get_hrdps)
 
 
-@ pytest.fixture()
-def mock_download_fail(monkeypatch):
-    """ fixture for env_canada.download """
-    def mock_requests_get(*args, **kwargs):
-        """ mock env_canada download method """
-        return MockResponse(status_code=400)
-    monkeypatch.setattr(requests, 'get', mock_requests_get)
-
-
 def test_get_hrdps_download_urls():
     """ test to see if get_download_urls methods gives the correct number of urls """
     total_num_of_urls = 49 * len(['TMP_TGL_2', 'RH_TGL_2'])
@@ -150,11 +92,7 @@ def test_get_hrdps_download_urls():
         time_utils.get_utc_now(), 0))) == total_num_of_urls
 
 
-def test_main_hrdps(mock_download,
-                    mock_session,
-                    mock_get_processed_file_count,
-                    mock_get_model_run_predictions_for_grid,
-                    mock_get_stations):
+def test_main_hrdps(mock_download, mock_session):
     """ run main method to see if it runs successfully. """
     # All files, except one, are marked as already having been downloaded, so we expect one file to
     # be processed.
