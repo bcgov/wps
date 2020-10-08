41f6ed4cbe473def97d765888217db4be9ae3389
Andrea Williams <andrea.williams@gov.bc.ca>
2020-10-07 17:16:39 -0700

    Created cronjob for HRDPS; added more unit tests

--- api/app/tests/models/test_env_canada.py
+++ api/app/tests/models/test_env_canada.py
@@ -1,6 +1,7 @@
 """ Unit tests for app/env_canada.py """
 
 import os
+import sys
 import logging
 import datetime
 from datetime import datetime
@@ -52,63 +53,113 @@ def mock_session(monkeypatch):
             " -120.525 50.62500000000001, -120.525 50.77500000000001))")
     shape = shapely.wkt.loads(geom)
 
-    url = ('https://dd.weather.gc.ca/model_gem_global/15km/grib2/lat_lon/00/000/'
-           'CMC_glb_TMP_TGL_2_latlon.15x.15_2021020300_P000.grib2')
-    processed_model_run = ProcessedModelRunUrl(url=url)
-
-    prediction_model = PredictionModel(id=1,
-                                       abbreviation='GDPS',
-                                       projection='latlon.15x.15',
-                                       name='Global Deterministic Prediction System')
-    prediction_model_run = PredictionModelRunTimestamp(
+    gdps_url = ('https://dd.weather.gc.ca/model_gem_global/15km/grib2/lat_lon/00/000/'
+                'CMC_glb_TMP_TGL_2_latlon.15x.15_2021020300_P000.grib2')
+    gdps_processed_model_run = ProcessedModelRunUrl(url=gdps_url)
+    gdps_prediction_model = PredictionModel(id=1,
+                                            abbreviation='GDPS',
+                                            projection='latlon.15x.15',
+                                            name='Global Deterministic Prediction System')
+    gdps_prediction_model_run = PredictionModelRunTimestamp(
         id=1, prediction_model_id=1, prediction_run_timestamp=time_utils.get_utc_now(),
-        prediction_model=prediction_model, complete=True)
+        prediction_model=gdps_prediction_model, complete=True)
 
-    def mock_get_session(*args):
+    hrdps_url = ('https://dd.weather.gc.ca/model_hrdps/continental/grib2/00/007/CMC_hrdps_continental_TMP_TGL_2_ps2.5km_2020100700_P007-00.grib2')
+    hrdps_processed_model_run = ProcessedModelRunUrl(url=hrdps_url)
+    hrdps_prediction_model = PredictionModel(id=3, abbreviation='HRDPS', projection='2.5km',
+                                             name='High Resolution Deterministic Prediction System')
+    hrdps_prediction_model_run = PredictionModelRunTimestamp(
+        id=2, prediction_model_id=3, prediction_run_timestamp=time_utils.get_utc_now(), prediction_model=hrdps_prediction_model, complete=True)
+
+    def mock_get_session_gdps(*args):
 
         return UnifiedAlchemyMagicMock(data=[
             (
                 [mock.call.query(PredictionModel),
                  mock.call.filter(PredictionModel.abbreviation == 'GDPS',
                                   PredictionModel.projection == 'latlon.15x.15')],
-                [prediction_model],
+                [gdps_prediction_model],
             ),
             (
                 [mock.call.query(ProcessedModelRunUrl),
-                 mock.call.filter(ProcessedModelRunUrl.url == url)],
-                [processed_model_run]
+                 mock.call.filter(ProcessedModelRunUrl.url == gdps_url)],
+                [gdps_processed_model_run]
             ),
             (
                 [mock.call.query(PredictionModelRunTimestamp)],
-                [prediction_model_run]
+                [gdps_prediction_model_run]
             ),
             (
                 [mock.call.query(PredictionModelGridSubset)],
                 [PredictionModelGridSubset(
-                    id=1, prediction_model_id=prediction_model.id, geom=from_shape(shape))]
+                    id=1, prediction_model_id=gdps_prediction_model.id, geom=from_shape(shape))]
             )
         ])
 
-    def mock_get_prediction_model_run_timestamp_records(*args, **kwargs):
-        return [prediction_model_run, ]
+    def mock_get_gdps_prediction_model_run_timestamp_records(*args, **kwargs):
+        return [gdps_prediction_model_run, ]
 
-    monkeypatch.setattr(app.db.database, 'get_write_session', mock_get_session)
+    monkeypatch.setattr(app.db.database, 'get_write_session',
+                        mock_get_session_gdps)
     monkeypatch.setattr(app.models.env_canada, 'get_prediction_model_run_timestamp_records',
-                        mock_get_prediction_model_run_timestamp_records)
+                        mock_get_gdps_prediction_model_run_timestamp_records)
+
+    def mock_get_session_hrdps(*args):
+
+        return UnifiedAlchemyMagicMock(data=[
+            (
+                [mock.call.query(PredictionModel),
+                 mock.call.filter(PredictionModel.abbreviation == 'HRDPS',
+                                  PredictionModel.projection == '2.5km')],
+                [hrdps_prediction_model],
+            ),
+            (
+                [mock.call.query(ProcessedModelRunUrl),
+                 mock.call.filter(ProcessedModelRunUrl == hrdps_url)],
+                [hrdps_processed_model_run]
+            ),
+            (
+                [mock.call.query(PredictionModelRunTimestamp)],
+                [hrdps_prediction_model_run]
+            ),
+            (
+                [mock.call.query(PredictionModelGridSubset)],
+                [PredictionModelGridSubset(
+                    id=2, prediction_model_id=hrdps_prediction_model.id, geom=from_shape(shape))]
+            )
+        ])
+
+    def mock_get_hrdps_prediction_model_run_timestamp_records(*args, **kwargs):
+        return [hrdps_prediction_model_run, ]
+
+    monkeypatch.setattr(app.db.database, 'get_write_session',
+                        mock_get_session_hrdps)
+    monkeypatch.setattr(app.models.env_canada, 'get_prediction_model_run_timestamp_records',
+                        mock_get_hrdps_prediction_model_run_timestamp_records)
 
 
 @ pytest.fixture()
 def mock_download(monkeypatch):
     """ fixture for env_canada.download """
-    def mock_requests_get(*args, **kwargs):
-        """ mock env_canada download method """
+    def mock_requests_get_gdps(*args, **kwargs):
+        """ mock env_canada download method for GDPS """
         dirname = os.path.dirname(os.path.realpath(__file__))
         filename = os.path.join(
             dirname, 'CMC_glb_RH_TGL_2_latlon.15x.15_2020071300_P000.grib2')
         with open(filename, 'rb') as file:
             content = file.read()
         return MockResponse(status_code=200, content=content)
-    monkeypatch.setattr(requests, 'get', mock_requests_get)
+    monkeypatch.setattr(requests, 'get', mock_requests_get_gdps)
+
+    def mock_requests_get_hrdps(*args, **kwargs):
+        """ mock env_canada download method for HRDPS """
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        filename = os.path.join(
+            dirname, 'CMC_hrdps_continental_RH_TGL_2_ps2.5km_2020100700_P007-00.grib2')
+        with open(filename, 'rb') as file:
+            content = file.read()
+        return MockResponse(status_code=200, content=content)
+    monkeypatch.setattr(requests, 'get', mock_requests_get_hrdps)
 
 
 @ pytest.fixture()
@@ -127,10 +178,26 @@ def test_get_gdps_download_urls():
         time_utils.get_utc_now(), 0))) == total_num_of_urls
 
 
-def test_main(mock_download, mock_session, mock_utcnow, mock_get_processed_file_count):
+def test_get_hrdps_download_urls():
+    """ test to see if get_download_urls methods gives the correct number of urls """
+    total_num_of_urls = 49 * len(['TMP_TGL_2', 'RH_TGL_2'])
+    assert len(list(env_canada.get_high_res_model_run_download_urls(
+        time_utils.get_utc_now(), 0))) == total_num_of_urls
+
+
+def test_main_gdps(mock_download, mock_session, mock_utcnow, mock_get_processed_file_count):
     """ run main method to see if it runs successfully. """
     # All files, except one, are marked as already having been downloaded, so we expect one file to
     # be processed.
+    sys.argv = ["argv", "GDPS"]
+    assert env_canada.main() == 1
+
+
+def test_main_hrdps(mock_download, mock_session, mock_utcnow, mock_get_processed_file_count):
+    """ run main method to see if it runs successfully. """
+    # All files, except one, are marked as already having been downloaded, so we expect one file to
+    # be processed.
+    sys.argv = ["argv", "HRDPS"]
     assert env_canada.main() == 1
 
 
