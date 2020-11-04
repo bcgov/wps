f49a9af1d9a88297224b55451907d5bd6acb998b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2020-11-04 13:39:57 -0800

    added unit tests to check for failing bots

--- api/app/tests/fireweather_bot/test_noon_forecasts.py
+++ api/app/tests/fireweather_bot/test_noon_forecasts.py
@@ -21,3 +21,24 @@ def test_noon_forecasts_bot(mocker: MockerFixture, mock_requests_session):  # py
     # There are 352 records in the csv fixture, one of which doesn't have a valid station name,
     # so we expect 351 records.
     assert save_noon_forecast_spy.call_count == 351
+
+
+def test_noon_forecasts_bot_fail(mocker: MockerFixture,
+                                 monkeypatch,
+                                 mock_requests_session):  # pylint: disable=unused-argument
+    """
+    Test that when the bot fails, a message is sent to rocket-chat, and our exit code is 1.
+    """
+
+    def mock_process_csv(self, filename: str):
+        raise Exception()
+
+    monkeypatch.setattr(noon_forecasts.NoonForecastBot, 'process_csv', mock_process_csv)
+    rocket_chat_spy = mocker.spy(noon_forecasts, 'send_rocketchat_notification')
+
+    with pytest.raises(SystemExit) as excinfo:
+        noon_forecasts.main()
+    # Assert that we exited without errors.
+    assert excinfo.value.code == 1
+    # Assert that rocket chat was called.
+    assert rocket_chat_spy.call_count == 1
