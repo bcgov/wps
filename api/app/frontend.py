da42c74a55fcf9b7c823bce6a714e4b0039cdb1e
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2020-11-06 15:38:49 -0800

    trying to get frontend to render public url correctly

--- api/app/frontend.py
+++ api/app/frontend.py
@@ -49,10 +49,7 @@ async def get_config(request: Request):
             'REACT_APP_KEYCLOAK_AUTH_URL': config.get('REACT_APP_KEYCLOAK_AUTH_URL'),
             'REACT_APP_KEYCLOAK_REALM': config.get('REACT_APP_KEYCLOAK_REALM'),
             'REACT_APP_KEYCLOAK_CLIENT': config.get('REACT_APP_KEYCLOAK_CLIENT'),
-            'REACT_APP_FIDER_LINK': config.get('REACT_APP_FIDER_LINK'),
-            'REACT_APP_MATOMO_URL': config.get('REACT_APP_MATOMO_URL'),
-            'REACT_APP_MATOMO_SITE_ID': config.get('REACT_APP_MATOMO_SITE_ID'),
-            'PUBLIC_URL': '.'
+            'REACT_APP_FIDER_LINK': config.get('REACT_APP_FIDER_LINK')
         })
 
 
@@ -63,11 +60,11 @@ async def get_index(request: Request):
         "index.html",
         {
             'request': request,
+            'PUBLIC_URL': '.',
+            'REACT_APP_KEYCLOAK_AUTH_URL': config.get('REACT_APP_KEYCLOAK_AUTH_URL'),
             'REACT_APP_MATOMO_URL': config.get('REACT_APP_MATOMO_URL'),
             'REACT_APP_MATOMO_SITE_ID': config.get('REACT_APP_MATOMO_SITE_ID'),
             'REACT_APP_MATOMO_CONTAINER': config.get('REACT_APP_MATOMO_CONTAINER'),
-            'PUBLIC_URL': config.get('PUBLIC_URL'),
-            'REACT_APP_KEYCLOAK_AUTH_URL': config.get('REACT_APP_KEYCLOAK_AUTH_URL')
         })
 
 
