01c78c1f41682cef93433570e88789350a1fde3c
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2020-11-09 11:36:50 -0800

    removed config.js from web project

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -3,18 +3,15 @@ export const HIDE_DISCLAIMER = process.env.REACT_APP_HIDE_DISCLAIMER
 // a number of process.env.* vars are set to {{*}} during the npm build (for the purposes of jina template
 // rendering). As such, we have to follow the pattern 'thing = window.env.* || process.env.*'
 export const KC_AUTH_URL =
-  (window.env.REACT_APP_KEYCLOAK_AUTH_URL || process.env.REACT_APP_KEYCLOAK_AUTH_URL) ??
+  (window.env?.REACT_APP_KEYCLOAK_AUTH_URL || process.env.REACT_APP_KEYCLOAK_AUTH_URL) ??
   ''
 export const KC_REALM =
-  (window.env.REACT_APP_KEYCLOAK_REALM || process.env.REACT_APP_KEYCLOAK_REALM) ?? ''
+  (window.env?.REACT_APP_KEYCLOAK_REALM || process.env.REACT_APP_KEYCLOAK_REALM) ?? ''
 export const KC_CLIENT =
-  (window.env.REACT_APP_KEYCLOAK_CLIENT || process.env.REACT_APP_KEYCLOAK_CLIENT) ?? ''
+  (window.env?.REACT_APP_KEYCLOAK_CLIENT || process.env.REACT_APP_KEYCLOAK_CLIENT) ?? ''
 export const REACT_APP_FIDER_LINK =
-  window.env.REACT_APP_FIDER_LINK || process.env.REACT_APP_FIDER_LINK
-// process.env.PUBLIC_URL is set to {{PUBLIC_URL}} during npm build
-export const PUBLIC_URL = window.env.PUBLIC_URL || process.env.PUBLIC_URL
-console.log('PUBLIC_URL', PUBLIC_URL)
-console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL)
+  window.env?.REACT_APP_FIDER_LINK || process.env.REACT_APP_FIDER_LINK
+export const PUBLIC_URL = window.env?.PUBLIC_URL || process.env.PUBLIC_URL
 
 export const WEATHER_STATION_MAP_LINK =
   'https://governmentofbc.maps.arcgis.com/apps/webappviewer/index.html?id=c36baf74b74a46978cf517579a9ee332'
