cf93bf379e76d6d9c9b767461f11ba4cdcc40353
Andrea Williams <54914403+andrea-williams@users.noreply.github.com>
2020-11-12 09:43:02 -0800

    Task/collect rdps/545 (#558)
    * RDPS model retrieval from EC
    * Set RDPS cronjob to run every 2 hours

---
+++ openshift/scripts/oc_provision_ec_rdps_cronjob.sh
@@ -0,0 +1,46 @@
+#!/bin/sh -l
+#
+source "$(dirname ${0})/common/common"
+
+#%
+#% OpenShift Deploy Helper
+#%
+#%   Intended for use with a pull request-based pipeline.
+#%   Suffixes incl.: pr-###.
+#%
+#% Usage:
+#%
+#%    ${THIS_FILE} [SUFFIX] [apply]
+#%
+#% Examples:
+#%
+#%   Provide a PR number. Defaults to a dry-run.
+#%   ${THIS_FILE} pr-0
+#%
+#%   Apply when satisfied.
+#%   ${THIS_FILE} pr-0 apply
+#%
+
+# Target project override for Dev or Prod deployments
+#
+PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
+
+# Process template
+OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/env_canada_rdps.cronjob.yaml \
+-p JOB_NAME=env-canada-rdps-${NAME_APP}-${SUFFIX} \
+-p NAME=${NAME_APP} \
+-p SUFFIX=${SUFFIX}"
+
+# Apply template (apply or use --dry-run)
+#
+OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
+[ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run"
+
+# Execute commands
+#
+eval "${OC_PROCESS}"
+eval "${OC_PROCESS} | ${OC_APPLY}"
+
+# Provide oc command instruction
+#
+display_helper "${OC_PROCESS} | ${OC_APPLY}"
