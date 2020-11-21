ff529777ed9e8e31d89a04ab0ad506269b5a24b8
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2020-11-20 17:11:34 -0800

    starting to add in matomo as part of deployment

---
+++ openshift/scripts/oc_provision_matomo_backup.sh
@@ -0,0 +1,93 @@
+#!/bin/sh -l
+#
+source "$(dirname ${0})/common/common"
+
+#%
+#% OpenShift Deploy Helper
+#%
+#%   Intended for use with a pull request-based pipeline.
+#%   Suffixes incl.: pr-###.
+#%
+#% Usage:
+#%
+#%    ${THIS_FILE} [SUFFIX] [apply]
+#%
+#% Examples:
+#%
+#%   Provide a PR number. Defaults to a dry-run.
+#%   ${THIS_FILE} pr-0
+#%
+#%   Apply when satisfied.
+#%   ${THIS_FILE} pr-0 apply
+#%
+
+
+# Target project override for Dev or Prod deployments
+#
+PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
+
+NAME="backup-mariadb-${NAME_APP}-${SUFFIX}"
+IMAGE_NAMESPACE=${PROJ_TOOLS}
+ 
+# Prepare names for patroni ephemeral instance for this PR.
+PATRONI_CLUSTER_NAME="patroni-${NAME_APP}-${SUFFIX}"
+APPLICATION_NAME="patroni-${NAME_APP}-${SUFFIX}"
+PATRONI_LEADER_SERVICE_NAME="patroni-leader-${NAME_APP}-${SUFFIX}"
+PATRONI_REPLICA_SERVICE_NAME="patroni-replica-${NAME_APP}-${SUFFIX}"
+SERVICE_ACCOUNT="patroniocp-${NAME_APP}-${SUFFIX}"
+
+
+
+oc -n auzhsi-dev process -f mariadb-backup-deploy-dev.json \
+    -p NAME="backup-mariadb" \
+    -p SOURCE_IMAGE_NAME="backup-mariadb" \
+    -p IMAGE_NAMESPACE="auzhsi-dev" \
+    -p TAG_NAME="latest" \
+    -p CONFIG_MAP_NAME="backup-mariadb-configmap" \
+    -p BACKUP_VOLUME_NAME="bk-auzhsi-dev-vgc3svkn4776" \
+    -p VERIFICATION_VOLUME_MOUNT_PATH="/var/lib/mysql/data" \
+    | oc -n auzhsi-dev apply -f -
+
+OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/mariadb-backup.dc.json \
+    -p NAME=${NAME} \
+    -p SOURCE_IMAGE_NAME=\"backup-mariadb\" \
+    -p IMAGE_NAMESPACE=${IMAGE_NAMESPACE} \
+    -p TAG_NAME= \
+    -p CONFIG_MAP_NAME= \
+    -p BACKUP_VOLUME_NAME= \
+    -p VERIFICATION_VOLUME_MOUNT_PATH= "
+
+
+# Process template
+# Note: A role issue is currenty preventing use of the image if it resides in the tools project.
+OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/patroni.yaml \
+-p NAME=${NAME_APP} \
+-p SUFFIX=${SUFFIX} \
+-p PATRONI_CLUSTER_NAME=${PATRONI_CLUSTER_NAME} \
+-p APPLICATION_NAME=${APPLICATION_NAME} \
+-p PATRONI_LEADER_SERVICE_NAME=${PATRONI_LEADER_SERVICE_NAME} \
+-p PATRONI_REPLICA_SERVICE_NAME=${PATRONI_REPLICA_SERVICE_NAME} \
+-p SERVICE_ACCOUNT=${SERVICE_ACCOUNT} \
+-p IMAGE_NAMESPACE=${IMAGE_NAMESPACE} \
+ ${IMAGE_VERSION:+ " -p IMAGE_VERSION=${IMAGE_VERSION}"} \
+ ${POD_MANAGEMENT_POLICY:+ " -p POD_MANAGEMENT_POLICY=${POD_MANAGEMENT_POLICY}"} \
+ ${PVC_SIZE:+ " -p PVC_SIZE=${PVC_SIZE}"} \
+ ${CPU_REQUEST:+ "-p CPU_REQUEST=${CPU_REQUEST}"} \
+ ${CPU_LIMIT:+ "-p CPU_LIMIT=${CPU_LIMIT}"} \
+ ${MEMORY_REQUEST:+ "-p MEMORY_REQUEST=${MEMORY_REQUEST}"} \
+ ${MEMORY_LIMIT:+ "-p MEMORY_LIMIT=${MEMORY_LIMIT}"}"
+
+
+# Apply template (apply or use --dry-run)
+#
+OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
+[ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run=client"
+
+# Execute commands
+#
+eval "${OC_PROCESS}"
+eval "${OC_PROCESS} | ${OC_APPLY}"
+
+# Provide oc command instruction
+#
+display_helper "${OC_PROCESS} | ${OC_APPLY}"
