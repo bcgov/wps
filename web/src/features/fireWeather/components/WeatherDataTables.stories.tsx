340a499cb30dcc68709b5cc292fe98bb320aff1f
Kyubin Han <kyubinhan11@gmail.com>
2020-12-09 17:11:51 -0800

    Modify the noon wx value table configuration

--- web/src/features/fireWeather/components/WeatherDataTables.stories.tsx
+++ web/src/features/fireWeather/components/WeatherDataTables.stories.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import NoonForecastTable from 'features/fireWeather/components/NoonForecastTable'
-import { forecastValues, modelValues, observedValues } from 'utils/storybook'
+import NoonWeatherValueTable from 'features/fireWeather/components/NoonWeatherValueTable'
 import HourlyObservationsTable from 'features/fireWeather/components/HourlyObservationsTable'
+import { forecastValues, modelValues, observedValues } from 'utils/storybook'
 import { isNoonInPST } from 'utils/date'
 import { ModelValue } from 'api/modelAPI'
 import { NoonForecastValue } from 'api/forecastAPI'
 
 storiesOf('Weather data tables', module)
-  .add('NoonForecastTable', () => {
+  .add('NoonWeatherValueTable', () => {
     const modelTableTitle = 'Interpolated global model noon values (PST, UTC−08:00): '
     const forecastTableTitle = 'Weather forecast noon values (PST, UTC−08:00): '
 
     return (
       <>
-        <NoonForecastTable
-          values={modelValues.filter(v => isNoonInPST(v.datetime)) as ModelValue[]}
+        <NoonWeatherValueTable
+          rows={modelValues.filter(v => isNoonInPST(v.datetime)) as ModelValue[]}
           title={modelTableTitle}
           testId=""
         />
-        <NoonForecastTable
-          values={forecastValues as NoonForecastValue[]}
+        <NoonWeatherValueTable
+          rows={forecastValues as NoonForecastValue[]}
           title={forecastTableTitle}
           testId=""
         />
@@ -29,5 +29,5 @@ storiesOf('Weather data tables', module)
     )
   })
   .add('HourlyObservationsTable', () => {
-    return <HourlyObservationsTable title="This is title!" values={observedValues} />
+    return <HourlyObservationsTable title="This is title!" rows={observedValues} />
   })
