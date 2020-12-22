c88928322db674c9fd3e58e846ed9fb3f96fd427
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2020-12-21 16:07:52 -0800

    updating tests to work with new pytest_bdd

--- api/app/tests/weather_models/test_models_predictions_db.py
+++ api/app/tests/weather_models/test_models_predictions_db.py
@@ -17,10 +17,9 @@ from app.db.models import (PredictionModelRunTimestamp, PredictionModel, ModelRu
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.fixture()
-def mock_session(monkeypatch, data):
+@pytest.fixture(name='mock_session')
+def mock_session_fixture(monkeypatch, data):
     """ Mocked out sqlalchemy session """
-    # pylint: disable=unused-argument
     if data:
         dirname = os.path.dirname(os.path.realpath(__file__))
         filename = os.path.join(dirname, data)
@@ -33,6 +32,7 @@ def mock_session(monkeypatch, data):
         prediction_model = PredictionModel(
             id=1, name='name', abbreviation='abbrev', projection='projection')
 
+        # pylint: disable=unused-argument
         def mock_get_session(*args):
             return UnifiedAlchemyMagicMock()
 
@@ -65,47 +65,47 @@ def mock_session(monkeypatch, data):
                             mock_get_model_run_predictions)
 
 
-@ scenario("test_models_predictions_db.feature", "Get model predictions from database")
+@pytest.mark.usefixtures("mock_jwt_decode")
+@scenario("test_models_predictions_db.feature", "Get model predictions from database")
 def test_db_predictions_scenario():
     """ BDD Scenario for predictions """
 
 
-@given("A database with <data>")
-def given_a_database(mock_session, data):  # pylint: disable=redefined-outer-name,unused-argument
+@given("A database with <data>", target_fixture='context')
+def given_a_database(data):
     """ Bind the data variable """
+    assert data
     return {}
 
 
-@given("station <codes>")
+@given("station <codes>", target_fixture='stations')
 def given_stations(codes):
     """ Turn provided string into array of codes. """
     return eval(codes)  # pylint: disable=eval-used
 
 
-# pylint: disable=unused-argument, redefined-outer-name
 @when("I call <endpoint>")
-def when_predictions(mock_jwt_decode, given_a_database, given_stations, endpoint: str):
+def when_predictions(context, stations, endpoint: str):
     """ post to endpoint """
     client = TestClient(app.main.app)
     response = client.post(
-        endpoint, headers={'Authorization': 'Bearer token'}, json={'stations': given_stations})
-    given_a_database['response_json'] = response.json()
-# pylint: enable=unused-argument, redefined-outer-name
+        endpoint, headers={'Authorization': 'Bearer token'}, json={'stations': stations})
+    context['response_json'] = response.json()
 
 
 @then('There are <num_prediction_values>')
-def assert_num_predictions(given_a_database, num_prediction_values):  # pylint: disable=redefined-outer-name
+def assert_num_predictions(context, num_prediction_values):
     """ Even though there are only two predictions in the database, we expect an interpolated noon value. """
     num_prediction_values = eval(num_prediction_values)  # pylint: disable=eval-used
-    assert len(given_a_database['response_json']['predictions']
+    assert len(context['response_json']['predictions']
                [num_prediction_values['index']]['values']) == num_prediction_values['len']
 
 
 @then('The <expected_response> is matched')
-def assert_response(given_a_database, expected_response):  # pylint: disable=redefined-outer-name
+def assert_response(context, expected_response):
     """ "Catch all" test that blindly checks the actual json response against an expected response. """
     dirname = os.path.dirname(os.path.realpath(__file__))
     filename = os.path.join(dirname, expected_response)
     with open(filename) as data_file:
         expected_json = json.load(data_file)
-        assert given_a_database['response_json'] == expected_json
+        assert context['response_json'] == expected_json
