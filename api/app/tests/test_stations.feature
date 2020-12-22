8c2f9bbfb94a0dc1c25bbaa4c6bba74440adc582
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2020-12-22 14:07:24 -0800

    resolved recursion error

--- api/app/tests/test_stations.feature
+++ api/app/tests/test_stations.feature
@@ -5,9 +5,9 @@ Feature: /stations/
         Then the response status code is <status>
         And there are active 16 weather stations
         And there is a station in <index> has <code>, <name>, <lat> and <long>
-        And the station has <ecodivision_name> with core_season <start_month> <start_day> - <end_month> <end_day>
+        And the station has <ecodivision_name> with <core_season>
 
         Examples:
-            | status | index | code | name         | lat     | long        | ecodivision_name                 | start_month | start_day | end_month | end_day |
-            | 200    | 0     | 67   | HAIG CAMP    | 49.3806 | -121.525967 | COOL HYPERMARITIME AND HIGHLANDS | 5           | 15        | 8         | 31      |
-            | 200    | 1     | 72   | UBC RESEARCH | 49.2645 | -122.573283 | COOL HYPERMARITIME AND HIGHLANDS | 5           | 15        | 8         | 31      |
+            | status | index | code | name         | lat     | long        | ecodivision_name                 | core_season                                                        |
+            | 200    | 0     | 67   | HAIG CAMP    | 49.3806 | -121.525967 | COOL HYPERMARITIME AND HIGHLANDS | {"start_month": 5, "start_day": 15, "end_month": 8, "end_day": 31} |
+            | 200    | 1     | 72   | UBC RESEARCH | 49.2645 | -122.573283 | COOL HYPERMARITIME AND HIGHLANDS | {"start_month": 5, "start_day": 15, "end_month": 8, "end_day": 31} |
--- api/app/tests/test_stations.py
+++ api/app/tests/test_stations.py
@@ -1,4 +1,5 @@
 """ Functional testing for API - stations using wf1 """
+import json
 from pytest_bdd import scenario, given, then
 from fastapi.testclient import TestClient
 from aiohttp import ClientSession
@@ -10,7 +11,7 @@ from app.tests.common import default_mock_client_get
 @pytest.mark.usefixtures('mock_env_with_use_wfwx')
 @scenario('test_stations.feature', 'Get weather stations from WFWX',
           example_converters=dict(status=int, index=int, code=int, name=str, lat=float,
-                                  long=float, ecodivision_name=str, core_season=dict))
+                                  long=float, ecodivision_name=str, core_season=json.loads))
 def test_stations_scenario():
     """ BDD Scenario. """
 
@@ -39,8 +40,7 @@ def active_16_weather_stations(response):
 
 
 @then("there is a station in <index> has <code>, <name>, <lat> and <long>")
-def there_is_a_station(response,  # pylint: too-many-arguments
-                       index, code, name, lat, long):
+def there_is_a_station(response, index, code, name, lat, long):  # pylint: disable=too-many-arguments
     """ We expect a station to have a code, name, lat and long. """
     assert (response.json()['weather_stations'][index]['code'] == code and
             response.json()['weather_stations'][index]['name'] == name and
@@ -48,13 +48,8 @@ def there_is_a_station(response,  # pylint: too-many-arguments
             response.json()['weather_stations'][index]['long'] == long)
 
 
-@then("the station has <ecodivision_name> with core_season <start_month> <start_day> - <end_month> <end_day>")
-def station_ecodivision_data(response,  # pylint: too-many-arguments
-                             index, ecodivision_name, start_month, start_day, end_month, end_day):
+@then("the station has <ecodivision_name> with <core_season>")
+def station_ecodivision_data(response, index, ecodivision_name, core_season: dict):
     """ We expect station's ecodivision to have name, start_month start_day - end_month end_day """
     assert (response.json()['weather_stations'][index]['ecodivision_name'] == ecodivision_name and
-            response.json()['weather_stations'][index]['core_season'] == {
-                "start_month": int(start_month),
-                "start_day": int(start_day),
-                "end_month": int(end_month),
-                "end_day": int(end_day)})
+            response.json()['weather_stations'][index]['core_season'] == core_season)
