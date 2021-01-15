8f02ec699e0971f6598d4ec6d014eec102d5002f
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-01-15 15:49:02 -0800

    changed order by severity

--- api/app/weather_models/fetch/c_haines.py
+++ api/app/weather_models/fetch/c_haines.py
@@ -11,13 +11,18 @@ async def fetch():
     """ Fetch polygon geojson
     """
     session = app.db.database.get_read_session()
+    # Ordering by severity, ascending is important to ensure that
+    # higher severity is placed over lower severity. (On the front end,
+    # it makes it easier to have the high severity border sit on top and
+    # pop nicely.)
     query = """select json_build_object(
         'type', 'FeatureCollection',
         'features', json_agg(ST_AsGeoJSON(t.*)::json)
     )
         from (
         select geom, severity from prediction_model_c_haines_polygons
-    ) as t(geom, severity) """
+        order by severity asc
+    ) as t(geom, severity)"""
     # something is wrong here.
     logger.info('fetching geojson from db...')
     # pylint: disable=no-member
