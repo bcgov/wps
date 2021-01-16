ca81de98a0163af274dbc446dabe87df110c6b6a
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-01-15 17:03:38 -0800

    storing model run and prediction timestamp

--- api/app/weather_models/fetch/c_haines.py
+++ api/app/weather_models/fetch/c_haines.py
@@ -1,5 +1,6 @@
 """ Fetch c-haines geojson
 """
+from datetime import datetime
 import logging
 import app.db.database
 
@@ -7,9 +8,10 @@ import app.db.database
 logger = logging.getLogger(__name__)
 
 
-async def fetch():
+async def fetch(model_run_timestamp: datetime, prediction_timestamp: datetime):
     """ Fetch polygon geojson
     """
+    # TODO: Add filters for model and timestamp (returning only the most recent model run)
     session = app.db.database.get_read_session()
     # Ordering by severity, ascending is important to ensure that
     # higher severity is placed over lower severity. (On the front end,
@@ -21,6 +23,9 @@ async def fetch():
     )
         from (
         select geom, severity from prediction_model_c_haines_polygons
+        where 
+            prediction_timestamp = '2021-01-15 04:00:00-08' and
+            model_run_timestamp = '2021-01-15 04:00:00-08'
         order by severity asc
     ) as t(geom, severity)"""
     # something is wrong here.
