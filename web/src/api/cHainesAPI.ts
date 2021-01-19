7b417049dafa31823dc76a7a342dc45bf8e56643
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-01-19 11:04:14 -0800

    react

--- web/src/api/cHainesAPI.ts
+++ web/src/api/cHainesAPI.ts
@@ -1,4 +1,5 @@
 import axios from 'api/axios'
+import { FeatureCollection } from 'geojson'
 
 export interface ModelRun {
   model_run_timestamp: string
@@ -12,5 +13,17 @@ export interface ModelRuns {
 export async function getModelRuns(): Promise<ModelRuns> {
   const url = '/c-haines/model-runs/'
   const { data } = await axios.get(url)
+  return data
+}
+
+export async function getCHainesGeoJSON(
+  model_run_timestamp: string,
+  prediction_timestamp: string
+): Promise<FeatureCollection> {
+  console.log('fetching c-haines', model_run_timestamp, prediction_timestamp)
+  const url = '/c-haines/'
+  const { data } = await axios.get(url, {
+    params: { model_run_timestamp, prediction_timestamp }
+  })
   return data
 }
