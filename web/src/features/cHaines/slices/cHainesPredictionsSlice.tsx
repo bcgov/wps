0f9018ea2f27cabdf04a39d4a26c308b95892e89
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-01-19 11:36:27 -0800

    grr

---
+++ web/src/features/cHaines/slices/cHainesPredictionsSlice.tsx
@@ -0,0 +1,69 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { getCHainesGeoJSON } from 'api/cHainesAPI'
+import { AppThunk } from 'app/store'
+import { logError } from 'utils/error'
+import { FeatureCollection } from 'geojson'
+
+interface State {
+  loading: boolean
+  error: string | null
+  model_runs: Record<string, Record<string, FeatureCollection>>
+}
+
+interface GeoJSONContext {
+  model_run_timestamp: string
+  prediction_timestamp: string
+  result: FeatureCollection
+}
+
+const initialState: State = {
+  loading: false,
+  error: null,
+  model_runs: {}
+}
+
+const cHainesPredictionsSlice = createSlice({
+  name: 'c-haines-predictions',
+  initialState: initialState,
+  reducers: {
+    getPredictionStart(state: State) {
+      state.loading = true
+    },
+    getPredictionSuccess(state: State, action: PayloadAction<GeoJSONContext>) {
+      state.model_runs[action.payload.model_run_timestamp][
+        action.payload.prediction_timestamp
+      ] = action.payload.result
+    },
+    getPredictionFailed(state: State, action: PayloadAction<string>) {
+      state.loading = false
+      state.error = action.payload
+    }
+  }
+})
+
+const {
+  getPredictionStart,
+  getPredictionSuccess,
+  getPredictionFailed
+} = cHainesPredictionsSlice.actions
+
+export default cHainesPredictionsSlice.reducer
+
+export const fetchCHainesGeoJSON = (
+  model_run_timestamp: string,
+  prediction_timestamp: string
+): AppThunk => async dispatch => {
+  try {
+    dispatch(getPredictionStart())
+    const geoJSON = await getCHainesGeoJSON(model_run_timestamp, prediction_timestamp)
+    const result = {
+      model_run_timestamp: model_run_timestamp,
+      prediction_timestamp: prediction_timestamp,
+      result: geoJSON
+    }
+    dispatch(getPredictionSuccess(result))
+  } catch (err) {
+    dispatch(getPredictionFailed(err.toString()))
+    logError(err)
+  }
+}
