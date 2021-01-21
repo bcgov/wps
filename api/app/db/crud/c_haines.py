7ce59417a8c3345b7d7f270aeee4b653302ce7b2
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-01-21 14:28:54 -0800

    normalized data properly

--- api/app/db/crud/c_haines.py
+++ api/app/db/crud/c_haines.py
@@ -1,27 +1,59 @@
 """ CRUD for CHaines
 """
-from datetime import timedelta
-from sqlalchemy import desc
+from datetime import timedelta, datetime
+from sqlalchemy import desc, asc
 from sqlalchemy.orm import Session
-from app.db.models import CHainesPoly, CHainesPrediction, PredictionModel
+from app.db.models import CHainesPrediction, CHainesModelRun, PredictionModel
 from app.time_utils import get_utc_now
 
 
-def get_model_runs(session: Session):
+def get_c_haines_model_run(session: Session, model_run_timestamp: datetime, prediction_model: PredictionModel):
+    return session.query(CHainesModelRun).filter(
+        CHainesModelRun.model_run_timestamp == model_run_timestamp,
+        CHainesModelRun.prediction_model_id == prediction_model.id
+    ).first()
+
+
+def create_c_haines_model_run(session: Session, model_run_timestamp: datetime, prediction_model: PredictionModel):
+    model_run = CHainesModelRun(model_run_timestamp=model_run_timestamp,
+                                prediction_model=prediction_model)
+    session.add(model_run)
+    return model_run
+
+
+def get_or_create_c_haines_model_run(session: Session, model_run_timestamp: datetime, prediction_model: PredictionModel):
+    model_run = get_c_haines_model_run(session, model_run_timestamp, prediction_model)
+    if not model_run:
+        model_run = create_c_haines_model_run(session, model_run_timestamp, prediction_model)
+    return model_run
+
+
+def get_c_haines_prediction(
+        session: Session,
+        model_run: CHainesModelRun,
+        prediction_timestamp: datetime):
+    """ Get the c-haines prediction """
+    return session.query(CHainesPrediction)\
+        .filter(CHainesPrediction.model_run_id == model_run.id,
+                CHainesPrediction.prediction_timestamp == prediction_timestamp)
+
+
+def get_model_run_predictions(session: Session):
     """ Get some recent model runs """
     start_date = get_utc_now() - timedelta(days=3)
-    return session.query(
-        CHainesPrediction.model_run_timestamp,
-        CHainesPrediction.prediction_timestamp,
-        PredictionModel.name,
-        PredictionModel.abbreviation)\
-        .join(PredictionModel, PredictionModel.id == CHainesPrediction.prediction_model_id)\
-        .filter(CHainesPrediction.model_run_timestamp >= start_date)\
-        .order_by(desc(CHainesPrediction.model_run_timestamp), CHainesPrediction.prediction_timestamp)
+    query = session.query(CHainesModelRun.id, CHainesModelRun.model_run_timestamp, PredictionModel.name, PredictionModel.abbreviation, CHainesPrediction.prediction_timestamp)\
+        .join(CHainesModelRun, CHainesModelRun.id == CHainesPrediction.model_run_id)\
+        .join(PredictionModel, PredictionModel.id == CHainesModelRun.prediction_model_id)\
+        .filter(CHainesModelRun.model_run_timestamp >= start_date)\
+        .order_by(desc(CHainesModelRun.model_run_timestamp), asc(CHainesPrediction.prediction_timestamp))
+    return query
 
-
-# def get_model_run_predictions(session: Session, model_run_timestamp: datetime):
-#     return session.query(CHainesPoly.prediction_timestamp)\
-#         .filter(CHainesPoly.model_run_timestamp == model_run_timestamp)\
-#         .group_by(CHainesPoly.prediction_timestamp)\
-#         .order_by(CHainesPoly.prediction_timestamp)
+    # return session.query(
+    #     CHainesPrediction.model_run_timestamp,
+    #     CHainesPrediction.prediction_timestamp,
+    #     PredictionModel.id,
+    #     PredictionModel.name,
+    #     PredictionModel.abbreviation)
+    # .join(PredictionModel, PredictionModel.id == CHainesPrediction.prediction_model_id)
+    # .filter(CHainesPrediction.model_run_timestamp >= start_date)
+    # .order_by(desc(CHainesPrediction.model_run_timestamp), asc(CHainesPrediction.prediction_timestamp))
