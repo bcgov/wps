6c4df4ea9cdcafd99e078b1e1d482c81599537d4
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-02-01 10:05:50 -0800

    added explanation

--- openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/health_check.sh
+++ openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/health_check.sh
@@ -2,8 +2,14 @@
 set -Eeu
 set -o pipefail
 
+# The bcdevops version has a check for 90% disk usage here:
+# ----
 # check for disk space. Fails if usage hits above 90%
 # df "${PATRONI_POSTGRESQL_DATA_DIR:-/home/postgres/pgdata}" --output=pcent | tail -n 1 | awk '{if ($1+0 > 90) exit 1; else exit 0;}'
+# ----
+# We've currently got a seperate script (disk_space_check.sh) that is called
+# as a livenessProbe, and will generate warnings if the available disk space
+# falls below a predefined size.
 
 # patronictl list outputs status for each of the pods in the Patroni cluster, but we are only interested in
 # the information for the individual pod that is currently running this script (identified by Openshift $(hostname))
