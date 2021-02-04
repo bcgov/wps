92b45dd064d3a11e85d88bf34b2c73d41fb59add
Kyubin Han <kyubinhan11@gmail.com>
2021-02-04 15:32:45 -0800

    Create WxDataForm component

---
+++ web/src/features/fireWeather/components/WxDataForm.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { useHistory } from 'react-router-dom'
+
+import WxStationDropdown from 'features/stations/components/WxStationDropdown'
+import TimeOfInterestPicker from 'features/fireWeather/components/TimeOfInterestPicker'
+import GetWxDataButton from 'features/fireWeather/components/GetWxDataButton'
+import { stationCodeQueryKey, timeOfInterestQueryKey } from 'utils/url'
+import { formatDateInISO } from 'utils/date'
+
+const useStyles = makeStyles({
+  stationDropdown: {
+    marginBottom: 10
+  }
+})
+
+interface Props {
+  codesFromQuery: number[]
+  toiFromQuery: string
+}
+
+const WxDataForm = ({ codesFromQuery, toiFromQuery }: Props) => {
+  const classes = useStyles()
+  const history = useHistory()
+  const [selectedCodes, setSelectedCodes] = useState<number[]>(codesFromQuery)
+  const [timeOfInterest, setTimeOfInterest] = useState(new Date(toiFromQuery))
+  const shouldGetBtnDisabled = selectedCodes.length === 0
+
+  const handleSubmit = () => {
+    // Update the url query with the new station codes
+    history.push({
+      search:
+        `${stationCodeQueryKey}=${selectedCodes.join(',')}&` +
+        `${timeOfInterestQueryKey}=${formatDateInISO(timeOfInterest)}`
+    })
+
+    // Create matomo event
+    // NOTE: This section is proof of concept - strongly consider re-factoring when adding other events.
+    // TODO: Re-evaluate this way of implementing Matomo once we know more about it.
+    if (window._mtm) {
+      // Create event, and push list of stations to the matomo data layer.
+      // see: https://developer.matomo.org/guides/tagmanager/integration-plugin#supporting-the-data-layer
+      window._mtm.push({ event: 'getWeatherData', stationCodes: selectedCodes })
+    }
+  }
+
+  return (
+    <form noValidate>
+      <WxStationDropdown
+        className={classes.stationDropdown}
+        stationCodes={selectedCodes}
+        onChange={setSelectedCodes}
+      />
+      <TimeOfInterestPicker
+        timeOfInterest={timeOfInterest}
+        onChange={setTimeOfInterest}
+      />
+      <GetWxDataButton onBtnClick={handleSubmit} disabled={shouldGetBtnDisabled} />
+    </form>
+  )
+}
+
+export default React.memo(WxDataForm)
--- web/src/features/fireWeather/pages/MoreCastPage.tsx
+++ web/src/features/fireWeather/pages/MoreCastPage.tsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { useLocation, useHistory } from 'react-router-dom'
-import { makeStyles } from '@material-ui/core/styles'
+import { useLocation } from 'react-router-dom'
 
 import { PageHeader, PageTitle, Container } from 'components'
-import WxStationDropdown from 'features/stations/components/WxStationDropdown'
 import WxDataDisplays from 'features/fireWeather/components/WxDataDisplays'
-import TimeOfInterestPicker from 'features/fireWeather/components/TimeOfInterestPicker'
+import WxDataForm from 'features/fireWeather/components/WxDataForm'
 import { fetchWxStations } from 'features/stations/slices/stationsSlice'
 import { fetchGlobalModelsWithBiasAdj } from 'features/fireWeather/slices/modelsSlice'
 import { fetchObservations } from 'features/fireWeather/slices/observationsSlice'
-import GetWxDataButton from 'features/fireWeather/components/GetWxDataButton'
 import { fetchForecasts } from 'features/fireWeather/slices/forecastsSlice'
 import { fetchGlobalModelSummaries } from 'features/fireWeather/slices/modelSummariesSlice'
 import { fetchForecastSummaries } from 'features/fireWeather/slices/forecastSummariesSlice'
@@ -18,24 +15,15 @@ import { fetchHighResModels } from 'features/fireWeather/slices/highResModelsSli
 import { fetchHighResModelSummaries } from 'features/fireWeather/slices/highResModelSummariesSlice'
 import { fetchRegionalModels } from 'features/fireWeather/slices/regionalModelsSlice'
 import { fetchRegionalModelSummaries } from 'features/fireWeather/slices/regionalModelSummariesSlice'
-import { getStationCodesFromUrl, stationCodeQueryKey } from 'utils/url'
-import { formatDateInISO } from 'utils/date'
-
-const useStyles = makeStyles({
-  stationDropdown: {
-    marginBottom: 10
-  }
-})
+import { getStationCodesFromUrl, getTimeOfInterestFromUrl } from 'utils/url'
 
 const MoreCastPage = () => {
-  const classes = useStyles()
   const dispatch = useDispatch()
   const location = useLocation()
-  const history = useHistory()
 
   const codesFromQuery = getStationCodesFromUrl(location.search)
-  const [selectedCodes, setSelectedCodes] = useState<number[]>(codesFromQuery)
-  const [timeOfInterest, setTimeOfInterest] = useState(new Date())
+  const toiFromQuery = getTimeOfInterestFromUrl(location.search)
+  const timeOfInterest = new Date(toiFromQuery)
 
   useEffect(() => {
     dispatch(fetchWxStations())
@@ -43,56 +31,24 @@ const MoreCastPage = () => {
 
   useEffect(() => {
     if (codesFromQuery.length > 0) {
-      const isoTOI = formatDateInISO(timeOfInterest)
-      dispatch(fetchObservations(codesFromQuery, isoTOI))
-      dispatch(fetchForecasts(codesFromQuery, isoTOI))
-      dispatch(fetchForecastSummaries(codesFromQuery, isoTOI))
-      dispatch(fetchHighResModels(codesFromQuery, isoTOI))
-      dispatch(fetchHighResModelSummaries(codesFromQuery, isoTOI))
-      dispatch(fetchRegionalModels(codesFromQuery, isoTOI))
-      dispatch(fetchRegionalModelSummaries(codesFromQuery, isoTOI))
-      dispatch(fetchGlobalModelsWithBiasAdj(codesFromQuery, isoTOI))
-      dispatch(fetchGlobalModelSummaries(codesFromQuery, isoTOI))
+      dispatch(fetchObservations(codesFromQuery, toiFromQuery))
+      dispatch(fetchForecasts(codesFromQuery, toiFromQuery))
+      dispatch(fetchForecastSummaries(codesFromQuery, toiFromQuery))
+      dispatch(fetchHighResModels(codesFromQuery, toiFromQuery))
+      dispatch(fetchHighResModelSummaries(codesFromQuery, toiFromQuery))
+      dispatch(fetchRegionalModels(codesFromQuery, toiFromQuery))
+      dispatch(fetchRegionalModelSummaries(codesFromQuery, toiFromQuery))
+      dispatch(fetchGlobalModelsWithBiasAdj(codesFromQuery, toiFromQuery))
+      dispatch(fetchGlobalModelSummaries(codesFromQuery, toiFromQuery))
     }
-
-    // Update local state to match with the url query
-    setSelectedCodes(codesFromQuery)
   }, [location]) // eslint-disable-line react-hooks/exhaustive-deps
-
-  const onSubmitClick = () => {
-    // Update the url query with the new station codes
-    history.push({ search: `${stationCodeQueryKey}=${selectedCodes.join(',')}` })
-
-    // Create matomo event
-    // NOTE: This section is proof of concept - strongly consider re-factoring when adding other events.
-    // TODO: Re-evaluate this way of implementing Matomo once we know more about it.
-    if (window._mtm) {
-      // Create event, and push list of stations to the matomo data layer.
-      // see: https://developer.matomo.org/guides/tagmanager/integration-plugin#supporting-the-data-layer
-      window._mtm.push({ event: 'getWeatherData', stationCodes: selectedCodes })
-    }
-  }
-
-  const shouldGetBtnDisabled = selectedCodes.length === 0
 
   return (
     <main>
       <PageHeader title="Predictive Services Unit" productName="MoreCast" />
       <PageTitle title="MoreCast - Weather Forecast Validation Tool" />
       <Container>
-        <form noValidate>
-          <WxStationDropdown
-            className={classes.stationDropdown}
-            stationCodes={selectedCodes}
-            onChange={setSelectedCodes}
-          />
-          <TimeOfInterestPicker
-            timeOfInterest={timeOfInterest}
-            onChange={setTimeOfInterest}
-          />
-          <GetWxDataButton onBtnClick={onSubmitClick} disabled={shouldGetBtnDisabled} />
-        </form>
-
+        <WxDataForm codesFromQuery={codesFromQuery} toiFromQuery={toiFromQuery} />
         <WxDataDisplays timeOfInterest={timeOfInterest} stationCodes={codesFromQuery} />
       </Container>
     </main>
