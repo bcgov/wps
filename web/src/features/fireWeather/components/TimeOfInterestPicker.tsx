8b1c27491a16ad6b4437a36cf9cab08b30b38c7b
Kyubin Han <kyubinhan11@gmail.com>
2021-02-08 14:08:22 -0800

    Add disclaimer for time of interest picker

--- web/src/features/fireWeather/components/TimeOfInterestPicker.tsx
+++ web/src/features/fireWeather/components/TimeOfInterestPicker.tsx
@@ -26,6 +26,7 @@ const TimeOfInterestPicker = (props: Props) => {
       label="Time of Interest (PST-08:00)"
       type="datetime-local"
       value={formatDateInPST(props.timeOfInterest, 'YYYY-MM-DDTHH:mm')}
+      helperText="Disclaimer: all data may not be available."
       className={classes.datePicker}
       InputLabelProps={{
         shrink: true
