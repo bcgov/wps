6aaec41a7891ca0ca2c1c3d301a826c425eb8d18
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-02-08 14:04:15 -0800

    changed post init slightly

--- openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ openshift/patroni-postgis/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -14,7 +14,7 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
   psql "$1" -w -c "CREATE DATABASE \"${APP_DATABASE}\" OWNER \"${APP_USER}\" ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
 
   echo "Creating PostGIS extension"
-  psql $APP_DATABASE -w -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+  psql "$1" -w -d $APP_DATABASE -c "CREATE EXTENSION IF NOT EXISTS postgis;"
   # There are various features we could install:
   # psql $APP_DATABASE -w -c "CREATE EXTENSION IF NOT EXISTS pg_trgm"
   # psql $APP_DATABASE -w -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
