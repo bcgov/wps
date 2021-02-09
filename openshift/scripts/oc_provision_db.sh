830f957bfb4ecca17688f0ad1f43a7cd2c37deef
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-02-09 15:06:53 -0800

    figuring out patroni issue

--- openshift/scripts/oc_provision_db.sh
+++ openshift/scripts/oc_provision_db.sh
@@ -34,10 +34,25 @@ PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 # SERVICE_ACCOUNT="patroniocp-${NAME_APP}-${SUFFIX}"
 IMAGE_STREAM_NAMESPACE=${IMAGE_STREAM_NAMESPACE:-${PROJ_TOOLS}}
 EPHEMERAL_STORAGE=${EPHEMERAL_STORAGE:-'False'}
-TEMPLATE=${TEMPLATE:-'patroni.yaml'}
+REMOVE_NSP=${REMOVE_NSP:-'False'}
+
+# Process pre-requisite template
+OC_PROCESS_PREREQUISITE="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/patroni_prerequisite.yaml \
+-p NAME=\"patroni-${NAME_APP}-${SUFFIX}\" \
+-p PARENT_NAME=\"${NAME_APP}\" \
+-p TARGET_NAMESPACE=${PROJ_TARGET} \
+-p IMAGE_STREAM_NAMESPACE=${IMAGE_STREAM_NAMESPACE} \
+ ${IMAGE_NAME:+ " -p IMAGE_NAME=${IMAGE_NAME}"} \
+ ${IMAGE_TAG:+ " -p IMAGE_TAG=${IMAGE_TAG}"} \
+ ${IMAGE_REGISTRY:+ " -p IMAGE_REGISTRY=${IMAGE_REGISTRY}"} \
+ ${PVC_SIZE:+ " -p PVC_SIZE=${PVC_SIZE}"} \
+ ${CPU_REQUEST:+ "-p CPU_REQUEST=${CPU_REQUEST}"} \
+ ${CPU_LIMIT:+ "-p CPU_LIMIT=${CPU_LIMIT}"} \
+ ${MEMORY_REQUEST:+ "-p MEMORY_REQUEST=${MEMORY_REQUEST}"} \
+ ${MEMORY_LIMIT:+ "-p MEMORY_LIMIT=${MEMORY_LIMIT}"}"
 
 # Process template
-OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/${TEMPLATE} \
+OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/patroni.yaml \
 -p NAME=\"patroni-${NAME_APP}-${SUFFIX}\" \
 -p PARENT_NAME=\"${NAME_APP}\" \
 -p TARGET_NAMESPACE=${PROJ_TARGET} \
@@ -52,32 +67,21 @@ OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/${TEMPLATE} \
  ${MEMORY_LIMIT:+ "-p MEMORY_LIMIT=${MEMORY_LIMIT}"}"
 
 
-# OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/patroni.yaml \
-# -p NAME=${NAME_APP} \
-# -p SUFFIX=${SUFFIX} \
-# -p PATRONI_CLUSTER_NAME=${PATRONI_CLUSTER_NAME} \
-# -p APPLICATION_NAME=${APPLICATION_NAME} \
-# -p PATRONI_LEADER_SERVICE_NAME=${PATRONI_LEADER_SERVICE_NAME} \
-# -p PATRONI_REPLICA_SERVICE_NAME=${PATRONI_REPLICA_SERVICE_NAME} \
-# -p SERVICE_ACCOUNT=${SERVICE_ACCOUNT} \
-# -p IMAGE_NAMESPACE=${IMAGE_NAMESPACE} \
-# -p TARGET_NAMESPACE=${PROJ_TARGET} \
-#  ${IMAGE_SERVER:+ " -p IMAGE_SERVER=${IMAGE_SERVER}"} \
-#  ${IMAGE_VERSION:+ " -p IMAGE_VERSION=${IMAGE_VERSION}"} \
-#  ${POD_MANAGEMENT_POLICY:+ " -p POD_MANAGEMENT_POLICY=${POD_MANAGEMENT_POLICY}"} \
-#  ${PVC_SIZE:+ " -p PVC_SIZE=${PVC_SIZE}"} \
-#  ${CPU_REQUEST:+ "-p CPU_REQUEST=${CPU_REQUEST}"} \
-#  ${CPU_LIMIT:+ "-p CPU_LIMIT=${CPU_LIMIT}"} \
-#  ${MEMORY_REQUEST:+ "-p MEMORY_REQUEST=${MEMORY_REQUEST}"} \
-#  ${MEMORY_LIMIT:+ "-p MEMORY_LIMIT=${MEMORY_LIMIT}"}"
-
 # In order to avoid running out of storage quote in our development environment, use
 # ephemeral storage by removing the pvc request from the template.
 if [ "$EPHEMERAL_STORAGE" = "True" ]
 then
     # Pipe the template to jq, and delete the pvc and volume claim items from the template.
-    OC_PROCESS="${OC_PROCESS} | jq 'del(.items[6].spec.template.spec.volumes[0].persistentVolumeClaim) \
-| del(.items[6].spec.volumeClaimTemplates)'"
+    OC_PROCESS="${OC_PROCESS} | jq 'del(.items[2].spec.template.spec.volumes[0].persistentVolumeClaim) \
+| del(.items[2].spec.volumeClaimTemplates)'"
+fi
+
+# OCP4 doesn't understand the NSP - so we remove it.
+if [ "$REMOVE_NSP" = "True" ]
+then
+    # Pipe the template to jq, and delete the pvc and volume claim items from the template.
+    OC_PROCESS_PREREQUISITE="${OC_PROCESS_PREREQUISITE} | jq 'del(.items[3]) \
+| del(.items[2].spec.volumeClaimTemplates)'"
 fi
 
 # Apply template (apply or use --dry-run)
@@ -87,9 +91,19 @@ OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
 
 # Execute commands
 #
+eval "${OC_PROCESS_PREREQUISITE}"
+eval "${OC_PROCESS_PREREQUISITE} | ${OC_APPLY}"
+
+# For some reason, if the stateful set is created too soon after creating
+# the pre-requisites then postgresql never starts up.
+# Sleep of 15 second seems to usually, but not always be enough.
+# echo "Waiting a while..."
+# sleep 15
+
 eval "${OC_PROCESS}"
 eval "${OC_PROCESS} | ${OC_APPLY}"
 
 # Provide oc command instruction
 #
+display_helper "${OC_PROCESS_PREREQUISITE} | ${OC_APPLY}"
 display_helper "${OC_PROCESS} | ${OC_APPLY}"
