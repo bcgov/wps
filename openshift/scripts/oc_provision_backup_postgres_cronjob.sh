70d5b3af8606848865c5f10f702784ef7e72ff1c
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-02-16 17:27:06 -0800

    trying to get cronjob to work

--- openshift/scripts/oc_provision_backup_postgres_cronjob.sh
+++ openshift/scripts/oc_provision_backup_postgres_cronjob.sh
@@ -27,36 +27,31 @@ source "$(dirname ${0})/common/common"
 PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 
 # Prepare variables for backups
-NAME="backup-postgres-${NAME_APP}-${SUFFIX}"
-CLUSTER_NAME="patroni-${NAME_APP}-${SUFFIX}"
+CONFIG_MAP_NAME="backup-postgres-${NAME_APP}-${SUFFIX}-config"
+CONFIG_VOLUME_NAME="${NAME_APP}-${SUFFIX}-config-volume"
+JOB_NAME="backup-postgres-${NAME_APP}-${SUFFIX}"
 IMAGE_NAMESPACE=${PROJ_TOOLS}
 EPHEMERAL_STORAGE=${EPHEMERAL_STORAGE:-'False'}
-# /-/_ : replace dash with underscore
-# ^^ to uppercase
-WPS_ENVIRONMENT_USERNAME_KEY="PATRONI_${NAME_APP}_${SUFFIX/-/_}_LEADER_USER"
-WPS_ENVIRONMENT_USERNAME_KEY=${WPS_ENVIRONMENT_USERNAME_KEY^^}
-WPS_ENVIRONMENT_PASSWORD_KEY="PATRONI_${NAME_APP}_${SUFFIX/-/_}_LEADER_PASSWORD"
-WPS_ENVIRONMENT_PASSWORD_KEY=${WPS_ENVIRONMENT_PASSWORD_KEY^^}
 
-OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/postgres-backup.dc.yaml \
-    -p NAME=${NAME} \
+OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/backup-postgres-cronjob.yaml \
+    -p CONFIG_VOLUME_NAME=${CONFIG_VOLUME_NAME} \
+    -p CONFIG_MAP_NAME=${CONFIG_MAP_NAME} \
+    -p DATABASE_SERVICE_NAME=patroni-${NAME_APP}-${SUFFIX}-leader \
+    -p DATABASE_NAME=${NAME_APP} \
+    -p DATABASE_DEPLOYMENT_NAME=wps-global \
+    -p JOB_NAME=${JOB_NAME} \
     -p IMAGE_NAMESPACE=${IMAGE_NAMESPACE} \
-    -p CLUSTER_NAME=${CLUSTER_NAME} \
-    -p WPS_ENVIRONMENT_USERNAME_KEY=${WPS_ENVIRONMENT_USERNAME_KEY} \
-    -p WPS_ENVIRONMENT_PASSWORD_KEY=${WPS_ENVIRONMENT_PASSWORD_KEY}"
-
+    ${JOB_PERSISTENT_STORAGE_NAME:+ " -p JOB_PERSISTENT_STORAGE_NAME=${JOB_PERSISTENT_STORAGE_NAME}"} \
+    ${SCHEDULE:+ " -p SCHEDULE=\"${SCHEDULE}\""} \
+    ${TAG_NAME:+ " -p TAG_NAME=${TAG_NAME}"}"
 
 # In order to avoid running out of storage quote in our development environment, use
 # ephemeral storage by removing the pvc request from the template.
 if [ "$EPHEMERAL_STORAGE" = "True" ]
 then
     # Pipe the template to jq, and delete the pvc and volume claim items from the template.
-    OC_PROCESS="${OC_PROCESS} | jq 'del(.items[0]) \
-| del(.items[0]) \
-| del(.items[1].spec.template.spec.volumes[0].persistentVolumeClaim) \
-| del(.items[1].spec.template.spec.volumes[1].persistentVolumeClaim)'"
+    OC_PROCESS="${OC_PROCESS} | jq 'del(.items[0].spec.jobTemplate.spec.template.spec.volumes[0].persistentVolumeClaim)'"
 fi
-
 
 # Apply template (apply or use --dry-run)
 #
