ec2465f220381210ad24b76a9f8ee117555734ca
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-03-12 14:32:11 -0800

    very basic test for worker

---
+++ api/app/tests/c_haines/test_c_haines_worker.py
@@ -0,0 +1,60 @@
+import os
+from datetime import datetime
+import pytest
+import requests
+from app import configure_logging
+from app.db.database import Session
+from app.db.models.weather_models import PredictionModel
+from app.db.models.c_haines import CHainesModelRun
+import app.db.crud.c_haines
+from app.c_haines.worker import main
+from app.tests.common import MockResponse
+
+configure_logging()
+
+
+@pytest.fixture()
+def mock_get_c_haines_model_run(monkeypatch):
+    """ fixture for crud """
+
+    # pylint: disable=unused-argument
+    def _mock_get_c_haines_model_run(session: Session,
+                                     model_run_timestamp: datetime,
+                                     prediction_model: PredictionModel):
+        return CHainesModelRun(id=1, model_run_timestamp=datetime.now(), prediction_model_id=1)
+    monkeypatch.setattr(app.db.crud.c_haines, 'get_c_haines_model_run', _mock_get_c_haines_model_run)
+
+
+@pytest.fixture()
+def mock_download(monkeypatch):
+    """ fixture for env_canada.download """
+    # pylint: disable=unused-argument
+    def mock_requests_get(*args, **kwargs):
+        """ mock env_canada download method """
+        print('mock download {}'.format(args[0]))
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        if 'TMP_ISBL' in args[0]:
+            if '700' in args[0]:
+                grib_filename = 'CMC_hrdps_continental_TMP_ISBL_0850_ps2.5km_2021012618_P048-00.grib2'
+            if '850' in args[0]:
+                grib_filename = 'CMC_hrdps_continental_TMP_ISBL_0700_ps2.5km_2021012618_P048-00.grib2'
+        elif 'DEPR_ISBL' in args[0]:
+            grib_filename = 'CMC_hrdps_continental_DEPR_ISBL_0850_ps2.5km_2021012618_P048-00.grib2'
+        filename = os.path.join(dirname, grib_filename)
+        with open(filename, 'rb') as file:
+            content = file.read()
+        return MockResponse(status_code=200, content=content)
+    monkeypatch.setattr(requests, 'get', mock_requests_get)
+
+
+@pytest.mark.usefixtures('mock_download', 'mock_get_c_haines_model_run')
+def test_c_haines_worker():
+    """ Test the c-haines worked.
+    This is not a very focused test. Through the magic of sqlalchmy, it will only
+    process one set of grib files. We check that the main process runs ok without raising
+    any exceptions.
+    """
+    try:
+        main()
+    except exception:
+        pytest.fail(exception)
