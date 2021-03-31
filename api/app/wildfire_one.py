a87e57a014a61b3823eb64e9f17d12fb8ff0f3f9
Andrea Williams <andrea.williams@gov.bc.ca>
2021-03-30 17:20:00 -0700

    Remove unnecessary logger statement

--- api/app/wildfire_one.py
+++ api/app/wildfire_one.py
@@ -101,7 +101,6 @@ async def _fetch_access_token(session: ClientSession) -> dict:
 async def _get_auth_header(session: ClientSession) -> dict:
     # Fetch access token
     token = await _fetch_access_token(session)
-    logger.info('%s', token)
     # Construct the header.
     header = {'Authorization': 'Bearer {}'.format(token['access_token'])}
     return header
