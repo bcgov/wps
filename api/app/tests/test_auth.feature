2ba7bd99294afd434689e18d14e018ed7e15839f
Conor Brady <con.brad@gmail.com>
2021-04-14 15:44:26 -0700

    Clean up tests

--- api/app/tests/test_auth.feature
+++ api/app/tests/test_auth.feature
@@ -1,11 +1,11 @@
 Feature: Authentication
 
     Scenario: Handling unauthenticated users
-        Given I'm mocking
+        Given Unauthenticated access audit logging is spied
         Given I am an unauthenticated user <token> when I access a protected <endpoint>
         Then I will get an error with <status> code
         And I will see an error <message>
-        And Assert called
+        And Unauthenticated access audit logs are created
 
         Examples:
             | token        | status | message                                                 | endpoint                                          |
@@ -16,8 +16,10 @@ Feature: Authentication
             | Bearer token | 401    | Could not validate the credential (Not enough segments) | /api/weather_models/GDPS/predictions/most_recent/ |
 
     Scenario: Verifying authenticated users
+        Given Unauthenticated access audit logging is spied
         Given I am an authenticated user when I access a protected <endpoint>
         Then I shouldn't get an unauthorized error <status> code
+        And Authenticated access audit logs are created
 
         Examples:
             | status | endpoint                                          |
--- api/app/tests/test_auth.py
+++ api/app/tests/test_auth.py
@@ -1,11 +1,9 @@
 """ Functional testing for authentication """
 from pytest_bdd import scenario, given, then
 from fastapi.testclient import TestClient
-from alchemy_mock.mocking import UnifiedAlchemyMagicMock
 from pytest_mock import MockerFixture
-from unittest.mock import patch
-import app.auth
 import pytest
+import app.auth
 import app.main
 
 
@@ -15,25 +13,24 @@ def test_auth_1st_scenario():
     """ BDD Scenario #1. """
 
 
-@given("I'm mocking", target_fixture='spy')
-def given_mock(mocker: MockerFixture):
+@given("Unauthenticated access audit logging is spied", target_fixture='create_audit_logs')
+def spy_on_unauthenticated_access_logs(mocker: MockerFixture):
+    """Spies on access audting logging for tests"""
     return mocker.spy(app.auth, 'create_api_access_audit_log')
 
 
 @given("I am an unauthenticated user <token> when I access a protected <endpoint>", target_fixture='response')
-def given_unauthenticated_user(mocker: MockerFixture, token: str, endpoint: str):
+def given_unauthenticated_user(token: str, endpoint: str):
     """ Make POST {endpoint} request which is protected """
-    # path where it's used, not where it's defined! (very important, always trips you up!)
     client = TestClient(app.main.app)
-    # save_hourly_actuals_spy = mocker.spy(app.auth, 'create_api_access_audit_log')
     response = client.post(endpoint, headers={'Authorization': token})
-
     return response
 
 
-@then("Assert called")
-def assert_called(spy):
-    assert spy.call_count == 1
+@then("Unauthenticated access audit logs are created")
+def no_access_is_logged(create_audit_logs, endpoint):
+    """Access audit logs are created"""
+    create_audit_logs.assert_called_once_with(None, False, endpoint)
 
 
 @then("I will get an error with <status> code")
@@ -52,6 +49,18 @@ def error_message(response, message: str):
 @scenario("test_auth.feature", "Verifying authenticated users", example_converters=dict(status=int))
 def test_auth_2nd_scenario():
     """ BDD Scenario #2. """
+
+
+@given("Authenticated access audit logging is spied", target_fixture='create_audit_logs')
+def spy_on_authenticated_access_logs(mocker: MockerFixture):
+    """Spies on access audting logging for tests"""
+    return mocker.spy(app.auth, 'create_api_access_audit_log')
+
+
+@then("Authenticated access audit logs are created")
+def access_is_logged(create_audit_logs, endpoint):
+    """Access audit logs are created"""
+    create_audit_logs.assert_called_once_with("test_username", True, endpoint)
 
 
 @given("I am an authenticated user when I access a protected <endpoint>", target_fixture='response_2')
