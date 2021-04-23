6f7bb1346abbdce5d5805c68776d17ec5856759a
Conor Brady <con.brad@gmail.com>
2021-04-23 09:36:55 -0700

    Make legend height smaller

--- web/src/features/fireWeather/components/AccuracyColorLegend.tsx
+++ web/src/features/fireWeather/components/AccuracyColorLegend.tsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles({
   root: {
     display: 'flex',
-    height: '80px',
+    height: '70px',
     flexDirection: 'column',
     padding: '5px'
   },
