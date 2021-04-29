9f981f9fd0b93c9158fa77c9fbc1593ca05775a6
Conor Brady <con.brad@gmail.com>
2021-04-29 15:47:41 -0700

    Fix tileLayer excessive re-rendering (#953)
    * One copy of unchanged tile layer
    
    * Commented reason for change

--- web/src/features/fireWeather/components/maps/WeatherMap.tsx
+++ web/src/features/fireWeather/components/maps/WeatherMap.tsx
@@ -29,6 +29,16 @@ const pointStyleFunction = (feature: any, resolution: any) => {
 const BC_ROAD_BASE_MAP_SERVER_URL =
   'https://maps.gov.bc.ca/arcgis/rest/services/province/roads_wm/MapServer'
 
+// Static source is allocated since our tiel source does not change and
+// a new source is not allocated every time WeatherMap is re-rendered,
+// which causes the TileLayer to re-render.
+const source = new olSource.XYZ({
+  url: `${BC_ROAD_BASE_MAP_SERVER_URL}/tile/{z}/{y}/{x}`,
+  // Normally we would get attribution text from `${BC_ROAD_BASE_MAP_SERVER_URL}?f=pjson`
+  // however this endpoint only allows the origin of http://localhost:3000, so the text has been just copied from that link
+  attributions: 'Government of British Columbia, DataBC, GeoBC'
+})
+
 const zoom = 6
 
 interface Props {
@@ -66,16 +76,7 @@ const WeatherMap = ({ redrawFlag, center, isCollapsed, setMapCenter }: Props) =>
       renderTooltip={renderTooltip}
       redrawFlag={redrawFlag}
     >
-      <TileLayer
-        source={
-          new olSource.XYZ({
-            url: `${BC_ROAD_BASE_MAP_SERVER_URL}/tile/{z}/{y}/{x}`,
-            // Normally we would get attribution text from `${BC_ROAD_BASE_MAP_SERVER_URL}?f=pjson`
-            // however this endpoint only allows the origin of http://localhost:3000, so the text has been just copied from that link
-            attributions: 'Government of British Columbia, DataBC, GeoBC'
-          })
-        }
-      />
+      <TileLayer source={source} />
       <VectorLayer
         source={
           new olSource.Vector({
