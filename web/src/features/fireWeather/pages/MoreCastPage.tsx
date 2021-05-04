98df44135df1f60e6e279c4417e32e7743018878
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-05-04 12:06:04 -0700

    modify useeffect to listen correctly (#983)

--- web/src/features/fireWeather/pages/MoreCastPage.tsx
+++ web/src/features/fireWeather/pages/MoreCastPage.tsx
@@ -64,7 +64,6 @@ const useStyles = makeStyles(theme => ({
 
 const MoreCastPage = () => {
   const classes = useStyles()
-  const dispatch = useDispatch()
   const location = useLocation()
 
   // We base our station & toi list entirely from the URL.
@@ -107,7 +106,10 @@ const MoreCastPage = () => {
     toggleTableView(newTableView.endsWith('true') ? 'true' : 'false')
   }
 
+  const dispatch = useDispatch()
   useEffect(() => {
+    const codesFromQuery = getStationCodesFromUrl(location.search)
+    const toiFromQuery = getTimeOfInterestFromUrl(location.search)
     if (codesFromQuery.length > 0) {
       dispatch(fetchObservations(codesFromQuery, toiFromQuery))
       dispatch(fetchForecasts(codesFromQuery, toiFromQuery))
@@ -123,7 +125,8 @@ const MoreCastPage = () => {
     dispatch(
       fetchWxStations(getDetailedStations, StationSource.unspecified, toiFromQuery)
     )
-  }, [toiFromQuery, codesFromQuery, dispatch])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location])
 
   return (
     <main className={classes.main}>
