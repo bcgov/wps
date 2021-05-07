047bfdcbf020bb16ba9543fac081e3f002a367dd
Conor Brady <con.brad@gmail.com>
2021-05-07 15:36:19 -0700

    Fix tests

--- api/app/tests/weather_models/crud.py
+++ api/app/tests/weather_models/crud.py
@@ -15,11 +15,12 @@ def get_actuals_left_outer_join_with_predictions(*args):  # pylint: disable=unus
             temperature=20,
             temp_valid=True,
             relative_humidity=50,
-            wind_speed=0,
+            wind_speed=2,
             rh_valid=True),
             ModelRunGridSubsetPrediction(
             tmp_tgl_2=[2, 3, 4, 5],
             rh_tgl_2=[10, 20, 30, 40],
+            wind_tgl_10=[1, 2, 3, 4],
             apcp_sfc_0=[2, 4, 3, 6],
             prediction_timestamp=datetime(2020, 10, 10, 18))],
         [HourlyActual(weather_date=datetime(2020, 10, 10, 19)), None],
@@ -27,18 +28,19 @@ def get_actuals_left_outer_join_with_predictions(*args):  # pylint: disable=unus
                       temperature=25,
                       temp_valid=True,
                       relative_humidity=70,
-                      wind_speed=0,
+                      wind_speed=2,
                       rh_valid=True), None],
         [HourlyActual(
             weather_date=datetime(2020, 10, 10, 21),
             temperature=30,
             temp_valid=True,
             relative_humidity=100,
-            wind_speed=0,
+            wind_speed=2,
             rh_valid=True),
             ModelRunGridSubsetPrediction(
             tmp_tgl_2=[1, 2, 3, 4],
             rh_tgl_2=[20, 30, 40, 50],
+            wind_tgl_10=[1, 2, 3, 4],
             apcp_sfc_0=[3, 6, 3, 4],
             prediction_timestamp=datetime(2020, 10, 10, 21))],
         # day 2
@@ -47,11 +49,12 @@ def get_actuals_left_outer_join_with_predictions(*args):  # pylint: disable=unus
             temperature=20,
             temp_valid=True,
             relative_humidity=50,
-            wind_speed=0,
+            wind_speed=2,
             rh_valid=True),
             ModelRunGridSubsetPrediction(
             tmp_tgl_2=[2, 3, 4, 5],
             rh_tgl_2=[10, 20, 30, 40],
+            wind_tgl_10=[1, 2, 3, 4],
             apcp_sfc_0=[2, 4, 3, 6],
             prediction_timestamp=datetime(2020, 10, 11, 18))],
         [HourlyActual(weather_date=datetime(2020, 10, 11, 19)), None],
@@ -66,11 +69,12 @@ def get_actuals_left_outer_join_with_predictions(*args):  # pylint: disable=unus
             temperature=30,
             temp_valid=True,
             relative_humidity=100,
-            wind_speed=0,
+            wind_speed=2,
             rh_valid=True),
             ModelRunGridSubsetPrediction(
             tmp_tgl_2=[1, 2, 3, 4],
             rh_tgl_2=[20, 30, 40, 50],
+            wind_tgl_10=[1, 2, 3, 4],
             apcp_sfc_0=[3, 6, 3, 4],
             prediction_timestamp=datetime(2020, 10, 11, 21))]
     ]
