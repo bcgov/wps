2a6670c702c53437dd5470b35e7666c59ac0b7b4
Conor Brady <con.brad@gmail.com>
2021-06-07 16:51:53 -0700

    Cronjob: Observations from API (#987)
    * Initially compiling
    
    * Fix lint and compile errors
    
    * Implement run code, add test
    
    * Adds pytest-asyncio for testing async functions, fixes tests but changes behaviour somewhat
    
    * Fix behaviour
    
    * Import fix
    
    * Log interpolation exception details
    
    * Validate metrics
    
    * Add test for validation logic
    
    * Fix tests and imports
    
    * Remove todo, fix comment
    
    * De-asyncify, make async calls synchronous
    
    * Better stack trace logging
    
    * Fix date range discrepancy
    
    * Add function comment
    
    * Remove pytest-asyncio dependency
    
    * Remove nest-asyncio
    
    * Reset poetry lock to main
    
    * Adds WFWX valid flag, includes it in validation, adds tests for it
    
    * Lint fix
    
    * Call WF1 API for stations in hourlies job
    
    * Fix timestamp types
    
    * auto-generated test fixtures
    
    * Fix lookup and fixture file names
    
    * Patch out WFWX for tests
    
    * Add WFWX env variables to hourly acutlats jobs
    
    * Add base url
    
    * Rejigger event loop and seperation of concerns
    
    * Undo naming of authenticate_session to signifiy its public
    
    * Rename method
    
    * Update comments
    
    * Only mime type for fetch raw stations broken?
    
    * Fixing broken things
    
    * Fix long line lint error
    
    * Allow null dewpoiunt
    
    * Allow dewpoint to be null fix field ereferences
    
    * Load ecodivision seasons once, increase TCP connection limit for fetching hourlies
    
    * Import fix
    
    * Undo response parsing
    
    * Fix comment
    
    * Dedup code
    
    * Handle unique violation exception separately
    
    * Fix lint issue
    
    * Logging enhancements
    
    * Undo excessive exception logging
    
    * Gracefully handle duplicate hourly actuals
    
    * Remove redundant ind
    
    * Make limit of connections smaller
    
    * Untested hourlies query builder
    
    * Fixed query class contract
    
    * Fix line too long
    
    * Fix query builder signature, implement untested hourly fetcher
    
    * Working with minimal network calls
    
    * Fix tests
    
    * Remove TCP connection limit
    
    * Lint fix
    
    * Dedup code and fix comment
    
    * Dedup code
    
    * Fix test
    
    * Credit singleton source
    
    * PR feedback, typing, comment and elegancy fixes
    
    * Fix variable naming
    
    * Fix test file import
    
    * Add test for query builder
    
    * Make less smelly
    
    * Add missing await
    
    * On-the fly dewpoint calculation
    
    * Handle None cases

---
+++ api/app/data/ecodivision_seasons.py
@@ -0,0 +1,77 @@
+""" Defines singleton class that keeps ecodivisions in memory for reuse """
+import os
+import json
+import logging
+import geopandas
+from shapely.geometry import Point
+
+
+class Singleton:
+    """ Non thread-safe singleton decorator.
+        Stolen from: https://stackoverflow.com/a/7346105
+    """
+
+    def __init__(self, decorated):
+        self._decorated = decorated
+
+    def instance(self):
+        """
+        Returns the singleton instance. Upon its first call, it creates a
+        new instance of the decorated class and calls its `__init__` method.
+        On all subsequent calls, the already created instance is returned.
+
+        """
+        try:
+            return self._instance
+        except AttributeError:
+            # pylint: disable=attribute-defined-outside-init
+            self._instance = self._decorated()
+            return self._instance
+
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self._decorated)
+
+
+dirname = os.path.dirname(__file__)
+core_season_file_path = os.path.join(
+    dirname, 'ecodivisions_core_seasons.json')
+ecodiv_shape_file_path = os.path.join(
+    dirname, 'ERC_ECODIV_polygon/ERC_ECODIV_polygon.shp')
+
+logger = logging.getLogger(__name__)
+
+
+@Singleton
+class EcodivisionSeasons:
+    """ Singleton that loads ecodivision data once and keeps it in memory for reuse.
+        No mutators, data loaded is immutable."""
+
+    def __init__(self):
+        with open(core_season_file_path) as file_handle:
+            core_seasons = json.load(file_handle)
+        self.core_seasons = core_seasons
+        self.ecodivisions = geopandas.read_file(ecodiv_shape_file_path)
+
+    def get_core_seasons(self):
+        """Returns core seasons"""
+        return self.core_seasons
+
+    def get_ecodivision_name(self, station_code: str,  latitude: str, longitude: str):
+        """ Returns the ecodivision name for a given lat/long coordinate """
+        # if station's latitude >= 60 (approx.), it's in the Yukon, so it won't be captured
+        # in the shapefile, but it's considered to be part of the SUB-ARCTIC HIGHLANDS ecodivision.
+        if latitude >= 60:
+            return 'SUB-ARCTIC HIGHLANDS'
+        station_coord = Point(float(longitude), float(latitude))
+        for _, ecodivision_row in self.ecodivisions.iterrows():
+            geom = ecodivision_row['geometry']
+            if station_coord.within(geom):
+                return ecodivision_row['CDVSNNM']
+
+        # If we've reached here, the ecodivision for the station has not been found.
+        logger.error('Ecodivision not found for station code %s at lat %f long %f',
+                     station_code, latitude, longitude)
+        return "DEFAULT"
