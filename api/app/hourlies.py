64b6f1d62ac4b40825e109a28fe343d151104424
Conor Brady <con.brad@gmail.com>
2021-06-07 16:51:53 -0700

    Cronjob: Observations from API (#987)
    * Initially compiling
    
    * Fix lint and compile errors
    
    * Implement run code, add test
    
    * Adds pytest-asyncio for testing async functions, fixes tests but changes behaviour somewhat
    
    * Fix behaviour
    
    * Import fix
    
    * Log interpolation exception details
    
    * Validate metrics
    
    * Add test for validation logic
    
    * Fix tests and imports
    
    * Remove todo, fix comment
    
    * De-asyncify, make async calls synchronous
    
    * Better stack trace logging
    
    * Fix date range discrepancy
    
    * Add function comment
    
    * Remove pytest-asyncio dependency
    
    * Remove nest-asyncio
    
    * Reset poetry lock to main
    
    * Adds WFWX valid flag, includes it in validation, adds tests for it
    
    * Lint fix
    
    * Call WF1 API for stations in hourlies job
    
    * Fix timestamp types
    
    * auto-generated test fixtures
    
    * Fix lookup and fixture file names
    
    * Patch out WFWX for tests
    
    * Add WFWX env variables to hourly acutlats jobs
    
    * Add base url
    
    * Rejigger event loop and seperation of concerns
    
    * Undo naming of authenticate_session to signifiy its public
    
    * Rename method
    
    * Update comments
    
    * Only mime type for fetch raw stations broken?
    
    * Fixing broken things
    
    * Fix long line lint error
    
    * Allow null dewpoiunt
    
    * Allow dewpoint to be null fix field ereferences
    
    * Load ecodivision seasons once, increase TCP connection limit for fetching hourlies
    
    * Import fix
    
    * Undo response parsing
    
    * Fix comment
    
    * Dedup code
    
    * Handle unique violation exception separately
    
    * Fix lint issue
    
    * Logging enhancements
    
    * Undo excessive exception logging
    
    * Gracefully handle duplicate hourly actuals
    
    * Remove redundant ind
    
    * Make limit of connections smaller
    
    * Untested hourlies query builder
    
    * Fixed query class contract
    
    * Fix line too long
    
    * Fix query builder signature, implement untested hourly fetcher
    
    * Working with minimal network calls
    
    * Fix tests
    
    * Remove TCP connection limit
    
    * Lint fix
    
    * Dedup code and fix comment
    
    * Dedup code
    
    * Fix test
    
    * Credit singleton source
    
    * PR feedback, typing, comment and elegancy fixes
    
    * Fix variable naming
    
    * Fix test file import
    
    * Add test for query builder
    
    * Make less smelly
    
    * Add missing await
    
    * On-the fly dewpoint calculation
    
    * Handle None cases

--- api/app/hourlies.py
+++ api/app/hourlies.py
@@ -3,11 +3,15 @@
 import math
 from typing import List
 from datetime import datetime, timedelta
+from aiohttp.client import ClientSession
+
+from aiohttp.connector import TCPConnector
 import app.db.database
 from app.db.crud.observations import get_hourly_actuals
 import app.stations
 from app import wildfire_one
 from app.schemas.observations import WeatherStationHourlyReadings, WeatherReading
+from app.utils.dewpoint import compute_dewpoint
 
 
 def get(value: object, condition: bool = True):
@@ -21,15 +25,12 @@ def get(value: object, condition: bool = True):
 
 async def fetch_hourly_readings_from_db(
         station_codes: List[int],
-        time_of_interest: datetime) -> List[WeatherStationHourlyReadings]:
+        date_from: datetime,
+        date_to: datetime) -> List[WeatherStationHourlyReadings]:
     """ Fetch the hourly readings from the database.
     """
-    stations = await app.stations.get_stations_by_codes(station_codes)
+    stations = await wildfire_one.get_stations_by_codes(station_codes)
     with app.db.database.get_read_session_scope() as session:
-        # by default, we want the past 5 days, and if available the next 10 days.
-        date_from = time_of_interest - timedelta(days=5)
-        # the UI is interested in hourly reading before and after the time of interest.
-        date_to = time_of_interest + timedelta(days=10)
         readings = get_hourly_actuals(session, station_codes, date_from, date_to)
         station_readings = None
         result = []
@@ -48,13 +49,23 @@ async def fetch_hourly_readings_from_db(
                 wind_speed=get(reading.wind_speed, reading.wspeed_valid),
                 wind_direction=get(reading.wind_direction, reading.wdir_valid),
                 precipitation=get(reading.precipitation, reading.precip_valid),
-                dewpoint=get(reading.dewpoint),
+                dewpoint=compute_dewpoint(get(reading.temperature), get(reading.relative_humidity)),
                 ffmc=get(reading.ffmc),
                 isi=get(reading.isi),
                 fwi=get(reading.fwi)
             )
             station_readings.values.append(weather_reading)
     return result
+
+
+def _get_time_interval(time_of_interest: datetime):
+    """ Returns the start and end datetimes for hourly readings based on given time of interest """
+    # by default, we want the past 5 days, and if available the next 10 days.
+    start_time_stamp = time_of_interest - timedelta(days=5)
+    # the UI is interested in hourly reading before and after the time of interest.
+    end_time_stamp = time_of_interest + timedelta(days=10)
+
+    return start_time_stamp, end_time_stamp
 
 
 async def get_hourly_readings(
@@ -64,8 +75,13 @@ async def get_hourly_readings(
     Reading 5 days before, and 10 days after the time of interest are returned.
     Depending on configuration, will read from WF1 or from local database.
     """
+    start_time_stamp, end_time_stamp = _get_time_interval(time_of_interest)
 
     if wildfire_one.use_wfwx():
-        return await wildfire_one.get_hourly_readings(station_codes, time_of_interest)
+        # Limit the number of concurrent connections.
+        async with ClientSession(connector=TCPConnector(limit=10)) as session:
+            header = await wildfire_one.get_auth_header(session)
+            return await wildfire_one.get_hourly_readings(
+                session, header, station_codes, start_time_stamp, end_time_stamp)
 
-    return await fetch_hourly_readings_from_db(station_codes, time_of_interest)
+    return await fetch_hourly_readings_from_db(station_codes, start_time_stamp, end_time_stamp)
