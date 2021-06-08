882cf7c90bf9b3474691077601e493bcd08b9e30
Andrea Williams <andrea.williams@gov.bc.ca>
2021-06-07 17:05:10 -0700

    Some cleanup

--- api/app/hfi_calc.py
+++ api/app/hfi_calc.py
@@ -14,20 +14,9 @@ from app.db.crud.hfi_calc import get_fire_centre_by_id, get_fire_centres, get_fu
 logger = logging.getLogger(__name__)
 
 
-async def _fetch_fire_centres_from_db():
-    """ Fetch a list of all fire centres. """
-    with app.db.database.get_read_session_scope() as session:
-        return get_fire_centres(session)
-
-
 async def fetch_fire_centre_by_id(id: int):
     with app.db.database.get_read_session_scope() as session:
         return get_fire_centre_by_id(session, id)
-
-
-async def fetch_fuel_types_from_db():
-    with app.db.database.get_read_session_scope() as session:
-        return get_fuel_types(session)
 
 
 async def fetch_fuel_type_by_id(id: int):
@@ -40,15 +29,9 @@ async def fetch_planning_area_by_id(id: int):
         return get_planning_area_by_id(session, id)
 
 
-async def _fetch_planning_areas_from_db(session: Session):
-    """ Fetch a list of all planning areas that should appear in the HFI Calculator worksheet
-    (across all fire centres). """
-    return get_planning_areas(session)
-
-
 async def fetch_hfi_station_data():
-    """  """
+    """ Fetch all station data relevant to HFI Calculator, including station's planning area
+    and fire centre, and station-specific info such as assigned fuel type and elevation. """
     with app.db.database.get_read_session_scope() as session:
         stations = get_planning_weather_stations(session)
-    print(stations)
     return stations
