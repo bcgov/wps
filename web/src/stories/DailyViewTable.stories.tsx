f4a400712d4e923c64f2f799ce5c0bbfac5cd281
Conor Brady <con.brad@gmail.com>
2021-06-08 10:43:53 -0700

    Fix ros subtable

--- web/src/stories/DailyViewTable.stories.tsx
+++ web/src/stories/DailyViewTable.stories.tsx
@@ -13,13 +13,13 @@ type PropTypes = React.ComponentProps<typeof DailyViewTable>
 const Template: Story<PropTypes> = args => <DailyViewTable {...args} />
 
 const testRowData = [
-  [159, 6.0, 24, 4.0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [237, 9.0, 37, 4.3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  [159, 6.0, 24, 4.0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [237, 9.0, 37, 4.3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 ]
 
 const stationData: StationData[] = [
-  { name: 'Test Station 1', metrics: testRowData[0] },
-  { name: 'Test Station 2', metrics: testRowData[1] }
+  { name: 'Test Station 1', metrics: testRowData[0], ros: { type01a: 1, type01b: 1 } },
+  { name: 'Test Station 2', metrics: testRowData[1], ros: { type01a: 2, type01b: 2 } }
 ]
 
 export const DailyTable = Template.bind({})
