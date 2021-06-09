7d0a98883a33b775c1d60ea6139690f2074716f7
Conor Brady <con.brad@gmail.com>
2021-06-09 10:24:11 -0700

    Fix bug add test

--- api/app/tests/test_wildfire_one.py
+++ api/app/tests/test_wildfire_one.py
@@ -1,5 +1,5 @@
 """ Unit testing for WFWX API code """
-from app.wildfire_one import BuildQueryAllHourliesByRange
+from app.wildfire_one import (BuildQueryAllHourliesByRange, BuildQueryDailesByStationCode)
 
 
 def test_build_all_hourlies_query():
@@ -11,4 +11,20 @@ def test_build_all_hourlies_query():
                           'size': '1000',
                           'page': 0,
                           'query': 'weatherTimestamp >=0;weatherTimestamp <1'
+                      })
+
+
+def test_build_dailies_by_station_code():
+    """ Verifies the query builder returns the correct url and parameters for dailies by station code """
+    query_builder = BuildQueryDailesByStationCode(0, 1, ['1', '2'])
+    result = query_builder.query(0)
+    assert result == ('https://wf1/wfwx/v1/dailies/search/'
+                      'findDailiesByStationIdIsInAndWeather'
+                      'TimestampBetweenOrderByStationIdAscWeatherTimestampAsc',
+                      {
+                          'size': '1000',
+                          'page': 0,
+                          'startingTimestamp': 0,
+                          'endingTimestamp': 1,
+                          'stationIds': ['1', '2']
                       })
