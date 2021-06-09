21079e497cd103a9a52d7ca8c10c1d7607eff5a5
Conor Brady <con.brad@gmail.com>
2021-06-09 13:11:06 -0700

    Lint fix

--- api/app/tests/test_wildfire_one.py
+++ api/app/tests/test_wildfire_one.py
@@ -18,8 +18,8 @@ def test_build_dailies_by_station_code():
     """ Verifies the query builder returns the correct url and parameters for dailies by station code """
     query_builder = BuildQueryDailesByStationCode(0, 1, ['1', '2'])
     result = query_builder.query(0)
-    assert result == ('https://wf1/wfwx/v1/dailies/search/'
-                      'findDailiesByStationIdIsInAndWeather'
+    assert result == ('https://wf1/wfwx/v1/dailies/search/' +
+                      'findDailiesByStationIdIsInAndWeather' +
                       'TimestampBetweenOrderByStationIdAscWeatherTimestampAsc',
                       {
                           'size': '1000',
