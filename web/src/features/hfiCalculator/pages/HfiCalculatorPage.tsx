b4215aa84faa4a783d5f45b2c278abc252d66954
Conor Brady <con.brad@gmail.com>
2021-06-09 13:44:43 -0700

    Initial component setup

--- web/src/features/hfiCalculator/pages/HfiCalculatorPage.tsx
+++ web/src/features/hfiCalculator/pages/HfiCalculatorPage.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import { Container, PageHeader, PageTitle } from 'components'
+import DailyViewTable from '../components/DailyViewTable'
 
 const HfiCalculatorPage: React.FunctionComponent = () => {
   return (
     <main data-testid="hfi-calculator-page">
       <PageHeader title="Predictive Services Unit" productName="HFI Calculator" />
       <PageTitle title="HFI Calculator" />
-      <Container>Hello World!</Container>
+      <Container maxWidth={'xl'}>
+        <DailyViewTable title={'Daily View Table'} stationData={[]}></DailyViewTable>
+      </Container>
     </main>
   )
 }
