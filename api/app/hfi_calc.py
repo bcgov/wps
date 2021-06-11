2fde837722e5dba98f10a9280784dfbf8870f75c
Andrea Williams <andrea.williams@gov.bc.ca>
2021-06-10 17:33:04 -0700

    Fixed pylint

--- api/app/hfi_calc.py
+++ api/app/hfi_calc.py
@@ -2,27 +2,30 @@
 """
 import logging
 import app.db.database
-from app.db.crud.hfi_calc import get_fire_centre_by_id, get_fuel_type_by_id, get_planning_area_by_id, get_planning_weather_stations
+from app.db.crud.hfi_calc import get_fire_centre_by_id,\
+    get_fuel_type_by_id,\
+    get_planning_area_by_id,\
+    get_planning_weather_stations
 
 logger = logging.getLogger(__name__)
 
 
-async def fetch_fire_centre_by_id(id: int):
+async def fetch_fire_centre_by_id(fire_centre_id: int):
     """ Fetches and returns FireCentre object identified by id """
     with app.db.database.get_read_session_scope() as session:
-        return get_fire_centre_by_id(session, id)
+        return get_fire_centre_by_id(session, fire_centre_id)
 
 
-async def fetch_fuel_type_by_id(id: int):
+async def fetch_fuel_type_by_id(fuel_type_id: int):
     """ Fetches and returns FuelType object identified by id. """
     with app.db.database.get_read_session_scope() as session:
-        return get_fuel_type_by_id(session, id)
+        return get_fuel_type_by_id(session, fuel_type_id)
 
 
-async def fetch_planning_area_by_id(id: int):
+async def fetch_planning_area_by_id(planning_area_id: int):
     """ Fetches and returns PlanningArea object identified by id. """
     with app.db.database.get_read_session_scope() as session:
-        return get_planning_area_by_id(session, id)
+        return get_planning_area_by_id(session, planning_area_id)
 
 
 async def fetch_hfi_station_data():
