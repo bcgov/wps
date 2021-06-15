f7711b3fd2c7a779df35c8c9ad7304ab290eea18
Andrea Williams <andrea.williams@gov.bc.ca>
2021-06-14 17:35:08 -0700

    Finished refactoring I think

--- api/app/routers/hfi_calc.py
+++ api/app/routers/hfi_calc.py
@@ -1,6 +1,6 @@
 """ Routers for HFI Calculator """
 import logging
-from typing import List
+from typing import Dict, List
 from fastapi import APIRouter, Response, Depends
 import app
 from app import wildfire_one
@@ -32,23 +32,25 @@ async def get_fire_centres(response: Response):
             station_query = get_fire_weather_stations(session)
             # Prepare a dictionary for storing station info in.
             station_info_dict = {}
+            # Prepare empty data structures to be used in HFIWeatherStationsResponse
+            fire_centres_list = []
+            planning_areas_by_fire_center = {}
+            stations_by_planning_area = {}
             # Iterate through all the database records, collecting all the data we need.
             for (station_record, fuel_type_record, planning_area_record, fire_centre_record) in station_query:
+                fire_centre = FireCentre(name=fire_centre_record.name)
+                planning_area = PlanningArea(name=planning_area_record.name, fire_centre=fire_centre)
+                fire_centres_list.append(fire_centre)
+                planning_areas_by_fire_center.get(fire_centre.name).append(planning_area)
                 station_info_dict[station_record.station_code] = {
                     'fuel_type': FuelType(
                         abbrev=fuel_type_record.abbrev,
                         description=fuel_type_record.description),
-                    'planning_area': PlanningArea(
-                        name=planning_area_record.name,
-                        fire_centre=FireCentre(name=fire_centre_record.name)
-                    )
-
+                    'planning_area': planning_area
                 }
 
             # We're still missing some data that we need from wfwx, so give it the list of stations
             wfwx_stations_data = await wildfire_one.get_stations_by_codes(list(station_info_dict.keys()))
-            # Prepare a list to store our final station objects in.
-            stations_list: List[WeatherStation] = []
             # Iterate through all the stations from wildfire one.
             for wfwx_station in wfwx_stations_data:
                 station_info = station_info_dict[wfwx_station.code]
@@ -63,9 +65,11 @@ async def get_fire_centres(response: Response):
                                                  station_props=station_properties,
                                                  planning_area=station_info['planning_area'])
 
-                stations_list.append(weather_station)
+                stations_by_planning_area.get(station_info['planning_area']).append(weather_station)
 
-            return HFIWeatherStationsResponse(stations=stations_list)
+            return HFIWeatherStationsResponse(fire_centres=fire_centres_list,
+                                              planning_areas_by_fire_center=planning_areas_by_fire_center,
+                                              stations_by_planning_area=stations_by_planning_area)
 
     except Exception as exc:
         logger.critical(exc, exc_info=True)
