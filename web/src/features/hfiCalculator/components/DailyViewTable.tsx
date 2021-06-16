c808a58c015d95501697ab23cd683ec30e822062
Conor Brady <con.brad@gmail.com>
2021-06-15 17:16:31 -0700

    Initial hook up, no dailies showing yet

--- web/src/features/hfiCalculator/components/DailyViewTable.tsx
+++ web/src/features/hfiCalculator/components/DailyViewTable.tsx
@@ -11,11 +11,13 @@ import {
   Typography
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import { selectHFIStations, selectHFIStationsLoading } from 'app/rootReducer'
-import { useSelector } from 'react-redux'
+import { FireCentre } from 'api/hfiCalcAPI'
+import { StationDaily } from 'api/hfiCalculatorAPI'
 
 interface Props {
   title: string
+  fireCentres: Record<string, FireCentre>
+  dailies: StationDaily[]
   testId?: string
 }
 
@@ -57,95 +59,77 @@ const useStyles = makeStyles({
 
 const DailyViewTable = (props: Props) => {
   const classes = useStyles()
-  const { fireCentres } = useSelector(selectHFIStations)
-  const stationDataLoading = useSelector(selectHFIStationsLoading)
 
   return (
     <div className={classes.display} data-testid={props.testId}>
-      {stationDataLoading && 'Loading...'}
-
-      {!stationDataLoading && (
-        <div>
-          <Typography component="div" variant="subtitle2">
-            {props.title}
-          </Typography>
-          <Paper className={classes.paper} elevation={1}>
-            <TableContainer className={classes.tableContainer}>
-              <Table
-                stickyHeader
-                size="small"
-                aria-label="daily table view of HFI by planning area"
-              >
-                <TableHead>
-                  <TableRow>
-                    {/* TableHead and TableRow don't apply classes.tableHeader styling - has to be assigned to TableCell */}
-                    <TableCell className={classes.tableHeader} key="header-location">
-                      Location
-                    </TableCell>
-                    <TableCell className={classes.tableHeader} key="header-elevation">
-                      Elev. (m)
-                    </TableCell>
-                    <TableCell className={classes.tableHeader} key="header-fuel-type">
-                      FBP Fuel Type
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {Object.entries(fireCentres).map(([centreName, centre]) => {
-                    return (
-                      <React.Fragment key={`fire-centre-${centreName}`}>
-                        <TableRow key={`fire-centre-${centreName}`}>
-                          <TableCell colSpan={3} className={classes.fireCentre}>
-                            {centre.name}
-                          </TableCell>
-                        </TableRow>
-                        {Object.entries(centre.planning_areas).map(([areaName, area]) => {
-                          return (
-                            <React.Fragment key={`zone-${areaName}`}>
+      <Typography component="div" variant="subtitle2">
+        {props.title}
+      </Typography>
+      <Paper className={classes.paper} elevation={1}>
+        <TableContainer>
+          <Table stickyHeader aria-label="daily table view of HFI by planning area">
+            <TableHead>
+              <TableRow>
+                {/* TableHead and TableRow don't apply classes.tableHeader styling - has to be assigned to TableCell */}
+                <TableCell className={classes.tableHeader} key="header-location">
+                  Location
+                </TableCell>
+                <TableCell className={classes.tableHeader} key="header-elevation">
+                  Elev. (m)
+                </TableCell>
+                <TableCell className={classes.tableHeader} key="header-fuel-type">
+                  FBP Fuel Type
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.entries(props.fireCentres).map(([centreName, centre]) => {
+                return (
+                  <React.Fragment key={`fire-centre-${centreName}`}>
+                    <TableRow key={`fire-centre-${centreName}`}>
+                      <TableCell colSpan={3} className={classes.fireCentre}>
+                        {centre.name}
+                      </TableCell>
+                    </TableRow>
+                    {Object.entries(centre.planning_areas).map(([areaName, area]) => {
+                      return (
+                        <React.Fragment key={`zone-${areaName}`}>
+                          <TableRow
+                            className={classes.planningArea}
+                            key={`zone-${areaName}`}
+                          >
+                            <TableCell className={classes.planningArea} colSpan={3}>
+                              {area.name}
+                            </TableCell>
+                          </TableRow>
+                          {Object.entries(area.stations).map(([stationCode, station]) => {
+                            return (
                               <TableRow
-                                className={classes.planningArea}
-                                key={`zone-${areaName}`}
+                                className={classes.station}
+                                key={`station-${stationCode}`}
                               >
-                                <TableCell className={classes.planningArea} colSpan={3}>
-                                  {area.name}
+                                <TableCell key={`station-${station.code}-name`}>
+                                  {station.station_props.name} ({station.code})
+                                </TableCell>
+                                <TableCell key={`station-${station.code}-elevation`}>
+                                  {station.station_props.elevation}
+                                </TableCell>
+                                <TableCell key={`station-${station.code}-fuel-type`}>
+                                  {station.station_props.fuel_type.abbrev}
                                 </TableCell>
                               </TableRow>
-                              {Object.entries(area.stations).map(
-                                ([stationCode, station]) => {
-                                  return (
-                                    <TableRow
-                                      className={classes.station}
-                                      key={`station-${stationCode}`}
-                                    >
-                                      <TableCell key={`station-${station.code}-name`}>
-                                        {station.station_props.name} ({station.code})
-                                      </TableCell>
-                                      <TableCell
-                                        key={`station-${station.code}-elevation`}
-                                      >
-                                        {station.station_props.elevation}
-                                      </TableCell>
-                                      <TableCell
-                                        key={`station-${station.code}-fuel-type`}
-                                      >
-                                        {station.station_props.fuel_type.abbrev}
-                                      </TableCell>
-                                    </TableRow>
-                                  )
-                                }
-                              )}
-                            </React.Fragment>
-                          )
-                        })}
-                      </React.Fragment>
-                    )
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Paper>
-        </div>
-      )}
+                            )
+                          })}
+                        </React.Fragment>
+                      )
+                    })}
+                  </React.Fragment>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   )
 }
--- web/src/features/hfiCalculator/pages/HfiCalculatorPage.tsx
+++ web/src/features/hfiCalculator/pages/HfiCalculatorPage.tsx
@@ -2,21 +2,57 @@ import React, { useEffect } from 'react'
 
 import { Container, PageHeader, PageTitle } from 'components'
 import DailyViewTable from 'features/hfiCalculator/components/DailyViewTable'
-import { useDispatch } from 'react-redux'
 import { fetchHFIStations } from '../slices/stationsSlice'
+import { fetchHFIDailies } from '../slices/hfiCalculatorSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { DateTime } from 'luxon'
+import { selectHFIDailies } from 'app/rootReducer'
+import { selectHFIStations, selectHFIStationsLoading } from 'app/rootReducer'
+import { makeStyles, CircularProgress } from '@material-ui/core'
 
 const HfiCalculatorPage: React.FunctionComponent = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchHFIStations())
+  const useStyles = makeStyles({
+    container: {
+      display: 'flex',
+      justifyContent: 'center'
+    }
   })
+  const classes = useStyles()
+
+  const dispatch = useDispatch()
+  const { dailies, loading } = useSelector(selectHFIDailies)
+  const { fireCentres } = useSelector(selectHFIStations)
+  const stationDataLoading = useSelector(selectHFIStationsLoading)
+  useEffect(() => {
+    // For now just give dailies for today
+    const startTime = DateTime.now()
+      .startOf('day')
+      .toUTC()
+      .valueOf()
+    const endTime = DateTime.now()
+      .endOf('day')
+      .toUTC()
+      .valueOf()
+    dispatch(fetchHFIDailies(startTime, endTime))
+    dispatch(fetchHFIStations())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <main data-testid="hfi-calculator-page">
       <PageHeader title="Predictive Services Unit" productName="HFI Calculator" />
       <PageTitle title="HFI Calculator" />
-      <Container>
-        <DailyViewTable title="HFI Calculator Daily View" testId="hfi-calc-daily-table" />
+      <Container className={classes.container} maxWidth={'xl'}>
+        {loading || stationDataLoading ? (
+          <CircularProgress />
+        ) : (
+          <DailyViewTable
+            title="HFI Calculator Daily View"
+            testId="hfi-calc-daily-table"
+            fireCentres={fireCentres}
+            dailies={dailies}
+          />
+        )}
       </Container>
     </main>
   )
