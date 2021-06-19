3b812264a02bc484e0cea39fb74390e493ff6739
Conor Brady <con.brad@gmail.com>
2021-06-18 17:21:41 -0700

    function rename

--- api/app/utils/time.py
+++ api/app/utils/time.py
@@ -16,8 +16,8 @@ def get_utc_now():
     return datetime.now(tz=timezone.utc)
 
 
-def get_utc_today_start_and_end():
-    """ Get the start and end datetime range for today in UTC"""
+def get_pst_today_start_and_end():
+    """ Get the start and end datetime range for today in PST since the app assumes all in PST."""
     today = datetime.now(tz=get_pst_tz())
     start = today.replace(hour=0, minute=0, second=0, microsecond=0)
     end = start + timedelta(days=1)
