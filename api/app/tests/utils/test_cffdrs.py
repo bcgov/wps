104cc62a1dc54c7a65c117825911b377be697a1b
Conor Brady <con.brad@gmail.com>
2021-06-28 14:35:21 -0700

    Add monkeypatch back

--- api/app/tests/utils/test_cffdrs.py
+++ api/app/tests/utils/test_cffdrs.py
@@ -3,24 +3,24 @@ import pytest
 from app.utils import cffdrs
 
 
-def test_ros(mock_cffdrs):
+def test_ros(monkeypatch, mock_cffdrs):
     """ ROS runs """
     assert cffdrs.rate_of_spread("C7", 1, 1, 1, 1) == 1.2966988409822604e-05
 
 
-def test_ros_no_isi(mock_cffdrs):
+def test_ros_no_isi(monkeypatch, mock_cffdrs):
     """ ROS fails """
     with pytest.raises(cffdrs.CFFDRSException):
         cffdrs.rate_of_spread("C7", None, 1, 1, 1)
 
 
-def test_ros_no_bui(mock_cffdrs):
+def test_ros_no_bui(monkeypatch, mock_cffdrs):
     """ ROS fails """
     with pytest.raises(cffdrs.CFFDRSException):
         cffdrs.rate_of_spread("C7", 1, None, 1, 1) == None
 
 
-def test_ros_no_params(mock_cffdrs):
+def test_ros_no_params(monkeypatch, mock_cffdrs):
     """ ROS fails """
     with pytest.raises(cffdrs.CFFDRSException):
         cffdrs.rate_of_spread("C7", None, None, None, None) == None
