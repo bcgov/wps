fddf2f7cec7dd5ecc0f4018c91b6e0cf1b9aa9cc
Conor Brady <con.brad@gmail.com>
2021-06-29 08:33:53 -0700

    Localize import to mocked function

--- api/app/utils/r_importer.py
+++ api/app/utils/r_importer.py
@@ -1,8 +1,7 @@
 """ Imports R libs, easier for mocking"""
 
-from rpy2.robjects.packages import importr
-
 
 def import_cffsdrs():
     """ Import cffdrs """
+    from rpy2.robjects.packages import importr
     return importr('cffdrs')
