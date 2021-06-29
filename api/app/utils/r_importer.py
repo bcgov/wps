1f94059c9699d692650bddc96a2b4701cd241331
Conor Brady <con.brad@gmail.com>
2021-06-29 09:40:59 -0700

    Move import to top of module

--- api/app/utils/r_importer.py
+++ api/app/utils/r_importer.py
@@ -1,8 +1,7 @@
 """ Imports R libs, easier for mocking"""
+from rpy2.robjects.packages import importr
 
 
 def import_cffsdrs():
     """ Import cffdrs """
-    # pylint: disable=import-outside-toplevel
-    from rpy2.robjects.packages import importr
     return importr('cffdrs')
