ab5f50825cc703b18ca5a8f1e022705263f664de
Conor Brady <con.brad@gmail.com>
2021-06-29 08:45:58 -0700

    Lint fix

--- api/app/utils/r_importer.py
+++ api/app/utils/r_importer.py
@@ -3,6 +3,6 @@
 
 def import_cffsdrs():
     """ Import cffdrs """
-    # pylint: disable= import-outside-toplevel
+    # pylint: disable=import-outside-toplevel
     from rpy2.robjects.packages import importr
     return importr('cffdrs')
