a94faf0821ebdb9f47fe9f8af2f036d58be935d1
Conor Brady <con.brad@gmail.com>
2021-06-30 10:14:43 -0700

    update param configs for fuel types

--- api/app/utils/cffdrs.py
+++ api/app/utils/cffdrs.py
@@ -40,28 +40,33 @@ class CFFDRSException(Exception):
 # To store in DB: PC, PDF, CC, CBH (attached to fuel type, red book)
 PARAMS_ERROR_MESSAGE = "One or more params passed to R call is None."
 
-DEFAULT_METRICS = {"PC": 1, "PDF": 1, "CC": 1, "CBH": 1}
-
-# TODO: Once PC, PDF, CC, CDH are confirmed, replace these defaults
-# and potentially store then in the DB as columns in FuelType
-FUEL_TYPE_LOOKUP = {"C1": DEFAULT_METRICS,
-                    "C2": DEFAULT_METRICS,
-                    "C3": DEFAULT_METRICS,
-                    "C4": DEFAULT_METRICS,
-                    "C5": DEFAULT_METRICS,
-                    "C6": DEFAULT_METRICS,
-                    "C7": DEFAULT_METRICS,
-                    "D1": DEFAULT_METRICS,
-                    "D2": DEFAULT_METRICS,
-                    "M1": DEFAULT_METRICS,
-                    "M2": DEFAULT_METRICS,
-                    "M3": DEFAULT_METRICS,
-                    "M4": DEFAULT_METRICS,
-                    "O1A": DEFAULT_METRICS,
-                    "O1B": DEFAULT_METRICS,
-                    "S1": DEFAULT_METRICS,
-                    "S2": DEFAULT_METRICS,
-                    "S3": DEFAULT_METRICS
+# PC, PDF, CC, CDH from the Red Book. Assumes values of 1 for unknown CC and CBH.
+# TODO: Store then in the DB as columns in FuelType
+FUEL_TYPE_LOOKUP = {"C1": {"PC": 100, "PDF": 0, "CC": 1, "CBH": 2},
+                    "C2": {"PC": 100, "PDF": 0, "CC": 1, "CBH": 3},
+                    "C3": {"PC": 100, "PDF": 0, "CC": 1, "CBH": 8},
+                    "C4": {"PC": 100, "PDF": 0, "CC": 1, "CBH": 4},
+                    "C5": {"PC": 100, "PDF": 0, "CC": 1, "CBH": 18},
+                    # There's a 2m and 7m C6 in RB. Opted for 7m.
+                    "C6": {"PC": 100, "PDF": 0, "CC": 1, "CBH": 7},
+                    "C7": {"PC": 100, "PDF": 0, "CC": 1, "CBH": 10},
+                    # No CBH listed in RB fire intensity class table for D1.
+                    "D1": {"PC": 0, "PDF": 0, "CC": 1, "CBH": 1},
+                    # No CBH listed in RB fire intensity class table for D2.
+                    "D2": {"PC": 0, "PDF": 0, "CC": 1, "CBH": 1},
+                    # 3 different PC configurations for M1. Opted for 50%.
+                    "M1": {"PC": 50, "PDF": 0, "CC": 1, "CBH": 6},
+                    # 3 different PC configurations for M2. Opted for 50%.
+                    "M2": {"PC": 50, "PDF": 0, "CC": 1, "CBH": 6},
+                    # 3 different PDF configurations for M3. Opted for 60%.
+                    "M3": {"PC": 0, "PDF": 60, "CC": 1, "CBH": 6},
+                    # 3 different PDF configurations for M4. Opted for 60%.
+                    "M4": {"PC": 0, "PDF": 60, "CC": 1, "CBH": 6},
+                    "O1A": {"PC": 0, "PDF": 0, "CC": 1, "CBH": 1},
+                    "O1B": {"PC": 0, "PDF": 0, "CC": 1, "CBH": 1},
+                    "S1": {"PC": 0, "PDF": 0, "CC": 1, "CBH": 1},
+                    "S2": {"PC": 0, "PDF": 0, "CC": 1, "CBH": 1},
+                    "S3": {"PC": 0, "PDF": 0, "CC": 1, "CBH": 1}
                     }
 
 
