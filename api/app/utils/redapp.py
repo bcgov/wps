ed4d2f230c210d093ec14323759ef70965c264e9
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-07-13 12:52:22 -0700

    added detach

--- api/app/utils/redapp.py
+++ api/app/utils/redapp.py
@@ -1,11 +1,129 @@
 """ Call Java REDapp code.
 """
-from datetime import datetime
+from datetime import datetime, date
 import jnius_config
 # import jnius - importing jnius on this level causes an segmentation fault.
 from app import config
 
 jnius_config.set_classpath(config.get('CLASSPATH'))
+
+
+class FBPCalculations:  # pylint: disable=missing-class-docstring, too-many-instance-attributes
+    """ Duplicate of Java class ca.cwfgm.fbp.FBPCalculations
+
+    public double ros_t;
+    public double ros_eq;
+    public double fros;
+    public double bros;
+    public double rso;
+    public double hfi;
+    public double ffi;
+    public double bfi;
+    public double area;
+    public double perimeter;
+    public double distanceHead;
+    public double distanceFlank;
+    public double distanceBack;
+    public double lb;
+    public double csi;
+    public double cfb;
+    public double sfc;
+    public double tfc;
+    public double cfc;
+    public double isi;
+    public double fmc;
+    public double wsv;
+    public double raz;
+    public double conifMixedWood;
+    public double deadBalsam;
+    public double grassCuring;
+    public double grassFuelLoad;
+    public double crownBase;
+    public double elapsedTime;
+    public double ffmc;
+    public double windSpeed;
+    public double windDirection;
+    public double bui;
+    public double dmc;
+    public double dc;
+    public double slopeValue;
+    public double aspect;
+    public double latitude;
+    public double longitude;
+    public double elevation;
+    public boolean acceleration;
+    public boolean useCrownBaseHeight;
+    public boolean useBui;
+    public boolean useBuildup;
+    public boolean useSlope;
+    public boolean useGreenup;
+    public int fuelType;
+    public String fireDescription;
+    public String headFireDescription;
+    public String flankFireDescription;
+    public String backFireDescription;
+
+    public Calendar m_date;
+
+    public boolean cfbPossible = true;
+    """
+
+    def __init__(self):  # pylint: disable=too-many-statements
+        """ Init variables """
+        # pylint: disable=invalid-name
+        self.ros_t: float = None
+        self.ros_eq: float = None
+        self.fros: float = None
+        self.bros: float = None
+        self.rso: float = None
+        self.hfi: float = None
+        self.ffi: float = None
+        self.bfi: float = None
+        self.area: float = None
+        self.perimeter: float = None
+        self.distanceHead: float = None
+        self.distanceFlank: float = None
+        self.distanceBack: float = None
+        self.lb: float = None
+        self.csi: float = None
+        self.cfb: float = None
+        self.sfc: float = None
+        self.tfc: float = None
+        self.cfc: float = None
+        self.isi: float = None
+        self.fmc: float = None
+        self.wsv: float = None
+        self.raz: float = None
+        self.conifMixedWood: float = None
+        self.deadBalsam: float = None
+        self.grassCuring: float = None
+        self.grassFuelLoad: float = None
+        self.crownBase: float = None
+        self.elapsedTime: float = None
+        self.ffmc: float = None
+        self.windSpeed: float = None
+        self.windDirection: float = None
+        self.bui: float = None
+        self.dmc: float = None
+        self.dc: float = None
+        self.slopeValue: float = None
+        self.aspect: float = None
+        self.latitude: float = None
+        self.longitude: float = None
+        self.elevation: float = None
+        self.acceleration: bool = None
+        self.useCrownBaseHeight: bool = None
+        self.useBui: bool = None
+        self.useBuildup: bool = None
+        self.useSlope: bool = None
+        self.useGreenup: bool = None
+        self.fuelType: int = None
+        self.fireDescription: str = None
+        self.headFireDescription: str = None
+        self.flankFireDescription: str = None
+        self.backFireDescription: str = None
+        self.m_date: date = None
+        self.cfbPossible: bool = True
 
 
 class UnmappedFuelType(Exception):
@@ -132,62 +250,8 @@ def FBPCalculateStatisticsCOM(elevation: float,  # pylint: disable=invalid-name,
     fire behaviour.
 
     public class FBPCalculations {
-        public double ros_t;
-        public double ros_eq;
-        public double fros;
-        public double bros;
-        public double rso;
-        public double hfi;
-        public double ffi;
-        public double bfi;
-        public double area;
-        public double perimeter;
-        public double distanceHead;
-        public double distanceFlank;
-        public double distanceBack;
-        public double lb;
-        public double csi;
-        public double cfb;
-        public double sfc;
-        public double tfc;
-        public double cfc;
-        public double isi;
-        public double fmc;
-        public double wsv;
-        public double raz;
-        public double conifMixedWood;
-        public double deadBalsam;
-        public double grassCuring;
-        public double grassFuelLoad;
-        public double crownBase;
-        public double elapsedTime;
-        public double ffmc;
-        public double windSpeed;
-        public double windDirection;
-        public double bui;
-        public double dmc;
-        public double dc;
-        public double slopeValue;
-        public double aspect;
-        public double latitude;
-        public double longitude;
-        public double elevation;
-        public boolean acceleration;
-        public boolean useCrownBaseHeight;
-        public boolean useBui;
-        public boolean useBuildup;
-        public boolean useSlope;
-        public boolean useGreenup;
-        public int fuelType;
-        public String fireDescription;
-        public String headFireDescription;
-        public String flankFireDescription;
-        public String backFireDescription;
 
-        public Calendar m_date;
-
-        public boolean cfbPossible = true;
-
+        # methods:
         public FBPCalculations() {
         initializeInputValues();
         initializeOutputValues();
@@ -197,92 +261,53 @@ def FBPCalculateStatisticsCOM(elevation: float,  # pylint: disable=invalid-name,
     """
     # pylint: disable=too-many-locals
     # we have to do a late import, otherwise we get a segmentation fault.
-    from jnius import autoclass  # pylint: disable=import-outside-toplevel
-    Calendar = autoclass('java.util.Calendar')  # pylint: disable=invalid-name
-    TimeZone = autoclass('java.util.TimeZone')  # pylint: disable=invalid-name
-    FBPCalculations = autoclass('ca.cwfgm.fbp.FBPCalculations')  # pylint: disable=invalid-name
+    import jnius  # pylint: disable=import-outside-toplevel
+    try:
+        Calendar = jnius.autoclass('java.util.Calendar')  # pylint: disable=invalid-name
+        TimeZone = jnius.autoclass('java.util.TimeZone')  # pylint: disable=invalid-name
+        FBPCalculationsClass = jnius.autoclass('ca.cwfgm.fbp.FBPCalculations')  # pylint: disable=invalid-name
 
-    if percentage_dead_balsam_fir is None:
-        percentage_dead_balsam_fir = 0.0
-    if grass_cure is None:
-        grass_cure = 0.0
+        if percentage_dead_balsam_fir is None:
+            percentage_dead_balsam_fir = 0.0
+        if grass_cure is None:
+            grass_cure = 0.0
 
-    fbp = FBPCalculations()
-    fbp.elevation = elevation
-    fbp.latitude = latitude
-    fbp.longitude = longitude
-    fbp.ffmc = ffmc
-    fbp.dmc = dmc
-    fbp.dc = dc
-    fbp.useBui = True
-    fbp.useBuildup = True
-    fbp.bui = bui
-    fbp.windSpeed = wind_speed
-    fbp.windDirection = wind_direction
-    fbp.fuelType = red_app_fuel_type_map(fuel_type)
-    fbp.conifMixedWood = percentage_conifer
-    fbp.deadBalsam = percentage_dead_balsam_fir
-    fbp.grassCuring = grass_cure
-    fbp.crownBase = crown_base_height
-    # grassFuelLoad
+        fbp = FBPCalculationsClass()
+        fbp.elevation = elevation
+        fbp.latitude = latitude
+        fbp.longitude = longitude
+        fbp.ffmc = ffmc
+        fbp.dmc = dmc
+        fbp.dc = dc
+        fbp.useBui = True
+        fbp.useBuildup = True
+        fbp.bui = bui
+        fbp.windSpeed = wind_speed
+        fbp.windDirection = wind_direction
+        fbp.fuelType = red_app_fuel_type_map(fuel_type)
+        fbp.conifMixedWood = percentage_conifer
+        fbp.deadBalsam = percentage_dead_balsam_fir
+        fbp.grassCuring = grass_cure
+        fbp.crownBase = crown_base_height
+        # grassFuelLoad
 
-    fbp.useSlope = False
+        fbp.useSlope = False
 
-    gmt = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
-    gmt.set(time_of_interest.year, time_of_interest.month, time_of_interest.day,
-            time_of_interest.hour, time_of_interest.minute, time_of_interest.second)
-    fbp.m_date = gmt
+        gmt = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
+        gmt.set(time_of_interest.year, time_of_interest.month, time_of_interest.day,
+                time_of_interest.hour, time_of_interest.minute, time_of_interest.second)
+        fbp.m_date = gmt
 
-    fbp.FBPCalculateStatisticsCOM()
+        fbp.FBPCalculateStatisticsCOM()
 
-    return fbp
+        copy = FBPCalculations()
+        for key in dir(copy):
+            if '__' not in key:
+                setattr(copy, key, getattr(fbp, key))
 
-
-def get_class_FWICalculations():  # pylint: disable=invalid-name
-    """
-    Return Java class ca.cwfgm.fwi.FWICalculations
-
-    public class FWICalculations {
-
-        public double ystrdyFFMC;
-        public double ystrdyDMC;
-        public double ystrdyDC;
-        public double noonTemp;
-        public double noonRH;
-        public double noonPrecip;
-        public double noonWindSpeed;
-        public double hrlyTemp;
-        public double hrlyRH;
-        public double hrlyPrecip;
-        public double hrlyWindSpeed;
-        public double dlyFFMC;
-        public double dlyDMC;
-        public double dlyDC;
-        public double dlyISI;
-        public double dlyBUI;
-        public double dlyFWI;
-        public double dlyDSR;
-        public double hlyHFFMC;
-        public double hlyHISI;
-        public double hlyHFWI;
-        public double prvhlyFFMC;
-        public boolean calcHourly;
-        public boolean useVanWagner;
-        public boolean useLawsonPreviousHour = false;
-        private WorldLocation wl;
-        public Calendar m_date;
-        public String m_init_timezone_code;
-
-        public double getLatitude();
-        public void setLatitude(double value);
-        public double getLongitude();
-        public void setLongitude(double value);
-
-        public void setTimezone(WTimeSpan span);
-        public void setDST(WTimeSpan span);
-
-        public void FWICalculateDailyStatisticsCOM();
-    """
-    # we have to do a late import, otherwise we get a segmentation fault.
-    from jnius import autoclass  # pylint: disable=import-outside-toplevel
-    return autoclass('ca.cwfgm.fwi.FWICalculations')
+        return copy
+    finally:
+        # Each time you create a native thread in Python and use Pyjnius, any call to Pyjnius
+        # methods will force attachment of the native thread to the current JVM. But you must
+        # detach it before leaving the thread, and Pyjnius cannot do it for you.
+        jnius.detach()
