e429dcbb914cef5c6e6220446e790ad2cc6e4a17
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-07-13 11:41:09 -0700

    added tests

---
+++ api/app/tests/fba/test_fba_error.py
@@ -0,0 +1,162 @@
+"""
+Unit tests for fire behavour calculator.
+"""
+from datetime import date
+import logging
+from pytest_bdd import scenario, given, then
+from app import configure_logging
+from app.utils.time import get_hour_20_from_date
+from app.utils.fba_calculator import calculate_fire_behavour_advisory, FBACalculatorWeatherStation
+from app.utils.redapp import FBPCalculateStatisticsCOM
+import pytest
+
+
+configure_logging()
+
+logger = logging.getLogger(__name__)
+
+
+def _str2float(value: str):
+    if value == 'None':
+        return None
+    return float(value)
+
+
+@pytest.mark.usefixtures('mock_jwt_decode')
+@scenario('test_fba_error.feature', 'Fire Behaviour Calculation',
+          example_converters=dict(elevation=float,
+                                  latitude=float,
+                                  longitude=float,
+                                  time_of_interest=date.fromisoformat,
+                                  wind_speed=float,
+                                  wind_direction=float,
+                                  percentage_conifer=_str2float,
+                                  percentage_dead_balsam_fir=_str2float,
+                                  crown_base_height=_str2float,
+                                  grass_cure=_str2float,
+                                  isi=float,
+                                  bui=float,
+                                  ffmc=float,
+                                  dmc=float,
+                                  dc=float,
+                                  fuel_type=str,
+                                  red_app_error_margin=float,
+                                  spreadsheet_error_margin=float,
+                                  spreadsheet_ros=float,
+                                  spreadsheet_hfi=float,
+                                  spreadsheet_cfb=float))
+def test_fire_behaviour_calculator_scenario():
+    """ BDD Scenario. """
+
+
+@given("""<elevation>, <latitude>, <longitude>, <time_of_interest>, <wind_speed>, <wind_direction>, """
+       """<percentage_conifer>, <percentage_dead_balsam_fir>, <grass_cure>, <crown_base_height>, """
+       """<isi>, <bui>, <ffmc>, <dmc>, <dc>, <fuel_type>""", target_fixture='result')
+def given_input(elevation: float,  # pylint: disable=too-many-arguments, invalid-name
+                latitude: float, longitude: float, time_of_interest: str,
+                wind_speed: float, wind_direction: float,
+                percentage_conifer: float, percentage_dead_balsam_fir: float, grass_cure: float,
+                crown_base_height: float,
+                isi: float, bui: float, ffmc: float, dmc: float, dc: float, fuel_type: str):
+    # get python result:
+    python_input = FBACalculatorWeatherStation(elevation=elevation,
+                                               fuel_type=fuel_type,
+                                               time_of_interest=time_of_interest,
+                                               percentage_conifer=percentage_conifer,
+                                               percentage_dead_balsam_fir=None,
+                                               grass_cure=grass_cure,
+                                               crown_base_height=crown_base_height,
+                                               lat=latitude,
+                                               long=longitude,
+                                               bui=bui,
+                                               ffmc=ffmc,
+                                               isi=isi,
+                                               wind_speed=wind_speed)
+    python_fba = calculate_fire_behavour_advisory(python_input)
+    # get java result:
+    java_fbp = FBPCalculateStatisticsCOM(elevation=elevation,
+                                         latitude=latitude,
+                                         longitude=longitude,
+                                         time_of_interest=get_hour_20_from_date(time_of_interest),
+                                         fuel_type=fuel_type,
+                                         ffmc=ffmc,
+                                         dmc=dmc,
+                                         dc=dc,
+                                         bui=bui,
+                                         wind_speed=wind_speed,
+                                         wind_direction=wind_direction,
+                                         percentage_conifer=percentage_conifer,
+                                         percentage_dead_balsam_fir=percentage_dead_balsam_fir,
+                                         grass_cure=grass_cure,
+                                         crown_base_height=crown_base_height)
+
+    return {
+        'python': python_fba,
+        'java': java_fbp
+    }
+
+
+def relative_error(actual: float, expected: float, precision: int = 2):
+    """ calculate the relative error between two values - default to precision of 2"""
+    actual = round(actual, precision)
+    expected = round(expected, precision)
+    return abs((actual-expected)/expected)
+
+
+@then("ROS is within <spreadsheet_error_margin> of <spreadsheet_ros>")
+def then_spreadsheet_ros(result: dict, spreadsheet_error_margin: float, spreadsheet_ros: float):
+    """ check the relative error of the ros """
+    actual = result['python'].ros
+    error = relative_error(actual, spreadsheet_ros)
+    logger.info('Python ROS %s, Spreadsheet ROS %s ; error: %s', actual, spreadsheet_ros, error)
+    assert error < spreadsheet_error_margin
+
+
+@then("CFB is within <spreadsheet_error_margin> of <spreadsheet_cfb>")
+def then_spreadsheet_cfb(result: dict, spreadsheet_error_margin: float, spreadsheet_cfb: float):
+    """ check the relative error of the cfb """
+    actual = result['python'].cfb
+    error = relative_error(actual, spreadsheet_cfb, 1)
+    logger.info('Python ROS %s, Spreadsheet ROS %s ; error: %s', actual, spreadsheet_cfb, error)
+    assert error < spreadsheet_error_margin
+
+
+@then("HFI is within <spreadsheet_error_margin> of <spreadsheet_hfi>")
+def then_spreadsheet_hfi(result: dict, spreadsheet_error_margin: float, spreadsheet_hfi: float):
+    """ check the relative error of the hfi """
+    actual = result['python'].hfi
+    error = relative_error(actual, spreadsheet_hfi)
+    logger.info('Python ROS %s, Spreadsheet ROS %s ; error: %s', actual, spreadsheet_hfi, error)
+    assert error < spreadsheet_error_margin
+
+
+@then("ROS is within <red_app_error_margin> of REDapp ROS")
+def then_red_app_ros(result: dict, red_app_error_margin: float):
+    """ check the relative error of ROS """
+    actual = result['python'].ros
+    expected = result['java'].ros_t
+    error = relative_error(actual, expected)
+    logger.info('Python ROS %s, REDapp ROS %s ; error: %s', actual, expected, error)
+    assert error < red_app_error_margin
+
+
+@then("CFB is within <red_app_error_margin> of REDapp CFB")
+def then_red_app_cfb(result: dict, red_app_error_margin: float):
+    """ check the relative error fo the CFB """
+    # python gives CFB as 0-1
+    actual = result['python'].cfb*100
+    # redapp gives CFB as 0-100
+    expected = result['java'].cfb
+    error = relative_error(actual, expected)
+    logger.info('Python CFB %s, REDapp CFB %s ; error: %s', actual, expected, error)
+    assert error < red_app_error_margin
+
+
+@then("HFI is within <red_app_error_margin> of REDapp HFI")
+def then_red_app_hfi(result: dict, red_app_error_margin: float):
+    """ check the relative error fo the CFB """
+    actual = result['python'].hfi
+    expected = result['java'].hfi
+    error = relative_error(actual, expected)
+    logger.info('Python HFI %s, REDapp HFI %s ; error: %s', actual, expected, error)
+    assert error < red_app_error_margin
--- api/app/utils/redapp.py
+++ api/app/utils/redapp.py
@@ -5,22 +5,137 @@ from datetime import datetime
 # from jnius import autoclass
 
 
+class UnmappedFuelType(Exception):
+    """ Raised for fuel types that don't have an index mapping """
+
+
+def red_app_fuel_type_map(fuel_type: str):
+    """
+    This is messy. REDapp has a combo box with fuel types, which it then maps
+    to a fuel type index.
+
+    new FuelValue(Cwfgm_Fuel_C1.class, "C-1:  Spruce-Lichen Woodland"), 
+    new FuelValue(Cwfgm_Fuel_C2.class, "C-2:  Boreal Spruce"), 
+    new FuelValue(Cwfgm_Fuel_C3.class, "C-3:  Mature Jack or Lodgepole Pine"), 
+    new FuelValue(Cwfgm_Fuel_C4.class, "C-4:  Immature Jack or Lodgepole Pine"), 
+    new FuelValue(Cwfgm_Fuel_C5.class, "C-5:  Red and White Pine"), 
+    new FuelValue(Cwfgm_Fuel_C6.class, "C-6:  Conifer Plantation"), 
+    new FuelValue(Cwfgm_Fuel_C7.class, "C-7:  Ponderosa Pine / Douglas Fir"), 
+    new FuelValue(Cwfgm_Fuel_D1.class, "D-1:  Leafless Aspen"), 
+    new FuelValue(Cwfgm_Fuel_D2.class, "D-2:  Green Aspen (w/ BUI Thresholding)"), 
+    new FuelValue(Cwfgm_Fuel_D1D2.class, "D-1/D-2:  Aspen"), 
+    new FuelValue(Cwfgm_Fuel_M1.class, "M-1:  Boreal Mixedwood - Leafless"), 
+    new FuelValue(Cwfgm_Fuel_M2.class, "M-2:  Boreal Mixedwood - Green"), 
+    new FuelValue(Cwfgm_Fuel_M1M2.class, "M-1/M-2:  Boreal Mixedwood"), 
+    new FuelValue(Cwfgm_Fuel_M3.class, "M-3:  Dead Balsam Fir / Mixedwood - Leafless"), 
+    new FuelValue(Cwfgm_Fuel_M4.class, "M-4:  Dead Balsam Fir / Mixedwood - Green"), 
+    new FuelValue(Cwfgm_Fuel_M3M4.class, "M-3/M-4:  Dead Balsam Fir / Mixedwood"), 
+    new FuelValue(Cwfgm_Fuel_O1a.class, "O-1a:  Matted Grass"), 
+    new FuelValue(Cwfgm_Fuel_O1b.class, "O-1b:  Standing Grass"), 
+    new FuelValue(Cwfgm_Fuel_O1ab.class, "O-1ab:  Grass"), 
+    new FuelValue(Cwfgm_Fuel_S1.class, "S-1:  Jack or Lodgepole Pine Slash"), 
+    new FuelValue(Cwfgm_Fuel_S2.class, "S-2:  White Spruce / Balsam Slash"), 
+    new FuelValue(Cwfgm_Fuel_S3.class, "S-3:  Coastal Cedar / Hemlock / Douglas-Fir Slash") };
+
+    comboIndexToFuel.put(Integer.valueOf(0), Integer.valueOf(0));
+    comboIndexToFuel.put(Integer.valueOf(1), Integer.valueOf(1));
+    comboIndexToFuel.put(Integer.valueOf(2), Integer.valueOf(2));
+    comboIndexToFuel.put(Integer.valueOf(3), Integer.valueOf(3));
+    comboIndexToFuel.put(Integer.valueOf(4), Integer.valueOf(4));
+    comboIndexToFuel.put(Integer.valueOf(5), Integer.valueOf(5));
+    comboIndexToFuel.put(Integer.valueOf(6), Integer.valueOf(6));
+    comboIndexToFuel.put(Integer.valueOf(7), Integer.valueOf(7));
+    comboIndexToFuel.put(Integer.valueOf(8), Integer.valueOf(8));
+    comboIndexToFuel.put(Integer.valueOf(9), Integer.valueOf(10));
+    comboIndexToFuel.put(Integer.valueOf(10), Integer.valueOf(11));
+    comboIndexToFuel.put(Integer.valueOf(11), Integer.valueOf(13));
+    comboIndexToFuel.put(Integer.valueOf(12), Integer.valueOf(14));
+    comboIndexToFuel.put(Integer.valueOf(13), Integer.valueOf(16));
+    comboIndexToFuel.put(Integer.valueOf(14), Integer.valueOf(17));
+    comboIndexToFuel.put(Integer.valueOf(15), Integer.valueOf(18));
+    comboIndexToFuel.put(Integer.valueOf(16), Integer.valueOf(19));
+    comboIndexToFuel.put(Integer.valueOf(17), Integer.valueOf(20));
+    comboIndexToFuel.put(Integer.valueOf(18), Integer.valueOf(21));
+
+    combobox index -> fuel type index
+    0:0 "C-1:  Spruce-Lichen Woodland"
+    1:1 "C-2:  Boreal Spruce"
+    2:2 "C-3:  Mature Jack or Lodgepole Pine"
+    3:3 "C-4:  Immature Jack or Lodgepole Pine"
+    4:4 "C-5:  Red and White Pine"
+    5:5 "C-6:  Conifer Plantation"
+    6:6 "C-7:  Ponderosa Pine / Douglas Fir"
+    7:7 "D-1:  Leafless Aspen"
+    8:8 "D-2:  Green Aspen (w/ BUI Thresholding)"
+    9:10 "D-1/D-2:  Aspen"
+    10:11 "M-1:  Boreal Mixedwood - Leafless"
+    11:13 "M-2:  Boreal Mixedwood - Green"
+    12:14 "M-1/M-2:  Boreal Mixedwood"
+    13:16 "M-3:  Dead Balsam Fir / Mixedwood - Leafless"
+    14:17 "M-4:  Dead Balsam Fir / Mixedwood - Green"
+    15:18 "M-3/M-4:  Dead Balsam Fir / Mixedwood"
+    16:19 "O-1a:  Matted Grass"
+    17:20 "O-1b:  Standing Grass"
+    18:21 "O-1ab:  Grass"
+    19"S-1:  Jack or Lodgepole Pine Slash"
+    20"S-2:  White Spruce / Balsam Slash"
+    21"S-3:  Coastal Cedar / Hemlock / Douglas-Fir Slash"
+    """
+    fuel_type_map = {
+        'C1': 0,
+        'C2': 1,
+        'C3': 2,
+        'C4': 3,
+        'C5': 4,
+        'C6': 5,
+        'C7': 6,
+        'D1': 7,
+        'D2': 8,
+        # D1/D2: Aspen # we don't have this as a dropdown right now!
+        'M1': 11,
+        'M2': 13,
+        # M1/M2: Boreal Mixedwood # we don't have this as a dropdown right now!
+        'M3': 16,
+        'M4': 17,
+        # M3/M4: Dead Balsam Fir / Mixedwood # we don't have this as a dropdown right now!
+        'O1A': 19,
+        'O1B': 20,
+        # O1ab: Grass # we don't have this as a dropdown right now!
+        # 'S1': - no mapping
+        # 'S2': - no mapping
+        # 'S3': - no mapping
+    }
+    if fuel_type in fuel_type_map:
+        return fuel_type_map[fuel_type]
+    raise UnmappedFuelType(fuel_type)
+
+
 def FBPCalculateStatisticsCOM(elevation: float,  # pylint: disable=invalid-name
                               latitude: float,
                               longitude: float,
                               time_of_interest: datetime,
+                              fuel_type: str,
                               ffmc: float,
                               dmc: float,
                               dc: float,
                               bui: float,
                               wind_speed: float,
-                              wind_direction: float):
+                              wind_direction: float,
+                              percentage_conifer: float,
+                              percentage_dead_balsam_fir: float,
+                              grass_cure: float,
+                              crown_base_height: float):
     """ ca.cwfgm.fbp.FBPCalculations::FBPCalculateStatisticsCOM
     """
     from jnius import autoclass  # pylint: disable=import-outside-toplevel
     Calendar = autoclass('java.util.Calendar')  # pylint: disable=invalid-name
     TimeZone = autoclass('java.util.TimeZone')  # pylint: disable=invalid-name
     FBPCalculations = autoclass('ca.cwfgm.fbp.FBPCalculations')  # pylint: disable=invalid-name
+
+    if percentage_dead_balsam_fir is None:
+        percentage_dead_balsam_fir = 0.0
+    if grass_cure is None:
+        grass_cure = 0.0
 
     fbp = FBPCalculations()
     fbp.elevation = elevation
@@ -29,9 +144,19 @@ def FBPCalculateStatisticsCOM(elevation: float,  # pylint: disable=invalid-name
     fbp.ffmc = ffmc
     fbp.dmc = dmc
     fbp.dc = dc
+    fbp.useBui = True
+    fbp.useBuildup = True
     fbp.bui = bui
     fbp.windSpeed = wind_speed
     fbp.windDirection = wind_direction
+    fbp.fuelType = red_app_fuel_type_map(fuel_type)
+    fbp.conifMixedWood = percentage_conifer
+    fbp.deadBalsam = percentage_dead_balsam_fir
+    fbp.grassCuring = grass_cure
+    fbp.crownBase = crown_base_height
+    # grassFuelLoad
+
+    fbp.useSlope = False
 
     gmt = Calendar.getInstance(TimeZone.getTimeZone("GMT"))
     gmt.set(time_of_interest.year, time_of_interest.month, time_of_interest.day,
