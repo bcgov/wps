0c0085fa70b12310aba9526e52477e650e3ecbcc
Conor Brady <con.brad@gmail.com>
2021-07-19 13:53:54 -0700

    Local state display and updates working

--- web/src/features/fireBehaviourCalculator/components/TestComponent.tsx
+++ web/src/features/fireBehaviourCalculator/components/TestComponent.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { useTable } from 'react-table'
-import { find, isEqual, isNull, isUndefined } from 'lodash'
+import { isNull, isUndefined } from 'lodash'
 import MaUTable from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import { FBCInputRow, GridMenuOption } from './FBCInputGrid'
-import { Autocomplete } from '@material-ui/lab'
-import { TextField } from '@material-ui/core'
 import { FuelTypes } from '../fuelTypes'
+import { buildRowCell } from '../tableRowBuilder'
 
-interface DatePickerProps {
+export interface DatePickerProps {
   testId?: string
   stationOptions: GridMenuOption[]
   fuelTypeOptions: GridMenuOption[]
@@ -51,8 +50,8 @@ const TestComponent = (props: DatePickerProps) => {
   const data = props.rows.map(row => ({
     weatherStation: buildStationOption(parseInt(row.weatherStation)),
     fuelType: buildFuelTypeMenuOption(row.fuelType),
-    grassCure: 0,
-    windSpeed: 0
+    grassCure: row.grassCure,
+    windSpeed: row.windSpeed
   }))
 
   const columns: any = React.useMemo(
@@ -81,57 +80,6 @@ const TestComponent = (props: DatePickerProps) => {
 
   const { getTableProps, headerGroups, rows, prepareRow } = tableInstance
 
-  const buildCell = (
-    cell: { column: { id: string }; value: GridMenuOption },
-    field: string,
-    rowId: number
-  ) => {
-    const changeHandler = (event: React.ChangeEvent<{}>, value: any) => {
-      console.log(field)
-
-      const rowToUpdate = find(props.rows, ['id', rowId])
-      if (rowToUpdate) {
-        const updatedRow = {
-          ...rowToUpdate,
-          ...{
-            [field as keyof FBCInputRow]: (value as GridMenuOption).value
-          }
-        }
-        props.updateRow(rowId, updatedRow)
-      }
-    }
-
-    if (cell.column.id === 'weatherStation') {
-      return (
-        <Autocomplete
-          options={props.stationOptions}
-          getOptionSelected={(option, value) => isEqual(option, value)}
-          getOptionLabel={option => option?.label}
-          id={`combo-box-fuel-types-${Math.random()}`}
-          style={{ width: 300 }}
-          renderInput={params => <TextField {...params} label="Select a station" />}
-          onChange={changeHandler}
-          value={cell.value}
-        />
-      )
-    }
-    if (cell.column.id === 'fuelType') {
-      return (
-        <Autocomplete
-          options={props.fuelTypeOptions}
-          getOptionSelected={(option, value) => isEqual(option, value)}
-          getOptionLabel={option => option?.label}
-          id={`combo-box-fuel-types-${Math.random()}`}
-          style={{ width: 300 }}
-          renderInput={params => <TextField {...params} label="Select a fuel type" />}
-          onChange={changeHandler}
-          value={cell.value}
-        />
-      )
-    }
-    return <TextField type="number" />
-  }
-
   return (
     <MaUTable {...getTableProps()}>
       <TableHead>
@@ -151,10 +99,9 @@ const TestComponent = (props: DatePickerProps) => {
           return (
             <TableRow {...row.getRowProps()}>
               {row.cells.map(cell => {
-                console.log(cell.column.id)
                 return (
                   <TableCell {...cell.getCellProps()}>
-                    {buildCell(cell, cell.column.id, parseInt(row.id))}
+                    {buildRowCell(props, cell, cell.column.id, parseInt(row.id))}
                   </TableCell>
                 )
               })}
---
+++ web/src/features/fireBehaviourCalculator/tableRowBuilder.tsx
@@ -0,0 +1,95 @@
+import { TextField } from '@material-ui/core'
+import { Autocomplete } from '@material-ui/lab'
+import { find, isEqual } from 'lodash'
+import React, { ChangeEvent } from 'react'
+import { GridMenuOption, FBCInputRow } from './components/FBCInputGrid'
+import { DatePickerProps } from './components/TestComponent'
+
+const buildWeatherStationCell = (
+  options: GridMenuOption[],
+  emptyLabel: string,
+  cell: { column: { id: string }; value: GridMenuOption },
+  changeHandler: (event: React.ChangeEvent<{}>, value: any) => void
+) => (
+  <Autocomplete
+    options={options}
+    getOptionSelected={(option, value) => isEqual(option, value)}
+    getOptionLabel={option => option?.label}
+    style={{ width: 300 }}
+    renderInput={params => <TextField {...params} label={cell.value ? '' : emptyLabel} />}
+    onChange={changeHandler}
+    value={cell.value}
+  />
+)
+
+function buildUpdatedOptionRow(rowToUpdate: FBCInputRow, field: string, value: any) {
+  return {
+    ...rowToUpdate,
+    ...{
+      [field as keyof FBCInputRow]: (value as GridMenuOption)?.value
+    }
+  }
+}
+
+function buildUpdatedNumberRow(rowToUpdate: FBCInputRow, field: string, value: any) {
+  return {
+    ...rowToUpdate,
+    ...{
+      [field as keyof FBCInputRow]: parseInt(value)
+    }
+  }
+}
+
+export const buildRowCell = (
+  props: DatePickerProps,
+  cell: { column: { id: string }; value: GridMenuOption },
+  field: string,
+  rowId: number
+) => {
+  const updateFBCRow = (
+    props: DatePickerProps,
+    rowId: number,
+    field: string,
+    value: any,
+    updatedRowBuilder: (
+      rowToUpdate: FBCInputRow,
+      field: string,
+      value: GridMenuOption | number
+    ) => FBCInputRow
+  ) => {
+    const rowToUpdate = find(props.rows, ['id', rowId])
+    if (rowToUpdate) {
+      const updatedRow = updatedRowBuilder(rowToUpdate, field, value)
+      props.updateRow(rowId, updatedRow)
+    }
+  }
+  const autoCompleteChangeHandler = (_: React.ChangeEvent<{}>, value: any | null) => {
+    updateFBCRow(props, rowId, field, value, buildUpdatedOptionRow)
+  }
+
+  const numberFieldChangeHandler = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    updateFBCRow(props, rowId, field, parseInt(event.target.value), buildUpdatedNumberRow)
+  }
+
+  if (cell.column.id === 'weatherStation') {
+    return buildWeatherStationCell(
+      props.stationOptions,
+      'Select a station',
+      cell,
+      autoCompleteChangeHandler
+    )
+  }
+  if (cell.column.id === 'fuelType') {
+    return buildWeatherStationCell(
+      props.fuelTypeOptions,
+      'Select a fuel type',
+      cell,
+      autoCompleteChangeHandler
+    )
+  }
+  return (
+    <TextField type="number" onChange={numberFieldChangeHandler} value={cell.value} />
+  )
+}
