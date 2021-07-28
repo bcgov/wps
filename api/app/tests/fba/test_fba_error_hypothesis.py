652fbfd0a74842adf2f04b48e8a367713a02fa6e
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-07-28 09:42:28 -0700

    cleaning up and getting tests to pass

--- api/app/tests/fba/test_fba_error_hypothesis.py
+++ api/app/tests/fba/test_fba_error_hypothesis.py
@@ -19,10 +19,6 @@ configure_logging()
 logger = logging.getLogger(__name__)
 
 
-class RelativeErrorException(Exception):
-    """ Exception raised when it's mathematically impossible to calculate the relative error. """
-
-
 def _str2float(value: str):
     if value == 'None':
         return None
@@ -35,15 +31,15 @@ def _random_date():
     return datetime.fromtimestamp(random.uniform(start.timestamp(), end.timestamp()), tz=dt_tz.utc)
 
 
-def relative_error(metric: str, actual: float, expected: float, precision: int = 2):
+def relative_error(actual: float, expected: float, precision: int = 2):
     """ calculate the relative error between two values - default to precision of 2"""
     actual = round(actual, precision)
     expected = round(expected, precision)
     if actual == expected:
         return 0
     if expected == 0:
-        raise RelativeErrorException(
-            f'unable to calculate relative error for {metric}; actual:{actual};expected:{expected}')
+        # Can't divide by 0! Taking this as a 100% difference
+        return 1
     return abs((actual-expected)/expected)
 
 
@@ -58,26 +54,24 @@ def check_metric(metric: str,
     """ Check relative error of a metric """
     # logging with %s became unreadable:
     # pylint: disable=logging-fstring-interpolation
-    if comparison_value is None:
-        logger.warning('Skipping %s! (%s) - note: %s', metric, comparison_value, note)
-    elif comparison_value < 0:
+    if comparison_value < 0:
         logger.warning('Skipping %s! (%s) - note: %s', metric, comparison_value, note)
     else:
         assert python_value >= 0
-        error = relative_error(f'{metric}', python_value, comparison_value)
+        error = relative_error(python_value, comparison_value)
         absolute_error = abs(python_value - comparison_value)
-        logger.info(
-            f'{fuel_type}: Python {python_value}, {metric} {comparison_value} ; error: {error}')
+        # logger.info(
+        #     f'{fuel_type}: Python {python_value}, {metric} {comparison_value} ; error: {error}')
         if error > acceptable_margin_of_error:
             logger.error('%s %s relative error %s > %s! (python: %s, expected: %s)',
                          fuel_type, metric, error, acceptable_margin_of_error, python_value, comparison_value)
         if metric_error_margin > 0.01:
-            logger.warning('%s: The acceptable margin of error (%s) for %s is set too high',
-                           fuel_type, metric_error_margin, metric)
+            logger.debug('%s: The acceptable margin of error (%s) for %s is set too high',
+                         fuel_type, metric_error_margin, metric)
         if error > metric_error_margin:
             # ok - fine, but for small numbers this isn't a big deal, so let's check the absolure difference.
             if absolute_error < 0.01:
-                logger.info('no big deal, the absolute difference (%s) is tiny!', absolute_error)
+                logger.debug('no big deal, the absolute difference (%s) is tiny!', absolute_error)
             else:
                 assert error < metric_error_margin, f'{fuel_type}:{metric}'
 
@@ -103,6 +97,7 @@ def test_fire_behaviour_calculator_scenario():
 def given_input(fuel_type: str, percentage_conifer: float, percentage_dead_balsam_fir: float,
                 grass_cure: float, crown_base_height: float, num_iterations: int):
     """ Take input and calculate actual and expected results """
+
     # get python result:
     random.seed(42)
     results = []
@@ -133,10 +128,10 @@ def given_input(fuel_type: str, percentage_conifer: float, percentage_dead_balsa
                    f"""temperature: {temperature}; relative_humidity: {relative_humidity}; """
                    f"""precipitation: {precipitation}; dc: {dc}; dmc: {dmc}; bui: {bui}; """
                    f"""ffmc: {ffmc}; isi: {isi}""")
-        logger.info(message)
+        logger.debug(message)
 
         test_entry = f"({index}) | {fuel_type} | {elevation} | {latitude} | {longitude} | {time_of_interest} | {wind_speed} | {wind_direction} | {percentage_conifer} | {percentage_dead_balsam_fir} | {grass_cure} | {crown_base_height} | {isi}  | {bui} | {ffmc} | {dmc} | {dc} | 0.01 | 0.01 | 0.01 | 0.01 | None | 0.01 | None | 0.01 | None | 0.01 | |"
-        logger.info(test_entry)
+        logger.debug(test_entry)
         python_input = FBACalculatorWeatherStation(elevation=elevation,
                                                    fuel_type=fuel_type,
                                                    time_of_interest=time_of_interest,
@@ -183,44 +178,8 @@ def given_input(fuel_type: str, percentage_conifer: float, percentage_dead_balsa
 
     return results
 
-    # def relative_error(metric: str, actual: float, expected: float, precision: int = 2):
-    #     """ calculate the relative error between two values - default to precision of 2"""
-    #     actual = round(actual, precision)
-    #     expected = round(expected, precision)
-    #     if actual == expected:
-    #         return 0
-    #     if expected == 0:
-    #         raise RelativeErrorException(
-    #             f'unable to calculate relative error for {metric}; actual:{actual};expected:{expected}')
-    #     return abs((actual-expected)/expected)
 
-    # def check_metric(metric: str,
-    #                  fuel_type: str,
-    #                  python_value: float,
-    #                  comparison_value: float,
-    #                  metric_error_margin: float, note: str = None):
-    #     """ Check relative error of a metric """
-    #     # logging with %s became unreadable:
-    #     # pylint: disable=logging-fstring-interpolation
-    #     if comparison_value is None:
-    #         logger.warning('Skipping %s! (%s) - note: %s', metric, comparison_value, note)
-    #     elif comparison_value < 0:
-    #         logger.warning('Skipping %s! (%s) - note: %s', metric, comparison_value, note)
-    #     else:
-    #         assert python_value >= 0
-    #         error = relative_error(f'{metric}', python_value, comparison_value)
-    #         logger.info(
-    #             f'{fuel_type}: Python {python_value}, {metric} {comparison_value} ; error: {error}')
-    #         if error > acceptable_margin_of_error:
-    #             logger.error('%s %s relative error %s > %s! (actual: %s, expected: %s)',
-    #                          fuel_type, metric, error, acceptable_margin_of_error, python_value, comparison_value)
-    #         if metric_error_margin > 0.01:
-    #             logger.warning('%s: The acceptable margin of error (%s) for %s is set too high',
-    #                            fuel_type, metric_error_margin, metric)
-    #         assert error < metric_error_margin, f'{fuel_type}:{metric}'
-
-
-@then("ROS is within <ros_margin_of_error> compared to REDapp")
+@ then("ROS is within <ros_margin_of_error> compared to REDapp")
 def then_ros_good(results: list, ros_margin_of_error: float):
     """ check the relative error of ROS """
     for index, result in enumerate(results):
@@ -229,68 +188,43 @@ def then_ros_good(results: list, ros_margin_of_error: float):
         isi = result['input']['isi']
         bui = result['input']['bui']
         java_isi = result['java'].isi
-        logger.info('java calculated isi: %s', java_isi)
         # assumptions:
         # ros_eq == ROScalc
         # ros_t  == ROStcalc
-        check_metric(f'({index})ROS input- isi:{isi}; bui:{bui}; wind_speed:{wind_speed}; ffmc:{ffmc}',
+        check_metric('ROS',
                      result['fuel_type'],
                      result['python'].ros,
-                     result['java'].ros_eq, ros_margin_of_error)
+                     result['java'].ros_eq,
+                     ros_margin_of_error,
+                     f'({index})ROS input- isi:{isi}; bui:{bui}; wind_speed:{wind_speed}; ffmc:{ffmc}')
 
 
-@then("HFI is within <hfi_margin_of_error> compared to REDapp")
+@ then("HFI is within <hfi_margin_of_error> compared to REDapp")
 def then_hfi_good(results: list, hfi_margin_of_error: float):
     """ check the relative error of HFI """
-    for index, result in enumerate(results):
-        # wind_speed = result['input']['wind_speed']
-        # ffmc = result['input']['ffmc']
-        # isi = result['input']['isi']
-        # bui = result['input']['bui']
-        # java_isi = result['java'].isi
-        # logger.info('java calculated isi: %s', java_isi)
-        check_metric(f'({index})HFI input',
+    for result in results:
+        check_metric(f'HFI',
                      result['fuel_type'],
                      result['python'].hfi,
                      result['java'].hfi, hfi_margin_of_error)
 
 
-@then("CFB is within <cfb_margin_of_error> compared to REDapp")
+@ then("CFB is within <cfb_margin_of_error> compared to REDapp")
 def then_cfb_good(results: list, cfb_margin_of_error: float):
     """ check the relative error of HFI """
     for index, result in enumerate(results):
-        # wind_speed = result['input']['wind_speed']
-        # ffmc = result['input']['ffmc']
-        # isi = result['input']['isi']
-        # bui = result['input']['bui']
-        # java_isi = result['java'].isi
-        # logger.info('java calculated isi: %s', java_isi)
-        check_metric(f'({index})CFB input',
+        check_metric('CFB',
                      result['fuel_type'],
                      result['python'].cfb*100.0,
-                     result['java'].cfb, cfb_margin_of_error)
+                     result['java'].cfb, cfb_margin_of_error, f'({index})CFB input')
 
 
-@then("1 Hour Spread is within <one_hour_spread_margin_of_error> compared to REDapp")
+@ then("1 Hour Spread is within <one_hour_spread_margin_of_error> compared to REDapp")
 def then_1_hour_spread_good(results: list, one_hour_spread_margin_of_error: float):
     """ check the relative error of HFI """
     for index, result in enumerate(results):
-        # wind_speed = result['input']['wind_speed']
-        # ffmc = result['input']['ffmc']
-        # isi = result['input']['isi']
-        # bui = result['input']['bui']
-        # java_isi = result['java'].isi
-        # logger.info('java calculated isi: %s', java_isi)
-        check_metric(f'({index})1 Hour Spread input',
+        check_metric('one_hour_size',
                      result['fuel_type'],
                      result['python'].sixty_minute_fire_size,
-                     result['java'].area, one_hour_spread_margin_of_error)
-
-# @then("1 HR Size is within <r_h1_em> of REDapp 1 HR Size")
-# def then_red_app_1hr(result: dict, r_h1_em: float):
-#     """ check the relative error of the a hour fire size"""
-#     check_metric('REDapp 1 HR Size',
-#                  result['fuel_type'],
-#                  result['python'].sixty_minute_fire_size,
-#                  result['java'].area,
-#                  r_h1_em)
+                     result['java'].area, one_hour_spread_margin_of_error,
+                     f'({index})1 Hour Spread input')
