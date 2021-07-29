8a06b40168631532182e8af5e9a7ad87e6a9bc66
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-07-29 09:51:32 -0700

    Evaluate C3 accuracy (#1171)
    - introduce "test_fba_error_random_sample" - N random iterations of FBA inputs.
    - re-factor "test_fba_error" (had too much code, and very difficult to trace changes).
    - fix path bug in api/app/utils/fba_calculator.py.
    - attempt to fix static analysis.
    - fix some deprecation warnings.
    - tinker with sonarcloud trying to fix checks [failed :(]
    changed singleton to not use exception in instantiation (error logging was confounding me! - beter not to use exceptions anyway)
    - fix wildfire one test
    - fix FMC longitude issue!

--- api/app/utils/singleton.py
+++ api/app/utils/singleton.py
@@ -8,20 +8,20 @@ class Singleton:
 
     def __init__(self, decorated):
         self._decorated = decorated
+        self._instance = None
 
     def instance(self):
         """
         Returns the singleton instance. Upon its first call, it creates a
         new instance of the decorated class and calls its `__init__` method.
         On all subsequent calls, the already created instance is returned.
-
         """
-        try:
-            return self._instance
-        except AttributeError:
-            # pylint: disable=attribute-defined-outside-init
+        # NOTE: Code use to attempt to return self._instance, and if an exception was thrown,
+        # would then call self._decorated to instantiate. This can result in logging of exceptions which
+        # can get confusing. Nice to first check if it's set, and if not - try to make it.
+        if self._instance is None:
             self._instance = self._decorated()
-            return self._instance
+        return self._instance
 
     def __call__(self):
         raise TypeError('Singletons must be accessed through `instance()`.')
