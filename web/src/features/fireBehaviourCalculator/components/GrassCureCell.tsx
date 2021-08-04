c2a2eaaa3cdcdc3464ca233cee95cd49ad72e840
Conor Brady <con.brad@gmail.com>
2021-08-04 15:58:47 -0700

    Bug/fba unnecessary loading/1202 (#1203)
    * Equal check for weather station
    
    * WeatherStation and FuelType fixes
    
    * Handle unnecessary grass cure percentage updates

--- web/src/features/fireBehaviourCalculator/components/GrassCureCell.tsx
+++ web/src/features/fireBehaviourCalculator/components/GrassCureCell.tsx
@@ -8,11 +8,14 @@ import {
   buildUpdatedNumberRow,
   updateFBCRow
 } from 'features/fireBehaviourCalculator/tableState'
-import { isUndefined, isNull } from 'lodash'
+import { isUndefined, isNull, isEqual } from 'lodash'
 import React, { ChangeEvent, useState } from 'react'
 
 interface GrassCureCellProps {
-  fbcInputGridProps: Pick<FBCInputGridProps, 'inputRows' | 'updateRow'>
+  fbcInputGridProps: Pick<
+    FBCInputGridProps,
+    'inputRows' | 'updateRow' | 'autoUpdateHandler'
+  >
   classNameMap: ClassNameMap<'grassCure'>
   value: number | undefined
   rowId: number
@@ -31,20 +34,30 @@ const grassCureNotSetForGrassType = (row: FBCInputRow): boolean => {
   return false
 }
 const GrassCureProps = (props: GrassCureCellProps) => {
+  const [lastRequestedGrassCure, setLastRequestedGrassCure] = useState(props.value)
   const [grassCurePercentage, setGrassCurePercentage] = useState(props.value)
 
   const changeHandler = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setGrassCurePercentage(parseInt(event.target.value))
   }
 
-  const blurHandler = () => {
-    updateFBCRow(
-      props.fbcInputGridProps,
-      props.rowId,
-      'grassCure',
-      grassCurePercentage,
-      buildUpdatedNumberRow
-    )
+  const handlePossibleUpdate = () => {
+    if (!isEqual(lastRequestedGrassCure, grassCurePercentage)) {
+      setLastRequestedGrassCure(grassCurePercentage)
+      updateFBCRow(
+        props.fbcInputGridProps,
+        props.rowId,
+        'grassCure',
+        grassCurePercentage,
+        buildUpdatedNumberRow
+      )
+    }
+  }
+
+  const enterHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter') {
+      handlePossibleUpdate()
+    }
   }
 
   const hasError = grassCureNotSetForGrassType(
@@ -59,18 +72,8 @@ const GrassCureProps = (props: GrassCureCellProps) => {
       variant="outlined"
       inputProps={{ min: 0, maxLength: 4, size: 4, max: 100 }}
       onChange={changeHandler}
-      onBlur={blurHandler}
-      onKeyDown={event => {
-        if (event.key === 'Enter') {
-          updateFBCRow(
-            props.fbcInputGridProps,
-            props.rowId,
-            'grassCure',
-            grassCurePercentage,
-            buildUpdatedNumberRow
-          )
-        }
-      }}
+      onBlur={handlePossibleUpdate}
+      onKeyDown={enterHandler}
       value={grassCurePercentage}
       error={hasError}
     />
