245dc451ca566b083971f9a64bdfcd3013334cd0
Conor Brady <con.brad@gmail.com>
2021-08-11 17:17:59 -0700

    Testing if custom action runs

---
+++ .github/actions/pr-test-url/index.js
@@ -0,0 +1,5 @@
+async function run() {
+  console.log("Hello, world!");
+}
+
+run();
---
+++ .github/actions/pr-test-url/out/index.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+async function run() {
+  console.log("Hello, world!");
+}
+
+run();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
