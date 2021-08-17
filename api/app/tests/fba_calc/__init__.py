25b04158172a097f51a871bb13a56dfec64cfeac
Andrea Williams <andrea.williams@gov.bc.ca>
2021-08-17 14:29:19 -0700

    Refactored tests __init__ to reduce cognitive complexity

--- api/app/tests/fba_calc/__init__.py
+++ api/app/tests/fba_calc/__init__.py
@@ -32,6 +32,35 @@ def relative_error(actual: float, expected: float, precision: int = 2):
     return float(round(error, precision))
 
 
+def calculate_error(fuel_type, metric, metric_error_margin, python_value, comparison_value, note):
+    """ Perform various calculations and checking of error between python_value
+    and comparison_value. """
+    margin_of_error = metric_error_margin
+    if python_value < 2 and comparison_value < 2:
+        logger.debug('when dealing with small numbers like %s and %s, we increase the margin of error',
+                     python_value, comparison_value)
+        margin_of_error = metric_error_margin * 4
+    error = relative_error(python_value, comparison_value)
+    if error > margin_of_error:
+        logger.error('%s %s relative error %s > %s! (actual: %s, expected: %s)',
+                     fuel_type, metric, error, margin_of_error, python_value, comparison_value)
+    if metric_error_margin > margin_of_error:
+        logger.warning('%s: The acceptable margin of error (%s) for %s is set too high',
+                       fuel_type, metric_error_margin, metric)
+    if error > metric_error_margin:
+        absolute_error = abs(python_value - comparison_value)
+        if absolute_error < margin_of_error:
+            logger.info('no big deal, the absolute difference (%s) is tiny!', absolute_error)
+        else:
+            assert_message = (f"""{fuel_type}:{metric} {error} > {metric_error_margin} """
+                              f"""(absolute error: {absolute_error}) ; actual: {python_value} """
+                              f"""expected: {comparison_value}""")
+            if note:
+                assert_message += f' {note}'
+            assert error < metric_error_margin, assert_message
+    return error
+
+
 def check_metric(metric: str,
                  fuel_type: str,
                  python_value: float,
@@ -47,29 +76,4 @@ def check_metric(metric: str,
         logger.warning('Skipping %s! (%s) - note: %s', metric, comparison_value, note)
     else:
         assert python_value >= 0
-
-        margin_of_error = metric_error_margin
-        if python_value < 2 and comparison_value < 2:
-            logger.debug('when dealing with small numbers like %s and %s, we increase the margin of error',
-                         python_value, comparison_value)
-            margin_of_error = metric_error_margin * 4
-
-        error = relative_error(python_value, comparison_value)
-        if error > margin_of_error:
-            logger.error('%s %s relative error %s > %s! (actual: %s, expected: %s)',
-                         fuel_type, metric, error, margin_of_error, python_value, comparison_value)
-        if metric_error_margin > margin_of_error:
-            logger.warning('%s: The acceptable margin of error (%s) for %s is set too high',
-                           fuel_type, metric_error_margin, metric)
-        if error > metric_error_margin:
-            absolute_error = abs(python_value - comparison_value)
-            if absolute_error < margin_of_error:
-                logger.info('no big deal, the absolute difference (%s) is tiny!', absolute_error)
-            else:
-                assert_message = (f"""{fuel_type}:{metric} {error} > {metric_error_margin} """
-                                  f"""(absolute error: {absolute_error}) ; actual: {python_value} """
-                                  f"""expected: {comparison_value}""")
-                if note:
-                    assert_message += f' {note}'
-                assert error < metric_error_margin, assert_message
-        return error
+        return calculate_error(fuel_type, metric, metric_error_margin, python_value, comparison_value, note)
