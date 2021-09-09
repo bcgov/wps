1c334967138a7b9a837b02a1c527a58b61b3846c
Conor Brady <con.brad@gmail.com>
2021-09-09 10:18:13 -0700

    More comprehensive tests

--- web/src/features/fbaCalculator/components/loadingIndicatorCell.test.tsx
+++ web/src/features/fbaCalculator/components/loadingIndicatorCell.test.tsx
@@ -1,11 +1,11 @@
-import { TableContainer, Table, TableBody, TableRow } from '@material-ui/core'
+import { TableContainer, Table, TableBody, TableRow, TableCell } from '@material-ui/core'
 import { render } from '@testing-library/react'
 import React from 'react'
 import LoadingIndicatorCell from 'features/fbaCalculator/components/LoadingIndicatorCell'
 
 describe('LoadingIndicatorCell', () => {
   it('should be loading on initial load', () => {
-    const { getByTestId } = render(
+    const { getByTestId, queryAllByTestId } = render(
       <TableContainer>
         <Table>
           <TableBody>
@@ -14,34 +14,36 @@ describe('LoadingIndicatorCell', () => {
                 loading={false}
                 rowUpdating={false}
                 initialLoad={true}
-              />
+              >
+                <TableCell data-testid="child"></TableCell>
+              </LoadingIndicatorCell>
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
     )
     expect(getByTestId('loading-indicator-fba')).toBeVisible()
+    expect(queryAllByTestId('child').length === 0)
   })
   it('should be loading on when row is updating', () => {
-    const { getByTestId } = render(
+    const { getByTestId, queryAllByTestId } = render(
       <TableContainer>
         <Table>
           <TableBody>
             <TableRow>
-              <LoadingIndicatorCell
-                loading={true}
-                rowUpdating={true}
-                initialLoad={false}
-              />
+              <LoadingIndicatorCell loading={true} rowUpdating={true} initialLoad={false}>
+                <TableCell data-testid="child"></TableCell>
+              </LoadingIndicatorCell>
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
     )
     expect(getByTestId('loading-indicator-fba')).toBeVisible()
+    expect(queryAllByTestId('child').length === 0)
   })
   it('should not be loading on when row is not updating', () => {
-    const { queryAllByTestId } = render(
+    const { getByTestId, queryAllByTestId } = render(
       <TableContainer>
         <Table>
           <TableBody>
@@ -50,16 +52,19 @@ describe('LoadingIndicatorCell', () => {
                 loading={true}
                 rowUpdating={false}
                 initialLoad={false}
-              />
+              >
+                <TableCell data-testid="child"></TableCell>
+              </LoadingIndicatorCell>
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
     )
     expect(queryAllByTestId('loading-indicator-fba').length === 0)
+    expect(getByTestId('child')).toBeVisible()
   })
   it('should not be loading on when nothing is loading', () => {
-    const { queryAllByTestId } = render(
+    const { getByTestId, queryAllByTestId } = render(
       <TableContainer>
         <Table>
           <TableBody>
@@ -68,12 +73,15 @@ describe('LoadingIndicatorCell', () => {
                 loading={false}
                 rowUpdating={false}
                 initialLoad={false}
-              />
+              >
+                <TableCell data-testid="child"></TableCell>
+              </LoadingIndicatorCell>
             </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
     )
     expect(queryAllByTestId('loading-indicator-fba').length === 0)
+    expect(getByTestId('child')).toBeVisible()
   })
 })
