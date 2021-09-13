52915c6b106caa1abeb64c520a7fbaeee4bf836e
Julian Foreman <julianforeman@me.com>
2021-09-13 13:05:25 -0700

    Added table instructions using react

---
+++ web/src/features/fbaCalculator/components/FBATableInstructions.tsx
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+const useStyles = makeStyles({
+  content: {
+    textAlign: 'center'
+  }
+})
+
+const FBATableInstructions = () => {
+  const classes = useStyles()
+  return (
+    <div className={classes.content}>
+      <p>Add a row to get started</p>
+      <p>
+        Build custom lists of weather stations by fire center, zone or fuel type. Bookmark
+        the URL to save your custom list.
+      </p>
+    </div>
+  )
+}
+
+export default React.memo(FBATableInstructions)
