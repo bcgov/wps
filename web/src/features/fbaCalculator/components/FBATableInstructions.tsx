0adeceb54525f96b71f5b8a4591fa4c4c8d19f78
JulianForeman <71847719+JulianForeman@users.noreply.github.com>
2021-09-14 15:02:30 -0700

    Added instructions for Firebat (#1308)
    * Added instructions for Firebat while table is empty and remove them once a row has been added.

---
+++ web/src/features/fbaCalculator/components/FBATableInstructions.tsx
@@ -0,0 +1,24 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+const useStyles = makeStyles({
+  content: {
+    marginLeft: '2em',
+    textAlign: 'left'
+  }
+})
+
+const FBATableInstructions = () => {
+  const classes = useStyles()
+  return (
+    <div data-testid={'fba-instructions'} className={classes.content}>
+      <p>Add a row to get started.</p>
+      <p>
+        Build custom lists of weather stations by fire center, zone or fuel type. Bookmark
+        the URL to save your custom list.
+      </p>
+    </div>
+  )
+}
+
+export default React.memo(FBATableInstructions)
