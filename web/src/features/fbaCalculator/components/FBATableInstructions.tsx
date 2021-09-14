176fd189c345dd583ef030e42215f8ae6c4674fb
Julian Foreman <julianforeman@me.com>
2021-09-14 13:18:35 -0700

    Fixed punctuation on 1 line of text.

--- web/src/features/fbaCalculator/components/FBATableInstructions.tsx
+++ web/src/features/fbaCalculator/components/FBATableInstructions.tsx
@@ -12,7 +12,7 @@ const FBATableInstructions = () => {
   const classes = useStyles()
   return (
     <div data-testid={'fba-instructions'} className={classes.content}>
-      <p>Add a row to get started</p>
+      <p>Add a row to get started.</p>
       <p>
         Build custom lists of weather stations by fire center, zone or fuel type. Bookmark
         the URL to save your custom list.
