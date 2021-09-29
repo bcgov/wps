6ebcf486f3f2240cc50779a80acd617db9553b10
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-09-29 10:55:11 -0700

    basic working script - works with s3cmd or with aws s3

--- api/scripts/backup_to_s3.sh
+++ api/scripts/backup_to_s3.sh
@@ -9,9 +9,18 @@ _datestamp=`date +\%Y/\%m`
 _target_filename="${PG_HOSTNAME}_${PG_DATABASE}_${_timestamp}.sql.gz"
 _target_folder="${PG_HOSTNAME}_${PG_DATABASE}/${_datestamp}"
 
-# Using s3cmd (https://github.com/s3tools/s3cmd) to upload the database backup to S3.
 
 # backupcontainer does:
 # PGPASSWORD=${_password} pg_dump -Fp -h "${_hostname}" ${_portArg} -U "${_username}" "${_database}" | gzip > ${_backupFile}
 
-PGPASSWORD="${PG_PASSWORD}" pg_dump -Fp -h "${PG_HOSTNAME}" -p ${PG_PORT} -U "${PG_USER}" "${PG_DATABASE}" | gzip | s3cmd --host "${AWS_HOSTNAME}" --access_key "${AWS_ACCESS_KEY}" --secret_key "${AWS_SECRET_KEY}" --host-bucket "${AWS_BUCKET}" --expiry-days 30 put - s3://${AWS_BUCKET}/backup/${_target_folder}/${_target_filename}
+# Using s3cmd (https://github.com/s3tools/s3cmd) to upload the database backup to S3:
+# pg_dump | gzip | s3cmd
+# this is tested and works.
+# PGPASSWORD="${PG_PASSWORD}" pg_dump -Fp -h "${PG_HOSTNAME}" -p ${PG_PORT} -U "${PG_USER}" "${PG_DATABASE}" | gzip | s3cmd --host "${AWS_HOSTNAME}" --access_key "${AWS_ACCESS_KEY}" --secret_key "${AWS_SECRET_KEY}" --host-bucket "${AWS_BUCKET}" --expiry-days 30 put - s3://${AWS_BUCKET}/backup/${_target_folder}/${_target_filename}
+
+# Using the official aws cli to upload the database back to S3:
+# pg_dump | gzip | aws s3:
+# this is tested and works.
+PGPASSWORD="${PG_PASSWORD}" pg_dump -Fp -h "${PG_HOSTNAME}" -p ${PG_PORT} -U "${PG_USER}" "${PG_DATABASE}" | gzip | AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY}" AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}" aws --endpoint="https://${AWS_HOSTNAME}" s3 cp - "s3://${AWS_BUCKET}/backup/${_target_folder}/${_target_filename}"
+
+
