5f81bbadab08929917b72fe229e119405ae15766
Conor Brady <con.brad@gmail.com>
2021-09-29 15:38:41 -0700

    Pulled out some helper functions

---
+++ web/src/features/hfiCalculator/util.ts
@@ -0,0 +1,53 @@
+import { StationDaily } from 'api/hfiCalculatorAPI'
+import { groupBy } from 'lodash'
+
+export const buildDailyMap = (dailies: StationDaily[]) => {
+  const dailiesMap = new Map<number, StationDaily>()
+
+  if (dailies !== undefined) {
+    dailies.forEach(daily => {
+      dailiesMap.set(daily.code, daily)
+    })
+  }
+
+  return dailiesMap
+}
+
+export const buildWeekliesByCode = (dailies: StationDaily[]) => {
+  const weekliesMap = new Map<number, StationDaily[]>()
+
+  if (dailies !== undefined) {
+    const weeklies = groupBy(dailies, 'code')
+    for (let i = 0; i < Object.keys(weeklies).length; i++) {
+      weekliesMap.set(Number(Object.keys(weeklies)[i]), Object.values(weeklies)[i])
+    }
+  }
+
+  return weekliesMap
+}
+
+export const buildWeekliesByDate = (dailies: StationDaily[]) => {
+  const weekliesMapDates = new Map<Date, StationDaily[]>()
+  if (dailies !== undefined) {
+    const weeklies = groupBy(dailies, 'date')
+    for (let i = 0; i < Object.keys(weeklies).length; i++) {
+      weekliesMapDates.set(new Date(Object.keys(weeklies)[i]), Object.values(weeklies[i]))
+      console.log(weekliesMapDates)
+    }
+  }
+}
+
+export const buildWeeklyDates = (weekliesMap: Map<number, StationDaily[]>) => {
+  const datesList: number[] = []
+
+  weekliesMap.forEach(value => {
+    for (let i = 0; i < value.length; i++) {
+      if (!datesList.includes(value[i].date.toUTC().toMillis())) {
+        datesList.push(value[i].date.toUTC().toMillis())
+      }
+    }
+  })
+
+  const dates = new Set(datesList)
+  return dates
+}
