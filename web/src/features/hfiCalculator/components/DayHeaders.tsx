8028b40bc49beb92ab2a0b055833e853869d17b2
Conor Brady <con.brad@gmail.com>
2021-10-05 11:15:27 -0700

    Simplify calculated planning area cells and fix bug

--- web/src/features/hfiCalculator/components/DayHeaders.tsx
+++ web/src/features/hfiCalculator/components/DayHeaders.tsx
@@ -1,5 +1,6 @@
 import { TableCell } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { NUM_WEEK_DAYS } from 'features/hfiCalculator/constants'
 import { range } from 'lodash'
 import React from 'react'
 import { getPrepWeeklyDateRange } from 'utils/date'
@@ -21,7 +22,7 @@ const DayHeaders = (props: DayHeadersProps) => {
   const classes = useStyles()
   return (
     <React.Fragment>
-      {range(5).map(i => (
+      {range(NUM_WEEK_DAYS).map(i => (
         <TableCell colSpan={6} className={classes.dayHeader} key={i}>
           {start
             .plus({ days: i })
--- web/src/features/hfiCalculator/constants.ts
+++ web/src/features/hfiCalculator/constants.ts
@@ -1,1 +1,2 @@
 export const DECIMAL_PLACES = 1
+export const NUM_WEEK_DAYS = 5
