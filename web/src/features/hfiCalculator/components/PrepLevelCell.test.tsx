acaa614b764faaca6e971879fa4808f0821fbabf
Julian Foreman <julianforeman@me.com>
2021-10-07 15:15:29 -0700

    Completed tests for DayHeaders, refactored to eliminate code duplication

--- web/src/features/hfiCalculator/components/PrepLevelCell.test.tsx
+++ web/src/features/hfiCalculator/components/PrepLevelCell.test.tsx
@@ -2,85 +2,46 @@ import { Table, TableBody, TableContainer, TableRow } from '@material-ui/core'
 import { render } from '@testing-library/react'
 import PrepLevelCell from 'features/hfiCalculator/components/PrepLevelCell'
 import React from 'react'
+
+const prepLevelTest = (MIG: number, expectedPrep: string, expectedClass: any) => {
+  const { getByTestId, getByText } = render(
+    <TableContainer>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <PrepLevelCell
+              testid={'weekly-prep-level-afton'}
+              areaName={'afton'}
+              meanIntensityGroup={MIG}
+            ></PrepLevelCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+
+  const cell = getByTestId('weekly-prep-level-afton')
+  expect(cell.className).toMatch(expectedClass)
+  expect(getByText(expectedPrep)).toBeDefined
+}
+
 describe('PrepLevelCell', () => {
   it('should return a cell with a classname of prepLevel1 and a text prep level of 1', () => {
-    const { getByTestId, getByText } = render(
-      <TableContainer>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <PrepLevelCell
-                testid={'weekly-prep-level-afton'}
-                areaName={'afton'}
-                meanIntensityGroup={1}
-              ></PrepLevelCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-    const cell = getByTestId('weekly-prep-level-afton')
-    expect(cell.className).toMatch(/makeStyles-prepLevel1-/)
-    expect(getByText('1')).toBeDefined
+    prepLevelTest(1, '1', /makeStyles-prepLevel1-/)
+  })
+  it('should return a cell with a classname of prepLevel1 and a text prep level of 1', () => {
+    prepLevelTest(2, '1', /makeStyles-prepLevel1-/)
   })
   it('should return a cell with a classname of prepLevel2 and a text prep level of 2', () => {
-    const { getByTestId, getByText } = render(
-      <TableContainer>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <PrepLevelCell
-                testid={'weekly-prep-level-afton'}
-                areaName={'afton'}
-                meanIntensityGroup={3}
-              ></PrepLevelCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-    const cell = getByTestId('weekly-prep-level-afton')
-    expect(cell.className).toMatch(/makeStyles-prepLevel2-/)
-    expect(getByText('2')).toBeDefined
+    prepLevelTest(3, '2', /makeStyles-prepLevel2-/)
   })
   it('should return a cell with a classname of prepLevel3 and a text prep level of 3', () => {
-    const { getByTestId, getByText } = render(
-      <TableContainer>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <PrepLevelCell
-                testid={'weekly-prep-level-afton'}
-                areaName={'afton'}
-                meanIntensityGroup={4}
-              ></PrepLevelCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-    const cell = getByTestId('weekly-prep-level-afton')
-    expect(cell.className).toMatch(/makeStyles-prepLevel3-/)
-    expect(getByText('3')).toBeDefined
+    prepLevelTest(4, '3', /makeStyles-prepLevel3-/)
   })
   it('should return a cell with a classname of prepLevel4 and a text prep level of 4', () => {
-    const { getByTestId, getByText } = render(
-      <TableContainer>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <PrepLevelCell
-                testid={'weekly-prep-level-afton'}
-                areaName={'afton'}
-                meanIntensityGroup={5}
-              ></PrepLevelCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-    const cell = getByTestId('weekly-prep-level-afton')
-    expect(cell.className).toMatch(/makeStyles-prepLevel4-/)
-    expect(getByText('4')).toBeDefined
+    prepLevelTest(5, '4', /makeStyles-prepLevel4-/)
+  })
+  it('should return a cell with a classname of prepLevel4 and a text prep level of 4', () => {
+    prepLevelTest(6, '4', /makeStyles-prepLevel4-/)
   })
 })
