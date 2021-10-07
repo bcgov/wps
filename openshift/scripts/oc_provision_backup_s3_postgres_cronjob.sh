32d1dba7e721f3f3d7ae64c55e3d8d584fb1b5ad
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-10-07 07:00:09 -0700

    removed reliance on config file, tinker ith docker

--- openshift/scripts/oc_provision_backup_s3_postgres_cronjob.sh
+++ openshift/scripts/oc_provision_backup_s3_postgres_cronjob.sh
@@ -29,6 +29,7 @@ PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 # Prepare variables for backups
 JOB_NAME="backup-postgres-${NAME_APP}-${SUFFIX}"
 IMAGE_NAMESPACE=${PROJ_TOOLS}
+CLUSTER_NAME="patroni-${NAME_APP}-${SUFFIX}"
 
 OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/backup-s3-postgres-cronjob.yaml \
     -p DATABASE_SERVICE_NAME=patroni-${NAME_APP}-${SUFFIX}-leader \
@@ -36,6 +37,7 @@ OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/backup-s3-postgres-
     -p JOB_NAME=${JOB_NAME} \
     -p IMAGE_NAMESPACE=${IMAGE_NAMESPACE} \
     -p APP_LABEL=${NAME_APP}-${SUFFIX} \
+    -p CLUSTER_NAME=${CLUSTER_NAME} \
     ${CPU_LIMIT:+ " -p CPU_LIMIT=${CPU_LIMIT}"} \
     ${CPU_REQUEST:+ " -p CPU_REQUEST=${CPU_REQUEST}"} \
     ${SCHEDULE:+ " -p SCHEDULE=\"${SCHEDULE}\""} \
