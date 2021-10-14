66dabdca7cc9df37d210352e36144061fcfcee02
Conor Brady <con.brad@gmail.com>
2021-10-14 09:11:30 -0700

    Story/weekly table view/1022 (#1399)
    * Implements weekly table via toggle on HFI calculator
    * Consolidates shared styles across table based products in app/theme.ts
    
    Co-authored-by: Julian Foreman <julianforeman@me.com>
    Co-authored-by: Andrea Williams <andrea.williams@gov.bc.ca>
    Co-authored-by: Sybrand Strauss <sybrand.strauss@gov.bc.ca>

---
+++ web/src/features/hfiCalculator/components/WeeklyROSCell.tsx
@@ -0,0 +1,37 @@
+import { TableCell } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { WeatherStation } from 'api/hfiCalcAPI'
+import { StationDaily } from 'api/hfiCalculatorAPI'
+import { fireTableStyles, UNSELECTED_STATION_COLOR } from 'app/theme'
+import { DECIMAL_PLACES } from 'features/hfiCalculator/constants'
+import React from 'react'
+
+export interface WeeklyROSCellProps {
+  daily: StationDaily
+  station: WeatherStation
+  error: boolean
+  isRowSelected: boolean
+}
+
+const useStyles = makeStyles({
+  ...fireTableStyles,
+  unselectedStation: {
+    ...fireTableStyles.sectionSeparatorBorder,
+    color: UNSELECTED_STATION_COLOR
+  }
+})
+const WeeklyROSCell = ({ daily, station, isRowSelected, error }: WeeklyROSCellProps) => {
+  const classes = useStyles()
+  return (
+    <TableCell
+      data-testid={`${station.code}-ros`}
+      className={
+        isRowSelected ? classes.sectionSeparatorBorder : classes.unselectedStation
+      }
+    >
+      {error ? '' : daily?.rate_of_spread?.toFixed(DECIMAL_PLACES)}
+    </TableCell>
+  )
+}
+
+export default React.memo(WeeklyROSCell)
---
+++ web/src/features/hfiCalculator/components/WeeklyViewTable.tsx
@@ -0,0 +1,166 @@
+import React, { useState } from 'react'
+
+import { TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { FireCentre } from 'api/hfiCalcAPI'
+import { StationDaily } from 'api/hfiCalculatorAPI'
+import FireTable from 'components/FireTable'
+import DayHeaders from 'features/hfiCalculator/components/DayHeaders'
+import DayIndexHeaders from 'features/hfiCalculator/components/DayIndexHeaders'
+import CalculatedPlanningAreaCells from 'features/hfiCalculator/components/CalculatedPlanningAreaCells'
+import { StaticCells } from 'features/hfiCalculator/components/StaticCells'
+import BaseStationAttributeCells from 'features/hfiCalculator/components/BaseStationAttributeCells'
+import GrassCureCell from 'features/hfiCalculator/components/GrassCureCell'
+import { isGrassFuelType } from 'features/hfiCalculator/validation'
+import { fireTableStyles } from 'app/theme'
+
+export interface Props {
+  fireCentres: Record<string, FireCentre>
+  stationCodes: number[]
+  weekliesByStationCode: Map<number, StationDaily[]>
+  currentDay: string
+  testId?: string
+}
+
+const useStyles = makeStyles({
+  ...fireTableStyles
+})
+
+export const WeeklyViewTable = (props: Props): JSX.Element => {
+  const classes = useStyles()
+
+  const [selected, setSelected] = useState<number[]>(props.stationCodes)
+
+  const stationCodeInSelected = (code: number) => {
+    return selected.includes(code)
+  }
+  const toggleSelectedStation = (code: number) => {
+    const selectedSet = new Set(selected)
+    if (stationCodeInSelected(code)) {
+      // remove station from selected
+      selectedSet.delete(code)
+    } else {
+      // add station to selected
+      selectedSet.add(code)
+    }
+    setSelected(Array.from(selectedSet))
+  }
+
+  return (
+    <FireTable
+      maxHeight={700}
+      ariaLabel="weekly table view of HFI by planning area"
+      testId="hfi-calc-weekly-table"
+    >
+      <TableHead>
+        <TableRow>
+          <DayHeaders isoDate={props.currentDay} />
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            {/* empty cell inserted for spacing purposes (aligns with checkboxes column) */}
+          </TableCell>
+          <TableCell key="header-location">Location</TableCell>
+          <TableCell key="header-elevation">
+            Elev.
+            <br />
+            (m)
+          </TableCell>
+          <TableCell key="header-fuel-type">
+            FBP
+            <br />
+            Fuel
+            <br />
+            Type
+          </TableCell>
+          <TableCell>
+            Grass
+            <br />
+            Cure
+            <br />
+            (%)
+          </TableCell>
+          <DayIndexHeaders />
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {Object.entries(props.fireCentres).map(([centreName, centre]) => {
+          return (
+            <React.Fragment key={`fire-centre-${centreName}`}>
+              <TableRow key={`fire-centre-${centreName}`}>
+                <TableCell className={classes.fireCentre} colSpan={30}>
+                  {centre.name}
+                </TableCell>
+              </TableRow>
+              {Object.entries(centre.planning_areas)
+                .sort((a, b) => (a[1].name < b[1].name ? -1 : 1))
+                .map(([areaName, area]) => {
+                  return (
+                    <React.Fragment key={`zone-${areaName}`}>
+                      <TableRow
+                        className={classes.planningArea}
+                        key={`zone-${areaName}`}
+                        data-testid={`zone-${areaName}`}
+                      >
+                        <TableCell className={classes.planningArea} colSpan={5}>
+                          {area.name}
+                        </TableCell>
+                        <CalculatedPlanningAreaCells
+                          area={area}
+                          areaName={areaName}
+                          selected={selected}
+                          planningAreaClass={classes.planningArea}
+                        />
+                      </TableRow>
+                      {Object.entries(area.stations)
+                        .sort((a, b) => (a[1].code < b[1].code ? -1 : 1))
+                        .map(([stationCode, station]) => {
+                          const dailies = props.weekliesByStationCode.get(station.code)
+                          const isRowSelected = stationCodeInSelected(station.code)
+                          const classNameForRow = !isRowSelected
+                            ? classes.unselectedStation
+                            : undefined
+
+                          return (
+                            <TableRow
+                              className={classNameForRow}
+                              key={`station-${stationCode}`}
+                            >
+                              <BaseStationAttributeCells
+                                station={station}
+                                className={classNameForRow}
+                                stationCodeInSelected={stationCodeInSelected}
+                                toggleSelectedStation={toggleSelectedStation}
+                              />
+                              <GrassCureCell
+                                value={
+                                  dailies ? dailies[0].grass_cure_percentage : undefined
+                                }
+                                isGrassFuelType={isGrassFuelType(station.station_props)}
+                                className={
+                                  isRowSelected ? undefined : classes.unselectedStation
+                                }
+                                selected={isRowSelected}
+                              />
+
+                              <StaticCells
+                                dailies={dailies}
+                                station={station}
+                                classNameForRow={classNameForRow}
+                                isRowSelected={isRowSelected}
+                              />
+                            </TableRow>
+                          )
+                        })}
+                    </React.Fragment>
+                  )
+                })}
+            </React.Fragment>
+          )
+        })}
+      </TableBody>
+    </FireTable>
+  )
+}
+
+export default React.memo(WeeklyViewTable)
