3d795440f3c6159eeab40a159f310719370b6446
Julian Foreman <julianforeman@me.com>
2021-10-19 15:23:11 -0700

    About this data popup modal for HFI calculator

---
+++ web/src/features/hfiCalculator/components/AboutDataModal.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Paper,
+  makeStyles
+} from '@material-ui/core'
+import { Clear } from '@material-ui/icons'
+
+export interface ColumnSelectionState {
+  label: string
+  selected: boolean
+}
+
+export interface ModalProps {
+  testId?: string
+  modalOpen: boolean
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const useStyles = makeStyles(() => ({
+  modalWindow: {
+    maxWidth: 'md'
+  },
+  closeIcon: {
+    position: 'absolute',
+    right: '0px'
+  }
+}))
+
+export const AboutDataModal = (props: ModalProps): JSX.Element => {
+  const classes = useStyles()
+
+  const handleClose = () => {
+    props.setModalOpen(false)
+  }
+
+  return (
+    <React.Fragment>
+      <Dialog
+        fullWidth
+        className={classes.modalWindow}
+        open={props.modalOpen}
+        onClose={handleClose}
+      >
+        <Paper>
+          <DialogTitle>
+            <IconButton className={classes.closeIcon} onClick={handleClose}>
+              <Clear />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <h1>About This Data</h1>
+            <p>
+              Forecasted weather outputs are for 13:00 and FWI Indices are for 17:00 PDT.
+            </p>
+            <p>These fire behaviour calculations assume flat terrain.</p>
+            <br />
+            <p>
+              Weather and fire behaviour indicies are sourced from the Wildfire One API.
+            </p>
+            <br />
+            <p>
+              Values are calculated used the{' '}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://r-forge.r-project.org/projects/cffdrs/"
+              >
+                Canadian Forest Fire Danger Rating System R Library
+              </a>{' '}
+              and are intended to provide general guidance for Prep Day.
+            </p>
+            <br />
+            <p>
+              If you have any questions about how values are calculated, please{' '}
+              <a href="mailto: bcws.predictiveservices@gov.bc.ca? subject= Predictive Services Unit - Fire Behaviour Advisory Calculator">
+                contact us.
+              </a>
+            </p>
+          </DialogContent>
+        </Paper>
+      </Dialog>
+    </React.Fragment>
+  )
+}
+
+export default React.memo(AboutDataModal)
