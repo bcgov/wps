cdbf8b0d9da264dff8af679b72b10678c0b0ac7b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-10-20 11:05:36 -0700

    Cleanup - delete backups in object store on PR cleanup. (#1432)
    - Delete backups in object store on pull request delete
    - Modified base image resource request/limits (noticed it was exceeding limit and stepping up slowly)

---
+++ openshift/scripts/oc_provision_backup_s3_cleanup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+#
+source "$(dirname ${0})/common/common"
+
+#%
+#% OpenShift Deploy Helper
+#%
+#%   Intended for use with a pull request-based pipeline.
+#%   Suffixes incl.: pr-###.
+#%
+#% Usage:
+#%
+#%    ${THIS_FILE} [SUFFIX]
+#%
+#% Examples:
+#%
+#%   ${THIS_FILE} pr-0
+
+CLUSTER_NAME="patroni-${NAME_APP}-${SUFFIX}"
+JOB="job/cleanup-s3-wps-${SUFFIX}"
+
+# create the job
+oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/backup-s3-cleanup-job.yaml \
+    -p SUFFIX=${SUFFIX} \
+    -p PROJ_TOOLS=${PROJ_TOOLS} \
+    -p CLUSTER_NAME=${CLUSTER_NAME} | jq '.items[0]' | oc -n ${PROJ_TARGET} create -f -
+# wait for the job to finish
+oc wait --for=condition=complete ${JOB} --timeout=60s
+# output the log for debugging
+oc logs -f ${JOB}
+# we're done, so get rid of the job
+oc delete ${JOB}
+
