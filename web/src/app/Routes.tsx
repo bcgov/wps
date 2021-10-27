b30d07c2d1ad8d8edb4e4d228e94751cbe45c302
Conor Brady <con.brad@gmail.com>
2021-10-26 17:03:35 -0700

    FireBehaviourAdvisory page with map (#1441)
    * FBATable with map

--- web/src/app/Routes.tsx
+++ web/src/app/Routes.tsx
@@ -12,11 +12,13 @@ import {
   MORECAST_ROUTE,
   HFI_CALC_ROUTE,
   C_HAINES_ROUTE,
-  FIRE_BEHAVIOR_CALC_ROUTE
+  FIRE_BEHAVIOR_CALC_ROUTE,
+  FIRE_BEHAVIOUR_ADVISORY_ROUTE
 } from 'utils/constants'
 import MoreCastPage from 'features/fireWeather/pages/MoreCastPage'
 import { NoMatchPage } from 'features/fireWeather/pages/NoMatchPage'
-import { FireBehaviourAdvisoryCalculator } from 'features/fbaCalculator/FireBehaviourAdvisoryCalculatorPage'
+import { FireBehaviourCalculator } from 'features/fbaCalculator/pages/FireBehaviourCalculatorPage'
+import { FireBehaviourAdvisoryPage } from 'features/fbaCalculator/pages/FireBehaviourAdvisoryPage'
 
 const shouldShowDisclaimer = HIDE_DISCLAIMER === 'false' || HIDE_DISCLAIMER === undefined
 const shouldAuthenticate =
@@ -51,7 +53,13 @@ const Routes: React.FunctionComponent = () => {
 
         <Route path={FIRE_BEHAVIOR_CALC_ROUTE}>
           <AuthWrapper shouldAuthenticate={shouldAuthenticate}>
-            <FireBehaviourAdvisoryCalculator />
+            <FireBehaviourCalculator />
+          </AuthWrapper>
+        </Route>
+
+        <Route path={FIRE_BEHAVIOUR_ADVISORY_ROUTE}>
+          <AuthWrapper shouldAuthenticate={shouldAuthenticate}>
+            <FireBehaviourAdvisoryPage />
           </AuthWrapper>
         </Route>
 
---
+++ web/src/features/fireWeather/components/maps/constants.ts
@@ -0,0 +1,14 @@
+import XYZ from 'ol/source/XYZ'
+
+export const BC_ROAD_BASE_MAP_SERVER_URL =
+  'https://maps.gov.bc.ca/arcgis/rest/services/province/roads_wm/MapServer'
+
+// Static source is allocated since our tile source does not change and
+// a new source is not allocated every time WeatherMap is re-rendered,
+// which causes the TileLayer to re-render.
+export const source = new XYZ({
+  url: `${BC_ROAD_BASE_MAP_SERVER_URL}/tile/{z}/{y}/{x}`,
+  // Normally we would get attribution text from `${BC_ROAD_BASE_MAP_SERVER_URL}?f=pjson`
+  // however this endpoint only allows the origin of http://localhost:3000, so the text has been just copied from that link
+  attributions: 'Government of British Columbia, DataBC, GeoBC'
+})
