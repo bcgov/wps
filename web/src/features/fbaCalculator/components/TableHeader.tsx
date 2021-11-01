f7789e4ec865f2297cc5525ad74bc045153d1172
JulianForeman <71847719+JulianForeman@users.noreply.github.com>
2021-11-01 16:22:43 -0700

    Table header edits & tooltips added (#1484)
    * Table header edits & tooltips added
    
    * Component for table header

---
+++ web/src/features/fbaCalculator/components/TableHeader.tsx
@@ -0,0 +1,50 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+interface TableHeaderProps {
+  text: string
+  largerMaxWidth?: boolean
+}
+
+const TableHeader = (props: TableHeaderProps) => {
+  let maxWidth = '200px'
+  if (!props.largerMaxWidth) {
+    maxWidth = '80px'
+  }
+  const useStyles = makeStyles({
+    header: {
+      overflow: 'hidden',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      maxWidth: maxWidth,
+      '& span': {
+        display: 'none'
+      },
+      '&:hover span': {
+        display: 'block',
+        backgroundColor: '#4f4f4f',
+        opacity: '0.8',
+        paddingLeft: '5px',
+        paddingRight: '5px',
+        maxWidth: '225px',
+        color: '#fff',
+        textAlign: 'center',
+        borderRadius: '6px',
+        /* Position the tooltip */
+        position: 'absolute',
+        left: '2px',
+        top: '41px'
+      }
+    }
+  })
+
+  const classes = useStyles()
+  return (
+    <div className={classes.header}>
+      {props.text}
+      <span>{props.text}</span>
+    </div>
+  )
+}
+
+export default React.memo(TableHeader)
