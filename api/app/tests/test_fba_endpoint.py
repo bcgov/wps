a5ce0c0885cf37d83f4f14090f8ccb2f152a853d
Conor Brady <con.brad@gmail.com>
2021-11-02 13:07:25 -0700

    Add a better test

--- api/app/tests/test_fba_endpoint.py
+++ api/app/tests/test_fba_endpoint.py
@@ -3,13 +3,14 @@ from pytest_bdd import scenario, given, then
 import pytest
 from starlette.testclient import TestClient
 from aiohttp import ClientSession
-from sqlalchemy.orm import Session
 from app.schemas.fba import FireCenterStation, FireCentre
 from app.tests.common import default_mock_client_get
 import app.main
+from app.tests import load_json_file
 import app.routers.hfi_calc
+from app.tests.test_hfi_dailies import AsyncIter
 
-fire_centers = [
+fire_centers = AsyncIter([
     FireCentre(
         id="1",
         name="Fire Center 1",
@@ -20,14 +21,14 @@ fire_centers = [
         name="Fire Center 2",
         stations=[FireCenterStation(code=3, name="s3", zone="k1"),
                   FireCenterStation(code=4, name="s4", zone="k1")])
-]
+])
 
 
 @pytest.mark.usefixtures("mock_jwt_decode")
 @scenario('test_fba_endpoint.feature', 'Get fire centres with their stations',
           example_converters=dict(
-              status=int,
-              num_fire_centres=int))
+            status=int,
+            expected_fire_centers=load_json_file(__file__)))
 def test_fba_fire_centers():
     """ BDD Scenario. """
 
@@ -36,12 +37,7 @@ def test_fba_fire_centers():
 def given_fba_fire_centers_request(monkeypatch):
     """ Make /fba/ request using mocked out ClientSession.
     """
-
-    def mock_get_fire_centers(_: Session):
-        return [fire_centers]
-
     monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
-    monkeypatch.setattr(app.wildfire_one.wfwx_api, 'get_fire_centers', mock_get_fire_centers)
 
     # Create API client and get the response.
     client = TestClient(app.main.app)
@@ -57,7 +53,7 @@ def assert_status_code(response, status):
     assert response.status_code == status
 
 
-@then('the response contains the list of fire centers')
-def assert_fire_centers_list(response):
-    """ Assert that each fire centre returned has at least 1 planning area assigned to it """
-    assert len(response.json()['fire_centers']) > 0
+@then('the response contains the list of <expected_fire_centers>')
+def assert_fire_centers_list(response, expected_fire_centers):
+    """ Assert that the fire centers returned are what is from the middleware """
+    assert response.json() == expected_fire_centers
