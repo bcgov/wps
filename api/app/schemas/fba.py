b7033bd4067936f86ac2dd1135340e663433618a
Conor Brady <con.brad@gmail.com>
2021-11-03 11:10:25 -0700

    Fire centre drop down (#1483)
    - Adds api/fba/fire-centers route
    - Adds fire_center_mapper to marshall station results from WFWX1 into unque FireCenters with their associated stations to serve to the frontend
    - Selected a fire center on the frontend renders the list of associated stations

---
+++ api/app/schemas/fba.py
@@ -0,0 +1,21 @@
+""" This module contains pydantic models related to the new formal/non-tinker fba. """
+
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class FireCenterStation(BaseModel):
+    """ A fire weather station has a code, name and geographical coordinate. """
+    code: int
+    name: str
+    zone: Optional[str]
+class FireCentre(BaseModel):
+    """ The highest-level organizational unit for wildfire planning. Each fire centre
+    has 1 or more planning areas within it. """
+    id: str
+    name: str
+    stations: List[FireCenterStation]
+
+class FireCenterListResponse(BaseModel):
+    """ Response for all fire centers, in a list """
+    fire_centers: List[FireCentre]
