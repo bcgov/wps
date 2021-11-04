6b8f8d9ad18d3ed24ad77da177e4aff9966a235e
Conor Brady <con.brad@gmail.com>
2021-11-04 14:49:51 -0700

    Added explanatory comments as suggested in code review

--- web/src/features/fbaCalculator/components/map/FBAMap.tsx
+++ web/src/features/fbaCalculator/components/map/FBAMap.tsx
@@ -63,10 +63,13 @@ const buildBCTileLayer = (extent: number[]) => {
   return new Tile({
     extent,
     opacity: 0.5,
+    // Preload tiles. Load low-resolution tiles up to preload levels. Infinity means as much as possible.
     preload: Infinity,
     source: new TileWMS({
       url: 'https://openmaps.gov.bc.ca/geo/pub/wms',
       params: {
+        // This is the WMS layer published by DataCB in the Data Catologue:
+        // https://catalogue.data.gov.bc.ca/dataset/bc-wildfire-fire-centres/resource/c33fd014-910f-44f6-b72e-9d7eed5100a9
         LAYERS: 'WHSE_LEGAL_ADMIN_BOUNDARIES.DRP_MOF_FIRE_CENTRES_SP',
         TILED: true,
         STYLES: '3458'
@@ -89,6 +92,11 @@ const FBAMap = (props: FBAMapProps) => {
   const [map, setMap] = useState<ol.Map | null>(null)
   const mapRef = useRef<HTMLDivElement | null>(null)
   useEffect(() => {
+    // The React ref is used to attach to the div rendered in our
+    // return statement of which this map's target is set to.
+    // The ref is a div of type  HTMLDivElement.
+
+    // Pattern copied from web/src/features/map/Map.tsx
     if (!mapRef.current) return
 
     const options: MapOptions = {
@@ -106,8 +114,22 @@ const FBAMap = (props: FBAMapProps) => {
       controls: defaultControls()
     }
 
+    // Create the map with the options above and set the target
+    // To the ref above so that it is rendered in that div
     const mapObject = new ol.Map(options)
     mapObject.setTarget(mapRef.current)
+
+    // Calculate extent based on size in pixel of the map.
+    //
+    // The extent is the minimum bounding rectangle (xmin, ymin and xmax, ymax)
+    // defined by coordinate pairs of a data source.
+    //
+    // We use the extent to limit the tiles requested from the BC tile layer added below.
+
+    // See:
+    // - https://en.wikipedia.org/wiki/Map_extent
+    // - https://openlayers.org/en/latest/apidoc/module-ol_extent.html
+    // - https://gis.stackexchange.com/questions/240979/difference-between-bounding-box-envelope-extent-bounds
 
     const extent = mapObject.getView().calculateExtent(mapObject.getSize())
     mapObject.addLayer(buildBCTileLayer(extent))
