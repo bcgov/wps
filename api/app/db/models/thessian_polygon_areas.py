42cb335fdcecadc9498f256c449f2396317f2789
Conor Brady <con.brad@gmail.com>
2021-11-22 13:47:56 -0800

    Code review fixes for table creation

--- api/app/db/models/thessian_polygon_areas.py
+++ api/app/db/models/thessian_polygon_areas.py
@@ -2,18 +2,27 @@
 """
 from geoalchemy2.types import Geometry
 from sqlalchemy import (Column, Integer,
-                        Sequence)
+                        Sequence, Index)
+from app.db.models.common import TZTimeStamp
+import app.utils.time as time_utils
 from app.db.database import Base
 
 
 class ThessianPolygonArea(Base):
     """ Thessian polygon for a given area """
-    __tablename__ = 'fire_area_thessian_polygons'
+    __tablename__ = 'thessian_polygon_area'
 
-    id = Column(Integer, Sequence('thessian_area_id_seq'),
+    id = Column(Integer, Sequence('thessian_polygon_areaid_seq'),
                 primary_key=True, nullable=False, index=True)
-    polygon = Column(Geometry(geometry_type='POLYGON', from_text='ST_GeomFromGeoJSON'))
+    geom = Column(Geometry(geometry_type='POLYGON', spatial_index=False))
+
+    create_date = Column(TZTimeStamp, nullable=False,
+                         default=time_utils.get_utc_now())
+    update_date = Column(TZTimeStamp, nullable=False,
+                         default=time_utils.get_utc_now())
 
     def __str__(self):
-        return (f'id:{self.id}, '
-                f'fire_center:{self.polygon}')
+        return (f'id: {self.id}')
+
+
+Index('idx_thessian_polygon_area_geom', ThessianPolygonArea.geom, postgresql_using='gist')
