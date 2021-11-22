d2d606ce2f72e24bd8d320ef3b5eb23fee2bbfa2
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2021-11-22 09:26:56 -0800

    messing around

--- openshift/scripts/oc_promote_ubuntu.sh
+++ openshift/scripts/oc_promote_ubuntu.sh
@@ -30,7 +30,25 @@ IMG_DEST="${NAME_APP}-ubuntu-${TAG_PROD}"
 # Import to new image and retag, leaving the original tag in
 #
 # TODO: importing the image in this way, results in previous tags persisting - we need to get rid of them!
+#
+# lets say we have wps-pr-1564:pr-1564, and we want to promote it to production.
+# keep in mind, that we're going to delete wps-pr-1564 when we clean up that PR, so we need to copy it.
+# we import it into wps-prod as wps-prod:pr-1564
+# it's now safe to delete wps-pr-1564:pr-1564 - we have a copy.
+# we can no tag this new image as our prod tag, so we mark it as wps-prod:prod
+# but hold - we can't keep all these old tags forever!
+#
+#  oc get is/wps-pr-1561 --output=json
+#  oc get is/wps-pr-1561 -o jsonpath='{.status.tags[*].items[0].created}'
+#
+#
+# TAG_N
+# TAG_N-1 = 
+# TAG_N-2 = TAG_N-1
+# TAG_N-3 = TAG_N-2
+# e.g. wps-ubuntu-ubuntu-prod:pr-123 --from=image-registry.openshift-image-registry.svc:5000/e1e498-tools/wps-ubuntu-ubuntu-pr-123:pr-123
 OC_IMG_IMPORT="oc -n ${PROJ_TOOLS} import-image ${IMG_DEST}:${SUFFIX} --from=${IMG_SOURCE} --confirm"
+# e.g. wps-ubuntu-ubuntu-prod:pr-123 image-registry.openshift-image-registry.svc:5000/e1e498-tools/wps-ubuntu-ubuntu-pr-123:prod
 OC_IMG_RETAG="oc -n ${PROJ_TOOLS} tag ${IMG_DEST}:${SUFFIX} ${IMG_DEST}:${TAG_PROD}"
 # TODO: now get rid of the source image!
 
