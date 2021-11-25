2701805ebe494bf1f265befea00a3536f614fccc
Conor Brady <con.brad@gmail.com>
2021-11-25 11:56:40 -0800

    Make fire center labels bigger

--- web/src/features/fba/components/featureStylers.ts
+++ web/src/features/fba/components/featureStylers.ts
@@ -4,22 +4,24 @@ import Geometry from 'ol/geom/Geometry'
 import { Fill, Stroke, Text } from 'ol/style'
 import Style from 'ol/style/Style'
 
-const createTextStyle = (feature: RenderFeature | ol.Feature<Geometry>) => {
+const fireCenterLabelStyler = (label: string) => {
   return new Text({
-    text: feature.getProperties().mof_fire_centre_name,
+    text: label,
     fill: new Fill({ color: 'black' }),
-    stroke: new Stroke({ color: 'black', width: 1 })
+    font: '16px sans-serif'
   })
 }
 
 export const fireCenterStyler = (
   feature: RenderFeature | ol.Feature<Geometry>
 ): Style => {
+  const label = feature.getProperties().mof_fire_centre_name
+
   return new Style({
     stroke: new Stroke({
       color: 'black',
       width: 3
     }),
-    text: createTextStyle(feature)
+    text: fireCenterLabelStyler(label)
   })
 }
