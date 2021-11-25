ae4ada595f4e9a31d689dac085f4e0adedaae99c
Conor Brady <con.brad@gmail.com>
2021-11-25 12:07:44 -0800

    Reuse style object

--- web/src/features/fba/components/featureStylers.ts
+++ web/src/features/fba/components/featureStylers.ts
@@ -4,25 +4,21 @@ import Geometry from 'ol/geom/Geometry'
 import { Fill, Stroke, Text } from 'ol/style'
 import Style from 'ol/style/Style'
 
-const fireCenterLabelStyler = (label: string) => {
-  return new Text({
-    text: label,
+const fireCenterLabelStyle = new Style({
+  stroke: new Stroke({
+    color: 'black',
+    width: 3
+  }),
+  text: new Text({
     fill: new Fill({ color: 'black' }),
     stroke: new Stroke({ color: 'black' }),
     font: '16px sans-serif'
   })
-}
+})
 
 export const fireCenterStyler = (
   feature: RenderFeature | ol.Feature<Geometry>
 ): Style => {
-  const label = feature.get('mof_fire_centre_name')
-
-  return new Style({
-    stroke: new Stroke({
-      color: 'black',
-      width: 3
-    }),
-    text: fireCenterLabelStyler(label)
-  })
+  fireCenterLabelStyle.getText().setText(feature.get('mof_fire_centre_name'))
+  return fireCenterLabelStyle
 }
