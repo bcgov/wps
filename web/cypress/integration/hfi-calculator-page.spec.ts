bcbdf163ba2ac21da5743673959ea63aa048a015
Conor Brady <con.brad@gmail.com>
2021-11-29 10:45:12 -0800

    Re-enable test

--- web/cypress/integration/hfi-calculator-page.spec.ts
+++ web/cypress/integration/hfi-calculator-page.spec.ts
@@ -48,7 +48,7 @@ describe('HFI Calculator Page', () => {
 
     // Re-enable fix for
     // https://app.zenhub.com/workspaces/wildfire-predictive-services-5e321393e038fba5bbe203b8/issues/bcgov/wps/1488
-    xit('should allow date of interest to be changed with DatePicker component', () => {
+    it('should allow date of interest to be changed with DatePicker component', () => {
       cy.visit(HFI_CALC_ROUTE)
       cy.wait(['@getFireCentres', '@getDaily'])
       cy.getByTestId('date-of-interest-picker').type('2021-07-22')
