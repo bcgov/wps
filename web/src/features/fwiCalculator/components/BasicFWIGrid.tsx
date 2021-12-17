aebb773c8cc1e8e4a15473a04f81089abe7d0541
Conor Brady <con.brad@gmail.com>
2021-12-16 16:34:47 -0800

    Output values

--- web/src/features/fwiCalculator/components/BasicFWIGrid.tsx
+++ web/src/features/fwiCalculator/components/BasicFWIGrid.tsx
@@ -7,116 +7,155 @@ import {
   TableRow,
   TableCell,
   TableBody,
-  TextField
+  TextField,
+  Grid
 } from '@material-ui/core'
 import { FWIStationCell } from 'features/fwiCalculator/components/FWIStationCell'
 
 export const BasicFWIGrid: React.FunctionComponent = () => {
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="Basic FWI Calculation Inputs">
-        <TableHead>
-          <TableRow>
-            <TableCell>Station Name</TableCell>
-            <TableCell>
-              <FWIStationCell />
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>Yesterday&apos;s FFMC</TableCell>
-            <TableCell>
-              <TextField
-                type="number"
-                inputMode="numeric"
-                size="small"
-                variant="outlined"
-                inputProps={{ min: 0, max: 100 }}
-                defaultValue={20}
-              />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Yesterday&apos;s DMC</TableCell>
-            <TableCell>
-              <TextField
-                type="number"
-                inputMode="numeric"
-                size="small"
-                variant="outlined"
-                inputProps={{ min: 0, max: 100 }}
-                defaultValue={11}
-              />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Yesterday&apos;s DC</TableCell>
-            <TableCell>
-              <TextField
-                type="number"
-                inputMode="numeric"
-                size="small"
-                variant="outlined"
-                inputProps={{ min: 0, max: 100 }}
-                defaultValue={5}
-              />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Today&apos;s 1300 Temperature</TableCell>
-            <TableCell>
-              <TextField
-                type="number"
-                inputMode="numeric"
-                size="small"
-                variant="outlined"
-                inputProps={{ min: 0, max: 100 }}
-                defaultValue={15}
-              />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Today&apos;s 1300 Relative Humidity</TableCell>
-            <TableCell>
-              <TextField
-                type="number"
-                inputMode="numeric"
-                size="small"
-                variant="outlined"
-                inputProps={{ min: 0, max: 100 }}
-                defaultValue={80}
-              />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Today&apos;s 1300 Wind Speed</TableCell>
-            <TableCell>
-              <TextField
-                type="number"
-                inputMode="numeric"
-                size="small"
-                variant="outlined"
-                inputProps={{ min: 0, max: 100 }}
-                defaultValue={8}
-              />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Today&apos;s Precipitation</TableCell>
-            <TableCell>
-              <TextField
-                type="number"
-                inputMode="numeric"
-                size="small"
-                variant="outlined"
-                inputProps={{ min: 0, max: 100 }}
-                defaultValue={75}
-              />
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    /** Input table */
+    <Grid container direction={'row'} spacing={2}>
+      <Grid item>
+        <TableContainer component={Paper}>
+          <Table aria-label="Basic FWI Calculation Inputs">
+            <TableHead>
+              <TableRow>
+                <TableCell>Station Name</TableCell>
+                <TableCell>
+                  <FWIStationCell />
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Yesterday&apos;s FFMC</TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    inputMode="numeric"
+                    size="small"
+                    variant="outlined"
+                    inputProps={{ min: 0, max: 100 }}
+                    defaultValue={20}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Yesterday&apos;s DMC</TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    inputMode="numeric"
+                    size="small"
+                    variant="outlined"
+                    inputProps={{ min: 0, max: 100 }}
+                    defaultValue={11}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Yesterday&apos;s DC</TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    inputMode="numeric"
+                    size="small"
+                    variant="outlined"
+                    inputProps={{ min: 0, max: 100 }}
+                    defaultValue={5}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s 1300 Temperature</TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    inputMode="numeric"
+                    size="small"
+                    variant="outlined"
+                    inputProps={{ min: 0, max: 100 }}
+                    defaultValue={15}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s 1300 Relative Humidity</TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    inputMode="numeric"
+                    size="small"
+                    variant="outlined"
+                    inputProps={{ min: 0, max: 100 }}
+                    defaultValue={80}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s 1300 Wind Speed</TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    inputMode="numeric"
+                    size="small"
+                    variant="outlined"
+                    inputProps={{ min: 0, max: 100 }}
+                    defaultValue={8}
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s Precipitation</TableCell>
+                <TableCell>
+                  <TextField
+                    type="number"
+                    inputMode="numeric"
+                    size="small"
+                    variant="outlined"
+                    inputProps={{ min: 0, max: 100 }}
+                    defaultValue={75}
+                  />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      <Grid item>
+        <TableContainer component={Paper}>
+          {/** Output table */}
+          <Table aria-label="Basic FWI Calculation Inputs">
+            <TableBody>
+              <TableRow>
+                <TableCell>Today&apos;s Standard FFMC</TableCell>
+                <TableCell>100</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s Standard DMC</TableCell>
+                <TableCell>5</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s Standard DC</TableCell>
+                <TableCell>5</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s Standard ISI</TableCell>
+                <TableCell>5</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s Standard BUI</TableCell>
+                <TableCell>5</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Today&apos;s Standard FWI</TableCell>
+                <TableCell>5</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   )
 }
