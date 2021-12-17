4688f31cc7f450b1cfc2d844f9875468e78407ab
Conor Brady <con.brad@gmail.com>
2021-12-16 16:53:45 -0800

    DatePicker spacing

--- web/src/features/fwiCalculator/FWICalculatorPage.tsx
+++ web/src/features/fwiCalculator/FWICalculatorPage.tsx
@@ -3,7 +3,6 @@ import { GeneralHeader, Container } from 'components'
 import React, { useState } from 'react'
 import DatePicker from 'components/DatePicker'
 import { DateTime } from 'luxon'
-import { formControlStyles, theme } from 'app/theme'
 import { PST_UTC_OFFSET } from 'utils/constants'
 import { pstFormatter } from 'utils/date'
 import { getStationCodesFromUrl } from 'utils/url'
@@ -11,21 +10,8 @@ import { useHistory, useLocation } from 'react-router-dom'
 import { BasicFWI } from 'features/fwiCalculator/components/BasicFWI'
 
 const useStyles = makeStyles(() => ({
-  ...formControlStyles,
-  listContainer: {
-    width: 700,
-    height: 700
-  },
-  mapContainer: {
-    width: 900,
-    height: 700
-  },
-  fireCenter: {
-    minWidth: 280,
-    margin: theme.spacing(1)
-  },
-  instructions: {
-    textAlign: 'left'
+  date: {
+    paddingBottom: 6
   }
 }))
 
@@ -37,13 +23,6 @@ export const FWICalculatorPage: React.FunctionComponent = () => {
 
   const codesFromQuery = getStationCodesFromUrl(location.search)
   const [stationCodes, setStationCodes] = useState<number[]>(codesFromQuery)
-
-  const emptyInstructions = (
-    <div data-testid={'fba-instructions'} className={classes.instructions}>
-      <p>Select a date to get started.</p>
-      <p>A selected date will populate this pane with its station details.</p>
-    </div>
-  )
 
   const [dateOfInterest, setDateOfInterest] = useState(
     pstFormatter(DateTime.now().setZone(`UTC${PST_UTC_OFFSET}`))
@@ -65,12 +44,12 @@ export const FWICalculatorPage: React.FunctionComponent = () => {
       <Container maxWidth={'xl'}>
         <h1>
           {/* (🔥🦇) */}
-          Fire Behaviour Advisory Tool
+          FWI Calculator
         </h1>
         <Grid container direction={'row'}>
           <Grid container spacing={2}>
             <Grid item>
-              <FormControl className={classes.formControl}>
+              <FormControl className={classes.date}>
                 <DatePicker date={dateOfInterest} updateDate={updateDate} />
               </FormControl>
             </Grid>
