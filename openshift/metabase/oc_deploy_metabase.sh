ce64b1a040555254d49c815f76ddf28909fbac09
Andrea Williams <andrea.williams@gov.bc.ca>
2021-12-21 15:13:39 -0800

    Bash scripts for implementing Metabase in openshift

---
+++ openshift/metabase/oc_deploy_metabase.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+#
+# Script based on instruction found in readme of
+# https://github.com/bcgov/nr-showcase-devops-tools/tree/master/tools/metabase
+
+export BASE_URL="https://raw.githubusercontent.com/bcgov/nr-showcase-devops-tools/master/tools/metabase/openshift"
+export ADMIN_EMAIL=Andrea.Williams@gov.bc.ca
+export NAMESPACE=e1e498-dev
+export PREFIX="app-pr-1625-"
+
+oc process -n $NAMESPACE -f $BASE_URL/metabase.secret.yaml -p ADMIN_EMAIL=$ADMIN_EMAIL -o yaml | oc create -n $NAMESPACE -f -
+
+oc process -n $NAMESPACE -f $BASE_URL/metabase.dc.yaml -p NAMESPACE=$NAMESPACE -p PREFIX=$PREFIX -o yaml | oc apply -n $NAMESPACE -f -
