1be72ad1f42741d611d9fa0771df31df2fe7cc5c
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-01-10 08:34:12 -0800

    busy creating test data

--- api/scripts/make_test_files.py
+++ api/scripts/make_test_files.py
@@ -3,6 +3,8 @@ from rpy2.robjects import DataFrame
 from rpy2.robjects.packages import importr
 import rpy2.robjects.conversion as cv
 from rpy2.rinterface import NULL
+import json
+import os
 
 
 def _none2null(_):
@@ -17,26 +19,38 @@ none_converter.py2rpy.register(type(None), _none2null)
 def main():
     cffdrs = importr("cffdrs")
 
-    dataFrame = DataFrame(
-        {
-            "FUELTYPE": "C1",
-            "LAT": 80.0,
-            "LONG": 100.0,
-            "ELV": 100.0,
-            "DJ": 10,
-            "FFMC": 10.0,
-            "BUI": 10.0,
-            "WS": 10.0,
-            "WD": 10.0,
-            "GS": 10.0,
-            "ACCEL": 1,
-            "ASPECT": 10.0,
-            "BUIEFF": 1,
-            "MINUTES": 60.0,
-        }
-    )
-    result = cffdrs._FBPcalc(dataFrame, "Primary")
-    print("result: {}".format(result))
+    base_folder = '../../../fire_behaviour_app/test/resources/'
+
+    with open(os.path.join(base_folder, 'FBCCalc_input.json'), 'r') as f:
+        data = json.load(f)
+
+    results = []
+
+    for item in data:
+        dataFrame = DataFrame(item)
+        prediction = cffdrs._FBPcalc(dataFrame, "All")
+        result = {}
+
+        for i in range(len(prediction)):
+            result[prediction.colnames[i]] = prediction[i][0]
+
+        results.append(result)
+
+    with open(os.path.join(base_folder, 'FBCCalc_output.json'), 'w') as f:
+        json.dump(results, f, indent=4)
+
+    # print(f'{prediction.colnames[i]}:{prediction[i][0]}')
+
+    # print(f'moo: {result["ID"]}')
+
+    # print('for a in result')
+    # for row in result:
+    #     print(row)
+    # for i in range(result.ncol):
+    # print(result[i])
+    # for name in result.colnames:
+    # print(name)
+    # print("result: {}".format(result))
 
 
 if __name__ == "__main__":
