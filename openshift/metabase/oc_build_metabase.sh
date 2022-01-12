2a811e69a9bdca44fddb5169078de3364a992eae
Andrea Williams <andrea.williams@gov.bc.ca>
2022-01-12 11:36:06 -0800

    Code review feedback

--- openshift/metabase/oc_build_metabase.sh
+++ openshift/metabase/oc_build_metabase.sh
@@ -3,7 +3,9 @@
 # Script based on instruction found in readme of
 # https://github.com/bcgov/nr-showcase-devops-tools/tree/master/tools/metabase
 
+# TODO: download metabase.bc.yaml from url, copy into our repo
 export BASE_URL="https://raw.githubusercontent.com/bcgov/nr-showcase-devops-tools/master/tools/metabase/openshift"
+# TODO: parameterize namespace
 export NAMESPACE="e1e498-dev"
 export METABASE_VERSION=v0.41.5
 
--- openshift/metabase/oc_network_policy_metabase.sh
+++ openshift/metabase/oc_network_policy_metabase.sh
@@ -3,7 +3,9 @@
 # Script based on instruction found in readme of
 # https://github.com/bcgov/nr-showcase-devops-tools/tree/master/tools/metabase
 
+# TODO: copy metabase.np.yaml file from source and save into our repo
 export BASE_URL="https://raw.githubusercontent.com/bcgov/nr-showcase-devops-tools/master/tools/metabase/openshift"
+# TODO: parameterize namespace & ns_prefix & ns_env
 export NAMESPACE=e1e498-prod
 export NS_PREFIX=e1e498
 export NS_ENV=dev
