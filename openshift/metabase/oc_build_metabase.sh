b4423b59133ad94dcd792d9f5ebc1e20a1f02a08
Andrea Williams <andrea.williams@gov.bc.ca>
2022-01-13 11:40:33 -0800

    Updated for prod

--- openshift/metabase/oc_build_metabase.sh
+++ openshift/metabase/oc_build_metabase.sh
@@ -6,7 +6,7 @@
 # TODO: download metabase.bc.yaml from url, copy into our repo
 export BASE_URL="https://raw.githubusercontent.com/bcgov/nr-showcase-devops-tools/master/tools/metabase/openshift"
 # TODO: parameterize namespace
-export NAMESPACE="e1e498-dev"
+export NAMESPACE="e1e498-prod"
 export METABASE_VERSION=v0.41.5
 
 oc process -n $NAMESPACE -f $BASE_URL/metabase.bc.yaml -p METABASE_VERSION=$METABASE_VERSION -o yaml | oc apply -n $NAMESPACE -f -
