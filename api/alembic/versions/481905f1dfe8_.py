37fe08923344af3d2eda28c7cb1a1eeafe3e662a
Andrea Williams <andrea.williams@gov.bc.ca>
2022-01-17 14:04:26 -0800

    Alembic merge heads

---
+++ api/alembic/versions/481905f1dfe8_.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: 481905f1dfe8
+Revises: 8efe0e7b9712, 39806f02cdec
+Create Date: 2022-01-17 12:40:01.613726
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '481905f1dfe8'
+down_revision = ('8efe0e7b9712', '39806f02cdec')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
