536cd7fde096c12bb3da53ec4cda3b93a0481c34
Conor Brady <con.brad@gmail.com>
2022-01-24 14:37:09 -0800

    Re-enable slice logic tests

--- web/src/features/hfiCalculator/slices/hfiCalculatorSlice.test.ts
+++ web/src/features/hfiCalculator/slices/hfiCalculatorSlice.test.ts
@@ -5,18 +5,18 @@ import {
 } from 'features/hfiCalculator/slices/hfiCalculatorSlice'
 import { buildStationDaily } from 'features/hfiCalculator/components/testHelpers'
 
-describe('calculateMeanIntensityGroup', () => {
-  xit('should return the average of all intensity groups within the list of StationDailies', () => {
+describe('calculateMeanIntensity functions', () => {
+  it('should return the average of all intensity groups within the list of StationDailies', () => {
     const stationDailies: StationDaily[] = [
       buildStationDaily(1, 2),
       buildStationDaily(2, 4)
     ]
     expect(calculateMeanIntensity(stationDailies)).toEqual(3)
   })
-  xit('should return undefined if there are no intensity groups to calculate in the selected dailies', () => {
+  it('should return undefined if there are no intensity groups to calculate in the selected dailies', () => {
     expect(calculateMeanIntensity([])).toEqual(undefined)
   })
-  xit('should return undefined if there is no mean intensity group to calculate', () => {
-    expect(calculateDailyMeanIntensities([], 1)).toEqual(undefined)
+  it('should return undefined if there is no mean intensity group to calculate', () => {
+    expect(calculateDailyMeanIntensities([], 1)).toEqual([undefined])
   })
 })
