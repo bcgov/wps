6c6a7734f400d97faf355e06892f0d1c7a69397f
Conor Brady <con.brad@gmail.com>
2022-01-28 15:31:04 -0800

    FireStartsDropdown tests

---
+++ web/src/features/hfiCalculator/components/fireStartsDropdown.test.tsx
@@ -0,0 +1,52 @@
+import { render, fireEvent, within, waitFor } from '@testing-library/react'
+import FireStartsDropdown from 'features/hfiCalculator/components/FireStartsDropdown'
+import { lowestFireStarts } from 'features/hfiCalculator/slices/hfiCalculatorSlice'
+import React from 'react'
+describe('FireStartsDropdown', () => {
+  const testAreaName = 'testArea'
+  const dayOffset = 0
+  it('should render with the default value', async () => {
+    const setFireStartsMock = jest.fn()
+    const { getByTestId } = render(
+      <FireStartsDropdown
+        fireStarts={lowestFireStarts}
+        areaName={testAreaName}
+        dayOffset={dayOffset}
+        setFireStarts={setFireStartsMock}
+      />
+    )
+    const autocomplete = getByTestId('fire-starts-dropdown')
+    const input = within(autocomplete).getByRole('textbox') as HTMLInputElement
+
+    await waitFor(() => expect(input.value).toBe(lowestFireStarts.label))
+    await waitFor(() => expect(setFireStartsMock).toBeCalledTimes(0))
+  })
+  it('should change value on change and call parent callback', async () => {
+    const setFireStartsMock = jest.fn()
+    const { getByTestId } = render(
+      <FireStartsDropdown
+        fireStarts={lowestFireStarts}
+        areaName={testAreaName}
+        dayOffset={dayOffset}
+        setFireStarts={setFireStartsMock}
+      />
+    )
+    const autocomplete = getByTestId('fire-starts-dropdown')
+    const input = within(autocomplete).getByRole('textbox') as HTMLInputElement
+
+    autocomplete.focus()
+    // assign value to input field
+    fireEvent.change(input, { target: { value: '6+' } })
+    fireEvent.keyDown(autocomplete, { key: 'ArrowDown' })
+    fireEvent.keyDown(autocomplete, { key: 'Enter' })
+
+    await waitFor(() => expect(input.value).toBe('6+'))
+    await waitFor(() => expect(setFireStartsMock).toBeCalledTimes(1))
+    await waitFor(() =>
+      expect(setFireStartsMock).toBeCalledWith(testAreaName, dayOffset, {
+        label: '6+',
+        value: 7
+      })
+    )
+  })
+})
