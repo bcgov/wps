0a2df821156994a7d4b04c38b5d59e5d3742341b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-02-03 12:40:48 -0800

    grr

--- api/alembic/versions/871c39cf6c26_fuel_type_abbreviation_should_be_unique.py
+++ api/alembic/versions/871c39cf6c26_fuel_type_abbreviation_should_be_unique.py
@@ -31,3 +31,4 @@ def downgrade():
     # op.drop_constraint('unique_station_code_for_planning_area',
     #    'planning_weather_stations', type_='unique')
     # op.drop_index(op.f('ix_fuel_types_abbrev'), table_name='fuel_types')
+    pass
