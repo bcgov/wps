96f6598acd4e7f373d2d7a30c27193c79de92026
Conor Brady <con.brad@gmail.com>
2022-02-03 15:32:58 -0800

    Adds HFI Error Banner (#1690)
    * Adds error banner
    
    * Add test for email link

---
+++ web/src/features/hfiCalculator/components/hfiErrorAlert.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react'
+import HFIErrorAlert from 'features/hfiCalculator/components/HFIErrorAlert'
+import React from 'react'
+
+describe('HFIErrorAlert', () => {
+  it('should render an alert', () => {
+    const { getByRole } = render(
+      <HFIErrorAlert
+        hfiDailiesError={'500 - no dailies'}
+        fireCentresError={'500 - no fire centres'}
+      />
+    )
+    const alertLink = getByRole('link')
+    expect(alertLink).toBeDefined()
+    expect(alertLink).toHaveAttribute(
+      'href',
+      'mailto:bcws.predictiveservices@gov.bc.ca?subject=Predictive Services Unit - HFI Error'
+    )
+  })
+})
