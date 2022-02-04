e39038256d9e4ce1f7464e87548f5f90969ed5f1
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-02-03 16:00:19 -0800

    fix c7b migration

--- api/alembic/versions/839f18e0ecc4_insert_c7b.py
+++ api/alembic/versions/839f18e0ecc4_insert_c7b.py
@@ -29,20 +29,35 @@ def get_fuel_type_id(fuel_type):
     return result[0][0]
 
 
+def get_planning_areas(fire_centre_name):
+    conn = op.get_bind()
+    cursor = conn.execute(
+        "SELECT planning_areas.id FROM planning_areas "
+        "INNER JOIN fire_centres on fire_centres.id = planning_areas.fire_centre_id "
+        f"WHERE fire_centres.name = '{fire_centre_name}'")
+    result = cursor.fetchall()
+    return ','.join(str(x[0]) for x in result)
+
+
 def upgrade():
     # INSERT C7B fuel type.
     op.execute(
-        f"INSERT INTO fuel_types (abbrev, description) values ('{c7b}', 'Proposed C7 model with curing: {c7b}')")
+        f"INSERT INTO fuel_types (abbrev, description) values ('{c7b}', "
+        f"'Proposed C7 model with curing: {c7b}')")
     # Point stations to C7B
     fuel_type_id = get_fuel_type_id(c7b)
-    command = f'UPDATE planning_weather_stations SET fuel_type_id = {fuel_type_id} WHERE station_code IN ({station_code_list_string})'
+    planning_area_ids = get_planning_areas('Kamloops Fire Centre')
+    command = (f'UPDATE planning_weather_stations SET fuel_type_id = {fuel_type_id} WHERE '
+               f'station_code IN ({station_code_list_string}) AND planning_area_id IN ({planning_area_ids})')
     op.execute(command)
 
 
 def downgrade():
     # Point stations back to C7
-    fuel_type_id = get_fuel_type_id('C7')
-    command = f'UPDATE planning_weather_stations SET fuel_type_id = {fuel_type_id} WHERE station_code IN ({station_code_list_string})'
+    c7_fuel_type_id = get_fuel_type_id('C7')
+    c7b_fuel_type_id = get_fuel_type_id(c7b)
+    command = (f'UPDATE planning_weather_stations SET fuel_type_id = {c7_fuel_type_id} '
+               f'WHERE fuel_type_id = {c7b_fuel_type_id}')
     op.execute(command)
     # DELETE C7B fuel type.
     op.execute(f"DELETE FROM fuel_types WHERE abbrev = '{c7b}'")
