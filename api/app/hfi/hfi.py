1e84665a0b33b514f6ac64bff0289cc6bcc0bd80
Conor Brady <con.brad@gmail.com>
2022-02-07 15:17:48 -0800

    Initial untested implementation

---
+++ api/app/hfi/hfi.py
@@ -0,0 +1,82 @@
+""" HFI calculation logic """
+
+from typing import Mapping, Optional, List
+from app.schemas.hfi_calc import DailyResult, FireCentre, PlanningAreaResult, ValidatedStationDaily
+from app.schemas.hfi_calc import FireStarts, StationDaily
+
+
+def calculate_hfi_results(fire_centre: Optional[FireCentre],
+                          planning_area_fire_starts: Mapping[str, FireStarts],
+                          dailies: List[StationDaily],
+                          num_prep_days: int,
+                          selected_station_codes: List[int]):
+    """ Computes HFI results based on parameter inputs """
+    planning_area_to_dailies: Mapping[str, PlanningAreaResult] = dict()
+    if fire_centre is None:
+        return planning_area_to_dailies
+
+    for area in fire_centre.planning_areas:
+        area_station_codes = map(lambda station: (station.code), area.stations)
+        area_dailies: List[StationDaily] = list(filter(lambda daily: (daily.code in area_station_codes and daily.code in selected_station_codes),
+                                                       dailies)).sort(key=lambda daily: daily.date)
+        # Initialize with defaults if empty
+        planning_area_fire_starts[area.name] = [0 for i in range(5)] \
+            if planning_area_fire_starts[area.name] is None \
+            else planning_area_fire_starts[area.name]
+
+        dailies_in_prep = [daily for _, daily in zip(range(num_prep_days), area_dailies)]
+
+        daily_results: List[DailyResult] = []
+        all_dailies_valid: bool = True
+        for index, prep_day_dailies in enumerate(dailies_in_prep):
+            daily_fire_starts: FireStarts = planning_area_fire_starts[area.name][index]
+            mean_intensity_group = calculate_mean_intensity(prep_day_dailies)
+            prep_level = calculate_prep_level(mean_intensity_group, daily_fire_starts)
+            validatedDailies: List[ValidatedStationDaily] = list(map(
+                lambda daily: (validate_station_daily(daily), prep_day_dailies)))
+            all_dailies_valid = all(map(lambda validated_daily: (validated_daily.valid), validatedDailies))
+            daily_result: DailyResult = DailyResult(
+                dateISO=prep_day_dailies[0].date,
+                dailies=validatedDailies,
+                fire_starts=daily_fire_starts,
+                mean_intensity_group=mean_intensity_group,
+                prep_level=prep_level)
+            daily_results.append(daily_result)
+
+        highest_daily_intensity_group = calculate_max_intensity_group(
+            list(map(lambda daily_result: (daily_result.mean_intensity_group, daily_results))))
+
+        mean_prep_level = calculate_mean_prep_level(
+            list(map(lambda daily_result: (daily_result.prep_level, daily_results))))
+
+        planning_area_to_dailies[area.name] = PlanningAreaResult(
+            all_dailies_valid=all_dailies_valid,
+            highest_daily_intensity_group=highest_daily_intensity_group,
+            mean_prep_level=mean_prep_level,
+            daily_results=daily_results)
+
+    return planning_area_to_dailies
+
+
+def calculate_max_intensity_group(mean_intensity_groups: List[Optional[float]]):
+    return max(mean_intensity_groups)
+
+
+def calculate_mean_prep_level(prep_levels: List[Optional[float]]):
+    return 0
+
+
+def calculate_mean_intensity(dailies: List[StationDaily]):
+    return 0
+
+
+def calculate_prep_level(mean_intensity_group: Optional[float], fire_starts: FireStarts):
+    if mean_intensity_group is None:
+        return None
+
+    rounded_mig = round(mean_intensity_group)
+    return fire_starts.lookup_table[rounded_mig]
+
+
+def validate_station_daily(daily: StationDaily):
+    return daily
--- api/app/schemas/hfi_calc.py
+++ api/app/schemas/hfi_calc.py
@@ -1,6 +1,6 @@
 """ This module contains pydandict schemas the HFI Calculator.
 """
-from typing import List, Optional
+from typing import List, Mapping, Optional
 from datetime import datetime
 from pydantic import BaseModel
 from app.schemas.shared import FuelType
@@ -38,6 +38,94 @@ class StationDaily(BaseModel):
 class StationDailyResponse(BaseModel):
     """ Response that includes list of station daily data."""
     dailies: List[StationDaily]
+
+
+class ValidatedStationDaily(BaseModel):
+    """ 
+    Station daily metrics and indices with a validity flag
+    """
+    code: Optional[int] = None
+    status: Optional[str] = None
+    temperature: Optional[float] = None
+    relative_humidity: Optional[float] = None
+    wind_speed: Optional[float] = None
+    wind_direction: Optional[float] = None
+    grass_cure_percentage: Optional[float] = None
+    precipitation: Optional[float] = None
+    ffmc: Optional[float] = None
+    dmc: Optional[float] = None
+    dc: Optional[float] = None
+    isi: Optional[float] = None
+    bui: Optional[float] = None
+    fwi: Optional[float] = None
+    danger_class: Optional[int] = None
+    observation_valid: Optional[bool] = None
+    observation_valid_comment: Optional[str] = None
+    rate_of_spread: Optional[float] = None
+    hfi: Optional[float] = None
+    intensity_group: Optional[int] = None
+    sixty_minute_fire_size: Optional[float] = None
+    fire_type: Optional[str] = None
+    error: Optional[bool] = False
+    error_message: Optional[str] = None
+    date: Optional[datetime] = None
+    valid: bool
+
+
+class FireStarts(BaseModel):
+    """ 
+    User facing label, value and lookup table of fire starts to prep level
+    """
+    label: str
+    value: int
+    lookup_table: Mapping[int, int]
+
+
+class DailyResult(BaseModel):
+    """ 
+    Prep level, MIG, fire starts and station daily results for a day in a prep week
+    """
+    dateISO: str
+    dailies: List[ValidatedStationDaily]
+    fire_starts: FireStarts
+    mean_intensity_group: Optional[float]
+    prep_level: Optional[float]
+
+
+class PlanningAreaResult(BaseModel):
+    """ 
+    Mean prep level / max intensity group, 
+    dailies and validity status of station dailies in a planning area
+    """
+    all_dailies_valid: bool
+    highest_daily_intensity_group: float
+    mean_prep_level: Optional[float]
+    daily_results: List[DailyResult]
+
+
+class HFIResultResponse(BaseModel):
+    """ 
+    Response that contains daily data, num prep days, selected station codes,
+    selected fire centre, fire starts, HFI results
+    """
+    num_prep_days: int
+    selected_prep_date: datetime
+    start_time_stamp: Optional[int]
+    end_time_stamp: Optional[int]
+    selected_station_codes: List[int]
+    selected_fire_center: Optional[str]
+    planning_area_hfi_results: Mapping[str, PlanningAreaResult]
+    planning_area_fire_starts: Mapping[str, List[FireStarts]]
+
+
+class HFIResultRequest(BaseModel):
+    num_prep_days: int
+    selected_prep_date: datetime
+    start_time_stamp: Optional[int]
+    end_time_stamp: Optional[int]
+    selected_station_codes: List[int]
+    selected_fire_center: Optional[str]
+    planning_area_fire_starts: Mapping[str, List[FireStarts]]
 
 
 class WeatherStationProperties(BaseModel):
