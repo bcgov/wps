2b2380292d2e20dcd106047f1a1932a64c8ab923
Conor Brady <con.brad@gmail.com>
2022-02-08 15:45:44 -0800

    Fix a bunch of runtime bugs

--- api/app/hfi/hfi.py
+++ api/app/hfi/hfi.py
@@ -1,5 +1,8 @@
 """ HFI calculation logic """
 
+from itertools import groupby
+from operator import attrgetter
+from statistics import mean
 from typing import Mapping, Optional, List
 from app.schemas.hfi_calc import DailyResult, FireCentre, FireStartRange, PlanningAreaResult, ValidatedStationDaily, lowest_fire_starts
 from app.schemas.hfi_calc import FireStartRange, StationDaily
@@ -17,13 +20,17 @@ def calculate_hfi_results(fire_centre: Optional[FireCentre],
 
     for area in fire_centre.planning_areas:
         area_station_codes = map(lambda station: (station.code), area.stations)
-        area_dailies: List[StationDaily] = list(filter(lambda daily: (daily.code in area_station_codes and daily.code in selected_station_codes),
-                                                       dailies))
+        area_dailies: List[StationDaily] = sorted(
+            list(filter(lambda daily: (daily.code in area_station_codes and daily.code in selected_station_codes),
+                        dailies)),
+            key=attrgetter('date'))
+
+        area_dailies_by_date = [list(g) for _, g in groupby(area_dailies, lambda area_daily: area_daily.date)]
         # Initialize with defaults if empty
         if area.name not in planning_area_fire_starts:
             planning_area_fire_starts[area.name] = [lowest_fire_starts for _ in range(num_prep_days)]
 
-        dailies_in_prep = area_dailies[:num_prep_days]
+        dailies_in_prep = area_dailies_by_date[:num_prep_days]
 
         daily_results: List[DailyResult] = []
         all_dailies_valid: bool = True
@@ -31,12 +38,12 @@ def calculate_hfi_results(fire_centre: Optional[FireCentre],
             daily_fire_starts: FireStartRange = planning_area_fire_starts[area.name][index]
             mean_intensity_group = calculate_mean_intensity(prep_day_dailies)
             prep_level = calculate_prep_level(mean_intensity_group, daily_fire_starts)
-            validatedDailies: List[ValidatedStationDaily] = list(map(
-                lambda daily: (validate_station_daily(daily), prep_day_dailies)))
-            all_dailies_valid = all(map(lambda validated_daily: (validated_daily.valid), validatedDailies))
+            validated_dailies: List[ValidatedStationDaily] = list(
+                map(validate_station_daily, prep_day_dailies))
+            all_dailies_valid = all(map(lambda validated_daily: (validated_daily.valid), validated_dailies))
             daily_result: DailyResult = DailyResult(
-                dateISO=prep_day_dailies[0].date,
-                dailies=validatedDailies,
+                dateISO=prep_day_dailies[0].date.isoformat(),
+                dailies=validated_dailies,
                 fire_starts=daily_fire_starts,
                 mean_intensity_group=mean_intensity_group,
                 prep_level=prep_level)
@@ -62,7 +69,11 @@ def calculate_max_intensity_group(mean_intensity_groups: List[Optional[float]]):
 
 
 def calculate_mean_prep_level(prep_levels: List[Optional[float]]):
-    return 0
+    valid_prep_levels = list(filter(None, prep_levels))
+    num_prep_levels = len(valid_prep_levels)
+    if num_prep_levels == 0:
+        return None
+    return mean(valid_prep_levels)
 
 
 def calculate_mean_intensity(dailies: List[StationDaily]):
@@ -74,8 +85,36 @@ def calculate_prep_level(mean_intensity_group: Optional[float], fire_starts: Fir
         return None
 
     rounded_mig = round(mean_intensity_group)
+    if rounded_mig == 0:
+        return None
     return fire_starts.lookup_table[rounded_mig]
 
 
 def validate_station_daily(daily: StationDaily):
-    return daily
+    return ValidatedStationDaily(
+        code=daily.code,
+        status=daily.status,
+        temperature=daily.temperature,
+        relative_humidity=daily.relative_humidity,
+        wind_speed=daily.wind_speed,
+        wind_direction=daily.wind_direction,
+        grass_cure_percentage=daily.grass_cure_percentage,
+        precipitation=daily.precipitation,
+        ffmc=daily.ffmc,
+        dmc=daily.dmc,
+        dc=daily.dc,
+        isi=daily.isi,
+        bui=daily.bui,
+        fwi=daily.fwi,
+        danger_class=daily.danger_class,
+        observation_valid=daily.observation_valid,
+        observation_valid_comment=daily.observation_valid_comment,
+        rate_of_spread=daily.rate_of_spread,
+        hfi=daily.hfi,
+        intensity_group=daily.intensity_group,
+        sixty_minute_fire_size=daily.sixty_minute_fire_size,
+        fire_type=daily.fire_type,
+        error=daily.error,
+        error_message=daily.error_message,
+        date=daily.date,
+        valid=True)
