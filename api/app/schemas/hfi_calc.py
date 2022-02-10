96edf0d7f7bcdea48ca4fb4adf7b2dd8a7c694a2
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-02-10 15:57:46 -0800

    implementing reading request from database

--- api/app/schemas/hfi_calc.py
+++ api/app/schemas/hfi_calc.py
@@ -1,6 +1,7 @@
 """ This module contains pydandict schemas the HFI Calculator.
 """
 from enum import Enum
+import math
 from typing import List, Mapping, Optional
 from datetime import datetime
 from pydantic import BaseModel
@@ -81,13 +82,23 @@ class FireStartRange(BaseModel):
     label: str
     value: int
     lookup_table: Mapping[int, int]
+    fire_starts_min: int
+    fire_starts_max: int
 
 
-lowest_fire_starts = FireStartRange(label='0-1', value=1, lookup_table={1: 1, 2: 1, 3: 2, 4: 3, 5: 4})
-one_2_two_starts = FireStartRange(label='1-2', value=2, lookup_table={1: 1, 2: 2, 3: 3, 4: 4, 5: 5})
-two_2_three_starts = FireStartRange(label='2-3', value=3, lookup_table={1: 2, 2: 3, 3: 4, 4: 5, 5: 6})
-three_2_six_starts = FireStartRange(label='3-6', value=6, lookup_table={1: 3, 2: 4, 3: 5, 4: 6, 5: 6})
-highest_fire_starts = FireStartRange(label='6+', value=7, lookup_table={1: 4, 2: 5, 3: 6, 4: 6, 5: 6})
+lowest_fire_starts = FireStartRange(
+    label='0-1', value=1, lookup_table={1: 1, 2: 1, 3: 2, 4: 3, 5: 4}, fire_starts_min=0, fire_starts_max=1)
+one_2_two_starts = FireStartRange(
+    label='1-2', value=2, lookup_table={1: 1, 2: 2, 3: 3, 4: 4, 5: 5}, fire_starts_min=1, fire_starts_max=2)
+two_2_three_starts = FireStartRange(
+    label='2-3', value=3, lookup_table={1: 2, 2: 3, 3: 4, 4: 5, 5: 6}, fire_starts_min=2, fire_starts_max=3)
+three_2_six_starts = FireStartRange(
+    label='3-6', value=6, lookup_table={1: 3, 2: 4, 3: 5, 4: 6, 5: 6}, fire_starts_min=3, fire_starts_max=6)
+highest_fire_starts = FireStartRange(
+    label='6+', value=7, lookup_table={1: 4, 2: 5, 3: 6, 4: 6, 5: 6}, fire_starts_min=6, fire_starts_max=math.inf)
+
+fire_start_ranges = [lowest_fire_starts, one_2_two_starts,
+                     two_2_three_starts, three_2_six_starts, highest_fire_starts]
 
 
 class DailyResult(BaseModel):
