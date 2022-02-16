6d0db2875e68f438750ee8a43e2054f9524e10bf
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-02-16 15:42:36 -0800

    removing duplicate code;

--- api/app/tests/hfi/test_hfi_endpoint_request_not_stored.py
+++ api/app/tests/hfi/test_hfi_endpoint_request_not_stored.py
@@ -1,6 +1,5 @@
 
 from typing import Tuple
-import json
 import pytest
 from pytest_bdd import scenario, given, then
 from fastapi.testclient import TestClient
@@ -54,18 +53,3 @@ def given_request_none_stored(monkeypatch: pytest.MonkeyPatch, mocker: MockFixtu
 def then_request_saved(result, request_saved: bool):
     """ Check request saved """
     assert result['saved'] == request_saved
-
-
-@then("the response status code is <status_code>")
-def then_status(result, status_code: int):
-    """ Check response status code """
-    assert result['response'].status_code == status_code, result['filename']
-
-
-@then("the response is <response_json>")
-def then_response(result, response_json: dict):
-    """ Check entire response """
-    if response_json is not None:
-        print('actual:\n{}'.format(json.dumps(result['response'].json(), indent=4)))
-        print('expected:\n{}'.format(json.dumps(response_json, indent=4)))
-        assert result['response'].json() == response_json, result['filename']
--- api/app/tests/hfi/test_hfi_endpoint_request_stored.py
+++ api/app/tests/hfi/test_hfi_endpoint_request_stored.py
@@ -1,6 +1,4 @@
-
 from typing import Tuple, List
-import json
 import pytest
 from pytest_bdd import scenario, given, then
 from fastapi.testclient import TestClient
@@ -26,29 +24,6 @@ def str_to_bool(input: str):
 def test_fire_behaviour_calculator_scenario_request_stored():
     """ BDD Scenario. """
     pass
-
-
-@given("I received a <request_json>, but don't have one stored", target_fixture='result')
-def given_request_none_stored(monkeypatch: pytest.MonkeyPatch, mocker: MockFixture, request_json: Tuple[dict, str]):
-    """ Handle request
-    """
-    # mock anything that uses aiohttp.ClientSession::get
-    monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
-
-    # mock out database calls:
-    mock_station_crud(monkeypatch)
-    mock_planning_area_crud(monkeypatch)
-
-    store_spy = mocker.spy(app.routers.hfi_calc, 'store_hfi_request')
-
-    client = TestClient(app.main.app)
-    headers = {'Content-Type': 'application/json',
-               'Authorization': 'Bearer token'}
-    return {
-        'response': client.post('/api/hfi-calc/', headers=headers, json=request_json[0]),
-        'filename': request_json[1],
-        'saved': store_spy.call_count == 1
-    }
 
 
 @given("I received a <request_json>, and have one stored <stored_request_json>", target_fixture='result')
@@ -77,24 +52,3 @@ def given_request_have_one_stored(monkeypatch,
         'response': client.post('/api/hfi-calc/', headers=headers, json=request_json[0]),
         'filename': request_json[1]
     }
-
-
-@then("request == saved = <request_saved>")
-def then_request_saved(result, request_saved: bool):
-    """ Check request saved """
-    assert result['saved'] == request_saved
-
-
-@then("the response status code is <status_code>")
-def then_status(result, status_code: int):
-    """ Check response status code """
-    assert result['response'].status_code == status_code, result['filename']
-
-
-@then("the response is <response_json>")
-def then_response(result, response_json: dict):
-    """ Check entire response """
-    if response_json is not None:
-        print('actual:\n{}'.format(json.dumps(result['response'].json(), indent=4)))
-        print('expected:\n{}'.format(json.dumps(response_json, indent=4)))
-        assert result['response'].json() == response_json, result['filename']
