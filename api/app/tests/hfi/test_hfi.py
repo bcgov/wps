13f75cb685ac5d5632f7df5b322c8195678af225
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-02-25 17:12:37 -0800

    updating tests

--- api/app/tests/hfi/test_hfi.py
+++ api/app/tests/hfi/test_hfi.py
@@ -78,35 +78,33 @@ def test_no_dailies_handled():
 
 def test_requested_fire_starts_unaltered(mocker: MockerFixture):
     """ Fire starts from user request remain unchanged """
-    def mock_get_planning_areas(session, fire_centre_id):
-        """ Returns mocked PlanningAreas. """
-        return [
-            hfi_calc_models.PlanningArea(id=1, fire_centre_id=1, name='Area 1',
-                                         order_of_appearance_in_list=1),
-            hfi_calc_models.PlanningArea(id=2, fire_centre_id=1, name='Area 2',
-                                         order_of_appearance_in_list=2)]
+    # def mock_get_fire_centre_stations(session, fire_centre_id):
+    #     """ Returns mocked stations per PlanningAreas """
+    #     return [(hfi_calc_models.PlanningWeatherStation(id=1, planning_area_id=1, station_code=1),
+    #              hfi_calc_models.FuelType(id=1, abbrev='C3', fuel_type_code='C3',
+    #                                       description='C3', percentage_conifer=100, percentage_dead_fir=0)),
+    #             (hfi_calc_models.PlanningWeatherStation(id=2, planning_area_id=2, station_code=2),
+    #             hfi_calc_models.FuelType(id=1, abbrev='C3', fuel_type_code='C3',
+    #                                      description='C3', percentage_conifer=100, percentage_dead_fir=0))]
 
-    def mock_get_fire_centre_stations(session, fire_centre_id):
-        """ Returns mocked stations per PlanningAreas """
-        return [hfi_calc_models.PlanningWeatherStation(id=1, planning_area_id=1, station_code=1),
-                hfi_calc_models.PlanningWeatherStation(id=2, planning_area_id=2, station_code=2)]
-
+    start_date = datetime.now()
     daily = StationDaily(
         code=1,
-        date=datetime.now(),
+        date=start_date,
         intensity_group=1
     )
-    mocker.patch('app.hfi.hfi.get_planning_areas', mock_get_planning_areas)
-    mocker.patch('app.hfi.hfi.get_fire_centre_stations',
-                 mock_get_fire_centre_stations)
-    with get_read_session_scope() as session:
-        result = calculate_hfi_results(fire_centre_id=kamloops_fc.id,
-                                       planning_area_fire_starts={
-                                           kamloops_fc.planning_areas[0].id: [highest_fire_starts]},
-                                       dailies=[daily],
-                                       num_prep_days=5,
-                                       selected_station_codes=[1, 2],
-                                       session=session)
+    # mocker.patch('app.routers.hfi_calc.get_fire_centre_stations',
+    #              mock_get_fire_centre_stations)
+
+    station = hfi_calc_models.PlanningWeatherStation(id=1, planning_area_id=1, station_code=1)
+
+    result = calculate_hfi_results(planning_area_fire_starts={
+        kamloops_fc.planning_areas[0].id: [highest_fire_starts]},
+        dailies=[daily],
+        num_prep_days=5,
+        selected_station_codes=[1, 2],
+        area_station_map={kamloops_fc.planning_areas[0].id: [station]},
+        start_date=start_date)
     assert result[0].daily_results[0].fire_starts == highest_fire_starts
 
 
