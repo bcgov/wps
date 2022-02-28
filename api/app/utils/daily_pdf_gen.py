63a3c0d1b15e9d25936868bffdf8e5513bcb4e1d
Conor Brady <con.brad@gmail.com>
2022-02-28 12:25:12 -0800

    Simplified function

--- api/app/utils/daily_pdf_gen.py
+++ api/app/utils/daily_pdf_gen.py
@@ -1,18 +1,18 @@
 import pdfkit
 from jinja2 import Template
 
+daily_template_path = "api/app/utils/rendered_daily_template.html"
+output_file_path = "api/app/utils/out.pdf"
+
 
 def generate_daily_pdf():
-    html = open('api/app/templates/template1.html').read()
-
-    template = Template(html)
-
-    with open('api/app/utils/rendered_template.html', 'w') as new_page:
-        new_page.write(template.render())
+    with open(daily_template_path, 'r') as daily_template:
+        template = Template(daily_template.read())
+        template.render()
 
     options = {
         'page-size': 'Tabloid'
     }
 
-    return pdfkit.from_file('api/app/utils/rendered_daily_template.html',
-                            'api/app/utils/out2.pdf', options)
+    return pdfkit.from_file(daily_template_path,
+                            output_file_path, options)
