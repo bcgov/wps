ca7878fdcab88362a44c7a2157586655f59b2078
Conor Brady <con.brad@gmail.com>
2022-02-28 12:32:33 -0800

    Fix template vs rendered html files

--- api/app/utils/daily_pdf_gen.py
+++ api/app/utils/daily_pdf_gen.py
@@ -1,18 +1,21 @@
 import pdfkit
 from jinja2 import Template
 
-daily_template_path = "api/app/utils/rendered_daily_template.html"
+daily_template_path = "api/app/utils/daily_template.html"
+daily_rendered_path = "api/app/utils/daily_rendered.html"
+
 output_file_path = "api/app/utils/out.pdf"
 
 
 def generate_daily_pdf():
     with open(daily_template_path, 'r') as daily_template:
         template = Template(daily_template.read())
-        template.render()
+        with open(daily_rendered_path, 'w') as new_page:
+            new_page.write(template.render(date='01-01-2022'))
 
     options = {
         'page-size': 'Tabloid'
     }
 
-    return pdfkit.from_file(daily_template_path,
+    return pdfkit.from_file(daily_rendered_path,
                             output_file_path, options)
