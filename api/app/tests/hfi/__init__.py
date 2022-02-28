a0100f6e743f82c9110a0966247b7d8d858e1f1c
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-02-28 09:31:57 -0800

    busy fixing tests

--- api/app/tests/hfi/__init__.py
+++ api/app/tests/hfi/__init__.py
@@ -32,14 +32,15 @@ def mock_station_crud(monkeypatch):
         """ Returns mocked WFWXWeatherStations codes. """
         return all_station_codes
 
-    def mock_get_station_with_fuel_types(_, station_codes: List[int]):
+    def mock_get_fire_centre_stations(_, fire_centre_id: int):
         """ Returns mocked WFWXWeatherStation with fuel types. """
         result = []
-        for station_code in station_codes:
-            planning_station = PlanningWeatherStation(station_code=station_code)
+        for station_code in [230, 239]:
+            planning_station = PlanningWeatherStation(station_code=station_code, planning_area_id=1)
             fuel_type = FuelType(abbrev='C3', fuel_type_code='C3', description='C3',
                                  percentage_conifer=100, percentage_dead_fir=0)
             result.append((planning_station, fuel_type))
         return result
 
     monkeypatch.setattr(app.utils.hfi_calculator, 'get_all_stations', mock_get_all_stations)
+    monkeypatch.setattr(app.routers.hfi_calc, 'get_fire_centre_stations', mock_get_fire_centre_stations)
