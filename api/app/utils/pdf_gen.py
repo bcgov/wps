bccdb67829dad8cda911ef3fb9b94b6346b349bb
Julian <julianforeman@me.com>
2022-03-01 10:00:08 -0800

    Dummy data for table & class structure for response

--- api/app/utils/pdf_gen.py
+++ api/app/utils/pdf_gen.py
@@ -1,3 +1,4 @@
+from mimetypes import init
 import pdfkit
 from jinja2 import Template
 import json
@@ -7,6 +8,16 @@ html = open('/Users/jforeman/Workspace/wps/api/app/templates/template1.html').re
 
 data = json.load(json_file)
 template = Template(html)
+
+
+class PrepCyclePdfResponse:
+    def __init__(self, planningAreaName, elev, fuelType, grassCure, dailies):
+        self.planningAreaName = planningAreaName
+        self.elev = elev
+        self.fuelType = fuelType
+        self.grassCure = grassCure
+        self.dailies = dailies  # dailies is the stations grouped by station code containing the dailies for each day in the prep cycle
+
 
 with open('/Users/jforeman/Workspace/wps/api/app/utils/rendered_template.html', 'w') as new_page:
     new_page.write(template.render(planningAreas=data['planning_area_hfi_results']))
