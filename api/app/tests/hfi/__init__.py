772f1562ebd72ede3cf5e963e906e46a74a889c9
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-03-02 11:34:48 -0800

    added a use case that includes c7b

--- api/app/tests/hfi/__init__.py
+++ api/app/tests/hfi/__init__.py
@@ -14,10 +14,16 @@ def mock_station_crud(monkeypatch):
 
     def mock_get_fire_centre_stations(_, fire_centre_id: int):
         """ Returns mocked WFWXWeatherStation with fuel types. """
+        def get_fuel_type_code_by_station_code(code: int):
+            if code == 230:
+                return 'C3'
+            return 'C7B'
         result = []
         for station_code in [230, 239]:
             planning_station = PlanningWeatherStation(station_code=station_code, planning_area_id=1)
-            fuel_type = FuelType(abbrev='C3', fuel_type_code='C3', description='C3',
+            fuel_type_code = get_fuel_type_code_by_station_code(station_code)
+            fuel_type = FuelType(abbrev=fuel_type_code, fuel_type_code=fuel_type_code,
+                                 description=fuel_type_code,
                                  percentage_conifer=100, percentage_dead_fir=0)
             result.append((planning_station, fuel_type))
         return result
