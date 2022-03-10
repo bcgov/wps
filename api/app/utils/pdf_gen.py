38dee4b3bf93d34665677ca367250f5e9afb4ee7
Julian <julianforeman@me.com>
2022-03-10 11:00:05 -0800

    Removing unused imports

--- api/app/utils/pdf_gen.py
+++ api/app/utils/pdf_gen.py
@@ -1,16 +1,6 @@
-from functools import reduce
-from itertools import groupby
-from typing import List
 import pdfkit
-from jinja2 import Environment, FunctionLoader, Template
-import json
-from app.utils.pdf_data_formatter import response_2_prep_cycle_jinja_format
+from jinja2 import Environment, FunctionLoader
 from app.hfi.prep_template import str_prep_template
-
-from app.schemas.hfi_calc import (HFIResultResponse, PlanningArea,
-                                  PrepCyclePDFData,
-                                  StationDaily,
-                                  ValidatedStationDaily)
 
 
 jinja_env = Environment(loader=FunctionLoader(str_prep_template), autoescape=True)
