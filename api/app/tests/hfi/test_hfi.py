d6ab9b4f4c24caafe1c50465e9f79367bc0ec3b9
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-03-17 17:58:28 -0700

    fixing tests

--- api/app/tests/hfi/test_hfi.py
+++ api/app/tests/hfi/test_hfi.py
@@ -7,7 +7,7 @@ from app.hfi.hfi_calc import (calculate_hfi_results,
                               calculate_max_intensity_group,
                               calculate_prep_level, validate_station_daily)
 import app.db.models.hfi_calc as hfi_calc_models
-from app.schemas.hfi_calc import (FireCentre,
+from app.schemas.hfi_calc import (FireCentre, FireStartRange,
                                   PlanningArea,
                                   StationDaily,
                                   WeatherStation,
@@ -44,9 +44,15 @@ kamloops_fc = FireCentre(
 )
 
 
+fire_start_ranges = [FireStartRange(label='moo', id=1), FireStartRange(label='moo', id=2)]
+fire_start_lookup = {1: {1: 1}, 2: {1: 1}}
+
+
 def test_no_dailies_handled():
     """ No dailies are handled """
-    result = calculate_hfi_results(planning_area_fire_starts={},
+    result = calculate_hfi_results(fire_start_ranges,
+                                   planning_area_fire_starts={},
+                                   fire_start_lookup=fire_start_lookup,
                                    dailies=[],
                                    num_prep_days=5,
                                    selected_station_codes=[1, 2],
@@ -68,14 +74,16 @@ def test_requested_fire_starts_unaltered(mocker: MockerFixture):
 
     station = hfi_calc_models.PlanningWeatherStation(id=1, planning_area_id=1, station_code=1)
 
-    result = calculate_hfi_results(planning_area_fire_starts={
-        kamloops_fc.planning_areas[0].id: [highest_fire_starts]},
-        dailies=[daily],
-        num_prep_days=5,
-        selected_station_codes=[1, 2],
-        area_station_map={kamloops_fc.planning_areas[0].id: [station]},
-        start_date=start_date)
-    assert result[0].daily_results[0].fire_starts == highest_fire_starts
+    result = calculate_hfi_results(fire_start_ranges,
+                                   planning_area_fire_starts={
+                                       kamloops_fc.planning_areas[0].id: [fire_start_ranges[-1]]},
+                                   fire_start_lookup=fire_start_lookup,
+                                   dailies=[daily],
+                                   num_prep_days=5,
+                                   selected_station_codes=[1, 2],
+                                   area_station_map={kamloops_fc.planning_areas[0].id: [station]},
+                                   start_date=start_date)
+    assert result[0].daily_results[0].fire_starts == fire_start_ranges[-1]
 
 
 def test_calculate_mean_intensity_basic():
@@ -164,54 +172,7 @@ def test_max_mean_intensity_with_none():
 
 def test_calculate_prep_level_empty():
     """ Calculates prep level of empty case """
-    for fire_start_range in all_ranges:
-        result = calculate_prep_level(None, fire_start_range)
-        assert result == None
-
-
-def test_lowest_prep_level():
-    """ Calculates prep level of lowest fire start range """
-    assert calculate_prep_level(1, lowest_fire_starts) == 1
-    assert calculate_prep_level(2, lowest_fire_starts) == 1
-    assert calculate_prep_level(3, lowest_fire_starts) == 2
-    assert calculate_prep_level(4, lowest_fire_starts) == 3
-    assert calculate_prep_level(5, lowest_fire_starts) == 4
-
-
-def test_1_2_prep_level():
-    """ Calculates prep level for 1-2 fire starts """
-    assert calculate_prep_level(1, one_2_two_starts) == 1
-    assert calculate_prep_level(2, one_2_two_starts) == 2
-    assert calculate_prep_level(3, one_2_two_starts) == 3
-    assert calculate_prep_level(4, one_2_two_starts) == 4
-    assert calculate_prep_level(5, one_2_two_starts) == 5
-
-
-def test_2_3_prep_level():
-    """ Calculates prep level for 2-3 fire starts """
-    assert calculate_prep_level(1, two_2_three_starts) == 2
-    assert calculate_prep_level(2, two_2_three_starts) == 3
-    assert calculate_prep_level(3, two_2_three_starts) == 4
-    assert calculate_prep_level(4, two_2_three_starts) == 5
-    assert calculate_prep_level(5, two_2_three_starts) == 6
-
-
-def test_3_6_prep_level():
-    """ Calculates prep level for 3-6 fire starts """
-    assert calculate_prep_level(1, three_2_six_starts) == 3
-    assert calculate_prep_level(2, three_2_six_starts) == 4
-    assert calculate_prep_level(3, three_2_six_starts) == 5
-    assert calculate_prep_level(4, three_2_six_starts) == 6
-    assert calculate_prep_level(5, three_2_six_starts) == 6
-
-
-def test_highest_prep_level():
-    """ Calculates prep level for 6+ fire starts """
-    assert calculate_prep_level(1, highest_fire_starts) == 4
-    assert calculate_prep_level(2, highest_fire_starts) == 5
-    assert calculate_prep_level(3, highest_fire_starts) == 6
-    assert calculate_prep_level(4, highest_fire_starts) == 6
-    assert calculate_prep_level(5, highest_fire_starts) == 6
+    assert calculate_prep_level(None, None, None) == None
 
 
 def test_valid_daily():
