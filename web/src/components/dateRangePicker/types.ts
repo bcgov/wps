015b8ff738cfcb9e233523fb3c2b8faf08e5ae10
Conor Brady <con.brad@gmail.com>
2022-03-28 13:04:53 -0700

    Fix comments

--- web/src/components/dateRangePicker/types.ts
+++ web/src/components/dateRangePicker/types.ts
@@ -6,10 +6,7 @@ export interface DateRange {
 export type Setter<T> = React.Dispatch<React.SetStateAction<T>> | ((value: T) => void)
 
 export enum NavigationAction {
-  // eslint-disable-next-line no-unused-vars
   Previous = -1,
-
-  // eslint-disable-next-line no-unused-vars
   Next = 1
 }
 
