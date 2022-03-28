1cea995511fe8c54c723d505abc4626b894a2be6
Conor Brady <con.brad@gmail.com>
2022-03-28 08:07:42 -0700

    Fix border color

--- web/src/features/hfiCalculator/components/PrepDateRangeSelector.tsx
+++ web/src/features/hfiCalculator/components/PrepDateRangeSelector.tsx
@@ -1,10 +1,11 @@
 import {
+  createTheme,
   Dialog,
   FormControl,
   IconButton,
   InputAdornment,
-  makeStyles,
-  TextField
+  TextField,
+  ThemeProvider
 } from '@material-ui/core'
 import * as materialIcons from '@material-ui/icons'
 import { PrepDateRange } from 'features/hfiCalculator/slices/hfiCalculatorSlice'
@@ -18,26 +19,29 @@ export interface PrepDateRangeSelectorProps {
   setDateRange: (newDateRange: DateRange) => void
 }
 
-const useStyles = makeStyles({
-  dateRangeTextField: {
-    '& .MuiOutlinedInput-input': {
-      color: 'white'
+export const dateRangePickerTheme = createTheme({
+  overrides: {
+    MuiOutlinedInput: {
+      root: {
+        '&.Mui-disabled': {
+          color: 'white',
+          borderColor: 'white',
+          '& $notchedOutline': {
+            borderColor: 'white'
+          }
+        }
+      }
     },
-    '& .MuiIconButton-root': {
-      color: 'white'
+    MuiFormLabel: {
+      root: {
+        '&.Mui-disabled': {
+          color: 'white'
+        }
+      }
     },
-    '& .MuiInputLabel-root': {
-      color: 'white'
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: 'white'
-      },
-      '&:hover fieldset': {
-        borderColor: 'white'
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: 'white'
+    MuiSvgIcon: {
+      root: {
+        fill: 'white'
       }
     }
   }
@@ -47,7 +51,6 @@ const PrepDateRangeSelector = ({
   dateRange,
   setDateRange
 }: PrepDateRangeSelectorProps) => {
-  const classes = useStyles()
   const dateDisplayFormat = 'MMMM dd'
   const startDate =
     dateRange && dateRange.start_date
@@ -62,49 +65,50 @@ const PrepDateRangeSelector = ({
   const toggleDateRangePicker = () => setDateRangePickerOpen(!dateRangePickerOpen)
 
   return (
-    <React.Fragment>
-      <TextField
-        className={`${classes.dateRangeTextField}`}
-        size="small"
-        id="outlined-basic"
-        variant="outlined"
-        disabled={true}
-        label={'Set prep period'}
-        onClick={() => setDateRangePickerOpen(!dateRangePickerOpen)}
-        value={
-          isUndefined(dateRange) ||
-          isUndefined(dateRange.start_date) ||
-          isUndefined(dateRange.end_date)
-            ? ''
-            : `${DateTime.fromISO(dateRange.start_date)
-                .toFormat(dateDisplayFormat)
-                .trim()} - ${DateTime.fromISO(dateRange.end_date)
-                .toFormat(dateDisplayFormat)
-                .trim()}
+    <ThemeProvider theme={dateRangePickerTheme}>
+      <React.Fragment>
+        <TextField
+          size="small"
+          id="outlined-basic"
+          variant="outlined"
+          disabled={true}
+          label={'Set prep period'}
+          onClick={() => setDateRangePickerOpen(!dateRangePickerOpen)}
+          value={
+            isUndefined(dateRange) ||
+            isUndefined(dateRange.start_date) ||
+            isUndefined(dateRange.end_date)
+              ? ''
+              : `${DateTime.fromISO(dateRange.start_date)
+                  .toFormat(dateDisplayFormat)
+                  .trim()} - ${DateTime.fromISO(dateRange.end_date)
+                  .toFormat(dateDisplayFormat)
+                  .trim()}
                       `
-        }
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <IconButton edge="end">
-                <materialIcons.DateRange />
-              </IconButton>
-            </InputAdornment>
-          )
-        }}
-      />
-      <FormControl>
-        <Dialog open={dateRangePickerOpen} onClose={toggleDateRangePicker}>
-          <DateRangePicker
-            initialDateRange={{ startDate, endDate }}
-            open={dateRangePickerOpen}
-            toggle={toggleDateRangePicker}
-            onChange={range => setDateRange(range)}
-            definedRanges={[]}
-          />
-        </Dialog>
-      </FormControl>
-    </React.Fragment>
+          }
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton edge="end">
+                  <materialIcons.DateRange />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+        <FormControl>
+          <Dialog open={dateRangePickerOpen} onClose={toggleDateRangePicker}>
+            <DateRangePicker
+              initialDateRange={{ startDate, endDate }}
+              open={dateRangePickerOpen}
+              toggle={toggleDateRangePicker}
+              onChange={range => setDateRange(range)}
+              definedRanges={[]}
+            />
+          </Dialog>
+        </FormControl>
+      </React.Fragment>
+    </ThemeProvider>
   )
 }
 
