a1de496be520265dfa73c42b30a1ddb82fa32b44
Julian <julianforeman@me.com>
2022-03-31 15:06:33 -0700

    Fixing path to scripts

--- openshift/scripts/oc_deploy_to_production.sh
+++ openshift/scripts/oc_deploy_to_production.sh
@@ -27,25 +27,25 @@ PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 
 set -ex
 echo Promote
-bash oc_promote.sh ${SUFFIX} ${RUN_TYPE}
-bash oc_promote_ubuntu.sh ${SUFFIX} ${RUN_TYPE}
+bash $(dirname ${0})/oc_promote.sh ${SUFFIX} ${RUN_TYPE}
+bash $(dirname ${0})/oc_promote_ubuntu.sh ${SUFFIX} ${RUN_TYPE}
 echo Provision database
-CPU_REQUEST=75m CPU_LIMIT=2000m MEMORY_REQUEST=2Gi MEMORY_LIMIT=16Gi PVC_SIZE=45Gi PROJ_TARGET=${PROJ_TARGET} bash oc_provision_db.sh prod ${RUN_TYPE}
+CPU_REQUEST=75m CPU_LIMIT=2000m MEMORY_REQUEST=2Gi MEMORY_LIMIT=16Gi PVC_SIZE=45Gi PROJ_TARGET=${PROJ_TARGET} bash $(dirname ${0})/oc_provision_db.sh prod ${RUN_TYPE}
 echo Deploy API
-WORKERS_PER_CORE=0.25 CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=3Gi MEMORY_LIMIT=6Gi REPLICAS=3 PROJ_TARGET=${PROJ_TARGET} VANITY_DOMAIN=psu.nrs.gov.bc.ca SECOND_LEVEL_DOMAIN=apps.silver.devops.gov.bc.ca bash ./oc_deploy.sh prod ${RUN_TYPE}
+WORKERS_PER_CORE=0.25 CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=3Gi MEMORY_LIMIT=6Gi REPLICAS=3 PROJ_TARGET=${PROJ_TARGET} VANITY_DOMAIN=psu.nrs.gov.bc.ca SECOND_LEVEL_DOMAIN=apps.silver.devops.gov.bc.ca bash $(dirname ${0})/oc_deploy.sh prod ${RUN_TYPE}
 echo Env Canada Subscriber
-PROJ_TARGET=${PROJ_TARGET} bash oc_provision_ec_gdps_cronjob.sh prod ${RUN_TYPE}
-PROJ_TARGET=${PROJ_TARGET} bash oc_provision_ec_hrdps_cronjob.sh prod ${RUN_TYPE}
-PROJ_TARGET=${PROJ_TARGET} bash oc_provision_ec_rdps_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} bash $(dirname ${0})/oc_provision_ec_gdps_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} bash $(dirname ${0})/oc_provision_ec_hrdps_cronjob.sh prod ${RUN_TYPE} 
+PROJ_TARGET=${PROJ_TARGET} bash $(dirname ${0})/oc_provision_ec_rdps_cronjob.sh prod ${RUN_TYPE}
 echo C-Haines
-PROJ_TARGET=${PROJ_TARGET} bash oc_provision_c_haines_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} bash $(dirname ${0})/oc_provision_c_haines_cronjob.sh prod ${RUN_TYPE}
 echo BC FireWeather cronjobs
 echo "Run forecast at 8h30 PDT and 16h30 PDT (so before and after noon)"
-PROJ_TARGET=${PROJ_TARGET} SCHEDULE="30 * * * *" bash oc_provision_wfwx_noon_forecasts_cronjob.sh prod ${RUN_TYPE}
-PROJ_TARGET=${PROJ_TARGET} SCHEDULE="15 * * * *" bash oc_provision_wfwx_hourly_actuals_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} SCHEDULE="30 * * * *" bash $(dirname ${0})/oc_provision_wfwx_noon_forecasts_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} SCHEDULE="15 * * * *" bash $(dirname ${0})/oc_provision_wfwx_hourly_actuals_cronjob.sh prod ${RUN_TYPE}
 echo Configure backups
-PROJ_TARGET=${PROJ_TARGET} CPU_REQUEST=1000m CPU_LIMIT=2000m bash oc_provision_backup_s3_postgres_cronjob.sh prod ${RUN_TYPE}
-PROJ_TARGET=${PROJ_TARGET} CPU_REQUEST=50m CPU_LIMIT=500m BACKUP_VOLUME_SIZE=3Gi bash oc_provision_backup_mariadb.sh prod ${RUN_TYPE}
-PROJ_TARGET=${PROJ_TARGET} CPU_REQUEST=50m CPU_LIMIT=500m bash oc_provision_backup_mariadb_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} CPU_REQUEST=1000m CPU_LIMIT=2000m bash $(dirname ${0})/oc_provision_backup_s3_postgres_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} CPU_REQUEST=50m CPU_LIMIT=500m BACKUP_VOLUME_SIZE=3Gi bash $(dirname ${0})/oc_provision_backup_mariadb.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} CPU_REQUEST=50m CPU_LIMIT=500m bash $(dirname ${0})/oc_provision_backup_mariadb_cronjob.sh prod ${RUN_TYPE}
 echo Configure
-PROJ_TARGET=${PROJ_TARGET} CERTBOT_STAGING=false DRYRUN=false bash oc_provision_certbot_cronjob.sh prod ${RUN_TYPE}
+PROJ_TARGET=${PROJ_TARGET} CERTBOT_STAGING=false DRYRUN=false bash $(dirname ${0})/oc_provision_certbot_cronjob.sh prod ${RUN_TYPE}
