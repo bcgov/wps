49795c02dc0e0d29f79ee28902a3defd87458de1
Conor Brady <con.brad@gmail.com>
2022-04-04 13:01:37 -0700

    Add to Daily Table

---
+++ web/src/features/hfiCalculator/components/DailyHFICell.tsx
@@ -0,0 +1,18 @@
+import HFICell from 'components/HFICell'
+import CalculatedCell from 'features/hfiCalculator/components/CalculatedCell'
+import React from 'react'
+
+export interface DailyHFICellProps {
+  testid: string | undefined
+  value: string | undefined
+  error: boolean
+  className: string | undefined
+}
+
+export const DailyHFICell = (props: DailyHFICellProps) => {
+  /** */
+  if (props.error) {
+    return <CalculatedCell {...props} />
+  }
+  return <HFICell value={Number(props.value)} />
+}
