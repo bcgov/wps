200d5678737cf77b73aa06c9ad7d1c5397924a00
Julian <julianforeman@me.com>
2022-04-04 15:03:24 -0700

    Making names more relevent to what they are testing for

--- web/src/features/hfiCalculator/components/MeanPrepLevelCell.test.tsx
+++ web/src/features/hfiCalculator/components/MeanPrepLevelCell.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import MeanPrepLevelCell from 'features/hfiCalculator/components/MeanPrepLevelCell'
 import React from 'react'
 
-const renderPrepLevel = (prepLevel: number | undefined, invalidForecast: boolean) => {
+const renderMeanPrepLevel = (prepLevel: number | undefined, invalidForecast: boolean) => {
   return render(
     <TableContainer>
       <Table>
@@ -22,45 +22,45 @@ const renderPrepLevel = (prepLevel: number | undefined, invalidForecast: boolean
   )
 }
 
-describe('PrepLevelCell', () => {
+describe('meanPrepLevelCell', () => {
   it('should return a cell with a classname of prepLevel1 and a text prep level of 1', () => {
-    const { getByTestId } = renderPrepLevel(1, false)
+    const { getByTestId } = renderMeanPrepLevel(1, false)
     const cell = getByTestId('weekly-prep-level-afton')
     expect(cell.className).toMatch(/makeStyles-meanPrepLevel1-/)
     expect(cell.innerHTML).toBe('1')
   })
   it('should return a cell with a classname of prepLevel2 and a text prep level of 2', () => {
-    const { getByTestId } = renderPrepLevel(2, false)
+    const { getByTestId } = renderMeanPrepLevel(2, false)
     const cell = getByTestId('weekly-prep-level-afton')
     expect(cell.className).toMatch(/makeStyles-meanPrepLevel2-/)
     expect(cell.innerHTML).toBe('2')
   })
   it('should return a cell with a classname of prepLevel3 and a text prep level of 3', () => {
-    const { getByTestId } = renderPrepLevel(3, false)
+    const { getByTestId } = renderMeanPrepLevel(3, false)
     const cell = getByTestId('weekly-prep-level-afton')
     expect(cell.className).toMatch(/makeStyles-meanPrepLevel3-/)
     expect(cell.innerHTML).toBe('3')
   })
   it('should return a cell with a classname of prepLevel4 and a text prep level of 4', () => {
-    const { getByTestId } = renderPrepLevel(4, false)
+    const { getByTestId } = renderMeanPrepLevel(4, false)
     const cell = getByTestId('weekly-prep-level-afton')
     expect(cell.className).toMatch(/makeStyles-meanPrepLevel4-/)
     expect(cell.innerHTML).toBe('4')
   })
   it('should return a cell with a classname of prepLevel5 and a text prep level of 5', () => {
-    const { getByTestId } = renderPrepLevel(5, false)
+    const { getByTestId } = renderMeanPrepLevel(5, false)
     const cell = getByTestId('weekly-prep-level-afton')
     expect(cell.className).toMatch(/makeStyles-meanPrepLevel5-/)
     expect(cell.innerHTML).toBe('5')
   })
   it('should return a cell with a classname of prepLevel6 and a text prep level of 6', () => {
-    const { getByTestId } = renderPrepLevel(6, false)
+    const { getByTestId } = renderMeanPrepLevel(6, false)
     const cell = getByTestId('weekly-prep-level-afton')
     expect(cell.className).toMatch(/makeStyles-meanPrepLevel6-/)
     expect(cell.innerHTML).toBe('6')
   })
   it('should return a cell with a classname of defaultBackground and a text prep level undefined', () => {
-    const { getByTestId } = renderPrepLevel(undefined, true)
+    const { getByTestId } = renderMeanPrepLevel(undefined, true)
     const cell = getByTestId('weekly-prep-level-afton')
     expect(cell.className).toMatch(/makeStyles-defaultBackground-/)
     const errorIcon = getByTestId('prep-level-error')
