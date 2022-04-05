37ab9b641524d9f4ddc0817ce8ea3aabf1a7a15a
Andrea Williams <andrea.williams@gov.bc.ca>
2022-04-05 16:32:14 -0700

    Fix unit test

--- web/src/features/hfiCalculator/components/weeklyRosCell.test.tsx
+++ web/src/features/hfiCalculator/components/weeklyRosCell.test.tsx
@@ -21,6 +21,7 @@ const renderWeeklyRos = (
               daily={daily}
               error={error}
               isRowSelected={isRowSelected}
+              isFirstDayOfPrepPeriod={true}
             ></WeeklyROSCell>
           </TableRow>
         </TableBody>
