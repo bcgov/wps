c2580be91aa5d3fb6e15d36aef29f5a9298ebdcf
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-04-06 11:17:30 -0700

    extract common code

--- api/app/tests/hfi/test_hfi_endpoint_request.py
+++ api/app/tests/hfi/test_hfi_endpoint_request.py
@@ -15,6 +15,33 @@ from app.tests import load_json_file_with_name, load_json_file
 from app.tests.hfi import mock_station_crud
 
 
+def _setup_mock(monkeypatch: pytest.MonkeyPatch):
+    # mock anything that uses aiohttp.ClientSession::get
+    monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
+
+    def mock_get_fire_weather_stations(_):
+        fire_centre = FireCentre(id=1, name='Kamloops Fire Centre')
+        planning_area_1 = PlanningArea(id=1, name='Kamloops (K2)', fire_centre_id=1,
+                                       order_of_appearance_in_list=1)
+        planning_area_2 = PlanningArea(id=2, name='Vernon (K4)', fire_centre_id=1,
+                                       order_of_appearance_in_list=2)
+        fuel_type_1 = FuelType(abbrev='O1B', description='neigh', fuel_type_code="O1B",
+                               percentage_conifer=0, percentage_dead_fir=0)
+        fuel_type_2 = FuelType(abbrev='C7B', description='moo', fuel_type_code='C7',
+                               percentage_conifer=100, percentage_dead_fir=0)
+        return (
+            (PlanningWeatherStation(station_code=230, fuel_type_id=1,
+             planning_area_id=1), fuel_type_1, planning_area_1, fire_centre),
+            (PlanningWeatherStation(station_code=239, fuel_type_id=2,
+             planning_area_id=2), fuel_type_2, planning_area_2, fire_centre)
+        )
+
+    monkeypatch.setattr(app.hfi.hfi_calc, 'get_fire_weather_stations', mock_get_fire_weather_stations)
+
+    # mock out database calls:
+    mock_station_crud(monkeypatch)
+
+
 @pytest.mark.usefixtures('mock_jwt_decode')
 @scenario('test_hfi_endpoint_request.feature', 'HFI - request')
 def test_fire_behaviour_calculator_scenario_no_request_stored():
@@ -44,30 +71,7 @@ def given_stored_request(monkeypatch, stored_request_json: Tuple[dict, str]):
 def given_hfi_calc_url(monkeypatch: pytest.MonkeyPatch, url: str, verb: str):
     """ Handle request
     """
-    # mock anything that uses aiohttp.ClientSession::get
-    monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
-
-    def mock_get_fire_weather_stations(_):
-        fire_centre = FireCentre(id=1, name='Kamloops Fire Centre')
-        planning_area_1 = PlanningArea(id=1, name='Kamloops (K2)', fire_centre_id=1,
-                                       order_of_appearance_in_list=1)
-        planning_area_2 = PlanningArea(id=2, name='Vernon (K4)', fire_centre_id=1,
-                                       order_of_appearance_in_list=2)
-        fuel_type_1 = FuelType(abbrev='O1B', description='neigh', fuel_type_code="O1B",
-                               percentage_conifer=0, percentage_dead_fir=0)
-        fuel_type_2 = FuelType(abbrev='C7B', description='moo', fuel_type_code='C7',
-                               percentage_conifer=100, percentage_dead_fir=0)
-        return (
-            (PlanningWeatherStation(station_code=230, fuel_type_id=1,
-             planning_area_id=1), fuel_type_1, planning_area_1, fire_centre),
-            (PlanningWeatherStation(station_code=239, fuel_type_id=2,
-             planning_area_id=2), fuel_type_2, planning_area_2, fire_centre)
-        )
-
-    monkeypatch.setattr(app.hfi.hfi_calc, 'get_fire_weather_stations', mock_get_fire_weather_stations)
-
-    # mock out database calls:
-    mock_station_crud(monkeypatch)
+    _setup_mock(monkeypatch)
 
     client = TestClient(app.main.app)
     headers = {'Content-Type': 'application/json',
@@ -88,30 +92,7 @@ def given_hfi_calc_url_with_request(
         monkeypatch: pytest.MonkeyPatch, url: str, request_json: Tuple[dict, str], verb: str):
     """ Handle request
     """
-    # mock anything that uses aiohttp.ClientSession::get
-    monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
-
-    def mock_get_fire_weather_stations(_):
-        fire_centre = FireCentre(id=1, name='Kamloops Fire Centre')
-        planning_area_1 = PlanningArea(id=1, name='Kamloops (K2)', fire_centre_id=1,
-                                       order_of_appearance_in_list=1)
-        planning_area_2 = PlanningArea(id=2, name='Vernon (K4)', fire_centre_id=1,
-                                       order_of_appearance_in_list=2)
-        fuel_type_1 = FuelType(abbrev='O1B', description='neigh', fuel_type_code="O1B",
-                               percentage_conifer=0, percentage_dead_fir=0)
-        fuel_type_2 = FuelType(abbrev='C7B', description='moo', fuel_type_code='C7',
-                               percentage_conifer=100, percentage_dead_fir=0)
-        return (
-            (PlanningWeatherStation(station_code=230, fuel_type_id=1,
-             planning_area_id=1), fuel_type_1, planning_area_1, fire_centre),
-            (PlanningWeatherStation(station_code=239, fuel_type_id=2,
-             planning_area_id=2), fuel_type_2, planning_area_2, fire_centre)
-        )
-
-    monkeypatch.setattr(app.hfi.hfi_calc, 'get_fire_weather_stations', mock_get_fire_weather_stations)
-
-    # mock out database calls:
-    mock_station_crud(monkeypatch)
+    _setup_mock(monkeypatch)
 
     client = TestClient(app.main.app)
     headers = {'Content-Type': 'application/json',
