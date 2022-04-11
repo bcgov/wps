6447efe9e4c62ce2436c52219dad74c10716c9b2
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-04-11 15:19:44 -0700

    busy styling toast

--- web/src/features/hfiCalculator/components/HFISuccessAlert.tsx
+++ web/src/features/hfiCalculator/components/HFISuccessAlert.tsx
@@ -1,3 +1,4 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import { Alert } from '@material-ui/lab'
 import { Snackbar } from '@material-ui/core'
 import React from 'react'
@@ -6,7 +7,25 @@ export interface HFISuccessAlertProps {
   message: string | null
 }
 
+const useStyles = makeStyles(
+  { root: {} }
+  // (theme: Theme) => {
+
+  // }
+  // createStyles({
+  //   root: {
+  //     marginTop: '200px'
+  //     // width: '100%',
+  //     // '& > * + *': {
+  //     //   marginTop: theme.spacing(2)
+  //     // },
+  //     // marginBottom: theme.spacing(2)
+  //   }
+  // })
+)
+
 const HFISuccessAlert = ({ message }: HFISuccessAlertProps) => {
+  const classes = useStyles()
   const [open, setOpen] = React.useState(true)
 
   const handleClose = () => {
@@ -14,16 +33,18 @@ const HFISuccessAlert = ({ message }: HFISuccessAlertProps) => {
   }
 
   return (
-    <Snackbar
-      open={open}
-      autoHideDuration={6000}
-      onClose={handleClose}
-      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-    >
-      <Alert onClose={handleClose} severity="success">
-        {message}
-      </Alert>
-    </Snackbar>
+    <div className={classes.root}>
+      <Snackbar
+        open={open}
+        // autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleClose} severity="success">
+          {message}
+        </Alert>
+      </Snackbar>
+    </div>
   )
 }
 
