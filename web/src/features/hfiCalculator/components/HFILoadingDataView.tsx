9be24021b1d19cf9663a976f774cf50c41c46534
Conor Brady <con.brad@gmail.com>
2022-04-21 11:43:53 -0700

    Bug fix: HFI initial loading state (#1925)
    * Encapsulate loading and error states in HFILoadingDataView component with tests to fix loading spinner on initial page load.

---
+++ web/src/features/hfiCalculator/components/HFILoadingDataView.tsx
@@ -0,0 +1,93 @@
+import {
+  Table,
+  TableBody,
+  Container,
+  CircularProgress,
+  makeStyles
+} from '@material-ui/core'
+import { FireCentre } from 'api/hfiCalcAPI'
+import { PrepDateRange } from 'features/hfiCalculator/slices/hfiCalculatorSlice'
+import EmptyFireCentreRow from 'features/hfiCalculator/components/EmptyFireCentre'
+import { isUndefined, isNull } from 'lodash'
+import React from 'react'
+import HFIErrorAlert from 'features/hfiCalculator/components/HFIErrorAlert'
+
+export interface HFILoadingDataViewProps {
+  loading: boolean
+  stationDataLoading: boolean
+  fireCentresLoading: boolean
+  fireCentresError: string | null
+  hfiError: string | null
+  children: JSX.Element
+  dateRange?: PrepDateRange
+  selectedFireCentre?: FireCentre
+}
+
+const useStyles = makeStyles(() => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}))
+const HFILoadingDataView = ({
+  loading,
+  stationDataLoading,
+  fireCentresLoading,
+  fireCentresError,
+  hfiError,
+  children,
+  dateRange,
+  selectedFireCentre
+}: HFILoadingDataViewProps) => {
+  const classes = useStyles()
+
+  const buildErrorNotification = () => {
+    if (!isNull(fireCentresError) || !isNull(hfiError)) {
+      return (
+        <HFIErrorAlert hfiDailiesError={hfiError} fireCentresError={fireCentresError} />
+      )
+    }
+    return <React.Fragment></React.Fragment>
+  }
+
+  const isLoading = () => {
+    return loading || stationDataLoading || fireCentresLoading
+  }
+
+  const isLoadingWithoutError = () =>
+    isLoading() && isNull(fireCentresError) && isNull(hfiError)
+
+  const isFireCentreUnselected = () =>
+    (isUndefined(selectedFireCentre) || isUndefined(dateRange)) && !isLoading()
+
+  const errorNotification = buildErrorNotification()
+
+  if (isFireCentreUnselected()) {
+    return (
+      <React.Fragment>
+        {errorNotification}
+        <Table>
+          <TableBody>
+            <EmptyFireCentreRow />
+          </TableBody>
+        </Table>
+      </React.Fragment>
+    )
+  } else if (isLoadingWithoutError()) {
+    return (
+      <Container className={classes.container} data-testid="loading-container">
+        {errorNotification}
+        <CircularProgress />
+      </Container>
+    )
+  }
+
+  return (
+    <React.Fragment>
+      {errorNotification}
+      {children}
+    </React.Fragment>
+  )
+}
+
+export default HFILoadingDataView
