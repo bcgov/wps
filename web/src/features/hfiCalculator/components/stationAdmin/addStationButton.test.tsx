ba81163166148d474acb7ce60a7f377a15621ea1
Conor Brady <con.brad@gmail.com>
2022-05-02 14:51:11 -0700

    Fix tests

--- web/src/features/hfiCalculator/components/stationAdmin/addStationButton.test.tsx
+++ web/src/features/hfiCalculator/components/stationAdmin/addStationButton.test.tsx
@@ -1,6 +1,7 @@
 import { createStore } from '@reduxjs/toolkit'
 import { fireEvent, render, waitFor } from '@testing-library/react'
 import rootReducer from 'app/rootReducer'
+import store from 'app/store'
 import AddStationButton from 'features/hfiCalculator/components/stationAdmin/AddStationButton'
 import React from 'react'
 import { Provider } from 'react-redux'
@@ -8,7 +9,7 @@ import { Provider } from 'react-redux'
 describe('ManageStationsButton', () => {
   it('should render the button without the modal showing', () => {
     const { getByTestId, queryByText } = render(
-      <Provider store={createStore(rootReducer)}>
+      <Provider store={store}>
         <AddStationButton fireCentreId={1} />
       </Provider>
     )
@@ -20,7 +21,7 @@ describe('ManageStationsButton', () => {
   })
   it('should render the modal when the button is clicked', async () => {
     const { getByTestId } = render(
-      <Provider store={createStore(rootReducer)}>
+      <Provider store={store}>
         <AddStationButton fireCentreId={1} />
       </Provider>
     )
