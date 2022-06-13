e6faa96fc2fb0100b195ee01bed6e432a9ff3cd1
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-06-13 15:32:27 -0700

    no matter if prune does not pass

--- openshift/scripts/prune.py
+++ openshift/scripts/prune.py
@@ -1,17 +1,21 @@
 import sys
 import json
 
-# read from stdin (we're expecting JSON from a "oc get is/blah --output=json")
-response = json.load(sys.stdin)
-# make a list of tags and dates
-tags = []
-for tag in response['status']['tags']:
-    if tag['items']:
-        tags.append({'tag': tag['tag'], 'created': tag['items'][0]['created']})
-# order ascending by date
-tags.sort(key=lambda x: x['created'])
-# now - we only want to keep the N most recent, so pop off the N most recent, the rest we'll delete
-tags = tags[:-5]
-# output the list of tags to stdout
-for tag in tags:
-    print(tag['tag'])
+try:
+    # read from stdin (we're expecting JSON from a "oc get is/blah --output=json")
+    response = json.load(sys.stdin)
+    # make a list of tags and dates
+    tags = []
+    for tag in response['status']['tags']:
+        if tag['items']:
+            tags.append({'tag': tag['tag'], 'created': tag['items'][0]['created']})
+    # order ascending by date
+    tags.sort(key=lambda x: x['created'])
+    # now - we only want to keep the N most recent, so pop off the N most recent, the rest we'll delete
+    tags = tags[:-5]
+    # output the list of tags to stdout
+    for tag in tags:
+        print(tag['tag'])
+except:
+    # if we can't read the JSON, just exit with an error
+    sys.exit(1)
