a192b172a04818876dcec66d0a85bbd43a6f7d6b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-07-04 16:22:27 -0700

    Web served up by NGINX (#2121)
    Introduced nginx to serve up static content
    Ripped out web build in api - no longer needed
    Ripped out front-end code from api
    Ripped out matomo (less configuration to worry about)

--- Makefile
+++ Makefile
@@ -13,6 +13,14 @@ docker-db:
 	# Run the database
 	docker-compose up db
 
+docker-web-server-build:
+	# Build the web in server mode (same dockerfile as used in production)
+	docker build -f ./Dockerfile.web --tag=wps/web .
+
+docker-web-server:
+	# Run the web in server mode (same dockerfile as used in production)
+	docker run -p 3000:3000 wps/web
+
 docker-shell-api:
 	# Shell into the dev container.
 	docker-compose run --rm api bash
--- web/src/types/window.d.ts
+++ web/src/types/window.d.ts
@@ -5,13 +5,7 @@ interface Window {
     REACT_APP_KEYCLOAK_AUTH_URL: string
     REACT_APP_KEYCLOAK_REALM: string
     REACT_APP_KEYCLOAK_CLIENT: string
+    API_BASE_URL: string | undefined
   }
   Cypress: {} | undefined
-  // TODO: Re-evaluate this way of implementing Matomo once we know more about it.
-  // Matomo Tracking (see https://developer.matomo.org/guides/spa-tracking)
-  _paq: any
-  // Matomo Tag Manager (see https://developer.matomo.org/guides/tagmanager/settingup)
-  _mtm: {
-    push: Function
-  }
 }
