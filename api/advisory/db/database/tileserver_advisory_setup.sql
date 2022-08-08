54db48b932bf40e5e671e93df4d85a503fdcf51b
Conor Brady <con.brad@gmail.com>
2022-08-08 16:08:02 -0700

    Story/classify hfi multiple days (#2200)
    - Replace `shapely` with `gdal.Warp` for reprojection
    - Ran `api/advisory/db/database/tileserver_advisory_setup.sql` to add advisory table to tileserver
    - Hook up frontend date picker state to vector tile query params
    - Add mouse pointer hover event to show HFI polygon value

---
+++ api/advisory/db/database/tileserver_advisory_setup.sql
@@ -0,0 +1,15 @@
+-- TODO: Remove when we have a migration toolchain in place
+CREATE TABLE IF NOT EXISTS advisory_fire_zones (
+    id SERIAL PRIMARY KEY,
+    for_date DATE NOT NULL,
+    mof_fire_zone_id integer NOT NULL,
+    elevated_hfi_area double precision NOT NULL,
+    elevated_hfi_percentage double precision NOT NULL
+);
+COMMENT ON TABLE advisory_fire_zones IS 'Information about advisories.';
+CREATE INDEX IF NOT EXISTS ix_advisory_fire_zones_id ON advisory_fire_zones(id int4_ops);
+CREATE INDEX IF NOT EXISTS ix_advisory_fire_zones_mof_fire_zone_id ON advisory_fire_zones(mof_fire_zone_id int4_ops);
+CREATE INDEX IF NOT EXISTS ix_advisory_date ON advisory_fire_zones ((for_date::date));
+
+ALTER USER tileserv WITH SUPERUSER;
+grant connect on database tileserv to tileserv; grant usage on schema public to tileserv; grant select on all tables in schema public to tileserv;
