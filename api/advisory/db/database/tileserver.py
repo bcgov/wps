8278d788f447d875e31cc514a1b7a6bc9146d8d6
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-08-09 14:39:22 -0700

    Switch to using an async driver (#2202)
    Introduce async database

--- api/advisory/db/database/tileserver.py
+++ api/advisory/db/database/tileserver.py
@@ -1,10 +1,10 @@
 """ The fire zone, fire centre and hfi polygons live in a different database - it's our "tileserver"
 database.
 """
-from typing import Generator
-from contextlib import contextmanager
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import create_engine
+from typing import AsyncGenerator
+from contextlib import asynccontextmanager
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from app import config
 
 tileserver_read_user = config.get('TILESERVER_READ_USER', 'tileserv')
@@ -15,61 +15,52 @@ tileserver_postgres_write_host = config.get('TILESERVER_POSTGRES_WRITE_HOST', 'l
 tileserver_postgres_database = config.get('TILESERVER_POSTGRES_DATABASE', 'tileserv')
 postgres_port = config.get('POSTGRES_PORT', '5432')
 
-TILESERVER_READ_STRING = f'postgresql://{tileserver_read_user}:{tileserver_postgres_password}@{tileserver_postgres_read_host}:{postgres_port}/{tileserver_postgres_database}'
-TILESERVER_WRITE_STRING = f'postgresql://{tileserver_write_user}:{tileserver_postgres_password}@{tileserver_postgres_write_host}:{postgres_port}/{tileserver_postgres_database}'
+TILESERVER_READ_STRING = f'postgresql+asyncpg://{tileserver_read_user}:{tileserver_postgres_password}@{tileserver_postgres_read_host}:{postgres_port}/{tileserver_postgres_database}'
+TILESERVER_WRITE_STRING = f'postgresql+asyncpg://{tileserver_write_user}:{tileserver_postgres_password}@{tileserver_postgres_write_host}:{postgres_port}/{tileserver_postgres_database}'
 
-tile_server_read_engine = create_engine(
-    TILESERVER_READ_STRING,
-    pool_size=int(config.get('POSTGRES_POOL_SIZE', 5)),
-    max_overflow=int(config.get('POSTGRES_MAX_OVERFLOW', 10)),
-    pool_pre_ping=True, connect_args={
-        'options': '-c timezone=utc'})
+# TODO: figure out connection pooling?
+tile_server_read_engine = create_async_engine(TILESERVER_READ_STRING)
 
-# connect to database - defaulting to always use utc timezone
-tile_server_write_engine = create_engine(TILESERVER_WRITE_STRING, connect_args={'options': '-c timezone=utc'})
+tile_server_write_engine = create_async_engine(TILESERVER_WRITE_STRING)
 
-tile_server_read_session = sessionmaker(
-    autocommit=False, autoflush=False, bind=tile_server_read_engine)
+tile_server_read_session_maker = sessionmaker(
+    autocommit=False, autoflush=False, bind=tile_server_read_engine, class_=AsyncSession)
 
-tile_server_write_session = sessionmaker(
-    autocommit=False, autoflush=False, bind=tile_server_write_engine)
+tile_server_write_session_maker = sessionmaker(
+    autocommit=False, autoflush=False, bind=tile_server_write_engine, class_=AsyncSession)
 
 
-def _get_tileserver_read_session() -> sessionmaker:
+def _get_tileserver_read_session() -> AsyncSession:
     """ abstraction used for mocking out a read session """
-    return tile_server_read_session()
+    return tile_server_read_session_maker()
 
 
-def _get_tileserver_write_session() -> sessionmaker:
+def _get_tileserver_write_session() -> AsyncSession:
     """ abstraction used for mocking out a write session """
-    return tile_server_write_session()
+    return tile_server_write_session_maker()
 
 
-@contextmanager
-def get_tileserver_read_session_scope() -> Generator[Session, None, None]:
+@asynccontextmanager
+async def get_tileserver_read_session_scope() -> AsyncGenerator[AsyncSession, None]:
     """
     Provide a transactional scope around a series of operations.
     """
     session = _get_tileserver_read_session()
     try:
         yield session
-        session.commit()
-    except Exception:
-        session.rollback()
-        raise
     finally:
-        session.close()
+        await session.close()
 
 
-@contextmanager
-def get_tileserver_write_session_scope() -> Generator[Session, None, None]:
+@asynccontextmanager
+async def get_tileserver_write_session_scope() -> AsyncGenerator[AsyncSession, None]:
     """Provide a transactional scope around a series of operations."""
     session = _get_tileserver_write_session()
     try:
         yield session
-        session.commit()
+        await session.commit()
     except:
-        session.rollback()
+        await session.rollback()
         raise
     finally:
-        session.close()
+        await session.close()
