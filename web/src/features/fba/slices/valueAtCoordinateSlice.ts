4ca05999fe299aec15334cd15227e9cdab840229
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-08-11 16:15:15 -0700

    added slice

---
+++ web/src/features/fba/slices/valueAtCoordinateSlice.ts
@@ -0,0 +1,54 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { AppThunk } from 'app/store'
+import { logError } from 'utils/error'
+import { getValueAtCoordinate } from 'api/fbaAPI'
+
+interface State {
+  loading: boolean
+  error: string | null
+  valueAtCoordinate: number | undefined
+}
+
+const initialState: State = {
+  loading: false,
+  error: null,
+  valueAtCoordinate: undefined
+}
+
+const valueAtCoordinateSlice = createSlice({
+  name: 'valueAtCoordinate',
+  initialState,
+  reducers: {
+    getValueAtCoordinateStart(state: State) {
+      state.error = null
+      state.loading = true
+      state.valueAtCoordinate = undefined
+    },
+    getValueAtCoordinateFailed(state: State, action: PayloadAction<string>) {
+      state.error = action.payload
+      state.loading = false
+    },
+    getValueAtCoordinateSuccess(state: State, action: PayloadAction<number>) {
+      state.error = null
+      state.valueAtCoordinate = action.payload
+      state.loading = false
+    }
+  }
+})
+
+export const { getValueAtCoordinateStart, getValueAtCoordinateFailed, getValueAtCoordinateSuccess } =
+  valueAtCoordinateSlice.actions
+
+export default valueAtCoordinateSlice.reducer
+
+export const fetchValueAtCoordinate =
+  (layer: string, latitude: number, longitude: number): AppThunk =>
+  async dispatch => {
+    try {
+      const value = await getValueAtCoordinate(layer, latitude, longitude)
+      dispatch(getValueAtCoordinateSuccess(value))
+    } catch (err) {
+      dispatch(getValueAtCoordinateFailed((err as Error).toString()))
+      logError(err)
+    }
+  }
