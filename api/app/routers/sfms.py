683fee8e62fc0620d31d5530cf647f6161c72f2d
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-08-22 14:59:18 -0700

    added a comment

--- api/app/routers/sfms.py
+++ api/app/routers/sfms.py
@@ -13,7 +13,20 @@ router = APIRouter(
 
 @router.post('/process')
 def process(request: ProcessRequest):
-    """ Process a request """
+    """
+    Trigger the SFMS process to run. The idea is that some other process has placed the assets
+    that are needed in the appropriate location, and then notifies us that we can start processing
+    them.
+    ```
+    curl -X 'POST' \\
+        'https://psu.nrs.gov.bc.ca/api/sfms/process' \\
+        -H 'accept: application/json' \\
+        -H 'Content-Type: application/json' \\
+        -d '{
+        "secret": "string"
+    }'
+    ```
+    """
     if request.secret == config.get('SFMS_SECRET'):
         # TODO: Put message on queue to trigger processing of new HFI data.
         return Response(status_code=200)
