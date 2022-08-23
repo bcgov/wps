9cd57cd7d51a33b2c52d5f311790dce42fec625c
Conor Brady <con.brad@gmail.com>
2022-08-23 15:38:01 -0700

    Crud and models

--- api/advisory/db/crud.py
+++ api/advisory/db/crud.py
@@ -1,10 +1,14 @@
 """ Create/Read/Update/Delete """
 from datetime import date
+import logging
+from time import perf_counter
 from typing import List
 from sqlalchemy import select
 from sqlalchemy.engine.row import Row
 from sqlalchemy.ext.asyncio import AsyncSession
-from advisory.db.models.tileserver import (Hfi, FireZone)
+from advisory.db.models.tileserver import (Hfi, FireZone, SimpleFireZone)
+
+logger = logging.getLogger(__name__)
 
 
 async def get_hfi_area_percentages(session: AsyncSession, for_date: date) -> List[Row]:
@@ -14,6 +18,7 @@ async def get_hfi_area_percentages(session: AsyncSession, for_date: date) -> Lis
     within that fire zone. Using those two values, you can then calculate the percentage of the
     zone that has a high hfi.
     """
+    perf_start = perf_counter()
     stmt = select(FireZone.id,
                   FireZone.mof_fire_zone_id,
                   FireZone.mof_fire_zone_name,
@@ -24,4 +29,33 @@ async def get_hfi_area_percentages(session: AsyncSession, for_date: date) -> Lis
         .where(Hfi.date == for_date)\
         .group_by(FireZone.id)
     result = await session.execute(stmt)
-    return result.all()
+    all = result.all()
+    perf_end = perf_counter()
+    delta = perf_end - perf_start
+    logger.info('%f delta count before and after hfi area / complex fire zone query', delta)
+    return all
+
+
+async def get_hfi_area_percentages(session: AsyncSession, for_date: date) -> List[Row]:
+    """ This uses simplified zone polygons to speed up query
+
+    For each fire zone, it gives you the area of the fire zone, and the area of hfi polygons
+    within that fire zone. Using those two values, you can then calculate the percentage of the
+    zone that has a high hfi.
+    """
+    perf_start = perf_counter()
+    stmt = select(SimpleFireZone.id,
+                  SimpleFireZone.mof_fire_zone_id,
+                  SimpleFireZone.mof_fire_zone_name,
+                  SimpleFireZone.geom.ST_Transform(3005).ST_Area().label('zone_area'),
+                  Hfi.wkb_geometry.ST_Union().ST_Intersection(SimpleFireZone.geom)
+                  .ST_Transform(3005).ST_Area().label('hfi_area'))\
+        .join(Hfi, Hfi.wkb_geometry.ST_Intersects(SimpleFireZone.geom))\
+        .where(Hfi.date == for_date)\
+        .group_by(SimpleFireZone.id)
+    result = await session.execute(stmt)
+    all = result.all()
+    perf_end = perf_counter()
+    delta = perf_end - perf_start
+    logger.info('%f delta count before and after hfi area / simple fire zones query', delta)
+    return all
