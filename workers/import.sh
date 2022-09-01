afe45e44884a7dd1b0ac2eb35f80ad4bdc508cb4
Sybrand Strauss <sybrand.strauss@gmail.com>
2022-08-31 17:38:57 -0700

    remove redundant folder

--- workers/import.sh
+++
@@ -1,4 +0,0 @@
-for file in `ls zones/*.json`; do
-    echo $file
-    ogr2ogr -f "PostgreSQL" PG:"dbname=tileserv host=localhost user=tileserv password=tileserv" $file -nlt MULTIPOLYGON -lco precision=NO -nln zones_wgs84
-done
