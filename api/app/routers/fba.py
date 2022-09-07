35dbca2461b7147f24a5e8ba6579d5d614f10db7
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-09-07 15:50:17 -0700

    squishing alembic migrations

--- api/app/routers/fba.py
+++ api/app/routers/fba.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix="/fba",
-    # dependencies=[Depends(authentication_required), Depends(audit)],
+    dependencies=[Depends(authentication_required), Depends(audit)],
 )
 
 
@@ -33,13 +33,13 @@ async def get_all_fire_centers(_=Depends(authentication_required)):
 
 @router.get('/fire-zone-areas/{run_type}/{run_date}/{for_date}',
             response_model=FireZoneAreaListResponse)
-# async def get_zones(for_date: date, _=Depends(authentication_required)):
-async def get_zones(run_type: RunType, run_date: date, for_date: date):
+async def get_zones(run_type: RunType, run_date: date, for_date: date, _=Depends(authentication_required)):
     """ Return area of each zone, and percentage of area of zone with high hfi. """
     async with get_async_read_session_scope() as session:
         zones = []
 
-        # this is a slow step! checking to see if it's there, then making it! that's nuts!
+        # this is a slow step! checking to see if it's there, then making it! this is just a
+        # temporary workaround until we have automation in place.
         hfi = await get_hfi(session, RunTypeEnum(run_type.value), run_date, for_date)
         if hfi.first() is None:
             await process_hfi(run_type, run_date, for_date)
