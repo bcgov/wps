54247f8ad69b0f9ab685fa73ac077f43ae3f0fd1
Andrea Williams <andrea.williams@gov.bc.ca>
2022-09-12 16:51:14 -0700

    Removing file from PR - it shouldn't have been there

--- api/app/wildfire_one/query_builders.py
+++ api/app/wildfire_one/query_builders.py
@@ -26,9 +26,7 @@ class BuildQueryStations(BuildQuery):
         super().__init__()
         self.param_query = None
         # In conversation with Dana Hicks, on Apr 20, 2021 - Dana said to show active, test and project.
-        # Added Disabled status to fix situation where stations Disabled in WFWX API are being used in
-        # HFI Calc, causing Planning Area-level calculations to break due to missing station data.
-        for status in ('ACTIVE', 'TEST', 'PROJECT', 'DISABLED'):
+        for status in ('ACTIVE', 'TEST', 'PROJECT'):
             if self.param_query:
                 self.param_query += f',stationStatus.id=="{status}"'
             else:
