c2e40d6b7b9f0b80db0c837c9cfa7e228d504ea7
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-09-14 16:34:17 -0700

    firming up naming on sfms streams

---
+++ api/app/auto_spatial_advisory/nats.py
@@ -0,0 +1,9 @@
+from typing import Final
+import logging
+from app import config
+
+logger = logging.getLogger(__name__)
+
+stream_prefix: Final = config.get('NATS_STREAM_PREFIX')
+stream_name: Final = f'{stream_prefix}sfms'
+subjects: Final = ['sfms.*', ]
