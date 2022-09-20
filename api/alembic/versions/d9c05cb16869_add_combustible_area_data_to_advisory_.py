a3b2b698aaef6ae89eca9156c03bf5f151cc7f22
Andrea Williams <andrea.williams@gov.bc.ca>
2022-09-20 12:32:43 -0700

    DB migration w contextmanager and generator

--- api/alembic/versions/d9c05cb16869_add_combustible_area_data_to_advisory_.py
+++ api/alembic/versions/d9c05cb16869_add_combustible_area_data_to_advisory_.py
@@ -6,8 +6,12 @@ Create Date: 2022-09-09 15:52:22.162223
 
 """
 from alembic import op
+import sqlalchemy as sa
+import geoalchemy2
+from sqlalchemy.orm.session import Session
 import asyncio
-from app.auto_spatial_advisory.calculate_combustible_land_area import calculate_combustible_area_by_fire_zone
+from shapely import wkb
+from app.auto_spatial_advisory.calculate_combustible_land_area import calculate_combustible_area_by_fire_zone, get_fuel_types_from_object_store
 
 
 # revision identifiers, used by Alembic.
@@ -16,17 +20,50 @@ down_revision = '0669994d4089'
 branch_labels = None
 depends_on = None
 
+shape_type_table = sa.Table('advisory_shape_types', sa.MetaData(),
+                            sa.Column('id', sa.Integer),
+                            sa.Column('name', sa.String))
+
+shape_table = sa.Table('advisory_shapes', sa.MetaData(),
+                       sa.Column('id', sa.Integer),
+                       sa.Column('source_identifier', sa.String),
+                       sa.Column('shape_type', sa.Integer),
+                       sa.Column('geom', geoalchemy2.Geometry))
+
+
+def get_fire_zone_shape_type_id(session: Session):
+    statement = shape_type_table.select().where(shape_type_table.c.name == 'fire_zone')
+    result = session.execute(statement).fetchone()
+    return result.id
+
+
+def get_fire_zones(session: Session, fire_zone_type_id: int):
+    statement = shape_table.select().where(shape_table.c.shape_type == fire_zone_type_id)
+    result = session.execute(statement).fetchall()
+    return result
+
 
 def upgrade():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    zone_areas = loop.run_until_complete(calculate_combustible_area_by_fire_zone())
+    session = Session(bind=op.get_bind())
 
-    for zone in zone_areas:
-        op.execute('UPDATE advisory_shapes SET combustible_area = {} WHERE source_identifier LIKE \'{}\''.format(
-            zone['combustible_area'], str(zone['source_identifier'])
-        ))
+    with get_fuel_types_from_object_store() as fuel_types:
+        # fetch all fire zones from DB
+        fire_zone_shape_type_id = get_fire_zone_shape_type_id(session)
+        zones = get_fire_zones(session, fire_zone_shape_type_id)
+
+        zone_areas = calculate_combustible_area_by_fire_zone(fuel_types, zones)
+        for tuple in zone_areas:
+            op.execute('UPDATE advisory_shapes SET combustible_area={} WHERE source_identifier LIKE \'{}\''.format(
+                tuple[1], tuple[0])
+            )
 
 
 def downgrade():
-    op.execute('UPDATE advisory_shapes SET combustible_area = NULL')
+    session = Session(bind=op.get_bind())
+    fire_zone_shape_id = get_fire_zone_shape_type_id(session)
+    zones = get_fire_zones(session, fire_zone_shape_id)
+
+    for zone in zones:
+        op.execute('UPDATE advisory_shapes SET combustible_area = NULL WHERE source_identifier LIKE \'{}\''.format(
+            str(zone['source_identifier'])
+        ))
--- api/app/auto_spatial_advisory/calculate_combustible_land_area.py
+++ api/app/auto_spatial_advisory/calculate_combustible_land_area.py
@@ -1,8 +1,10 @@
-from typing import Dict, List
+from contextlib import contextmanager
+from typing import Generator, Tuple
 import logging
 from osgeo import gdal, ogr
 import asyncio
-from geoalchemy2 import functions
+from geoalchemy2.shape import to_shape
+from shapely import wkb
 from sqlalchemy import select
 from app.db.database import get_async_read_session_scope
 from app.db.models.auto_spatial_advisory import Shape
@@ -20,7 +22,12 @@ The fuel types vector file is retrieved from our object store bucket, and the fi
 logger = logging.getLogger(__name__)
 
 
-async def calculate_combustible_area_by_fire_zone() -> List[Dict[int, float]]:
+@contextmanager
+def get_fuel_types_from_object_store():
+    """
+    Fetches simplified fuel type layer shapefile from object store, filters out
+    non-combustible fuel types, and returns the layer.
+    """
     # Simplified fuel type layer that SFMS system uses (fbp2021.tif) has been converted to EPSG:3005,
     # polygonized, and saved to our object store bucket.
     gdal.SetConfigOption('AWS_SECRET_ACCESS_KEY', config.get('OBJECT_STORE_SECRET'))
@@ -31,62 +38,43 @@ async def calculate_combustible_area_by_fire_zone() -> List[Dict[int, float]]:
     fuel_types_vector_filepath = f'/vsis3/{bucket}/ftl/fuel_types_epsg_3005.shp'
 
     driver = ogr.GetDriverByName('ESRI Shapefile')
-    try:
-        fuel_types = driver.Open(fuel_types_vector_filepath, gdal.GA_ReadOnly)
-        logger.info('Retrieving fuel types layer from {}'.format(fuel_types_vector_filepath))
-    except Exception as exc:
-        logger.error('Failed to retrieve fuel types layer from {}'.format(fuel_types_vector_filepath))
-        logger.error(exc)
-
-    if fuel_types is not None:
-        fuel_types_layer = fuel_types.GetLayer()
-    else:
-        logger.error('Retrieved None when ogr opened {}'.format(fuel_types_vector_filepath))
-        return []
-
-    async with get_async_read_session_scope() as session:
-        stmt = select(Shape.id,
-                      Shape.source_identifier,
-                      functions.ST_AsText(Shape.geom).label('geom_wkt'),
-                      Shape.geom.ST_Area().label('zone_area'))
-        result = await session.execute(stmt)
-        all_zones = result.all()
+    fuel_types = driver.Open(fuel_types_vector_filepath, gdal.GA_ReadOnly)
+    logger.info('Retrieving fuel types layer from {}'.format(fuel_types_vector_filepath))
+    fuel_types_layer = fuel_types.GetLayer()
 
     # Filter out non-combustible fuel types
     fuel_types_layer.SetAttributeFilter('"Band 1" > 0 and ("Band 1" < 99 or "Band 1" > 103)')
 
-    zone_combustible_area_dicts: List[Dict[int, float]] = []
+    yield fuel_types_layer
 
-    for zone in all_zones:
-        zone_geom = ogr.CreateGeometryFromWkt(zone.geom_wkt)
+
+def calculate_combustible_area_by_fire_zone(fuel_types_layer, zones) -> Generator[Tuple[str, float], None, None]:
+    """
+    Given layer of combustible fuel types for BC and fire zone ID and geometry,
+    calculates the intersection of zone geom and combustible fuels.
+    Yields fire zone's combustible area in square metres.
+    """
+    for zone in zones:
+        zone_wkb = zone.geom
+        shapely_zone_geom = to_shape(zone_wkb)
+        zone_wkt = shapely_zone_geom.wkt
+        zone_geom = ogr.CreateGeometryFromWkt(zone_wkt)
         fuel_types_layer.SetSpatialFilter(zone_geom)
         # create GeometryCollection of all filtered fuel type polygons
         geom_collection = ogr.Geometry(ogr.wkbGeometryCollection)
         for fuel_type_geom in fuel_types_layer:
             geom_collection.AddGeometry(fuel_type_geom.geometry())
-
         # ensure the GeometryCollection is valid
         geom_collection_valid = geom_collection.MakeValid()
-
         # this line is necessary because otherwise Fraser Fire Zone can't undergo spatial
         # comparison for calculating Intersection. Likely due to the fuel types polygons
         # self-intersecting somewhere within the Fraser Fire Zone. Buffer(0) tries to
         # correct the self-intersection by "guessing", but it isn't always right.
         # https://gis.stackexchange.com/questions/311209/how-to-fix-invalid-polygon-with-self-intersection-python
         buffered_geom_collection = geom_collection_valid.Buffer(0)
-
         # get intersection
         intersection = buffered_geom_collection.Intersection(zone_geom)
         if intersection is not None and intersection.GetArea() > 0:
-            intersect_area = intersection.GetArea()
-            zone_dict = {'source_identifier': zone.source_identifier, 'combustible_area': intersect_area}
-            zone_combustible_area_dicts.append(zone_dict)
-
-    logger.info('Finished calculating combustible areas for all fire zones.')
-    return zone_combustible_area_dicts
-
-
-if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(calculate_combustible_area_by_fire_zone())
+            yield (str(zone['source_identifier']), intersection.GetArea())
+        else:
+            yield (None, None)
