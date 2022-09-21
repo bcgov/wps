4b5fc188cd0b6cec916bbf9cfe12ed2ca1aaf13c
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-09-21 15:46:33 -0700

    Sybrand/combust (#2260)
    Pull the SFMS fuel type layer into our database as a collection of polygons
    Provide another approach of calculating combustible area in zone, using PostGIS to intersect the zones & fuel types

--- api/app/db/crud/auto_spatial_advisory.py
+++ api/app/db/crud/auto_spatial_advisory.py
@@ -6,7 +6,8 @@ from typing import List
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.engine.row import Row
-from app.db.models.auto_spatial_advisory import Shape, ClassifiedHfi, HfiClassificationThreshold, RunTypeEnum
+from app.db.models.auto_spatial_advisory import (
+    Shape, ClassifiedHfi, HfiClassificationThreshold, RunTypeEnum, FuelType)
 
 
 logger = logging.getLogger(__name__)
@@ -29,6 +30,10 @@ async def save_hfi(session: AsyncSession, hfi: ClassifiedHfi):
     session.add(hfi)
 
 
+async def save_fuel_type(session: AsyncSession, fuel_type: FuelType):
+    session.add(fuel_type)
+
+
 async def get_hfi(session: AsyncSession, run_type: RunTypeEnum, run_date: date, for_date: date):
     stmt = select(ClassifiedHfi).where(
         ClassifiedHfi.run_type == run_type,
@@ -36,6 +41,43 @@ async def get_hfi(session: AsyncSession, run_type: RunTypeEnum, run_date: date, 
         ClassifiedHfi.run_date == run_date)
     result = await session.execute(stmt)
     return result.scalars()
+
+
+async def get_combustible_area(session: AsyncSession):
+    """ Get the combustible area for each "shape". This is slow, and we don't expect it to run
+    in real time.
+
+    This method isn't being used right now, but you can calculate the combustible area for each
+    zone as follows:
+
+    ```python
+    from app.db.crud.auto_spatial_advisory import get_combustible_area
+    from app.db.database import get_async_read_session_scope
+
+    async with get_async_read_session_scope() as session:
+    result = await get_combustible_area(session)
+
+    for record in result:
+        print(record)
+        print(record['combustible_area']/record['zone_area'])
+    ```
+
+    """
+    logger.info('starting zone/combustible area intersection query')
+    perf_start = perf_counter()
+    stmt = select(Shape.id,
+                  Shape.source_identifier,
+                  Shape.geom.ST_Area().label('zone_area'),
+                  FuelType.geom.ST_Union().ST_Intersection(Shape.geom).ST_Area().label('combustible_area'))\
+        .join(FuelType, FuelType.geom.ST_Intersects(Shape.geom))\
+        .where(FuelType.fuel_type_id.not_in((-10000, 99, 100, 102, 103)))\
+        .group_by(Shape.id)
+    result = await session.execute(stmt)
+    all_combustible = result.all()
+    perf_end = perf_counter()
+    delta = perf_end - perf_start
+    logger.info('%f delta count before and after hfi area + zone/area intersection query', delta)
+    return all_combustible
 
 
 async def get_hfi_area(session: AsyncSession,
@@ -48,6 +90,7 @@ async def get_hfi_area(session: AsyncSession,
     within that fire zone. Using those two values, you can then calculate the percentage of the
     zone that has a high hfi.
     """
+    logger.info('starting zone/area intersection query')
     perf_start = perf_counter()
     stmt = select(Shape.id,
                   Shape.source_identifier,
@@ -63,5 +106,5 @@ async def get_hfi_area(session: AsyncSession,
     all_hfi = result.all()
     perf_end = perf_counter()
     delta = perf_end - perf_start
-    logger.info('%f delta count before and after hfi area / complex fire zone query', delta)
+    logger.info('%f delta count before and after hfi area + zone/area intersection query', delta)
     return all_hfi
