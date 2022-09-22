80127df46401fa8731368ee1b1bbeacff8d067a2
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-09-22 12:12:44 -0700

    default to 4 workers for dev, 8 workers for prod

--- api/start.sh
+++ api/start.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env sh
 set -e
 
+# run pre-start scripts
 ./prestart.sh
-poetry run gunicorn app.main:app --keep-alive 65 --workers 8 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8080
+# set some defaults
+GUNICORN_WORKERS="${GUNICORN_WORKERS:-4}"
+# start the server
+poetry run gunicorn app.main:app --keep-alive 65 --workers $GUNICORN_WORKERS --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8080
