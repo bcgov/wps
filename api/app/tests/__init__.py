06cd33eaca60c0b88cd4dcd0cddc50066741f7f1
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-09-22 17:20:17 -0700

    bring them back

--- api/app/tests/__init__.py
+++ api/app/tests/__init__.py
@@ -1,2 +1,190 @@
 """ Util & common files for tests
 """
+from typing import IO, Any, Callable, Optional, Tuple
+import os
+import sys
+import datetime
+import json
+import importlib
+import jsonpickle
+from app.db.models.common import TZTimeStamp
+
+
+def get_complete_filename(module_path: str, filename: str):
+    """ Get the full path of a filename, given it's module path """
+    dirname = os.path.dirname(os.path.realpath(module_path))
+    return os.path.join(dirname, filename)
+
+
+def _load_json_file(module_path: str, filename: str) -> Optional[dict]:
+    """ Load json file given a module path and a filename """
+    if filename == 'None':  # Not the best solution...
+        return None
+    if filename:
+        with open(get_complete_filename(module_path, filename), encoding="utf-8") as file_pointer:
+            return json.load(file_pointer)
+    return None
+
+
+def _load_json_file_with_name(module_path: str, filename: str) -> Tuple[Optional[dict], str]:
+    """ Load json file given a module path and a filename """
+    if filename == 'None':  # Not the best solution...
+        return None, filename
+    if filename:
+        with open(get_complete_filename(module_path, filename), encoding="utf-8") as file_pointer:
+            return json.load(file_pointer), filename
+    return None, filename
+
+
+def load_json_file(module_path: str) -> Callable[[str], dict]:
+    """ Return a function that can load json from a filename and return a dict """
+    def _json_loader(filename: str):
+        return _load_json_file(module_path, filename)
+    return _json_loader
+
+
+def load_json_file_with_name(module_path: str) -> Callable[[str], dict]:
+    """ Return a function that can load a json from a filename and return a dict, but also the filename """
+    def _json_loader(filename: str):
+        return _load_json_file_with_name(module_path, filename)
+    return _json_loader
+
+
+def json_converter(item: object):
+    """ Add datetime serialization """
+    if isinstance(item, datetime.datetime):
+        return item.isoformat()
+    return None
+
+
+def dump_sqlalchemy_row_data_to_json(response, target: IO[Any]):
+    """ Useful for dumping sqlalchemy responses to json in for unit tests. """
+    result = []
+    for response_row in response:
+        result.append(jsonpickle.encode(response_row))
+    target.write(jsonpickle.encode(result))
+
+
+def dump_sqlalchemy_mapped_object_response_to_json(response, target: IO[Any]):
+    """ Useful for dumping sqlalchemy responses to json in for unit tests.
+
+    e.g. if we want to store the response for GDPS predictions for two stations, we could write the
+    following code:
+    ```python
+    query = get_station_model_predictions_order_by_prediction_timestamp(
+        session, [322, 838], ModelEnum.GDPS, back_5_days, now)
+    with open('tmp.json', 'w') as tmp:
+        dump_sqlalchemy_response_to_json(query, tmp)
+    ```
+    """
+    result = []
+    for row in response:
+        result_row = []
+        for record in row:
+            # Copy the dict so we can safely change it.
+            data = dict(record.__dict__)
+            # Pop internal value
+            data.pop('_sa_instance_state')
+            result_row.append(
+                {
+                    'module': type(record).__module__,
+                    'class': type(record).__name__,
+                    'data': data
+                }
+            )
+        result.append(result_row)
+    json.dump(result, fp=target, default=json_converter, indent=3)
+
+
+def load_sqlalchemy_response_from_json(filename):
+    """ Load a sqlalchemy response from a json file """
+    with open(filename, 'r', encoding="utf-8") as tmp:
+        data = json.load(tmp)
+    return load_sqlalchemy_response_from_object(data)
+
+
+def de_serialize_record(record):
+    """ De-serailize a single sqlalchemy record """
+    module = importlib.import_module(record['module'])
+    class_ = getattr(module, record['class'])
+    record_data = {}
+    for key, value in record['data'].items():
+        # Handle the special case, where the type is timestamp, converting the string to the
+        # correct data type.
+        if isinstance(getattr(class_, key).type, TZTimeStamp):
+            record_data[key] = datetime.datetime.fromisoformat(value)
+        else:
+            record_data[key] = value
+    return class_(**record_data)
+
+
+def load_sqlalchemy_response_from_object(data: object):
+    """ Load a sqlalchemy response from an object """
+    # Usualy the data is a list of objects - or a list of list of objects.
+    # e.g.: [ { record }]
+    # e.g.: or [ [{record}, {record}]]
+    if isinstance(data, list):
+        result = []
+        for row in data:
+            result_row = []
+            for record in row:
+                object_ = de_serialize_record(record)
+                result_row.append(object_)
+            result.append(result_row)
+        return result
+    # Sometimes though, we're only expecting a single record, not a list.
+    return de_serialize_record(data)
+
+
+def apply_crud_mapping(monkeypatch, crud_mapping: dict, module_path: str):
+    """ Mock the sql response
+    The crud response was generated by temporarily introducing
+    "dump_sqlalchemy_row_data_to_json" and "dump_sqlalchemy_mapped_object_response_to_json"
+    in code - and saving the database responses.
+    """
+
+    if crud_mapping:
+        for item in crud_mapping:
+            if item['serializer'] == "jsonpickle":
+                _jsonpickle_patch_function(monkeypatch,
+                                           item['module'], item['function'], item['json'], module_path)
+            else:
+                _json_patch_function(monkeypatch,
+                                     item['module'], item['function'], item['json'], module_path)
+
+    return {}
+
+
+def _jsonpickle_patch_function(
+        monkeypatch,
+        module_name: str,
+        function_name: str,
+        json_filename: str,
+        module_path: str):
+    """ Patch module_name.function_name to return de-serialized json_filename """
+    def mock_get_data(*_):
+        filename = get_complete_filename(module_path, json_filename)
+        with open(filename, encoding="utf-8") as file_pointer:
+            rows = jsonpickle.decode(file_pointer.read())
+            for row in rows:
+                # Workaround to remain compatible with old tests. Ideally we would just always pickle the row.
+                if isinstance(row, str):
+                    yield jsonpickle.decode(row)
+                    continue
+                yield row
+
+    monkeypatch.setattr(importlib.import_module(module_name), function_name, mock_get_data)
+
+
+def _json_patch_function(monkeypatch,
+                         module_name: str,
+                         function_name: str,
+                         json_filename: str,
+                         module_path: str):
+    """ Patch module_name.function_name to return de-serialized json_filename """
+    def mock_get_data(*_):
+        filename = get_complete_filename(module_path, json_filename)
+        with open(filename, encoding="utf-8") as file_pointer:
+            return json.load(file_pointer)
+
+    monkeypatch.setattr(importlib.import_module(module_name), function_name, mock_get_data)
--- api/app/tests/conftest.py
+++ api/app/tests/conftest.py
@@ -1,1 +1,196 @@
 """ Global fixtures """
+from datetime import timezone, datetime
+import logging
+from unittest.mock import MagicMock
+import requests
+import pytest
+from pytest_mock import MockerFixture
+from pytest_bdd import then, parsers
+import app.utils.s3
+from app.utils.time import get_pst_tz
+from app import auth
+from app.tests.common import (
+    MockJWTDecode, default_aiobotocore_get_session, default_mock_requests_get,
+    default_mock_requests_post, default_mock_requests_session_get,
+    default_mock_requests_session_post)
+import app.db.database
+from app.schemas.shared import WeatherDataRequest
+import app.wildfire_one.wildfire_fetchers
+import app.utils.redis
+from app.tests import load_json_file
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(autouse=True)
+def mock_env(monkeypatch):
+    """ Automatically mock environment variable """
+    monkeypatch.setenv("BASE_URI", "https://python-test-base-uri")
+    monkeypatch.setenv("USE_WFWX", "False")
+    monkeypatch.setenv("WFWX_USER", "user")
+    monkeypatch.setenv("WFWX_SECRET", "secret")
+    monkeypatch.setenv(
+        "WFWX_AUTH_URL", "https://wf1/pub/oauth2/v1/oauth/token")
+    monkeypatch.setenv("WFWX_BASE_URL", "https://wf1/wfwx")
+    monkeypatch.setenv("WFWX_MAX_PAGE_SIZE", "1000")
+    monkeypatch.setenv("KEYCLOAK_PUBLIC_KEY", "public_key")
+    monkeypatch.setenv("BC_FIRE_WEATHER_USER", "someuser")
+    monkeypatch.setenv("BC_FIRE_WEATHER_SECRET", "password")
+    monkeypatch.setenv("BC_FIRE_WEATHER_FILTER_ID", "1")
+    monkeypatch.setenv("OPENSHIFT_BASE_URI",
+                       "https://console.pathfinder.gov.bc.ca:8443")
+    monkeypatch.setenv("PROJECT_NAMESPACE", "project_namespace")
+    monkeypatch.setenv("STATUS_CHECKER_SECRET", "some_secret")
+    monkeypatch.setenv("PATRONI_CLUSTER_NAME", "some_suffix")
+    monkeypatch.setenv("ROCKET_URL_POST_MESSAGE",
+                       "https://rc-notifications-test.ca/api/v1/chat.postMessage")
+    monkeypatch.setenv("ROCKET_AUTH_TOKEN", "sometoken")
+    monkeypatch.setenv("ROCKET_USER_ID", "someid")
+    monkeypatch.setenv("ROCKET_CHANNEL", "#channel")
+    monkeypatch.setenv("OPENSHIFT_NAMESPACE_API", "apis/apps/v1beta1/namespaces/")
+    monkeypatch.setenv("OBJECT_STORE_SERVER", "some server")
+    monkeypatch.setenv("OBJECT_STORE_USER_ID", "some user id")
+    monkeypatch.setenv("OBJECT_STORE_SECRET", "some secret")
+    monkeypatch.setenv("OBJECT_STORE_BUCKET", "some bucket")
+
+
+@pytest.fixture(autouse=True)
+def mock_aiobotocore_get_session(monkeypatch):
+    """ Patch the session by default """
+    monkeypatch.setattr(app.utils.s3, 'get_session', default_aiobotocore_get_session)
+
+
+@pytest.fixture(autouse=True)
+def mock_requests(monkeypatch):
+    """ Patch all calls to request.get by default.
+    """
+    monkeypatch.setattr(requests, 'get', default_mock_requests_get)
+    monkeypatch.setattr(requests, 'post', default_mock_requests_post)
+
+
+@pytest.fixture(autouse=True)
+def mock_redis(monkeypatch):
+    """ Patch redis by default """
+    class MockRedis():
+        """ mocked redis class """
+
+        def __init__(self) -> None:
+            """ Mock init """
+
+        def get(self, name):  # pylint: disable=unused-argument
+            """ mock get """
+            return None
+
+        def set(self,  # pylint: disable=unused-argument, invalid-name, unused-argument, too-many-arguments
+                name, value,
+                ex=None, px=None, nx=False, xx=False, keepttl=False):  # pylint: disable=invalid-name
+            """ mock set """
+
+        def delete(self, name):  # pylint: disable=unused-argument
+            """ mock delete """
+
+    def create_mock_redis():
+        return MockRedis()
+    monkeypatch.setattr(app.utils.redis, '_create_redis', create_mock_redis)
+
+
+@pytest.fixture(autouse=True)
+def mock_get_now(monkeypatch):
+    """ Patch all calls to app.util.time: get_utc_now and get_pst_now  """
+    # May 21, 2020
+    timestamp = 1590076213962 / 1000
+
+    # The default value for WeatherDataRequest cannot be mocked out, as it
+    # is declared prior to test mocks being loaded. We manipulate the class
+    # directly in order to have the desire default be deterministic.
+    WeatherDataRequest.__fields__['time_of_interest'].default = datetime.fromtimestamp(
+        timestamp, tz=timezone.utc)
+
+    def mock_utc_now():
+        return datetime.fromtimestamp(timestamp, tz=timezone.utc)
+
+    def mock_pst_now():
+        return datetime.fromtimestamp(timestamp, tz=get_pst_tz())
+
+    monkeypatch.setattr(app.utils.time, '_get_utc_now', mock_utc_now)
+    monkeypatch.setattr(app.utils.time, '_get_pst_now', mock_pst_now)
+
+
+@pytest.fixture(autouse=True)
+def mock_get_pst_today_start_and_end(monkeypatch):
+    """ Patch all calls to app.util.time: get_pst_today_start_and_end  """
+
+    def mock_get_pst_today():
+        start = datetime.fromtimestamp(1623974400, tz=get_pst_tz())
+        end = datetime.fromtimestamp(1624060800, tz=get_pst_tz())
+        return start, end
+
+    monkeypatch.setattr(app.utils.time, 'get_pst_today_start_and_end', mock_get_pst_today)
+
+
+@pytest.fixture(autouse=True)
+def mock_session(monkeypatch):
+    """ Ensure that all unit tests mock out the database session by default! """
+    monkeypatch.setattr(app.db.database, '_get_write_session', MagicMock())
+    monkeypatch.setattr(app.db.database, '_get_read_session', MagicMock())
+
+
+@pytest.fixture()
+def mock_env_with_use_wfwx(monkeypatch):
+    """ Set environment variable USE_WFWX to 'True' """
+    monkeypatch.setenv("USE_WFWX", 'True')
+
+
+@pytest.fixture()
+def mock_jwt_decode(monkeypatch):
+    """ Mock pyjwt's decode method """
+
+    # pylint: disable=unused-argument
+    def mock_function(*args, **kwargs):
+        return MockJWTDecode()
+
+    monkeypatch.setattr("jwt.decode", mock_function)
+
+
+@pytest.fixture()
+def mock_requests_session(monkeypatch):
+    """ Patch all calls to requests.Session.* """
+    monkeypatch.setattr(requests.Session, 'get',
+                        default_mock_requests_session_get)
+    monkeypatch.setattr(requests.Session, 'post',
+                        default_mock_requests_session_post)
+    return monkeypatch
+
+
+@pytest.fixture(autouse=True)
+def spy_access_logging(mocker: MockerFixture):
+    """Spies on access audting logging for tests"""
+    return mocker.spy(auth, 'create_api_access_audit_log')
+
+
+@then(parsers.parse('the response status code is {status}'), converters={'status': int})
+def assert_status_code(response, status: int):
+    """ Assert that we receive the expected status code """
+    assert response['response'].status_code == status
+
+
+@then("the response isn't cached")
+def then_response_not_cached(response):
+    """ Check that the response isn't being cached """
+    if response['response'].status_code == 200:
+        assert response['response'].headers.get('cache-control', None) == 'max-age=0'
+
+
+@then(parsers.parse("the response is {response_json}"),
+      converters={'response_json': load_json_file(__file__)})
+def then_response(response, response_json: dict):
+    """ Check entire response """
+    if response_json is not None:
+        # it's very useful having this code hang around:
+        # import json
+        # from app.tests import get_complete_filename
+        # actual = response['response'].json()
+        # actual_filename = get_complete_filename(__file__, 'actual.json')
+        # with open(actual_filename, 'w', encoding="utf-8") as file_pointer:
+        #     json.dump(actual, file_pointer, indent=2)
+        assert response['response'].json() == response_json
