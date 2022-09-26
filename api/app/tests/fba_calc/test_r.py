38a564eb9d5f879f197c20dc11426cfaed14dca9
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-09-26 16:37:05 -0700

    use packaging.version to compare

--- api/app/tests/fba_calc/test_r.py
+++ api/app/tests/fba_calc/test_r.py
@@ -1,5 +1,6 @@
 """ Test the we have a good version of R """
 import logging
+from packaging import version
 from rpy2 import robjects
 
 logger = logging.getLogger(__name__)
@@ -9,19 +10,16 @@ def test_r_version():
     """ Test the we have a good version of R """
     response = robjects.r("version")
     # The response we get from R is pretty gross. It's a list of strings.
-    # We're looking for somethin like: "['R version 4.1.2 (2021-11-01)']"
+    # We're looking for somethin like: "['R version 4fb .1.2 (2021-11-01)']"
     # Would be nice to fix to a particular version, but each developers has a different
     # version of R!
     for item in response:
         # If it contains "version", it's what we're looking for.
         if 'version' in str(item):
             # Look for the version number.
-            version = str(item).split(' ')[3]
-            logger.info('R version: %s', version)
+            r_version = str(item).split(' ')[3]
+            logger.info('R version: %s', r_version)
             # If the major version is >= 4.1.2, we're good.
-            major, minor, patch = version.split('.')
-            assert int(major) >= 4
-            assert int(minor) >= 1
-            assert int(patch) >= 2
+            assert version.parse(r_version) >= version.parse('4.1.2')
             return
     assert False
