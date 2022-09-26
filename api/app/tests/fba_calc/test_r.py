67893425fd9500fe717a474df06c4111bff3e3a7
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2022-09-26 16:21:31 -0700

    what is the R version in githun?

--- api/app/tests/fba_calc/test_r.py
+++ api/app/tests/fba_calc/test_r.py
@@ -1,6 +1,8 @@
 """ Test the we have a good version of R """
-
+import logging
 from rpy2 import robjects
+
+logger = logging.getLogger(__name__)
 
 
 def test_r_version():
@@ -15,6 +17,7 @@ def test_r_version():
         if 'version' in str(item):
             # Look for the version number.
             version = str(item).split(' ')[3]
+            logger.info('R version: %s', version)
             # If the major version is >= 4.1.2, we're good.
             major, minor, patch = version.split('.')
             assert int(major) >= 4
