b8ac7ecc1ea5e67db6948cc7f04f03a91a7d3ad0
Conor Brady <con.brad@gmail.com>
2022-09-27 14:22:34 -0700

    Test with locally working consumer

--- api/app/auto_spatial_advisory/nats_consumer.py
+++ api/app/auto_spatial_advisory/nats_consumer.py
@@ -1,17 +1,3 @@
-# Copyright 2016-2018 The NATS Authors
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import asyncio
 import nats
 from nats.aio.client import Client as NATS
@@ -32,7 +18,7 @@ async def run():
         print("Connection is closed")
 
     nc = await nats.connect(
-        "localhost:4222",
+        server,
         ping_interval=10,
         max_reconnect_attempts=24,
         disconnected_cb=disconnected_cb,
