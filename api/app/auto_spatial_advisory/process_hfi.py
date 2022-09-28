010f5c31f66549d7e381b3482738be73f0aa7b1d
Andrea Williams <andrea.williams@gov.bc.ca>
2022-09-27 17:13:30 -0700

    Process HFI classification via nats msg

--- api/app/auto_spatial_advisory/process_hfi.py
+++ api/app/auto_spatial_advisory/process_hfi.py
@@ -26,6 +26,15 @@ class RunType(Enum):
     FORECAST = 'forecast'
     ACTUAL = 'actual'
 
+    @staticmethod
+    def from_str(label):
+        if label in ('forecast', 'Forecast', 'FORECAST'):
+            return RunType.FORECAST
+        elif label in ('actual', 'Actual', 'ACTUAL'):
+            return RunType.ACTUAL
+        else:
+            raise NotImplementedError
+
 
 class UnknownHFiClassification(Exception):
     """ Raised when the hfi classification is not one of the expected values. """
