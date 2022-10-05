a074c337d62bf2f349dd5e999dfd90e9e315bd16
Conor Brady <con.brad@gmail.com>
2022-10-05 11:54:29 -0700

    Fix potential None case

--- api/app/utils/transform_to_epsg_3005.py
+++ api/app/utils/transform_to_epsg_3005.py
@@ -20,6 +20,10 @@ def transform_shapefile_to_epsg_3005(source_file, new_filename):
         logger.error('Could not open %s', source_file)
 
     source_layer = source_data.GetLayer()
+
+    if source_data is None:
+        logger.error('Could not get layer form %s', source_file)
+
     input_spatial_ref = source_layer.GetSpatialRef()
 
     output_spatial_ref = osr.SpatialReference()
