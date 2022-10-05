89642479282157c6b304207751c5670ac9809322
Conor Brady <con.brad@gmail.com>
2022-10-05 11:49:10 -0700

    Lint fixes

--- api/app/utils/transform_to_epsg_3005.py
+++ api/app/utils/transform_to_epsg_3005.py
@@ -1,3 +1,5 @@
+""" Shapefile helper functions related to transforms
+"""
 import logging
 import sys
 import os
@@ -65,8 +67,7 @@ def transform_shapefile_to_epsg_3005(source_file, new_filename):
     output_dataset = None
 
     del source_data, output_dataset, coords_transform
-
-    return 'Transformed shapefile written to {}'.format(new_filename)
+    return f"Transformed shapefile written to {new_filename}"
 
 
 def transform_geotiff_to_epsg_3005(source_file, new_filename):
@@ -84,16 +85,18 @@ def transform_geotiff_to_epsg_3005(source_file, new_filename):
 
     del source_data
 
-    return 'Transformed geotiff written to {}'.format(new_filename)
+    return f"Transformed geotiff written to {new_filename}"
 
 
 def transform_file(filepath, new_filename):
     if filepath[-3:] == '.tif':
-        transform_geotiff_to_epsg_3005(filepath, new_filename)
-    elif filepath[-3:] == '.shp':
-        transform_shapefile_to_epsg_3005(filepath, new_filename)
-    else:
-        return 'Invalid file format.'
+        result = transform_geotiff_to_epsg_3005(filepath, new_filename)
+        return result
+    if filepath[-3:] == '.shp':
+        result = transform_shapefile_to_epsg_3005(filepath, new_filename)
+        return result
+
+    return 'Invalid file format.'
 
 
 if __name__ == '__main__':
