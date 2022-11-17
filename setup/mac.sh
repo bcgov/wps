4197b58bfdd5e2edef10db4b4b1fa3cb3bf79e4f
Conor Brady <con.brad@gmail.com>
2022-11-17 13:55:06 -0800

    Uncomment

--- setup/mac.sh
+++ setup/mac.sh
@@ -3,44 +3,44 @@
 ## Run this to install all project dependencies on a mac
 
 ### homebrew
-#curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
 ### openjdk
-#brew install openjdk
+brew install openjdk
 
 ### Symlink it for mac
-#sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
 ### gdal
-#brew install gdal
+brew install gdal
 
 ### For generated HFI Calculator PDFs
-#brew install --cask wkhtmltopdf
+brew install --cask wkhtmltopdf
 
 ### pyenv
-#brew install pyenv
-#pyenv install 3.10.4
-#pyenv global 3.10.4
+brew install pyenv
+pyenv install 3.10.4
+pyenv global 3.10.4
 
-### poetry -- run 
-#curl -sSL https://install.python-poetry.org | python -
+### poetry
+curl -sSL https://install.python-poetry.org | python -
 echo "poetry installed, run poetry_setup.sh in api"
 
 ### r
-# brew install --cask r
-# brew install udunits
-# brew install proj
+brew install --cask r
+brew install udunits
+brew install proj
 
 echo "installing r packages, this takes awhile..."
-# r -e 'install.packages(c("rgdal","sf", "units"),,"https://mac.R-project.org")'
-# r -e "install.packages('cffdrs', repos = 'http://cran.us.r-project.org')"
+r -e 'install.packages(c("rgdal","sf", "units"),,"https://mac.R-project.org")'
+r -e "install.packages('cffdrs', repos = 'http://cran.us.r-project.org')"
 echo "finished installing r packages"
 
 ### postgres
 echo "installing and configuring postgres"
-# brew install postgresql
-# brew services start postgresql
-# brew install postgis
+brew install postgresql
+brew services start postgresql
+brew install postgis
 psql -d postgres -c "create database wps;"
 psql -d wps -c "create extension postgis;"
 psql -d wps -c "
