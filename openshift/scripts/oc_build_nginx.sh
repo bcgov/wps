0cf76a667f6764320930db371ea9109039cd6652
Conor Brady <con.brad@gmail.com>
2022-11-17 17:07:42 -0800

    Building and deploying

--- openshift/scripts/oc_build_nginx.sh
+++ openshift/scripts/oc_build_nginx.sh
@@ -2,11 +2,11 @@
 #
 source "$(dirname ${0})/common/envars"
 
-PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
+PROJ_TARGET="e1e498-tools"
 
 OC_NGINX_BUILD="oc -n ${PROJ_TARGET} apply -f ${TEMPLATE_PATH}/tileserv/nginx_build.yaml"
-OC_NGINX_IS="oc create imagestream nginx-tileserv"
-OC_NGINX_START_BUILD="oc start-build nginx-tileserv --from-dir=${TEMPLATE_PATH}/tileserv --follow"
+OC_NGINX_IS="oc -n ${PROJ_TARGET} create imagestream nginx-tileserv"
+OC_NGINX_START_BUILD="oc -n ${PROJ_TARGET} start-build nginx-tileserv --from-dir=${TEMPLATE_PATH}/tileserv --follow"
 
 # Execute commands
 #
--- openshift/scripts/oc_provision_tileserv.sh
+++ openshift/scripts/oc_provision_tileserv.sh
@@ -30,14 +30,6 @@ PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/tileserv/tileserv.yaml \
 -p SUFFIX=${SUFFIX}"
 
-# Nginx build
-OC_NGINX_BUILD="oc -n ${PROJ_TARGET} new-build nginx~https://github.com/bcgov/wps-vector-tileserver.git --context-dir=openshift --name=nginx-tilecache-${SUFFIX}"
-[ "${APPLY}" ] || OC_NGINX_BUILD="${OC_NGINX_BUILD} --dry-run=true"
-
-
-OC_NGINX_START_BUILD="oc -n ${PROJ_TARGET} start-build nginx-tilecache-${SUFFIX} --follow"
-[ "${APPLY}" ] || OC_NGINX_START_BUILD="${OC_NGINX_START_BUILD} --dry-run=true"
-
 # Apply template (apply or use --dry-run)
 #
 OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
@@ -45,8 +37,6 @@ OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
 
 # Execute commands
 #
-eval "${OC_NGINX_BUILD}"
-eval "${OC_NGINX_START_BUILD}"
 eval "${OC_PROCESS}"
 eval "${OC_PROCESS} | ${OC_APPLY}"
 
