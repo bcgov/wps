7908e1152a06a496aa94f997f8f70aced17d0093
Conor Brady <con.brad@gmail.com>
2022-11-21 16:41:04 -0800

    Fix target metadata

--- tileserv/alembic/env.py
+++ tileserv/alembic/env.py
@@ -21,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = models.base.Base
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
