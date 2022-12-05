149eb0f2f2b009486a4192f28555b9c6a422560c
Conor Brady <con.brad@gmail.com>
2022-12-05 15:28:52 -0800

    Task/tileserv pipeline (#2411)
    - Adds tileserv infrastructure as part of deployment pipeline
    - `nginx` cache and `pg_tileserv` are deployed together as a deployment config
        - `nginx` is built from scratch to enable run permissions
        - `pg_tileserv` is pulled in and and cached in artifactory
        - The `postgresql` cluster is deployed via crunchy which abstracts away a lot of patroni: https://access.crunchydata.com/documentation/postgres-operator/v5/tutorial/getting-started/
     - migrations are run in an init-container before deployment of the above
    
    
    Notes:
    - Sonarcloud complains about duplication in migrations and models, which we can't avoid since definitions need to be unique
    -  The `tileserv` module lives on its own outside of our current `api/app` root module since it includes it's own migrations and I think we should move to a more per app module layout so we can selectively deploy applications in the future by leveraging pants: https://www.pantsbuild.org/

--- openshift/scripts/oc_deploy_to_production.sh
+++ openshift/scripts/oc_deploy_to_production.sh
@@ -29,6 +29,7 @@ set -ex
 echo Promote
 MODULE_NAME=api bash $(dirname ${0})/oc_promote.sh ${SUFFIX} ${RUN_TYPE}
 MODULE_NAME=web bash $(dirname ${0})/oc_promote.sh ${SUFFIX} ${RUN_TYPE}
+# MODULE_NAME=tileserv bash $(dirname ${0})/oc_promote.sh ${SUFFIX} ${RUN_TYPE} # TODO: enable in follow up PR
 echo Provision database
 CPU_REQUEST=75m CPU_LIMIT=2000m MEMORY_REQUEST=2Gi MEMORY_LIMIT=16Gi PVC_SIZE=45Gi PROJ_TARGET=${PROJ_TARGET} bash $(dirname ${0})/oc_provision_db.sh prod ${RUN_TYPE}
 echo Provision NATS
