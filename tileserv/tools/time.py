3119925acd0fde7ee1010b318d13e8e79bce48b1
Conor Brady <con.brad@gmail.com>
2022-12-07 16:29:50 -0800

    Skeleton entrypoint functions and time helpers

---
+++ tileserv/tools/time.py
@@ -0,0 +1,22 @@
+import pytz
+from datetime import date, datetime, time
+
+
+def pin_issue_date_to_time(issue_date: date, issue_time: time):
+    """ Pin the time of an issue date to given time"""
+    return datetime.combine(issue_date, issue_time, tzinfo=True)
+
+
+def morning_time(issue_date: date):
+    """ Pin the time of an issue date to morning time, 08:00 """
+    return pin_issue_date_to_time(issue_date, time(8, 0, 0, tzinfo=pytz.timezone('America/Vancouver')))
+
+
+def noon_time(issue_date: date):
+    """ Pin the time of an issue date to noon time, 13:00 """
+    return pin_issue_date_to_time(issue_date, time(13, 0, 0, tzinfo=pytz.timezone('America/Vancouver')))
+
+
+def evening_time(issue_date: date):
+    """ Pin the time of an issue date to evening time, 16:45 """
+    return pin_issue_date_to_time(issue_date, time(16, 45, 0, tzinfo=pytz.timezone('America/Vancouver')))
