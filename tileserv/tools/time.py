402b082d8a4f526a237dccffee43465fb5a070cb
Conor Brady <con.brad@gmail.com>
2022-12-08 13:56:00 -0800

    Use zoneinfo add time tests

--- tileserv/tools/time.py
+++ tileserv/tools/time.py
@@ -1,22 +1,22 @@
-import pytz
+from zoneinfo import ZoneInfo
 from datetime import date, datetime, time
 
 
 def pin_issue_date_to_time(issue_date: date, issue_time: time):
     """ Pin the time of an issue date to given time"""
-    return datetime.combine(issue_date, issue_time, tzinfo=True)
+    return datetime.combine(issue_date, issue_time, tzinfo=ZoneInfo('America/Vancouver'))
 
 
 def morning_time(issue_date: date):
     """ Pin the time of an issue date to morning time, 08:00 """
-    return pin_issue_date_to_time(issue_date, time(8, 0, 0, tzinfo=pytz.timezone('America/Vancouver')))
+    return pin_issue_date_to_time(issue_date, time(8, 0, 0))
 
 
 def noon_time(issue_date: date):
     """ Pin the time of an issue date to noon time, 13:00 """
-    return pin_issue_date_to_time(issue_date, time(13, 0, 0, tzinfo=pytz.timezone('America/Vancouver')))
+    return pin_issue_date_to_time(issue_date, time(13, 0, 0))
 
 
 def evening_time(issue_date: date):
     """ Pin the time of an issue date to evening time, 16:45 """
-    return pin_issue_date_to_time(issue_date, time(16, 45, 0, tzinfo=pytz.timezone('America/Vancouver')))
+    return pin_issue_date_to_time(issue_date, time(16, 45, 0))
