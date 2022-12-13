5535c219b5a4569a8f494e3833c5a3d8a1e6998d
Conor Brady <con.brad@gmail.com>
2022-12-12 16:04:16 -0800

    Rename

--- tileserv/tests/tools/test_tif_pusher.py
+++ tileserv/tests/tools/test_tif_pusher.py
@@ -1,17 +1,14 @@
-from dateutil.tz import tzutc
 import datetime
 
 from tileserv.tools.tif_pusher import build_post_body_for_tiff
 
-utc = tzutc()
-
 
 def test_get_post_body_forecast():
     run_date = datetime.date(2022, 9, 3)
-    object = {'Key': 'sfms/uploads/forecast/2022-09-03/hfi20220904.tif'}
+    tif_object = {'Key': 'sfms/uploads/forecast/2022-09-03/hfi20220904.tif'}
 
-    post_body = build_post_body_for_tiff(object, run_date)
-    assert post_body.get("key") == object.get("Key")
+    post_body = build_post_body_for_tiff(tif_object, run_date)
+    assert post_body.get("key") == tif_object.get("Key")
     assert post_body.get("runtype") == "forecast"
     assert post_body.get("for_date") == "2022-09-04"
     assert post_body.get("run_date") == run_date.isoformat()
@@ -19,10 +16,10 @@ def test_get_post_body_forecast():
 
 def test_get_post_body_actual():
     run_date = datetime.date(2022, 9, 3)
-    object = {'Key': 'sfms/uploads/actual/2022-09-03/hfi20220904.tif'}
+    tif_object = {'Key': 'sfms/uploads/actual/2022-09-03/hfi20220904.tif'}
 
-    post_body = build_post_body_for_tiff(object, run_date)
-    assert post_body.get("key") == object.get("Key")
+    post_body = build_post_body_for_tiff(tif_object, run_date)
+    assert post_body.get("key") == tif_object.get("Key")
     assert post_body.get("runtype") == "actual"
     assert post_body.get("for_date") == "2022-09-04"
     assert post_body.get("run_date") == run_date.isoformat()
