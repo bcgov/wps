ad229c621267860a7dc47abc9d4b65d1f084d2ba
Conor Brady <con.brad@gmail.com>
2022-12-14 13:20:46 -0800

    Larger pvc sizes for tileserv prod, smaller for dev (#2463)
    * Larger pvc sizes for tileserv prod, smaller for dev
    
    * Parameterize replica count

--- openshift/scripts/oc_provision_tileserv.sh
+++ openshift/scripts/oc_provision_tileserv.sh
@@ -29,7 +29,10 @@ OBJ_NAME="tileserv-${SUFFIX}"
 
 # Process template
 OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/tileserv/tileserv.yaml \
--p SUFFIX=${SUFFIX}"
+-p SUFFIX=${SUFFIX} \
+-p REPLICAS=${REPLICAS} \
+-p WAL_PVC_SIZE=${WAL_PVC_SIZE} \
+-p DATA_PVC_SIZE=${DATA_PVC_SIZE}"
 
 # Apply a template (apply or use --dry-run=client)
 #
