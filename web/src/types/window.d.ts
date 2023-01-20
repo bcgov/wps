139f1af56a08b45203dce4bf9cf1cfac97459622
Darren Boss <darren.boss@gov.bc.ca>
2023-01-20 13:48:07 -0800

    Revert the reversion

--- web/src/types/window.d.ts
+++ web/src/types/window.d.ts
@@ -7,6 +7,8 @@ interface Window {
     REACT_APP_KEYCLOAK_CLIENT: string
     API_BASE_URL: string | undefined
     RASTER_SERVER_BASE_URL: string | undefined
+    REACT_APP_MS_TEAMS_SPRINT_REVIEW_URL: string
+    REACT_APP_MIRO_SPRINT_REVIEW_BOARD_URL: string
   }
   Cypress: {} | undefined
 }
