f619e42f7d090a85e7ed5d4c702b95f461b1585a
Conor Brady <con.brad@gmail.com>
2023-01-31 15:47:22 -0800

    threshold to 20

--- web/src/features/fba/pages/FireBehaviourAdvisoryPage.tsx
+++ web/src/features/fba/pages/FireBehaviourAdvisoryPage.tsx
@@ -66,7 +66,7 @@ export const FireBehaviourAdvisoryPage: React.FunctionComponent = () => {
 
   const [fireCenter, setFireCenter] = useState<FireCenter | undefined>(undefined)
 
-  const [advisoryThreshold, setAdvisoryThreshold] = useState(10)
+  const [advisoryThreshold, setAdvisoryThreshold] = useState(20)
   const [issueDate, setIssueDate] = useState<DateTime | null>(null)
   const [selectedFireZoneID, setSelectedFireZoneID] = useState<number | null>(null)
   const [dateOfInterest, setDateOfInterest] = useState(
