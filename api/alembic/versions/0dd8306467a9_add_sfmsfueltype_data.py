166590a70578649f367ca968c7240f9c1af39e2d
Andrea Williams <andrea.williams@gov.bc.ca>
2023-01-31 17:05:16 -0800

    In progress

---
+++ api/alembic/versions/0dd8306467a9_add_sfmsfueltype_data.py
@@ -0,0 +1,24 @@
+"""Add SFMSFuelType data
+
+Revision ID: 0dd8306467a9
+Revises: fdbf57102d06
+Create Date: 2023-01-31 16:56:22.323537
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0dd8306467a9'
+down_revision = 'fdbf57102d06'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
