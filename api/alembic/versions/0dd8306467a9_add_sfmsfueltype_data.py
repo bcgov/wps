cbec4fd0e7f6c0da40f070de0ec31a05ef6fbba9
Andrea Williams <andrea.williams@gov.bc.ca>
2023-01-31 17:28:55 -0800

    Migrations and data import

--- api/alembic/versions/0dd8306467a9_add_sfmsfueltype_data.py
+++ api/alembic/versions/0dd8306467a9_add_sfmsfueltype_data.py
@@ -7,6 +7,7 @@ Create Date: 2023-01-31 16:56:22.323537
 """
 from alembic import op
 import sqlalchemy as sa
+import json
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,18 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    # Load fuel types data
+    with open('app/data/bc_fbp_fuel_type_lookup_table_sfms.json') as fuel_types_file:
+        fuel_types_data = json.load(fuel_types_file)
+
+    for fuel_type in fuel_types_data:
+        fuel_type_id = fuel_type.get('grid_value')
+        fuel_type_code = '\'' + fuel_type.get('fuel_type') + '\''
+        description = '\'' + fuel_type.get('descriptive_name') + '\''
+
+        op.execute('INSERT INTO sfms_fuel_types (fuel_type_id, fuel_type_code, description) VALUES ({}, {}, {})'.format(
+            fuel_type_id, fuel_type_code, description))
 
 
 def downgrade():
-    pass
+    op.execute('DELETE FROM sfms_fuel_types')
