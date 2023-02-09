613862c2e6b9eb67095810e42f380582a9b1590c
Conor Brady <con.brad@gmail.com>
2023-02-08 16:19:10 -0800

    Remove icons, can't import them directly

--- web/src/utils/env.ts
+++ web/src/utils/env.ts
@@ -6,7 +6,9 @@ let ENV = {
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL as string,
   KC_AUTH_URL: import.meta.env.VITE_KEYCLOAK_AUTH_URL as string,
   KC_REALM: import.meta.env.VITE_KEYCLOAK_REALM as string,
-  KC_CLIENT: import.meta.env.VITE_KEYCLOAK_CLIENT as string
+  KC_CLIENT: import.meta.env.VITE_KEYCLOAK_CLIENT as string,
+  MS_TEAMS_SPRINT_REVIEW_URL: import.meta.env.VITE_MS_TEAMS_SPRINT_REVIEW_URL as string,
+  MIRO_SPRINT_REVIEW_BOARD_URL: import.meta.env.VITE_MIRO_SPRINT_REVIEW_BOARD_URL as string
 }
 
 // If the app is built using 'npm run build'
@@ -21,7 +23,9 @@ if (import.meta.env.NODE_ENV === 'production') {
     TEST_AUTH: undefined,
     KC_AUTH_URL: window.env.VITE_KEYCLOAK_AUTH_URL,
     KC_REALM: window.env.VITE_KEYCLOAK_REALM,
-    KC_CLIENT: window.env.VITE_KEYCLOAK_CLIENT
+    KC_CLIENT: window.env.VITE_KEYCLOAK_CLIENT,
+    MS_TEAMS_SPRINT_REVIEW_URL: window.env.VITE_MS_TEAMS_SPRINT_REVIEW_URL,
+    MIRO_SPRINT_REVIEW_BOARD_URL: window.env.VITE_MIRO_SPRINT_REVIEW_BOARD_URL
   }
 }
 
@@ -33,5 +37,7 @@ export const {
   KC_REALM,
   KC_CLIENT,
   TEST_AUTH,
-  SM_LOGOUT_URL
+  SM_LOGOUT_URL,
+  MS_TEAMS_SPRINT_REVIEW_URL,
+  MIRO_SPRINT_REVIEW_BOARD_URL
 } = ENV
