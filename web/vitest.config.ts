3b0190c578d6814a1120a5251d86527a059b1082
Conor Brady <con.brad@gmail.com>
2023-02-08 16:48:37 -0800

    Fix vitest resolution

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,10 +1,12 @@
 import { mergeConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    plugins: [tsconfigPaths()],
     test: {
       globals: true,
       environment: 'jsdom',
