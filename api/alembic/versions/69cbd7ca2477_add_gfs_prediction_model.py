e343a2ebbc5f4b72033a9c22b11a4eafff3f9916
Andrea Williams <andrea.williams@gov.bc.ca>
2023-02-28 10:58:27 -0800

    Fixed Alembic multiple heads

--- api/alembic/versions/69cbd7ca2477_add_gfs_prediction_model.py
+++ api/alembic/versions/69cbd7ca2477_add_gfs_prediction_model.py
@@ -1,7 +1,7 @@
 """Add GFS prediction model
 
 Revision ID: 69cbd7ca2477
-Revises: 971848399c46
+Revises: de8355996f8e
 Create Date: 2023-02-09 14:30:49.597571
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '69cbd7ca2477'
-down_revision = '971848399c46'
+down_revision = 'de8355996f8e'
 branch_labels = None
 depends_on = None
 
