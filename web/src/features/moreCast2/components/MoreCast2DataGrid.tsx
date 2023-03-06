1f0605c67b6d8b3297610dffc56e3e2a4969c676
Darren Boss <darren.boss@gov.bc.ca>
2023-03-06 11:40:06 -0800

    Enable editing

--- web/src/features/moreCast2/components/MoreCast2DataGrid.tsx
+++ web/src/features/moreCast2/components/MoreCast2DataGrid.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import makeStyles from '@mui/styles/makeStyles'
-import { DataGrid, GridColDef, GridValueFormatterParams, GridValueGetterParams } from '@mui/x-data-grid'
+import {
+  DataGrid,
+  GridColDef,
+  GridValueFormatterParams,
+  GridValueGetterParams,
+  GridValueSetterParams
+} from '@mui/x-data-grid'
 import { isNumber } from 'lodash'
 import { DateTime } from 'luxon'
+import { ModelChoice } from 'api/moreCast2API'
 import { MoreCast2ForecastRow } from 'features/moreCast2/interfaces'
 
 interface MoreCast2DataGridProps {
   rows: MoreCast2ForecastRow[]
-  setForecastRows: React.Dispatch<React.SetStateAction<MoreCast2ForecastRow[]>>
 }
 
 const useStyles = makeStyles({
@@ -17,11 +23,35 @@ const useStyles = makeStyles({
   }
 })
 
+const NOT_AVAILABLE = 'N/A'
+
 const MoreCast2DataGrid = (props: MoreCast2DataGridProps) => {
   const classes = useStyles()
-  const predictionItemValueGetter = (params: GridValueGetterParams, precision: number) => {
-    const value = params?.value?.value
-    return isNumber(value) && !isNaN(value) ? value.toFixed(precision) : 'N/A'
+  const { rows } = props
+
+  const predictionItemValueFormatter = (params: GridValueFormatterParams, precision: number) => {
+    const value = params?.value
+    return isNumber(value) && !isNaN(value) ? value.toFixed(precision) : NOT_AVAILABLE
+  }
+
+  const predictionItemValueGetter = (params: GridValueGetterParams) => {
+    return params?.value?.value
+  }
+
+  const predictionItemValueSetter = (params: GridValueSetterParams, field: string) => {
+    const oldValue = params.row[field].value
+    const newValue = Number(params.value)
+
+    if (isNaN(oldValue) && isNaN(newValue)) {
+      return { ...params.row }
+    }
+
+    if (newValue !== params.row[field].value) {
+      params.row[field].choice = ModelChoice.MANUAL
+      params.row[field].value = newValue
+    }
+
+    return { ...params.row }
   }
 
   const columns: GridColDef[] = [
@@ -42,57 +72,72 @@ const MoreCast2DataGrid = (props: MoreCast2DataGridProps) => {
       field: 'temp',
       disableColumnMenu: true,
       disableReorder: true,
+      editable: true,
       headerName: 'Temp',
       sortable: false,
       type: 'number',
       width: 120,
-      valueGetter: params => predictionItemValueGetter(params, 1)
+      valueFormatter: params => predictionItemValueFormatter(params, 1),
+      valueGetter: params => predictionItemValueGetter(params),
+      valueSetter: params => predictionItemValueSetter(params, 'temp')
     },
     {
       field: 'rh',
       disableColumnMenu: true,
       disableReorder: true,
+      editable: true,
       headerName: 'RH',
       sortable: false,
       type: 'number',
       width: 120,
-      valueGetter: params => predictionItemValueGetter(params, 0)
+      valueFormatter: params => predictionItemValueFormatter(params, 0),
+      valueGetter: params => predictionItemValueGetter(params),
+      valueSetter: params => predictionItemValueSetter(params, 'rh')
     },
     {
       field: 'windDirection',
       disableColumnMenu: true,
       disableReorder: true,
+      editable: true,
       headerName: 'Wind Dir',
       sortable: false,
       type: 'number',
       width: 120,
-      valueGetter: params => predictionItemValueGetter(params, 0)
+      valueFormatter: params => predictionItemValueFormatter(params, 0),
+      valueGetter: params => predictionItemValueGetter(params),
+      valueSetter: params => predictionItemValueSetter(params, 'windDirection')
     },
     {
       field: 'windSpeed',
       disableColumnMenu: true,
       disableReorder: true,
+      editable: true,
       headerName: 'Wind Speed',
       sortable: false,
       type: 'number',
       width: 120,
-      valueGetter: params => predictionItemValueGetter(params, 1)
+      valueFormatter: params => predictionItemValueFormatter(params, 1),
+      valueGetter: params => predictionItemValueGetter(params),
+      valueSetter: params => predictionItemValueSetter(params, 'windSpeed')
     },
     {
       field: 'precip',
       disableColumnMenu: true,
       disableReorder: true,
+      editable: true,
       headerName: 'Precip',
       sortable: false,
       type: 'number',
       width: 120,
-      valueGetter: params => predictionItemValueGetter(params, 1)
+      valueFormatter: params => predictionItemValueFormatter(params, 1),
+      valueGetter: params => predictionItemValueGetter(params),
+      valueSetter: params => predictionItemValueSetter(params, 'precip')
     }
   ]
 
   return (
     <div className={classes.root} data-testid={`morecast2-data-grid`}>
-      <DataGrid columns={columns} rows={props.rows}></DataGrid>
+      <DataGrid columns={columns} experimentalFeatures={{ newEditingApi: true }} rows={rows}></DataGrid>
     </div>
   )
 }
