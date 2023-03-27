333676bf405b09e0c234c8e9f2fd6267826f77ff
Conor Brady <con.brad@gmail.com>
2023-03-27 11:45:18 -0700

    Adjust schedule

--- openshift/scripts/oc_provision_certbot_cronjob.sh
+++ openshift/scripts/oc_provision_certbot_cronjob.sh
@@ -12,4 +12,4 @@ DRYRUN="${DRYRUN:-true}"
 export CERTBOT_EMAIL=BCWS.PredictiveServices@gov.bc.ca
 export CERTBOT_SERVER=$(oc get secret wps-global -o jsonpath='{.data.certbot-server}' | base64 --decode)
 
-oc process -n $PROJ_TARGET -f "https://raw.githubusercontent.com/BCDevOps/certbot/v1.0.2/openshift/certbot.dc.yaml" -p CERTBOT_EMAIL=$CERTBOT_EMAIL -p CERTBOT_SERVER=$CERTBOT_SERVER -p CERTBOT_SUBSET=true -p CERTBOT_DEBUG=true | oc apply -n $PROJ_TARGET -f -
+oc process -n $PROJ_TARGET -f "https://raw.githubusercontent.com/BCDevOps/certbot/v1.0.2/openshift/certbot.dc.yaml" -p CRON_SCHEDULE="0 */12 * * *" -p CERTBOT_EMAIL=$CERTBOT_EMAIL -p CERTBOT_SERVER=$CERTBOT_SERVER -p CERTBOT_SUBSET=true -p CERTBOT_DEBUG=true | oc apply -n $PROJ_TARGET -f -
