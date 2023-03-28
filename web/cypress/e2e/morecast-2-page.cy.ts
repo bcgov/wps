4e211281401189daff6435f26a4c5ba201e5c47f
Conor Brady <con.brad@gmail.com>
2023-03-28 14:12:27 -0700

    Adjust tests

--- web/cypress/e2e/morecast-2-page.cy.ts
+++ web/cypress/e2e/morecast-2-page.cy.ts
@@ -3,18 +3,19 @@ import { MORE_CAST_2_ROUTE } from '../../src/utils/constants'
 describe('More Cast 2 Page', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/fba/fire-centers', { fixture: 'fba/fire-centers.json' }).as('fireCenters')
+    cy.intercept('GET', 'api/stations/groups').as('stationGroups')
 
     cy.visit(MORE_CAST_2_ROUTE)
   })
 
   it('Renders the initial page', () => {
-    cy.getByTestId('fire-center-dropdown').should('be.visible')
+    cy.getByTestId('station-group-dropdown').should('be.visible')
     cy.getByTestId('morecast2-data-grid').should('be.visible')
     cy.getByTestId('morecast2-station-panel').should('be.visible')
     cy.getByTestId('weather-model-dropdown').should('be.visible')
   })
 
-  it('Writes typed fire center to local storage', () => {
+  xit('Writes typed fire center to local storage', () => {
     cy.clearLocalStorage()
     cy.getByTestId('fire-center-dropdown').should('be.visible').type('North').type('{downarrow}').type('{enter}')
     cy.getAllLocalStorage().then(result => {
@@ -26,7 +27,7 @@ describe('More Cast 2 Page', () => {
     })
   })
 
-  it('Writes clicked fire center to local storage', () => {
+  xit('Writes clicked fire center to local storage', () => {
     cy.clearLocalStorage()
     cy.getByTestId('fire-center-dropdown').should('be.visible').click().get('li[data-option-index="1"').click()
     cy.getAllLocalStorage().then(result => {
