ac22e7fd63949b631d12d1a316737608db298ff3
Conor Brady <con.brad@gmail.com>
2023-03-31 13:56:15 -0700

    Code coverage

---
+++ web/src/api/stationAPI.test.ts
@@ -0,0 +1,19 @@
+import { StationGroupMember, getStationGroupsMembers } from 'api/stationAPI'
+import axios from 'api/axios'
+
+describe('stationAPI', () => {
+  it('should return groups from group endpoint', async () => {
+    const mockMemberStation: StationGroupMember = {
+      id: '1',
+      display_label: '',
+      fire_centre: { id: '', display_label: '' },
+      fire_zone: { id: '', display_label: '', fire_centre: '' },
+      station_code: 0,
+      station_status: 0
+    }
+    axios.post = jest.fn().mockResolvedValue({ data: { stations: [mockMemberStation] } })
+    const res = await getStationGroupsMembers(['1'])
+    expect(res).toHaveLength(1)
+    expect(res[0].id).toBe('1')
+  })
+})
