5ab900c0b4615c78416ac51d7318e3a72fa55d0d
Conor Brady <con.brad@gmail.com>
2023-03-31 15:31:56 -0700

    Remove tests

--- web/cypress/e2e/morecast-2-page.cy.ts
+++ web/cypress/e2e/morecast-2-page.cy.ts
@@ -14,28 +14,4 @@ describe('More Cast 2 Page', () => {
     cy.getByTestId('morecast2-station-panel').should('be.visible')
     cy.getByTestId('weather-model-dropdown').should('be.visible')
   })
-
-  xit('Writes typed fire center to local storage', () => {
-    cy.clearLocalStorage()
-    cy.getByTestId('fire-center-dropdown').should('be.visible').type('North').type('{downarrow}').type('{enter}')
-    cy.getAllLocalStorage().then(result => {
-      expect(result).to.deep.equal({
-        'http://localhost:3030': {
-          preferredMoreCast2FireCenter: '42'
-        }
-      })
-    })
-  })
-
-  xit('Writes clicked fire center to local storage', () => {
-    cy.clearLocalStorage()
-    cy.getByTestId('fire-center-dropdown').should('be.visible').click().get('li[data-option-index="1"').click()
-    cy.getAllLocalStorage().then(result => {
-      expect(result).to.deep.equal({
-        'http://localhost:3030': {
-          preferredMoreCast2FireCenter: '60'
-        }
-      })
-    })
-  })
 })
