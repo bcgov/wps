79b58774a53f0eb1fe9a8a73c814bd8a7e1903cb
Andrea Williams <54914403+andrea-williams@users.noreply.github.com>
2023-04-14 13:27:58 -0700

    Changed GFS_BASE_URL & filename construction/parsing (#2783)
    * Changed GFS_BASE_URL & filename construction/parsing
    * Convert request to Eastern timezone
    * Updated/added unit tests for GFS URL generation

--- api/app/jobs/noaa.py
+++ api/app/jobs/noaa.py
@@ -1,12 +1,13 @@
 """ A script that downloads weather models from NCEI NOAA HTTPS data server
 """
 import os
+import re
 import sys
 import datetime
+import pytz
 from typing import Generator
 import logging
 import tempfile
-import requests
 from sqlalchemy.orm import Session
 from app.db.crud.weather_models import (get_processed_file_record,
                                         get_prediction_model,
@@ -28,46 +29,30 @@ if __name__ == "__main__":
 
 logger = logging.getLogger(__name__)
 
-GFS_BASE_URL = "https://www.ncei.noaa.gov/data/global-forecast-system/access/grid-004-0.5-degree/forecast/"
+GFS_GRID = '0p25'  # 0.25 degree grid
+GFS_BASE_URL = f"https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_{GFS_GRID}.pl?"
+# weather variables are APCP: total precip, TMP: temperature, UGRD: U-component of wind,
+# VGRD: V-component of wind
+GFS_VARS = ['APCP', 'RH', 'TMP', 'UGRD', 'VGRD']
+GFS_LEVELS = ['surface', '2_m_above_ground', '10_m_above_ground']
+GFS_TOP_LAT = 60
+GFS_BOTTOM_LAT = 48
+GFS_LEFT_LON = -139
+GFS_RIGHT_LON = -114
 
 
 def get_gfs_model_run_hours():
-    """ Yield GFS model run hours ("0000", "0600", "1200", "1800") """
+    """ Yield GFS model run hours ("00", "06", "12", "18") """
     for hour in range(0, 19, 6):
-        hour_str = format(hour, '02d') + "00"
+        hour_str = format(hour, '02d')
         yield hour_str
 
 
-def get_date_strings_from_datetime(datetime: datetime.datetime) -> tuple[str, str]:
-    """ Returns strings for year_mo and year_mo_date to be used when requesting
+def get_year_mo_date_string_from_datetime(datetime: datetime.datetime) -> str:
+    """ Returns string for year_mo_date to be used when requesting
     grib files from NOAA"""
-    year_mo = f"{datetime.year}" + format(datetime.month, '02d')
-    year_mo_date = year_mo + format(datetime.day, '02d')
-    return (year_mo, year_mo_date)
-
-
-def get_date_for_download(now: datetime.datetime) -> tuple[str, str]:
-    """ Returns tuple of formatted strings for year_month and year_month_date.
-    NOAA publishes GFS model data several days behind schedule, so we need to 
-    determine the most recent date for which GFS data is available to download.
-    """
-    year_mo, year_mo_date = get_date_strings_from_datetime(now)
-    days_backward = 0
-    while assert_gfs_folder_exists(year_mo, year_mo_date) is False:
-        logger.warning('GFS data folder for %s not found on NOAA server', year_mo_date)
-        days_backward += 1
-        previous_day = now - datetime.timedelta(days=days_backward)
-        year_mo, year_mo_date = get_date_strings_from_datetime(previous_day)
-
-    return previous_day
-
-
-def assert_gfs_folder_exists(year_mo: str, year_mo_date: str) -> bool:
-    """ Returns boolean value indicating whether the GFS data folder exists on the NOAA HTTPS server
-    for the given year_month_date string """
-    url = GFS_BASE_URL + f'{year_mo}/{year_mo_date}'
-    response = requests.get(url, timeout=60)
-    return response.status_code == 200
+    year_mo_date = f"{datetime.year}" + format(datetime.month, '02d') + format(datetime.day, '02d')
+    return year_mo_date
 
 
 def get_gfs_model_run_download_urls(download_date: datetime.datetime, model_cycle: str) -> Generator[str, None, None]:
@@ -77,16 +62,16 @@ def get_gfs_model_run_download_urls(download_date: datetime.datetime, model_cycl
     # but GFS model run timestamps are in UTC. 12:00 PST = 20:00 UTC, so we need to pull
     # data for the 18:00 and 21:00 UTC model runs, then perform linear interpolation to
     # calculate noon values.
-    if model_cycle == '0000':
+    if model_cycle == '00':
         before_noon = list(range(18, 265, 24))
         after_noon = list(range(21, 265, 24))
-    elif model_cycle == '0600':
+    elif model_cycle == '06':
         before_noon = list(range(12, 253, 24))
         after_noon = list(range(15, 256, 24))
-    elif model_cycle == '1200':
+    elif model_cycle == '12':
         before_noon = list(range(6, 247, 24))
         after_noon = list(range(9, 250, 24))
-    elif model_cycle == '1800':
+    elif model_cycle == '18':
         before_noon = list(range(0, 241, 24))
         after_noon = list(range(3, 244, 24))
 
@@ -94,24 +79,38 @@ def get_gfs_model_run_download_urls(download_date: datetime.datetime, model_cycl
     # sort list purely for human convenience when debugging. Functionally it doesn't matter
     all_hours.sort()
 
-    year_mo, year_mo_date = get_date_strings_from_datetime(download_date)
+    # download_date has UTC timezone. Need to convert to EDT (-4h) timezone, which is what
+    # nomads.ncep.noaa server uses
+    download_date_to_est = download_date.astimezone(pytz.timezone('US/Eastern'))
+    year_mo_date = get_year_mo_date_string_from_datetime(download_date_to_est)
 
     for fcst_hour in all_hours:
         hhh = format(fcst_hour, '03d')
-        filename = f'{year_mo}/{year_mo_date}/gfs_4_{year_mo_date}_{model_cycle}_{hhh}.grb2'
-        yield GFS_BASE_URL + filename
+        filter_str = f'dir=%2Fgfs.{year_mo_date}%2F{model_cycle}%2Fatmos&file=gfs.t{model_cycle}z.pgrb2.{GFS_GRID}.'\
+            f'f{hhh}&'
+        wx_vars_filter_str = ''
+        for var in GFS_VARS:
+            wx_vars_filter_str += f'var_{var}=on&'
+        levels_filter_str = ''
+        for level in GFS_LEVELS:
+            levels_filter_str += f'lev_{level}=on&'
+        subregion_filter_str = f'subregion=&toplat={GFS_TOP_LAT}&leftlon={GFS_LEFT_LON}&rightlon={GFS_RIGHT_LON}'\
+            f'&bottomlat={GFS_BOTTOM_LAT}'
+        filter_str += wx_vars_filter_str + levels_filter_str + subregion_filter_str
+        yield GFS_BASE_URL + filter_str
 
 
 def parse_url_for_timestamps(url: str):
     """ Interpret the model_run_timestamp and prediction_timestamp from a model's URL """
-    # only need the substring after the final '/'
-    timestamp_info = url.split('/')[-1]
-    # timestamp_info has format 'gfs_4_{model run date yyyymmdd}_{model run hour xxxx}_{forecast hour xxx}.grb2'
-    forecast_hour = timestamp_info[-8:-5]
-    model_run_hour = timestamp_info[-13:-9]
-    model_run_date = timestamp_info[6:14]
+    # sample URL: 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?dir=%2Fgfs.20230412%2F00%2Fatmos&file=gfs.t00z.pgrb2.0p25.f018var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    model_run_datetime_str = re.search("dir=\%2Fgfs.\d*\%2F\d*", url).group(0)
+    # extract date string only from gfs.20230411 in sample URL
+    model_run_date = model_run_datetime_str[-13:-5]
+    model_run_hour = model_run_datetime_str[-2:]
+    forecast_hour_str = re.search("file=gfs.t\d*z.pgrb2.0p25.f\d*", url).group(0)
+    forecast_hour = forecast_hour_str[-3:]
     model_run_datetime_str = model_run_date + ' ' + model_run_hour
-    model_run_timestamp = datetime.datetime.strptime(model_run_datetime_str, '%Y%m%d %H%M')
+    model_run_timestamp = datetime.datetime.strptime(model_run_datetime_str, '%Y%m%d %H')
     model_run_timestamp = model_run_timestamp.replace(tzinfo=datetime.timezone.utc)
     prediction_timestamp = model_run_timestamp + datetime.timedelta(hours=int(forecast_hour))
 
@@ -167,7 +166,8 @@ class GFS():
                                                   model_run_timestamp, prediction_timestamp)
                         # download the file:
                         with tempfile.TemporaryDirectory() as temporary_path:
-                            downloaded = download(url, temporary_path, 'REDIS_CACHE_NOAA', 'REDIS_NOAA_CACHE_EXPIRY')
+                            downloaded = download(url, temporary_path,
+                                                  'REDIS_CACHE_NOAA', 'GFS', 'REDIS_NOAA_CACHE_EXPIRY')
                             if downloaded:
                                 self.files_downloaded += 1
                                 # If we've downloaded the file ok, we can now process it.
@@ -193,9 +193,8 @@ class GFS():
         logger.info('Processing {} model run cycle {}'.format(
             self.model_type, model_cycle))
 
-        download_date = get_date_for_download(self.now)
         # Get the urls for the current model run.
-        urls = list(get_gfs_model_run_download_urls(download_date, model_cycle))
+        urls = list(get_gfs_model_run_download_urls(self.now, model_cycle))
 
         # Process all the urls.
         self.process_model_run_urls(urls)
--- api/app/tests/weather_models/test_noaa_gfs.py
+++ api/app/tests/weather_models/test_noaa_gfs.py
@@ -78,43 +78,55 @@ def mock_download(monkeypatch):
     monkeypatch.setattr(requests, 'get', mock_requests_get_gfs)
 
 
-@pytest.fixture()
-def mock_assert_folder_exists(monkeypatch):
-    """ fixture for NOAA download folder """
-    def mock_assert_gfs_folder_exists(*args, **kwargs):
-        """ MockResponse with status code 404 or 200 depending on date specified """
-        request_date = kwargs.get('request_date')
-        if request_date == '20230228':
-            return MockResponse(status_code=200)
-        return MockResponse(status_code=404)
-    monkeypatch.setattr(noaa, 'assert_gfs_folder_exists', mock_assert_gfs_folder_exists)
-
-
-def test_get_gfs_model_run_download_urls_for_0000_utc():
+def test_get_gfs_model_run_download_urls_for_00_utc():
+    # March 2 at 00:00 UTC is equivalent to March 1 in Eastern timezone - should return URL for previous day
     # for a given date and model run cycle, there should be 2 time intervals * 10 days into future (11 days total)
     expected_num_of_urls = 2 * 11
-    actual_urls = list(noaa.get_gfs_model_run_download_urls(datetime(2023, 3, 2, 20), '0000'))
+    actual_urls = list(noaa.get_gfs_model_run_download_urls(datetime(2023, 3, 2, 00, tzinfo=timezone.utc), '00'))
     assert len(actual_urls) == expected_num_of_urls
-    assert actual_urls[0] == noaa.GFS_BASE_URL + '202303/20230302/gfs_4_20230302_0000_018.grb2'
-    assert actual_urls[-1] == noaa.GFS_BASE_URL + '202303/20230302/gfs_4_20230302_0000_261.grb2'
+    assert actual_urls[0] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230301%2F00%2Fatmos&file=gfs.t00z.pgrb2.0p25.f018&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    assert actual_urls[-1] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230301%2F00%2Fatmos&file=gfs.t00z.pgrb2.0p25.f261&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
 
 
-def test_get_gfs_model_run_download_urls_for_0600_utc():
+def test_get_gfs_model_run_download_urls_for_06_utc():
+    # Feb 15 at 06:00 UTC is still Feb 15 (at 02:00) in Eastern timezone - should return URL for same day
     # for a given date and model run cycle, there should be 2 time intervals * 10 days into future (11 days total incl. today)
     expected_num_of_urls = 2 * 11
-    actual_urls = list(noaa.get_gfs_model_run_download_urls(datetime(2023, 2, 15, 0), '0600'))
+    actual_urls = list(noaa.get_gfs_model_run_download_urls(datetime(2023, 2, 15, 6, tzinfo=timezone.utc), '06'))
     assert len(actual_urls) == expected_num_of_urls
-    assert actual_urls[0] == noaa.GFS_BASE_URL + '202302/20230215/gfs_4_20230215_0600_012.grb2'
-    assert actual_urls[1] == noaa.GFS_BASE_URL + '202302/20230215/gfs_4_20230215_0600_015.grb2'
-    assert actual_urls[-1] == noaa.GFS_BASE_URL + '202302/20230215/gfs_4_20230215_0600_255.grb2'
+    assert actual_urls[0] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230215%2F06%2Fatmos&file=gfs.t06z.pgrb2.0p25.f012&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    assert actual_urls[1] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230215%2F06%2Fatmos&file=gfs.t06z.pgrb2.0p25.f015&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    assert actual_urls[-1] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230215%2F06%2Fatmos&file=gfs.t06z.pgrb2.0p25.f255&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+
+
+def test_get_gfs_model_run_download_urls_for_12_utc():
+    # March 9 at 12:00 UTC is still March 9 in Eastern timezone - should return URL for same day
+    # for a given date and model run cycle, there should be 2 time intervals * 10 days into future (11 days total incl. today)
+    expected_num_of_urls = 2 * 11
+    actual_urls = list(noaa.get_gfs_model_run_download_urls(datetime(2023, 3, 9, 12, tzinfo=timezone.utc), '12'))
+    assert len(actual_urls) == expected_num_of_urls
+    assert actual_urls[0] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230309%2F12%2Fatmos&file=gfs.t12z.pgrb2.0p25.f006&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    assert actual_urls[1] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230309%2F12%2Fatmos&file=gfs.t12z.pgrb2.0p25.f009&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    assert actual_urls[-1] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230309%2F12%2Fatmos&file=gfs.t12z.pgrb2.0p25.f249&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+
+
+def test_get_gfs_model_run_download_urls_for_18_utc():
+    # Jan 27 at 18:00 UTC is still Jan 27 in Eastern timezone - should return URL for same day
+    # for a given date and model run cycle, there should be 2 time intervals * 10 days into future (11 days total incl. today)
+    expected_num_of_urls = 2 * 11
+    actual_urls = list(noaa.get_gfs_model_run_download_urls(datetime(2023, 1, 27, 18, tzinfo=timezone.utc), '18'))
+    assert len(actual_urls) == expected_num_of_urls
+    assert actual_urls[0] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230127%2F18%2Fatmos&file=gfs.t18z.pgrb2.0p25.f000&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    assert actual_urls[1] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230127%2F18%2Fatmos&file=gfs.t18z.pgrb2.0p25.f003&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    assert actual_urls[-1] == noaa.GFS_BASE_URL + 'dir=%2Fgfs.20230127%2F18%2Fatmos&file=gfs.t18z.pgrb2.0p25.f243&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
 
 
 def test_parse_url_for_timestamps_simple():
     """ simple test case for noaa.parse_url_for_timestamps(): model_run_timestamp and prediction_timestamp
     are on the same day """
-    url = 'https://example.com/model/gfs_4_20230221_0600_012.grb2'
-    expected_model_run_timestamp = datetime(2023, 2, 21, 6, 0, tzinfo=timezone.utc)
-    expected_prediction_timestamp = datetime(2023, 2, 21, 18, 0, tzinfo=timezone.utc)
+    url = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?dir=%2Fgfs.20230413%2F12%2Fatmos&file=gfs.t12z.pgrb2.0p25.f006&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    expected_model_run_timestamp = datetime(2023, 4, 13, 12, 0, tzinfo=timezone.utc)
+    expected_prediction_timestamp = datetime(2023, 4, 13, 18, 0, tzinfo=timezone.utc)
     actual_model_run_timestamp, actual_prediction_timestamp = noaa.parse_url_for_timestamps(url)
     assert expected_model_run_timestamp == actual_model_run_timestamp
     assert expected_prediction_timestamp == actual_prediction_timestamp
@@ -123,33 +135,29 @@ def test_parse_url_for_timestamps_simple():
 def test_parse_url_for_timestamps_complex():
     """ more complex test case for noaa.parse_url_for_timestamps(): model_run_timestamp and
     prediction_timestamp are on different days """
-    url = 'https://example.com/model/gfs_4_20230221_0600_039.grb2'
-    expected_model_run_timestamp = datetime(2023, 2, 21, 6, 0, tzinfo=timezone.utc)
-    expected_prediction_timestamp = datetime(2023, 2, 22, 21, 0, tzinfo=timezone.utc)
+    url = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?dir=%2Fgfs.20230413%2F18%2Fatmos&file=gfs.t18z.pgrb2.0p25.f243&var_APCP=on&var_RH=on&var_TMP=on&var_UGRD=on&var_VGRD=on&lev_surface=on&lev_2_m_above_ground=on&lev_10_m_above_ground=on&subregion=&toplat=60&leftlon=-139&rightlon=-114&bottomlat=48'
+    expected_model_run_timestamp = datetime(2023, 4, 13, 18, 0, tzinfo=timezone.utc)
+    expected_prediction_timestamp = datetime(2023, 4, 23, 21, 0, tzinfo=timezone.utc)
     actual_model_run_timestamp, actual_prediction_timestamp = noaa.parse_url_for_timestamps(url)
     assert expected_model_run_timestamp == actual_model_run_timestamp
     assert expected_prediction_timestamp == actual_prediction_timestamp
 
 
-def test_get_datestrings_from_datetime():
+def test_get_year_mo_date_string_from_datetime():
     test_cases = [
         {
             'date': datetime(2023, 2, 14, 0, tzinfo=timezone.utc),
-            'expected_year_mo': '202302',
             'expected_year_mo_date': '20230214'
         },
         {
             'date': datetime(2023, 6, 5, 0, tzinfo=timezone.utc),
-            'expected_year_mo': '202306',
             'expected_year_mo_date': '20230605'
         },
         {
             'date': datetime(2023, 10, 31, 20, tzinfo=timezone.utc),
-            'expected_year_mo': '202310',
             'expected_year_mo_date': '20231031'
         },
     ]
     for test in test_cases:
-        actual_strings = noaa.get_date_strings_from_datetime(test.get('date'))
-        assert test.get('expected_year_mo') == actual_strings[0]
-        assert test.get('expected_year_mo_date') == actual_strings[1]
+        actual_string = noaa.get_year_mo_date_string_from_datetime(test.get('date'))
+        assert test.get('expected_year_mo_date') == actual_string
