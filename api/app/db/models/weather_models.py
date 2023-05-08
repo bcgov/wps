20c33616fd8ba558c56761f11d8456d4122627fb
Andrea Williams <andrea.williams@gov.bc.ca>
2023-05-08 16:40:58 -0700

    Added BA cols to db table; ML not working yet

--- api/app/db/models/weather_models.py
+++ api/app/db/models/weather_models.py
@@ -202,12 +202,18 @@ class WeatherStationModelPrediction(Base):
     bias_adjusted_rh = Column(Float, nullable=True)
     # Accumulated precipitation over calendar day measured in UTC (units kg.m^-2)
     apcp_sfc_0 = Column(Float, nullable=True)
+    # Accumulated precipitation adjusted for bias
+    bias_adjusted_apcp = Column(Float, nullable=True)
     # Change in accumulated precipitation between current and previous prediction_timestamp
     delta_precip = Column(Float, nullable=True)
     # Wind direction 10m above ground.
     wdir_tgl_10 = Column(Float, nullable=True)
+    # Wind direction adjusted for bias
+    bias_adjusted_wdir = Column(Float, nullable=True)
     # Wind speed 10m above ground.
     wind_tgl_10 = Column(Float, nullable=True)
+    # Wind speed adjusted for bias
+    bias_adjusted_wind_speed = Column(Float, nullable=True)
     # Date this record was created.
     create_date = Column(TZTimeStamp, nullable=False,
                          default=time_utils.get_utc_now())
@@ -216,5 +222,6 @@ class WeatherStationModelPrediction(Base):
                          default=time_utils.get_utc_now())
 
     def __str__(self):
-        return ('{self.station_code} {self.prediction_timestamp} {self.tmp_tgl_2} {self.apcp_sfc_0} '
-                '{self.delta_precip}').format(self=self)
+        return ('{self.station_code} {self.prediction_timestamp} {self.tmp_tgl_2} {self.bias_adjusted_temperature} '
+                '{self.rh_tgl_2} {self.bias_adjusted_rh} {self.wdir_tgl_10} {self.bias_adjusted_wdir} {self.wind_tgl_10} '
+                '{self.bias_adjusted_wind_speed} {self.apcp_sfc_0} {self.delta_precip} {self.bias_adjusted_apcp}').format(self=self)
