77b3e3b5e7a6addc69c441359a4499423da4240e
Andrea Williams <andrea.williams@gov.bc.ca>
2023-05-16 10:46:13 -0700

    Updated error message for NOAA job

--- api/app/jobs/noaa.py
+++ api/app/jobs/noaa.py
@@ -404,7 +404,7 @@ def main():
     except Exception as exception:
         # We catch and log any exceptions we may have missed.
         logger.error('unexpected exception processing', exc_info=exception)
-        rc_message = ':poop: Encountered error retrieving GFS model data from NOAA'
+        rc_message = ':poop: Encountered error retrieving {sys.argv[1]} model data from NOAA'
         send_rocketchat_notification(rc_message, exception)
         # Exit with a failure code.
         sys.exit(os.EX_SOFTWARE)
