8911bda488401efefcc74b3ffe1eb08d2ae956c5
Conor Brady <con.brad@gmail.com>
2023-05-17 10:41:57 -0700

    Only auth with wf1 if we're a forecaster

--- web/src/features/auth/components/MoreCast2AuthWrapper.tsx
+++ web/src/features/auth/components/MoreCast2AuthWrapper.tsx
@@ -4,7 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch } from 'app/store'
 import { TEST_AUTH, WF1_AUTH_URL } from 'utils/env'
 import { wf1Authenticate, wf1AuthenticateError } from 'features/auth/slices/wf1AuthenticationSlice'
-import { selectWf1Authentication } from 'app/rootReducer'
+import { selectAuthentication, selectWf1Authentication } from 'app/rootReducer'
+import { ROLES } from 'features/auth/roles'
 
 interface Props {
   children: React.ReactElement
@@ -13,7 +14,10 @@ interface Props {
 const MoreCast2AuthWrapper = ({ children }: Props) => {
   const dispatch: AppDispatch = useDispatch()
 
+  const { isAuthenticated, roles } = useSelector(selectAuthentication)
   const { error } = useSelector(selectWf1Authentication)
+
+  const isAuthenticatedForecaster = isAuthenticated && roles.includes(ROLES.MORECAST_2.WRITE_FORECAST)
 
   useEffect(() => {
     async function fetchData() {
@@ -21,9 +25,16 @@ const MoreCast2AuthWrapper = ({ children }: Props) => {
         dispatch(wf1Authenticate('test token'))
         return
       }
-      if (!window.location.href.includes('access_token')) {
+
+      if (!isAuthenticatedForecaster) {
+        return children
+      }
+
+      if (!window.location.href.includes('access_token') && isAuthenticatedForecaster) {
         window.location.href = WF1_AUTH_URL
-      } else {
+      }
+
+      if (window.location.href.includes('access_token')) {
         const wf1Token = window.location.href.split('#access_token=')[1].split('&')[0]
         try {
           jwtDecode.default(wf1Token)
