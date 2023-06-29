521ff44488a5eb04f8233318c3f767550152b71f
Conor Brady <con.brad@gmail.com>
2023-06-29 15:58:15 -0700

    Fix sticky cell css

--- web/src/components/StickyCell.tsx
+++ web/src/components/StickyCell.tsx
@@ -3,21 +3,13 @@ import { styled } from '@mui/material/styles'
 import { theme } from 'app/theme'
 import React from 'react'
 
-const PREFIX = 'StickyCell'
-
-const classes = {
-  sticky: `${PREFIX}-sticky`
-}
-
 const StyledTableCell = styled(TableCell, {
   shouldForwardProp: prop => prop !== 'zIndexOffset'
 })((props: Pick<StickyCellProps, 'left' | 'zIndexOffset' | 'backgroundColor'>) => ({
-  [`& .${classes.sticky}`]: {
-    left: props.left,
-    position: 'sticky',
-    zIndex: theme.zIndex.appBar + props.zIndexOffset,
-    backgroundColor: props.backgroundColor ? props.backgroundColor : undefined
-  }
+  left: props.left,
+  position: 'sticky',
+  zIndex: theme.zIndex.appBar + props.zIndexOffset,
+  backgroundColor: props.backgroundColor ? props.backgroundColor : undefined
 }))
 
 interface StickyCellProps {
@@ -34,10 +26,10 @@ const StickyCell = (props: StickyCellProps) => {
   return (
     <StyledTableCell
       data-testid={`stickyCell-fba`}
-      className={`${classes.sticky} ${props.className}`}
       colSpan={props.colSpan}
-      left={0}
-      zIndexOffset={0}
+      left={props.left}
+      zIndexOffset={props.zIndexOffset}
+      backgroundColor={props.backgroundColor}
     >
       {props.children}
     </StyledTableCell>
