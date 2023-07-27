00c673419fe2edd1a34c829e0716cc5b4b44e313
Conor Brady <con.brad@gmail.com>
2023-07-27 13:40:22 -0700

    Running cypress

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,30 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import istanbul from 'vite-plugin-istanbul'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-const cypress = process.env.CYPRESS
-
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  if (cypress) {
-    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), '.env.cypress'))
-  }
-  console.log(`Has cypress set: ${cypress}`)
-  return {
-    server: {
-      port: 3030
-    },
-    plugins: [
-      react(),
-      tsconfigPaths(),
-      istanbul({
-        include: 'src/*',
-        exclude: ['node_modules'],
-        extension: ['.tsx', '.ts'],
-        requireEnv: true,
-        cypress: true
-      })
-    ]
-  }
+export default defineConfig({
+  server: {
+    port: 3030
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules'],
+      extension: ['.tsx', '.ts'],
+      cypress: true,
+      requireEnv: false
+    })
+  ]
 })
