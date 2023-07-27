a44e19a968287bcda175b286140fe595eba584e5
Conor Brady <con.brad@gmail.com>
2023-07-27 14:47:29 -0700

    disabled hmr

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,7 +6,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3030
+    port: 3030,
+    hmr: false
   },
   plugins: [
     react(),
