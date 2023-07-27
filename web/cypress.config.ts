393d92652db51ed007096251f4fbbc15e75f0865
Conor Brady <con.brad@gmail.com>
2023-07-27 15:22:55 -0700

    Undo large timeouts

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -5,8 +5,6 @@ import vitePreprocessor from 'cypress-vite'
 export default defineConfig({
   screenshotOnRunFailure: false,
   video: false,
-  defaultCommandTimeout: 100000,
-  requestTimeout: 100000,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
