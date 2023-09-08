36af101940339b1fcbfd6800e41a45e380ec1105
Conor Brady <con.brad@gmail.com>
2023-09-08 16:15:19 -0700

    Tests for critical hours

--- api/app/tests/fire_behavior/test_critical_hours.py
+++ api/app/tests/fire_behavior/test_critical_hours.py
@@ -21,7 +21,7 @@ def test_critical_hours_4000():
                                          wind_speed=10.3,
                                          prev_daily_ffmc=94.561,
                                          last_observed_morning_rh_values={7.0: 54.0, 8.0: 47.0, 9.0: 46.0, 10.0: 45.0, 11.0: 44.0, 12.0: 38.0})
-    assert crit_hours_4000 == CriticalHoursHFI(start=7.00, end=0.00)
+    assert crit_hours_4000 == CriticalHoursHFI(start=7.00, end=1.00)
 
 
 def test_critical_hours_4000_manual_different_cfb():
@@ -39,7 +39,83 @@ def test_critical_hours_4000_manual_different_cfb():
                                          wind_speed=10.3,
                                          prev_daily_ffmc=94.561,
                                          last_observed_morning_rh_values={7.0: 54.0, 8.0: 47.0, 9.0: 46.0, 10.0: 45.0, 11.0: 44.0, 12.0: 38.0})
-    assert crit_hours_4000 == CriticalHoursHFI(start=7.00, end=0.00)
+    assert crit_hours_4000 == CriticalHoursHFI(start=7.00, end=1.00)
+
+
+def test_critical_hours_4000_high_rh():
+    """ A high daily FFMC with consistently high morning RH hourly values for target hfi of 4000 """
+    crit_hours_4000 = get_critical_hours(target_hfi=4000,
+                                         fuel_type=FuelTypeEnum.C2,
+                                         percentage_conifer=100,
+                                         percentage_dead_balsam_fir=None,
+                                         bui=148.558,
+                                         grass_cure=None,
+                                         crown_base_height=3.0,
+                                         daily_ffmc=92.08498474903236,
+                                         fmc=120,
+                                         cfb=0.9615453070844759,
+                                         cfl=0.8,
+                                         wind_speed=10.3,
+                                         prev_daily_ffmc=94.561,
+                                         last_observed_morning_rh_values={7.0: 88.0, 8.0: 88.0, 9.0: 88.0, 10.0: 88.0, 11.0: 88.0, 12.0: 88.0})
+    assert crit_hours_4000 == CriticalHoursHFI(start=10.00, end=1.00)
+
+
+def test_critical_hours_10000_high_rh():
+    """ A high daily FFMC with consistently high morning RH hourly values for target hfi of 10000 """
+    crit_hours_4000 = get_critical_hours(target_hfi=10000,
+                                         fuel_type=FuelTypeEnum.C2,
+                                         percentage_conifer=100,
+                                         percentage_dead_balsam_fir=None,
+                                         bui=148.558,
+                                         grass_cure=None,
+                                         crown_base_height=3.0,
+                                         daily_ffmc=92.08498474903236,
+                                         fmc=120,
+                                         cfb=0.9615453070844759,
+                                         cfl=0.8,
+                                         wind_speed=10.3,
+                                         prev_daily_ffmc=94.561,
+                                         last_observed_morning_rh_values={7.0: 88.0, 8.0: 88.0, 9.0: 88.0, 10.0: 88.0, 11.0: 88.0, 12.0: 88.0})
+    assert crit_hours_4000 == CriticalHoursHFI(start=13.00, end=21.00)
+
+
+def test_critical_hours_low_ffmc_4000():
+    """ A low daily ffmc produces no critical hours for target HFI 4000 """
+    crit_hours_4000 = get_critical_hours(target_hfi=4000,
+                                         fuel_type=FuelTypeEnum.C2,
+                                         percentage_conifer=100,
+                                         percentage_dead_balsam_fir=None,
+                                         bui=148.558,
+                                         grass_cure=None,
+                                         crown_base_height=3.0,
+                                         daily_ffmc=55,
+                                         fmc=120,
+                                         cfb=0.9615453070844759,
+                                         cfl=0.8,
+                                         wind_speed=10.3,
+                                         prev_daily_ffmc=55,
+                                         last_observed_morning_rh_values={7.0: 54.0, 8.0: 47.0, 9.0: 46.0, 10.0: 45.0, 11.0: 44.0, 12.0: 38.0})
+    assert crit_hours_4000 is None
+
+
+def test_critical_hours_low_ffmc_10000():
+    """ A low daily ffmc produces no critical hours for target HFI 10000 """
+    crit_hours_10000 = get_critical_hours(target_hfi=10000,
+                                          fuel_type=FuelTypeEnum.C2,
+                                          percentage_conifer=100,
+                                          percentage_dead_balsam_fir=None,
+                                          bui=148.558,
+                                          grass_cure=None,
+                                          crown_base_height=3.0,
+                                          daily_ffmc=55,
+                                          fmc=120,
+                                          cfb=0.9615453070844759,
+                                          cfl=0.8,
+                                          wind_speed=10.3,
+                                          prev_daily_ffmc=55,
+                                          last_observed_morning_rh_values={7.0: 54.0, 8.0: 47.0, 9.0: 46.0, 10.0: 45.0, 11.0: 44.0, 12.0: 38.0})
+    assert crit_hours_10000 is None
 
 
 def test_overnight_diurnal():
