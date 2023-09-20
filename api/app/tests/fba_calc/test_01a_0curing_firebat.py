4b795968e77e5e004dbb207933166cc5bb72e757
Conor Brady <con.brad@gmail.com>
2023-09-20 10:56:07 -0700

    01a

---
+++ api/app/tests/fba_calc/test_01a_0curing_firebat.py
@@ -0,0 +1,74 @@
+
+from httpx import AsyncClient
+from aiohttp import ClientSession
+import pytest
+from app.fire_behaviour.cffdrs import CFFDRS
+from app.tests.common import default_mock_client_get
+
+
+firebat_url = '/api/fba-calc/stations'
+
+CFFDRS.instance()
+
+
+@pytest.fixture()
+async def async_client():
+    from app.main import app as test_app
+
+    async with AsyncClient(app=test_app, base_url="https://test") as test_client:
+        yield test_client
+
+
+@pytest.mark.anyio
+@pytest.mark.usefixtures('mock_jwt_decode')
+async def test_01a_0curing_request_response(anyio_backend, async_client: AsyncClient, monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
+
+    response = await async_client.post(firebat_url, json={
+        "date": "2021-07-06",
+        "stations": [
+            {
+                "id": 0,
+                "station_code": 230,
+                "fuel_type": "O1A",
+                "grass_cure": "0"
+            }
+        ]
+    })
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "date": "2021-07-06",
+        "stations": [
+            {
+                "id": 0,
+                "station_code": 230,
+                "station_name": "HORSEFLY",
+                "zone_code": "C3",
+                "elevation": 701,
+                "fuel_type": "O1A",
+                "status": "FORECAST",
+                "temp": 25.2,
+                "rh": 31.0,
+                "wind_direction": 282,
+                "wind_speed": 9.2,
+                "precipitation": 0.0,
+                "grass_cure": 0.0,
+                "fine_fuel_moisture_code": 93.305,
+                "drought_code": 340.544,
+                "initial_spread_index": 10.881,
+                "build_up_index": 117.899,
+                "duff_moisture_code": 103.923,
+                "fire_weather_index": 35.794,
+                "head_fire_intensity": 0.0,
+                "rate_of_spread": 0.0,
+                "fire_type": "SUR",
+                "percentage_crown_fraction_burned": 0.0,
+                "flame_length": 0.0,
+                "sixty_minute_fire_size": 0.0,
+                "thirty_minute_fire_size": 0.0,
+                "critical_hours_hfi_4000": None,
+                "critical_hours_hfi_10000": None
+            }
+        ]
+    }
