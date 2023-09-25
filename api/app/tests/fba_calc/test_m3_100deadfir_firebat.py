798b7ec6bc03f9781f0ced4b83fec7bcaae5158b
Conor Brady <con.brad@gmail.com>
2023-09-25 11:44:05 -0700

    Wind speed and wind speed informed variables use cffdrs (#3123)

---
+++ api/app/tests/fba_calc/test_m3_100deadfir_firebat.py
@@ -0,0 +1,65 @@
+
+from httpx import AsyncClient
+from aiohttp import ClientSession
+import pytest
+import math
+from app.fire_behaviour.cffdrs import CFFDRS
+from app.tests.common import default_mock_client_get
+
+
+firebat_url = '/api/fba-calc/stations'
+
+CFFDRS.instance()
+
+
+@pytest.fixture()
+async def async_client():
+    from app.main import app as test_app
+
+    async with AsyncClient(app=test_app, base_url="https://test") as test_client:
+        yield test_client
+
+
+@pytest.mark.anyio
+@pytest.mark.usefixtures('mock_jwt_decode')
+async def test_m3_100deadfir_request_response(anyio_backend, async_client: AsyncClient, monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
+
+    response = await async_client.post(firebat_url, json={
+        "date": "2021-07-05",
+        "stations": [
+            {
+                "id": 0,
+                "station_code": 230,
+                "fuel_type": "M3",
+                "crown_base_height": 6,
+                "percentage_dead_balsam_fir": 100
+            }
+        ]
+    })
+
+    assert response.status_code == 200
+
+    assert response.json()['stations'][0]['id'] == 0
+    assert response.json()['stations'][0]['station_code'] == 230
+    assert response.json()['stations'][0]['station_name'] == 'HORSEFLY'
+    assert response.json()['stations'][0]['zone_code'] == 'C3'
+    assert response.json()['stations'][0]['elevation'] == 701
+    assert response.json()['stations'][0]['fuel_type'] == 'M3'
+
+    assert math.isclose(response.json()['stations'][0]['fine_fuel_moisture_code'], 90.683, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['drought_code'], 340.544, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['initial_spread_index'], 7.51, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['build_up_index'], 117.899, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['duff_moisture_code'], 103.923, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['fire_weather_index'], 27.913, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['head_fire_intensity'], 42325.023, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['rate_of_spread'], 31.281, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['percentage_crown_fraction_burned'], 0.999, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['flame_length'], 11.878, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['sixty_minute_fire_size'], 247.423, abs_tol=0.1)
+    assert math.isclose(response.json()['stations'][0]['thirty_minute_fire_size'], 43.333, abs_tol=0.001)
+
+    assert response.json()['stations'][0]['fire_type'] == 'CC'
+    assert response.json()['stations'][0]['critical_hours_hfi_4000'] == {"start": 10.0, "end": 6.0}
+    assert response.json()['stations'][0]['critical_hours_hfi_10000'] == {"start": 12.0, "end": 0.0}
---
+++ api/app/tests/fba_calc/test_m4_60deadfir_firebat.py
@@ -0,0 +1,65 @@
+
+from httpx import AsyncClient
+from aiohttp import ClientSession
+import pytest
+import math
+from app.fire_behaviour.cffdrs import CFFDRS
+from app.tests.common import default_mock_client_get
+
+
+firebat_url = '/api/fba-calc/stations'
+
+CFFDRS.instance()
+
+
+@pytest.fixture()
+async def async_client():
+    from app.main import app as test_app
+
+    async with AsyncClient(app=test_app, base_url="https://test") as test_client:
+        yield test_client
+
+
+@pytest.mark.anyio
+@pytest.mark.usefixtures('mock_jwt_decode')
+async def test_m4_60deadfir_request_response(anyio_backend, async_client: AsyncClient, monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(ClientSession, 'get', default_mock_client_get)
+
+    response = await async_client.post(firebat_url, json={
+        "date": "2021-07-05",
+        "stations": [
+            {
+                "id": 0,
+                "station_code": 230,
+                "fuel_type": "M4",
+                "crown_base_height": 6,
+                "percentage_dead_balsam_fir": 60
+            }
+        ]
+    })
+
+    assert response.status_code == 200
+
+    assert response.json()['stations'][0]['id'] == 0
+    assert response.json()['stations'][0]['station_code'] == 230
+    assert response.json()['stations'][0]['station_name'] == 'HORSEFLY'
+    assert response.json()['stations'][0]['zone_code'] == 'C3'
+    assert response.json()['stations'][0]['elevation'] == 701
+    assert response.json()['stations'][0]['fuel_type'] == 'M4'
+
+    assert math.isclose(response.json()['stations'][0]['fine_fuel_moisture_code'], 90.683, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['drought_code'], 340.544, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['initial_spread_index'], 7.51, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['build_up_index'], 117.899, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['duff_moisture_code'], 103.923, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['fire_weather_index'], 27.913, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['head_fire_intensity'], 11698.516, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['rate_of_spread'], 9.527, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['percentage_crown_fraction_burned'], 0.796, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['flame_length'], 6.245, abs_tol=0.001)
+    assert math.isclose(response.json()['stations'][0]['sixty_minute_fire_size'], 20.792, abs_tol=0.1)
+    assert math.isclose(response.json()['stations'][0]['thirty_minute_fire_size'], 3.496, abs_tol=0.001)
+
+    assert response.json()['stations'][0]['fire_type'] == 'IC'
+    assert response.json()['stations'][0]['critical_hours_hfi_4000'] == {"start": 13.0, "end": 22.0}
+    assert response.json()['stations'][0]['critical_hours_hfi_10000'] == {"start": 15.0, "end": 19.0}
