5eb7bd98d4717d2da3c6039a436bb61e233e95bc
Brett Edwards <brett.1.edwards@gov.bc.ca>
2023-10-05 10:26:39 -0700

    add tests

--- web/src/features/fba/components/map/legend.test.tsx
+++ web/src/features/fba/components/map/legend.test.tsx
@@ -1,5 +1,6 @@
 import Legend from 'features/fba/components/map/Legend'
 import { render, waitFor, within } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import React from 'react'
 
 describe('Legend', () => {
@@ -22,9 +23,36 @@ describe('Legend', () => {
     const zoneStatus = getByTestId('zone-checkbox')
     const zoneStatusCheckbox = within(zoneStatus).getByRole('checkbox')
     await waitFor(() => expect(zoneStatusCheckbox).toBeChecked())
+    await waitFor(() => expect(setShowZoneStatus).not.toHaveBeenCalled())
 
     const hfi = getByTestId('hfi-checkbox')
     const hfiCheckbox = within(hfi).getByRole('checkbox')
     await waitFor(() => expect(hfiCheckbox).not.toBeChecked())
+    await waitFor(() => expect(setShowHFI).not.toHaveBeenCalled())
+  })
+
+  it('should call click handlers on checkboxes', async () => {
+    const onToggleLayer = jest.fn()
+    const setShowZoneStatus = jest.fn()
+    const setShowHFI = jest.fn()
+    const { getByTestId } = render(
+      <Legend
+        onToggleLayer={onToggleLayer}
+        setShowZoneStatus={setShowZoneStatus}
+        setShowHFI={setShowHFI}
+        showHFI={false}
+        showZoneStatus={true}
+      />
+    )
+
+    const zoneStatus = getByTestId('zone-checkbox')
+    const zoneStatusCheckbox = within(zoneStatus).getByRole('checkbox')
+    await userEvent.click(zoneStatusCheckbox)
+    await waitFor(() => expect(setShowZoneStatus).toBeCalledTimes(1))
+
+    const hfi = getByTestId('hfi-checkbox')
+    const hfiCheckbox = within(hfi).getByRole('checkbox')
+    await userEvent.click(hfiCheckbox)
+    await waitFor(() => expect(setShowHFI).toBeCalledTimes(1))
   })
 })
