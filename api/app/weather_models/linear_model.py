935e54ede960b5fc87911d47e24b5a100cf49d5f
Conor Brady <con.brad@gmail.com>
2023-10-20 15:33:47 -0700

    Add more logging info

--- api/app/weather_models/linear_model.py
+++ api/app/weather_models/linear_model.py
@@ -31,8 +31,9 @@ class LinearModel():
         for hour in self._samples.hours():
             try:
                 self._models[hour].fit(self._samples.np_x(hour), self._samples.np_y(hour))
-            except ValueError as e:
-                logger.info(e)
+            except ValueError as _:
+                logger.error("Error trying to fit data for model at hour: %s, dumping samples, x: %s, y: %s",
+                             hour, self._samples.np_x(hour), self._samples.np_y(hour), exc_info=True)
 
     def predict(self, hour: int, model_wind_dir: List[List[int]]):
         try:
