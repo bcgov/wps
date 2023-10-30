f577fcb66756aa5d75d025aec6f0c95e33c90199
Darren Boss <darren.boss@gov.bc.ca>
2023-10-30 11:42:47 -0700

    observations crud functions

--- api/app/db/crud/observations.py
+++ api/app/db/crud/observations.py
@@ -2,10 +2,11 @@
 """
 import datetime
 from typing import List
-from sqlalchemy import and_, select
+from sqlalchemy import and_, select, text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import Session
-from app.db.models.weather_models import ModelRunPrediction, PredictionModelRunTimestamp
+from app.db.models.weather_models import (ModelRunPrediction, PredictionModel, PredictionModelRunTimestamp,
+                                          WeatherStationModelPrediction)
 from app.db.models.observations import HourlyActual
 
 
@@ -69,3 +70,48 @@ def get_accumulated_precipitation(session: Session, station_code: int, start_dat
     if result is None:
         return 0
     return result
+
+
+def get_accumulated_precip_by_24h_interval(session: Session, station_code: int, start_datetime: datetime, end_datetime: datetime):
+    """ Get the accumulated precip for 24 hour intervals for a given station code within the specified time interval.
+    :param session: The ORM/database session.
+    :param station_code: The numeric code identifying the weather station of interest.
+    :param start_datetime: The earliest date and time of interest.
+    :param end_datetime: The latest date and time of interest.
+    """
+    # Note: I couldn't construct this query in SQLAlchemy, hence the need for the 'text' based query.
+    stmt = """
+        SELECT day, station_code, sum(precipitation) actual_precip_24h
+        FROM
+            generate_series(\'{}\', \'{}\', '24 hours'::interval) day
+        LEFT JOIN
+            hourly_actuals
+        ON 
+            weather_date <@ tstzrange(day, day + '24 hours', '(]')
+        WHERE
+            station_code = {}
+        GROUP BY
+            day, station_code;
+    """.format(start_datetime, end_datetime, station_code)
+    result = session.execute(text(stmt))
+    return result.all()
+
+
+def get_predicted_daily_precip(session: Session, model: PredictionModel, station_code: int, start_datetime: datetime, end_datetime: datetime):
+    """ Gets rows from WeatherStationModelPrediction for the given model and station within the
+    specified time interval at 20:00:00 UTC each day.
+    :param session: The ORM/database session
+    :param model: The numeric weather prediction model
+    :param station_code: The code identifying the weather station.
+    :param start_datetime: The earliest date and time of interest.
+    :param end_datetime: The latest date and time of interest. 
+    """
+    result = session.query(WeatherStationModelPrediction)\
+        .join(PredictionModelRunTimestamp, PredictionModelRunTimestamp.id == WeatherStationModelPrediction.prediction_model_run_timestamp_id)\
+        .filter(PredictionModelRunTimestamp.prediction_model_id == model.id)\
+        .filter(WeatherStationModelPrediction.station_code == station_code)\
+        .filter(WeatherStationModelPrediction.prediction_timestamp >= start_datetime)\
+        .filter(WeatherStationModelPrediction.prediction_timestamp < end_datetime)\
+        .filter(func.date_part('hour', WeatherStationModelPrediction.prediction_timestamp) == 20)\
+        .order_by(WeatherStationModelPrediction.prediction_timestamp)
+    return result.all()
