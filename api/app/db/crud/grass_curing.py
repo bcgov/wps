fae6b6244c29ffa24345525ab3b1baa81bee2e68
Darren Boss <darren.boss@gov.bc.ca>
2024-02-02 09:39:09 -0800

    Remove unused crud

--- api/app/db/crud/grass_curing.py
+++ api/app/db/crud/grass_curing.py
@@ -7,15 +7,6 @@ from sqlalchemy.sql import func
 from app.db.models.grass_curing import PercentGrassCuring
 
 
-def save_all_percent_grass_curing(session: AsyncSession, records: list[PercentGrassCuring]):
-    """ Bulk save multiple PercentGrassCuring records.
-    
-    :param session: A session object for asynchronous database access.
-    :type session: AsyncSession
-    """
-    session.add_all(records)
-
-
 async def save_percent_grass_curing(session: AsyncSession, percent_grass_curing: PercentGrassCuring):
     """ Add a new PercentGrassCuring record.
 
@@ -36,19 +27,3 @@ async def get_last_percent_grass_curing_for_date(session: AsyncSession):
     stmt = select(func.max(PercentGrassCuring.for_date))
     result = await session.execute(stmt)
     return result.scalar()
-
-
-async def get_percent_grass_curing_by_date(session: AsyncSession, for_date: date):
-    """ Retrieve the PercentGrassCuring records for all stations for the given date.
-
-    :param session: A session object for asynchronous database access.
-    :type session: AsyncSession
-    :param for_date: The date of interest.
-    :type for_date: Date
-    :return: A list of percent grass curing for all stations for the given date.
-    :rtype: ProcessedSnow
-    """
-    stmt = select(PercentGrassCuring)\
-        .where(PercentGrassCuring.for_date == for_date)
-    result = await session.execute(stmt)
-    return result
