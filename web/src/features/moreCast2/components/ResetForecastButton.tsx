5131bcd17b5d805ffaee0d0101a3ec8c517a8c7a
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-03-12 15:43:57 -0700

    more tests

--- web/src/features/moreCast2/components/ResetForecastButton.tsx
+++ web/src/features/moreCast2/components/ResetForecastButton.tsx
@@ -13,35 +13,36 @@ export interface ResetForecastButtonProps {
   setAllRows: React.Dispatch<React.SetStateAction<MoreCast2Row[]>>
 }
 
-const ResetForecastButton = ({ className, enabled, label, allRows, setAllRows }: ResetForecastButtonProps) => {
-  /**
-   * Reset forecast rows to their default state. Temp, RH, Wind Dir & Speed are cleared,
-   * Precip is set to 0, and GC is carried forward from last submitted value.
-   */
-  const resetForecastRows = () => {
-    const resetRows = allRows.map(row => {
-      const rowToReset = { ...row }
-      Object.keys(rowToReset).forEach(key => {
-        if (key.includes(WeatherDeterminate.FORECAST)) {
-          const isPrecipField = key.includes('precip')
-          const field = rowToReset[key as keyof MoreCast2Row] as PredictionItem
-          // Submitted forecasts have a ModelChoice.FORECAST, we don't want to reset those
-          if (field.choice != ModelChoice.FORECAST && !isNaN(field.value)) {
-            field.value = isPrecipField ? 0 : NaN
-            field.choice = ''
-          }
+/**
+ * Reset forecast rows to their default state. Temp, RH, Wind Dir & Speed are cleared,
+ * Precip is set to 0, and GC is carried forward from last submitted value.
+ */
+export const resetForecastRows = (rows: MoreCast2Row[]) => {
+  const resetRows = rows.map(row => {
+    const rowToReset = { ...row }
+    Object.keys(rowToReset).forEach(key => {
+      if (key.includes(WeatherDeterminate.FORECAST)) {
+        const isPrecipField = key.includes('precip')
+        const field = rowToReset[key as keyof MoreCast2Row] as PredictionItem
+        // Submitted forecasts have a ModelChoice.FORECAST, we don't want to reset those
+        if (field.choice != ModelChoice.FORECAST && !isNaN(field.value)) {
+          field.value = isPrecipField ? 0 : NaN
+          field.choice = ''
         }
-      })
-      return rowToReset
+      }
     })
-    setAllRows(resetRows)
-    fillGrassCuringForecast(allRows)
-  }
+    return rowToReset
+  })
+  fillGrassCuringForecast(resetRows)
+  return resetRows
+}
 
+const ResetForecastButton = ({ className, enabled, label, allRows, setAllRows }: ResetForecastButtonProps) => {
   const storedDraftForecast = new MorecastDraftForecast(localStorage)
 
   const handleResetClick = () => {
-    resetForecastRows()
+    const resetRows = resetForecastRows(allRows)
+    setAllRows(resetRows)
     storedDraftForecast.clearDraftForecasts()
   }
 
--- web/src/features/moreCast2/components/resetForecastButton.test.tsx
+++ web/src/features/moreCast2/components/resetForecastButton.test.tsx
@@ -1,8 +1,12 @@
 import { render } from '@testing-library/react'
 import store from 'app/store'
-import ResetForecastButton from 'features/moreCast2/components/ResetForecastButton'
+import ResetForecastButton, { resetForecastRows } from 'features/moreCast2/components/ResetForecastButton'
+import { buildValidActualRow, buildValidForecastRow } from 'features/moreCast2/rowFilters.test'
+import { DateTime } from 'luxon'
 import React from 'react'
 import { Provider } from 'react-redux'
+
+const TEST_DATE = DateTime.now()
 
 describe('SaveForecastButton', () => {
   it('should render the button as enabled', () => {
@@ -26,5 +30,30 @@ describe('SaveForecastButton', () => {
     const manageStationsButton = getByTestId('reset-forecast-button')
     expect(manageStationsButton).toBeInTheDocument()
     expect(manageStationsButton).toBeDisabled()
+  })
+  it('should reset the forecast rows to their initial load state', () => {
+    const mockRowData = [
+      buildValidForecastRow(111, TEST_DATE.plus({ days: 1 }), 'MANUAL'),
+      buildValidForecastRow(222, TEST_DATE.plus({ days: 1 }), 'MANUAL'),
+      buildValidActualRow(222, TEST_DATE.minus({ days: 1 }))
+    ]
+
+    const resetRows = resetForecastRows(mockRowData)
+
+    expect(resetRows[0].tempForecast?.value).toBe(NaN)
+    expect(resetRows[0].rhForecast?.value).toBe(NaN)
+    expect(resetRows[0].windSpeedForecast?.value).toBe(NaN)
+    expect(resetRows[0].precipForecast?.value).toBe(0)
+  })
+  it('should not reset rows with submitted forecasts or actuals', () => {
+    const mockRowData = [
+      buildValidForecastRow(111, TEST_DATE.plus({ days: 1 }), 'FORECAST'),
+      buildValidForecastRow(222, TEST_DATE.plus({ days: 1 }), 'FORECAST'),
+      buildValidActualRow(222, TEST_DATE.minus({ days: 1 }))
+    ]
+
+    const resetRows = resetForecastRows(mockRowData)
+
+    expect(resetRows).toEqual(mockRowData)
   })
 })
