62d38577e1169303fd9b13d9820de029b83e9069
Darren Boss <darren.boss@gov.bc.ca>
2024-03-12 15:46:48 -0700

    Look back 3 weeks

--- api/app/jobs/viirs_snow.py
+++ api/app/jobs/viirs_snow.py
@@ -236,7 +236,7 @@ class ViirsSnowJob():
         today = date.today()
         if last_processed_date is None:
             # Case to cover the initial run of VIIRS snow processing (ie. start processing one week ago)
-            next_date = today - timedelta(days=10)
+            next_date = today - timedelta(days=21)
         else:
             # Start processing the day after the last record of a successful job.
             next_date = last_processed_date + timedelta(days=1)
