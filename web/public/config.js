0e5af41ec67980a27fb007512f2c9bbccd7315e5
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-03-19 15:55:40 -0700

    config sample

--- web/public/config.js
+++ web/public/config.js
@@ -16,5 +16,6 @@ const config = {
   RASTER_SERVER_BASE_URL: 'https://wps-dev-raster-tileserver.apps.silver.devops.gov.bc.ca/v0.0.1',
   REACT_APP_MS_TEAMS_SPRINT_REVIEW_URL: 'http://localhost:3000',
   REACT_APP_MIRO_SPRINT_REVIEW_BOARD_URL: 'http://localhost:3000',
-  REACT_APP_PMTILES_BUCKET: 'https://My_S3_Bucket'
+  REACT_APP_PMTILES_BUCKET: 'https://My_S3_Bucket',
+  REACT_APP_MUI_LICENSE_KEY: 'key'
 }
