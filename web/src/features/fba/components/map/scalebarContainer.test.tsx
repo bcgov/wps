44f6dc0e502a249e0449dead9cc7eb11591eaf89
dgboss <darren.boss@gov.bc.ca>
2024-04-02 13:51:08 -0700

    Add scalebar to ASA (#3501)

---
+++ web/src/features/fba/components/map/scalebarContainer.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import ScalebarContainer from 'features/fba/components/map/ScaleBarContainer'
+
+describe('ScalebarContainer', () => {
+  it('should render', () => {
+    const ref = React.createRef<HTMLElement | null>()
+    const { getByTestId } = render(<ScalebarContainer ref={ref} />)
+    const scalebarContainer = getByTestId('scalebar-container')
+    expect(scalebarContainer).toBeVisible()
+  })
+})
