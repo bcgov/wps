876902862784a6051053d57e0045ac7baf32e109
Conor Brady <con.brad@gmail.com>
2024-04-25 09:54:04 -0700

    Limit hourlies endpoint to only store ffmc files (#3563)

--- api/app/auto_spatial_advisory/sfms.py
+++ api/app/auto_spatial_advisory/sfms.py
@@ -10,6 +10,10 @@ def is_hfi_file(filename: str) -> bool:
     "Returns true if filename starts with 'hfi'"
     return filename.startswith("hfi")
 
+def is_ffmc_file(filename: str) -> bool:
+    "Returns true if filename starts with 'fine_fuel_moisture_code'"
+    return filename.startswith("fine_fuel_moisture_code")
+
 def get_date_part(filename: str) -> str:
     """ Get the date part of the filename.
     Filename example: hfi20220823.tif
