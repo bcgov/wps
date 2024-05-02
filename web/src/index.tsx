cc12c54dfa9c8717fe7b925951ba3192e1c97013
Conor Brady <con.brad@gmail.com>
2024-05-02 13:09:50 -0700

    Only init sentry if we're in prod on frontend

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -10,18 +10,20 @@ import * as serviceWorker from 'serviceWorker'
 import * as Sentry from '@sentry/react'
 
 const render = () => {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    environment: SENTRY_ENV,
-    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: [API_BASE_URL],
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  })
+  if (SENTRY_ENV === 'production') {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      environment: SENTRY_ENV,
+      integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+      // Performance Monitoring
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [API_BASE_URL],
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    })
+  }
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const container = document.getElementById('root')
