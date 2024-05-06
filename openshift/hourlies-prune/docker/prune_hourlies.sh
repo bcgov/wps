09029964000866465e35b3267784ffa7b877ed61
Conor Brady <con.brad@gmail.com>
2024-05-06 12:00:01 -0700

    Try unix variant

--- openshift/hourlies-prune/docker/prune_hourlies.sh
+++ openshift/hourlies-prune/docker/prune_hourlies.sh
@@ -34,7 +34,7 @@ then
 fi
 
 hourlies_path="sfms/uploads/hourlies"
-day_before_yesterday=$(TZ=America/Vancouver date -v-2d -I)
+day_before_yesterday=$(TZ=America/Vancouver date -d '2 days ago' -I)
 
 echo "Cleaning up hourlies from ${AWS_BUCKET}/${hourlies_path}/${day_before_yesterday}"
 AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY}" AWS_SECRET_ACCESS_KEY="${AWS_SECRET_KEY}" aws --endpoint="https://${AWS_HOSTNAME}" s3 rm --recursive "s3://${AWS_BUCKET}/${hourlies_path}/${day_before_yesterday}/"
