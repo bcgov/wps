91fcc9e397fe52a0abd70c3bfdcbaf57a5391785
Darren Boss <darren.boss@gov.bc.ca>
2024-05-08 13:38:13 -0700

    Store last logged in time in local storage

--- web/src/features/auth/components/MoreCast2AuthWrapper.tsx
+++ web/src/features/auth/components/MoreCast2AuthWrapper.tsx
@@ -5,6 +5,7 @@ import { TEST_AUTH, WF1_AUTH_URL } from 'utils/env'
 import { wf1Authenticate, wf1AuthenticateError } from 'features/auth/slices/wf1AuthenticationSlice'
 import { selectAuthentication, selectWf1Authentication } from 'app/rootReducer'
 import { ROLES } from 'features/auth/roles'
+import { DateTime } from 'luxon'
 
 interface Props {
   children: React.ReactElement
@@ -30,12 +31,20 @@ const MoreCast2AuthWrapper = ({ children }: Props) => {
         setRenderChildren(true)
       }
 
-      if (!window.location.href?.includes('access_token') && isAuthenticatedForecaster) {
+      const lastLoginString = localStorage.getItem('last_morecast_login') ?? '0'
+      const lastLogin = parseInt(lastLoginString)
+      const now = DateTime.now().toUnixInteger()
+
+      // Force a redirect to WF1 authentication if there is no access_token or the last time Morecast
+      // was logged into was more than 60 minutes ago in order to handle a user reloading a page with an
+      // old access token
+      if (!window.location.href?.includes('access_token') || now - lastLogin > 30) {
         window.location.href = `${WF1_AUTH_URL}&redirect_uri=${window.location.href}`
       }
 
       if (window.location.href?.includes('access_token')) {
         const wf1Token = window.location.href.split('#access_token=')[1].split('&')[0]
+        localStorage.setItem('last_morecast_login', DateTime.now().toUnixInteger().toString())
         try {
           dispatch(wf1Authenticate(wf1Token))
           setRenderChildren(true)
