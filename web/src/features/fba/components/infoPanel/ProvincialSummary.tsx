72bb01135723528648dd2af501d3ff9ec2f2d07f
Darren Boss <darren.boss@gov.bc.ca>
2024-05-15 08:55:59 -0700

    Sort fire centres alphabetically Maintain accordion state

--- web/src/features/fba/components/infoPanel/ProvincialSummary.tsx
+++ web/src/features/fba/components/infoPanel/ProvincialSummary.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { selectProvincialSummary } from 'features/fba/slices/provincialSummarySlice'
 import FireCentreInfo from 'features/fba/components/infoPanel/FireCentreInfo'
@@ -7,6 +7,7 @@ import { isNull, isUndefined } from 'lodash'
 import { Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { INFO_PANEL_CONTENT_BACKGORUND } from 'app/theme'
+import { CARIBOO_FC, COASTAL_FC, KAMLOOPS_FC, NORTHWEST_FC, PRINCE_GEORGE_FC, SOUTHEAST_FC } from 'utils/constants'
 
 interface ProvincialSummaryProps {
   advisoryThreshold: number
@@ -16,10 +17,25 @@ export const NO_DATA_MESSAGE = 'Choose a date of interest above.'
 
 // Displays advisory status of all fire zone units in all fire centres across BC.
 const ProvincialSummary = ({ advisoryThreshold }: ProvincialSummaryProps) => {
+  const [fireCentreExpanded, setFireCentreExpanded] = useState<Record<string, boolean>>({
+    CARIBOO_FC: false,
+    COASTAL_FC: true,
+    KAMLOOPS_FC: false,
+    NORTHWEST_FC: false,
+    PRINCE_GEORGE_FC: false,
+    SOUTHEAST_FC: false
+  })
   const provincialSummary = useSelector(selectProvincialSummary)
   const theme = useTheme()
   const noProvincialSummary =
     isNull(provincialSummary) || isUndefined(provincialSummary) || Object.keys(provincialSummary).length === 0
+
+  const handleFireCentreAccordionChanged =
+    (fireCenterName: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      const newValue = { ...fireCentreExpanded }
+      newValue[fireCenterName] = isExpanded
+      setFireCentreExpanded(newValue)
+    }
 
   return (
     <div data-testid="provincial-summary">
@@ -36,16 +52,20 @@ const ProvincialSummary = ({ advisoryThreshold }: ProvincialSummaryProps) => {
             {NO_DATA_MESSAGE}
           </Typography>
         ) : (
-          Object.keys(provincialSummary).map(key => {
-            return (
-              <FireCentreInfo
-                key={key}
-                advisoryThreshold={advisoryThreshold}
-                fireCentreName={key}
-                fireZoneUnitInfos={provincialSummary[key]}
-              />
-            )
-          })
+            Object.keys(provincialSummary)
+              .sort((a, b) => a.localeCompare(b))
+              .map(key => {
+                return (
+                  <FireCentreInfo
+                    key={key}
+                    advisoryThreshold={advisoryThreshold}
+                  expanded={fireCentreExpanded[key]}
+                  fireCentreName={key}
+                  fireZoneUnitInfos={provincialSummary[key]}
+                  onChangeExpaneded={handleFireCentreAccordionChanged}
+                />
+              )
+            })
         )}
       </InfoAccordion>
     </div>
