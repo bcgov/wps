74d19e92b6ec1030d558ba31c7d305bc649cab5c
Darren Boss <darren.boss@gov.bc.ca>
2024-05-16 11:19:36 -0700

    - return None from cffdrs dc and dmc functions - add tests for cffdrs dc and dmc

--- api/app/tests/fire_behavior/test_cffdrs.py
+++ api/app/tests/fire_behavior/test_cffdrs.py
@@ -6,7 +6,7 @@ import pytest
 import math
 from app.schemas.fba_calc import FuelTypeEnum
 from app.fire_behaviour import cffdrs
-from app.fire_behaviour.cffdrs import (pandas_to_r_converter, hourly_fine_fuel_moisture_code, CFFDRSException)
+from app.fire_behaviour.cffdrs import pandas_to_r_converter, hourly_fine_fuel_moisture_code, CFFDRSException
 
 
 start_date = datetime(2023, 8, 17)
@@ -33,44 +33,49 @@ def test_pandas_to_r_converter():
 def test_hourly_ffmc_calculates_values():
     ffmc_old = 80.0
     df = hourly_fine_fuel_moisture_code(df_hourly, ffmc_old)
-    
+
     assert not df['hffmc'].isnull().any()
 
 
 def test_hourly_ffmc_no_temperature():
     ffmc_old = 80.0
-    df_hourly = pd.DataFrame({'datetime': [hourly_datetimes[0], hourly_datetimes[1]], 'celsius': [12, 1], 'precipitation': [0, 1], 'ws': [14, 12], 'rh':[50, 50]})
+    df_hourly = pd.DataFrame(
+        {
+            'datetime': [hourly_datetimes[0], hourly_datetimes[1]],
+            'celsius': [12, 1],
+            'precipitation': [0, 1],
+            'ws': [14, 12],
+            'rh': [50, 50],
+        }
+    )
 
     with pytest.raises(CFFDRSException):
         hourly_fine_fuel_moisture_code(df_hourly, ffmc_old)
-    
+
 
 def test_ros():
-    """ ROS runs """
-    ros =cffdrs.rate_of_spread(FuelTypeEnum.C7, 1, 1, 1, 1, pc=100, pdf=None,
-                                 cc=None, cbh=10)
+    """ROS runs"""
+    ros = cffdrs.rate_of_spread(FuelTypeEnum.C7, 1, 1, 1, 1, pc=100, pdf=None, cc=None, cbh=10)
     assert math.isclose(ros, 1.2966988409822604e-05)
 
 
 def test_ros_no_isi():
-    """ ROS fails """
+    """ROS fails"""
     with pytest.raises(cffdrs.CFFDRSException):
-        cffdrs.rate_of_spread(FuelTypeEnum.C7, None, 1, 1, 1, pc=100, pdf=None,
-                              cc=None, cbh=10)
+        cffdrs.rate_of_spread(FuelTypeEnum.C7, None, 1, 1, 1, pc=100, pdf=None, cc=None, cbh=10)
 
 
 def test_ros_no_bui():
-    """ ROS fails """
+    """ROS fails"""
     with pytest.raises(cffdrs.CFFDRSException):
-        cffdrs.rate_of_spread(FuelTypeEnum.C7, 1, None, 1, 1, pc=100, pdf=None,
-                              cc=None, cbh=10)
+        cffdrs.rate_of_spread(FuelTypeEnum.C7, 1, None, 1, 1, pc=100, pdf=None, cc=None, cbh=10)
 
 
 def test_ros_no_params():
-    """ ROS fails """
+    """ROS fails"""
     with pytest.raises(cffdrs.CFFDRSException):
-        cffdrs.rate_of_spread(FuelTypeEnum.C7, None, None, None, None, pc=100, pdf=None,
-                              cc=None, cbh=10)
+        cffdrs.rate_of_spread(FuelTypeEnum.C7, None, None, None, None, pc=100, pdf=None, cc=None, cbh=10)
+
 
 @pytest.mark.parametrize(
     "ffmc,temperature,precipitation,relative_humidity,wind_speed",
@@ -85,10 +90,31 @@ def test_ros_no_params():
     ],
 )
 def test_failing_ffmc(ffmc, temperature, precipitation, relative_humidity, wind_speed):
-    """ Test that we can handle None values when attempting to calculate ffmc """
-    res = cffdrs.fine_fuel_moisture_code(ffmc=ffmc, 
-                                        temperature=temperature,
-                                        precipitation=precipitation,
-                                        relative_humidity=relative_humidity,
-                                        wind_speed=wind_speed)
+    """Test that we can handle None values when attempting to calculate ffmc"""
+    res = cffdrs.fine_fuel_moisture_code(
+        ffmc=ffmc,
+        temperature=temperature,
+        precipitation=precipitation,
+        relative_humidity=relative_humidity,
+        wind_speed=wind_speed,
+    )
+    assert res is None
+
+
+@pytest.mark.parametrize(
+    'dmc,temperature,relative_humidity,precipitation',
+    [(None, 10, 90, 1), (100, None, 90, 1), (100, 10, None, 1), (100, 10, 90, None)],
+)
+def test_failing_dmc(dmc, temperature, relative_humidity, precipitation):
+    """Test that we can handle None values when attempting to calculate dmc"""
+    res = cffdrs.duff_moisture_code(dmc, temperature, relative_humidity, precipitation)
+    assert res is None
+
+
+@pytest.mark.parametrize(
+    'dc,temperature,relative_humidity,precipitation', [(None, 10, 90, 1), (100, None, 90, 1), (100, 10, 90, None)]
+)
+def test_failing_dc(dc, temperature, relative_humidity, precipitation):
+    """Test that we can handle None values when attempting to calculate dc"""
+    res = cffdrs.drought_code(dc, temperature, relative_humidity, precipitation)
     assert res is None
