bcc6ee1fe39ffd48b99d3016e9822164fff57641
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-05-22 08:49:13 -0700

    Fuel Grid Changes (#3637)
    - Removes raster reprojections from HFI calculations
    - Reprojects polygons from database to match raster crs

--- api/app/tests/fba/test_process_fuel_type_area.py
+++ api/app/tests/fba/test_process_fuel_type_area.py
@@ -8,8 +8,8 @@ HFI_RASTER = np.array([[1000, 2000, 3000, 4005], [5000, 10001, 11000, 12000], [3
 def test_get_warped_fuel_type_s3_key():
     bucket = "abcde"
     key = get_fuel_type_s3_key(bucket)
-    assert key == f"/vsis3/{bucket}/ftl/fuel_types_from_sfms_epsg_3005.tif"
-
+    assert key == f"/vsis3/{bucket}/sfms/static/fbp2024.tif"
+    
 
 def test_classify_by_threshold_1():
     result = classify_by_threshold(HFI_RASTER, 1)
