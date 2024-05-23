d198c308899b365d04a6695877dd10ee5671cae8
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-05-23 11:56:34 -0700

    use alchemy

--- api/alembic/versions/d392e4a5a499_delete_fuel_stats_for_re_calculation.py
+++ api/alembic/versions/d392e4a5a499_delete_fuel_stats_for_re_calculation.py
@@ -6,7 +6,9 @@ Create Date: 2024-05-23 09:39:44.787270
 
 """
 from alembic import op
-
+from datetime import datetime
+from sqlalchemy.orm.session import Session
+from app.db.models.auto_spatial_advisory import AdvisoryFuelStats, RunParameters
 
 # revision identifiers, used by Alembic.
 revision = 'd392e4a5a499'
@@ -16,20 +18,15 @@ depends_on = None
 
 
 def upgrade():
-    stmt = """
-            DELETE
-            FROM
-                advisory_fuel_stats afs
-            WHERE
-                afs.run_parameters in (
-                SELECT
-                    id
-                FROM
-                    run_parameters rp
-                WHERE
-                    rp.for_date >= '2024-05-01')
-            """
-    op.execute(stmt)
+    session = Session(bind=op.get_bind())
+    start_date = datetime(2024, 5, 1)
+
+    # Query for run_parameters IDs based on the start_date
+    rp_result = session.query(RunParameters.id).filter(RunParameters.for_date >= start_date).all()
+    rp_ids = [row.id for row in rp_result]
+
+    # Delete fuel stats with run_parameters IDs in rp_ids
+    session.query(AdvisoryFuelStats).filter(AdvisoryFuelStats.run_parameters.in_(rp_ids)).delete()
 
 
 def downgrade():
