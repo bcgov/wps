68c0d489ed85f7c930340bfcfe09f2486a64f310
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-05-30 16:57:25 -0700

    actual values

--- api/app/tests/weather_models/test_interpolate.py
+++ api/app/tests/weather_models/test_interpolate.py
@@ -1,6 +1,7 @@
 from app.weather_models.interpolate import construct_interpolated_noon_prediction, SCALAR_MODEL_VALUE_KEYS_FOR_INTERPOLATION, interpolate_between_two_points
 from datetime import datetime
 from app.db.models.weather_models import ModelRunPrediction
+import math
 
 
 def generate_model_run_prediction(prediction_datetime: datetime, weather_values: float) -> ModelRunPrediction:
@@ -18,16 +19,17 @@ def generate_model_run_prediction(prediction_datetime: datetime, weather_values:
 def test_construct_interpolated_noon_prediction():
     prediction_a_timestamp = datetime(2024, 5, 1, 18, 0, 0)
     prediction_b_timestamp = datetime(2024, 5, 1, 21, 0, 0)
-    prediction_a = generate_model_run_prediction(prediction_a_timestamp, 1.1)
-    prediction_b = generate_model_run_prediction(prediction_b_timestamp, 4.4)
+    prediction_a = generate_model_run_prediction(prediction_a_timestamp, 1.0)
+    prediction_b = generate_model_run_prediction(prediction_b_timestamp, 4.0)
+    linear_interp_val = 3.0
     
     noon_prediction = construct_interpolated_noon_prediction(prediction_a, prediction_b, SCALAR_MODEL_VALUE_KEYS_FOR_INTERPOLATION)
     assert noon_prediction.prediction_timestamp.hour == 20
-    assert noon_prediction.apcp_sfc_0 is not None
-    assert noon_prediction.rh_tgl_2 is not None
-    assert noon_prediction.wdir_tgl_10 is not None
-    assert noon_prediction.wind_tgl_10 is not None
-    assert noon_prediction.tmp_tgl_2 is not None
+    assert math.isclose(noon_prediction.apcp_sfc_0, linear_interp_val, abs_tol=0)
+    assert math.isclose(noon_prediction.rh_tgl_2, linear_interp_val, abs_tol=0)
+    assert math.isclose(noon_prediction.wdir_tgl_10, linear_interp_val, abs_tol=0)
+    assert math.isclose(noon_prediction.wind_tgl_10, linear_interp_val, abs_tol=0)
+    assert math.isclose(noon_prediction.tmp_tgl_2, linear_interp_val, abs_tol=0)
 
 def test_interpolate_between_two_points():
     interp_value = interpolate_between_two_points(1, 4, 1, 4, 3)
