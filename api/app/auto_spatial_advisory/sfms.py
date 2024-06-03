6c3ac0c0e6b1662516ac5ae76077f31e33504d9f
Conor Brady <con.brad@gmail.com>
2024-06-03 14:38:29 -0700

    Add bui numba test to run in dev

--- api/app/auto_spatial_advisory/sfms.py
+++ api/app/auto_spatial_advisory/sfms.py
@@ -3,6 +3,7 @@
 import os
 from datetime import date
 from typing import Final
+from numba import vectorize, float64
 from app.schemas.auto_spatial_advisory import SFMSFile, SFMSRunType
 from app.utils.time import get_hour_20, get_vancouver_now
 
@@ -99,3 +100,55 @@ def get_sfms_file_message(filename: str, meta_data: dict) -> SFMSFile:
                     for_date=date(year=int(for_date[0:4]),
                                   month=int(for_date[4:6]),
                                   day=int(for_date[6:8])))
+
+@vectorize(['float32(float32, float32)',
+            'float64(float64, float64)'])
+def bui(dmc, dc):
+    """
+    Buildup Index Calculation
+
+    Parameters
+    ----------
+    dmc : float
+       Duff Moisture Code
+    dc : float
+       Drought Code
+
+    Returns
+    -------
+    float
+        Buildup Index
+
+    Notes
+    -----
+    All code is based on a C code library that was written by Canadian
+    Forest Service Employees, which was originally based on
+    the Fortran code listed in the reference below. All equations
+    in this code refer to that document.
+
+    Equations and FORTRAN program for the Canadian Forest Fire
+    Weather Index System. 1985. Van Wagner, C.E.; Pickett, T.L.
+    Canadian Forestry Service, Petawawa National Forestry
+    Institute, Chalk River, Ontario. Forestry Technical Report 33.
+    18 p.
+
+    Additional reference on FWI system
+
+    Development and structure of the Canadian Forest Fire Weather
+    Index System. 1987. Van Wagner, C.E. Canadian Forestry Service,
+    Headquarters, Ottawa. Forestry Technical Report 35. 35 p.
+    """
+    if dmc < 0:
+        raise ValueError(f'Invalid dmc: {dmc}')
+    if dc < 0:
+        raise ValueError(f'Invalid dc: {dc}')
+    # Eq. 27a
+    bui1 = 0 if (dmc == 0 and dc == 0) else (0.8 * dc * dmc / (dmc + 0.4 * dc))
+    # Eq. 27b - next 3 lines
+    p = 0 if (dmc == 0) else ((dmc - bui1) / dmc)
+    cc = 0.92 + ((0.0114 * dmc) ** 1.7)
+    bui0 = dmc - cc * p
+    # Constraints
+    bui0 = 0 if (bui0 < 0) else bui0
+    bui1 = bui0 if (bui1 < dmc) else bui1
+    return bui1
