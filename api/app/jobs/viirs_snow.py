2431731087e768ab1e44e395b9009a4303c10016
Darren Boss <darren.boss@gov.bc.ca>
2024-06-11 11:50:43 -0700

    Check all dates for snow data

--- api/app/jobs/viirs_snow.py
+++ api/app/jobs/viirs_snow.py
@@ -262,7 +262,7 @@ class ViirsSnowJob():
                     # expected to occur and there is no need to send a notification to RocketChat.
                     if http_error.response.status_code == 501:
                         logger.info(f"VIIRS snow data is unavailable for date: {date_string}. Exiting job.")
-                        break
+                        continue
                     else:
                         # If a different HTTPError occurred re-raise and let the next exception handler send a notification to RocketChat.
                         raise http_error
