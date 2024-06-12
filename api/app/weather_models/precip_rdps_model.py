096b33c771158c9341db8790d22014a1787a02d1
Conor Brady <con.brad@gmail.com>
2024-06-12 11:24:58 -0700

    skeleton impl

---
+++ api/app/weather_models/precip_rdps_model.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from datetime import datetime
+from numba import vectorize
+
+@dataclass
+class TemporalPrecip:
+    """Precip values at certain times"""
+    timestamp: str
+    precip_amount: float
+    
+    def is_after(self, other) -> bool:
+        return self.timestamp > other.timestamp
+
+def generate_24_hour_accumulating_precip_raster(current_time: datetime):
+    """
+    Given a UTC datetime, grab the raster for that date and compute the 
+    """
+    pass
+
+
+def compute_precip_difference(later_precip: TemporalPrecip, earlier_precip: TemporalPrecip):
+    """
+    Simple function to compute difference between later and earlier precip values
+    to be vectorized with numba.
+    """
+    assert later_precip.is_after(earlier_precip)
+    return vectorized_diff(later_precip.precip_amount, earlier_precip.precip_amount)
+
+
+def _diff(value_a: float, value_b: float):
+    return value_a - value_b
+
+vectorized_diff = vectorize(_diff)
