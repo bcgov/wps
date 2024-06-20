3d40dc7469785c6c3d3e943b331772bdaddbae8e
dgboss <darren.boss@gov.bc.ca>
2024-06-20 15:02:28 -0700

    Save RDPS to S3 for SFMS (#3747)

--- api/app/db/models/__init__.py
+++ api/app/db/models/__init__.py
@@ -9,9 +9,16 @@ Base = declarative_base()
 from app.db.models.forecasts import NoonForecast
 from app.db.models.observations import HourlyActual
 from app.db.models.api_access_audits import APIAccessAudit
-from app.db.models.weather_models import (ProcessedModelRunUrl, PredictionModel, PredictionModelRunTimestamp,
-                                          PredictionModelGridSubset, ModelRunGridSubsetPrediction,
-                                          WeatherStationModelPrediction)
+from app.db.models.weather_models import (
+    ProcessedModelRunUrl,
+    PredictionModel,
+    PredictionModelRunTimestamp,
+    PredictionModelGridSubset,
+    ModelRunGridSubsetPrediction,
+    WeatherStationModelPrediction,
+    SavedModelRunForSFMSUrl,
+    ModelRunForSFMS,
+)
 from app.db.models.hfi_calc import (FireCentre, FuelType, PlanningArea, PlanningWeatherStation)
 from app.db.models.auto_spatial_advisory import (Shape, ShapeType, HfiClassificationThreshold,
                                                  ClassifiedHfi, RunTypeEnum, ShapeTypeEnum, FuelType, HighHfiArea, RunParameters)
