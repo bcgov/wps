3461fd66093a67b46fda78a7b9e4757a9ff0fd0f
Conor Brady <con.brad@gmail.com>
2024-06-20 15:24:43 -0700

    Update code, add spellings

--- api/app/weather_models/precip_rdps_model.py
+++ api/app/weather_models/precip_rdps_model.py
@@ -3,17 +3,21 @@ from datetime import datetime, timedelta
 import numpy
 from numba import vectorize
 from app.utils.s3 import read_into_memory
+from app.weather_models import ModelEnum
+
 
 @dataclass
 class TemporalPrecip:
     """Precip values at certain times"""
+
     timestamp: str
     precip_amount: float | numpy.ndarray
-    
+
     def is_after(self, other) -> bool:
         return self.timestamp > other.timestamp
-    
-# TODO change when we have stored precip 
+
+
+# TODO change when we have stored precip
 RDPS_PRECIP_S3_PREFIX = "sfms/temp/prefix"
 
 
@@ -23,46 +27,33 @@ async def generate_24_hour_accumulating_precip_raster(current_time: datetime):
     and the date for 24 hours before to compute the difference.
     """
     (today_key, yesterday_key) = get_raster_keys_to_diff(current_time)
-    day_data = await read_into_memory(f'{RDPS_PRECIP_S3_PREFIX}/{today_key}')
+    day_data = await read_into_memory(f"{RDPS_PRECIP_S3_PREFIX}/{today_key}")
     if yesterday_key is None:
         # If we don't have yesterday that means we just return today, that happens
         # when current_time is an anchor hour at 00Z or 12Z and we just return the RDPS
         # model run precip amount.
         return day_data
 
-
     yesterday_time = current_time - timedelta(days=1)
-    yesterday_data = await read_into_memory(f'{RDPS_PRECIP_S3_PREFIX}/{yesterday_key}')
+    yesterday_data = await read_into_memory(f"{RDPS_PRECIP_S3_PREFIX}/{yesterday_key}")
 
     later_precip = TemporalPrecip(timestamp=current_time, precip_amount=day_data)
     earlier_precip = TemporalPrecip(timestamp=yesterday_time, precip_amount=yesterday_data)
     return compute_precip_difference(later_precip, earlier_precip)
 
-def get_raster_keys_to_diff(timestamp: datetime): 
+
+def get_raster_keys_to_diff(timestamp: datetime):
     """
-    Decides which raster files to use for calculating
-    the 24 hour accumulating precip given a timestamp.
-
-    We anchor 00Z and 12Z runs against the RDPS grib rasters,
-    for every other hour we calculate the difference between the 
-    previous days's computed and stored hour.
-
-    Returns the keys to the tifs in the object store
+    To maintain the invariant that only the same model runs are diffed, we need to grab the latest
+    model run that has data greater than 24 hours ago, but less than 36 hours ago.
     """
-    if timestamp.utcoffset() is None or timestamp.utcoffset().total_seconds != 0.0:
-        raise ValueError("timestamp must be a UTC timestamp")
-    
-    if timestamp.hour == 0 or timestamp.hour == 12:
-        ## Case 1: Anchor hour at 00Z or 12Z, return RDPS model
-        today = timestamp - timedelta(hours=1)
-        return (f'{RDPS_PRECIP_S3_PREFIX}/{today.isoformat()}/{timestamp.hour}Z.tif')
-
-    ## Case 2: Otherwise calculate the difference with the assumed tifs existing
-    # TODO we only have the hour before, can we compare it to the value from the hour from yesterday?
-    today = timestamp - timedelta(hours=1)
-    yesterday = timestamp - timedelta(days=1)
-    return (f'{RDPS_PRECIP_S3_PREFIX}/{yesterday.isoformat()}/{timestamp.hour}Z.tif',
-                f'{RDPS_PRECIP_S3_PREFIX}/{today.isoformat()}/{timestamp.hour}Z.tif')
+    target_model_run_date = timestamp - timedelta(hours=24)
+    target_date_key = f"{target_model_run_date.year}-{target_model_run_date.month}-{target_model_run_date.day}"
+    # From earlier model run, get the keys for 24 hours before timestamp and the timestamp to perform the diff
+    # TODO figure out the filename, for model runs we use the RDPS naming, what do we use for intermediate diffs?
+    earlier_key = f"weather_models/{ModelEnum.RDPS.lower()}/{target_date_key}/{target_model_run_date.hour:02d}/precip/todo-filename"
+    later_key = f"weather_models/{ModelEnum.RDPS.lower()}/{target_date_key}/{timestamp.hour:02d}/precip/todo-filename"
+    return (earlier_key, later_key)
 
 
 def compute_precip_difference(later_precip: TemporalPrecip, earlier_precip: TemporalPrecip):
@@ -77,5 +68,6 @@ def compute_precip_difference(later_precip: TemporalPrecip, earlier_precip: Temp
 
 def _diff(value_a: float, value_b: float):
     return value_a - value_b
+
 
 vectorized_diff = vectorize(_diff)
