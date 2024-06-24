d40833c78146646a07d5d87b97f1483f396398e4
Conor Brady <con.brad@gmail.com>
2024-06-24 16:36:00 -0700

    delete old function, add failure tests

--- api/app/tests/weather_models/test_rdps_filename_marshaller.py
+++ api/app/tests/weather_models/test_rdps_filename_marshaller.py
@@ -1,3 +1,4 @@
+import pytest
 from datetime import datetime, timezone
 from app.weather_models.rdps_filename_marshaller import parse_rdps_filename, compose_rdps_filename
 
@@ -7,6 +8,30 @@ def test_parse_filename_ok():
     assert start_date == "20240624"
     assert run_hour == "12"
     assert forecast_hour == "001"
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        # missing CMC
+        ("reg_APCP_SFC_0_ps10km_2024062412_P001.grib2"),
+        # missing reg
+        ("CMC_APCP_SFC_0_ps10km_2024062412_P001.grib2"),
+        # missing accumulated precip
+        ("CMC_reg_SFC_0_ps10km_2024062412_P001.grib2"),
+        # missing level type
+        ("CMC_reg_APCP_0_ps10km_2024062412_P001.grib2"),
+        # missing level
+        ("CMC_reg_APCP_SFC_ps10km_2024062412_P001.grib2"),
+        # missing ps10km
+        ("CMC_reg_APCP_SFC_0_2024062412_P001.grib2"),
+        # missing grib2 extension
+        ("CMC_reg_APCP_SFC_0_ps10km_2024062412_P001"),
+    ],
+)
+def test_parse_filename_failure(filename):
+    with pytest.raises(Exception) as _:
+        parse_rdps_filename(filename)
 
 
 def test_compose_rdps_filename_ok():
