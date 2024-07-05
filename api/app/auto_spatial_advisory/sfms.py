f08acb8ef4876523a3aec96abdefbc23bcebae87
dgboss <darren.boss@gov.bc.ca>
2024-07-05 09:42:14 -0700

    Vectorize dc and dmc (#3764)

--- api/app/auto_spatial_advisory/sfms.py
+++ api/app/auto_spatial_advisory/sfms.py
@@ -1,4 +1,6 @@
-from cffdrs import bui
+from cffdrs import bui, dc, dmc
 from numba import vectorize
 
 vectorized_bui = vectorize(bui)
+vectorized_dc = vectorize(dc)
+vectorized_dmc = vectorize(dmc)
