06be34670f390c3afdebb29dd9b89e45d532a6e4
Darren Boss <darren.boss@gov.bc.ca>
2024-07-09 13:09:37 -0700

    TPI status work

---
+++ web/src/features/fba/components/viz/ElevationLabel.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Grid, Typography } from '@mui/material'
+
+interface ElevationLabelProps {
+  label: string
+}
+
+const ElevationLabel = ({ label }: ElevationLabelProps) => {
+  return (
+    <Grid sx={{ alignItems: 'center', display: 'flex', height: '25%', justifyContent: 'flex-end' }} xs={12}>
+      <Typography sx={{ fontSize: '0.75em' }}>{label}</Typography>
+    </Grid>
+  )
+}
+
+export default React.memo(ElevationLabel)
