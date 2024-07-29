491f327ee940c91f7341374b49bc26a15a565da2
Darren Boss <darren.boss@gov.bc.ca>
2024-07-29 11:43:12 -0700

    Mockup changes

--- web/src/features/fba/components/viz/ElevationFlag.tsx
+++ web/src/features/fba/components/viz/ElevationFlag.tsx
@@ -1,46 +1,28 @@
 import React from 'react'
 import { Box, Grid, Typography } from '@mui/material'
 
-export enum HFIRiskStatus {
-  ADVISORY = 'Advisory',
-  LOW = 'Low',
-  WARNING = 'Warning'
-}
-
-const ElevationRiskStatus = {
-  Low: {
-    color: '#CCCCCC',
-    label: HFIRiskStatus.LOW
-  },
-  Advisory: {
-    color: '#FFD2AB',
-    label: HFIRiskStatus.ADVISORY
-  },
-  Warning: {
-    color: '#CDA3A4',
-    label: HFIRiskStatus.WARNING
-  }
-}
+const FLAG_COLOUR = '#CCCCCC'
 
 interface ElevationFlagProps {
-  status: HFIRiskStatus
+  percent: number
 }
 
-const ElevationFlag = ({ status }: ElevationFlagProps) => {
+const ElevationFlag = ({ percent }: ElevationFlagProps) => {
   return (
     <Grid sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, justifyContent: 'flex-start' }} xs={12}>
       <Box
         sx={{
           alignItems: 'center',
-          backgroundColor: ElevationRiskStatus[status].color,
+          backgroundImage: `linear-gradient(to right, ${FLAG_COLOUR} ${percent}%, #FFFFFF00 ${percent}%)`,
+          border: `1px solid ${FLAG_COLOUR}`,
           clipPath: 'polygon(0 50%, 10% 0, 100% 0, 100% 100%, 10% 100%)',
           display: 'flex',
           height: '32px',
           justifyContent: 'center',
-          width: '100px'
+          width: '65%'
         }}
       >
-        <Typography sx={{ color: '#003466', fontSize: '0.75em' }}>{ElevationRiskStatus[status].label}</Typography>
+        <Typography sx={{ fontSize: '0.75em' }}>{percent}%</Typography>
       </Box>
     </Grid>
   )
