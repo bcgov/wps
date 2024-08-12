f17e43cc0b9a34d7a1879f1c52400940576a2bf1
jacques metevier <jmetevier@gmail.com>
2024-08-12 13:49:56 -0700

    smells

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
 import { Provider } from 'react-redux'
-import App from 'app/App'
+// import App from 'app/App'
 import { SENTRY_DSN, SENTRY_ENV, API_BASE_URL } from 'utils/env'
 
 import 'index.css'
