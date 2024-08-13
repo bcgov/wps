7e055baa9bbdef8454f5ed2e27709a8a9dd57796
Conor Brady <con.brad@gmail.com>
2024-08-12 17:00:54 -0700

    Use urllib encode to encode pg pass (#3839)
    CrunchyDB generates password with special characters that end up being used in connection strings, this PR encodes them using https://docs.python.org/3/library/urllib.parse.html#urllib.parse.quote
    
    Also adjusts the connection string referenced by alembic to escape `%` with `%%`. Pieced this together from issues here: https://github.com/sqlalchemy/alembic/discussions/1280 and here: https://github.com/mlflow/mlflow/issues/1487

--- api/app/db/database.py
+++ api/app/db/database.py
@@ -1,6 +1,7 @@
-""" Setup database to perform CRUD transactions
-"""
+"""Setup database to perform CRUD transactions"""
+
 import logging
+import urllib.parse
 from typing import Generator, AsyncGenerator
 from contextlib import contextmanager, asynccontextmanager
 from sqlalchemy import create_engine
@@ -9,34 +10,31 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from .. import config
 
 logger = logging.getLogger(__name__)
+write_user = config.get("POSTGRES_WRITE_USER", "wps")
+read_user = config.get("POSTGRES_READ_USER", "wpsread")
+postgres_password = urllib.parse.quote(config.get("POSTGRES_PASSWORD", "wps"), safe="~()*!.'")
+postgres_write_host = config.get("POSTGRES_WRITE_HOST", "localhost")
+postgres_read_host = config.get("POSTGRES_READ_HOST", "localhost")
+postgres_port = config.get("POSTGRES_PORT", "5432")
+postgres_database = config.get("POSTGRES_DATABASE", "wps")
 
-write_user = config.get('POSTGRES_WRITE_USER', 'wps')
-read_user = config.get('POSTGRES_READ_USER', 'wpsread')
-postgres_password = config.get('POSTGRES_PASSWORD', 'wps')
-postgres_write_host = config.get('POSTGRES_WRITE_HOST', 'localhost')
-postgres_read_host = config.get('POSTGRES_READ_HOST', 'localhost')
-postgres_port = config.get('POSTGRES_PORT', '5432')
-postgres_database = config.get('POSTGRES_DATABASE', 'wps')
+DB_WRITE_STRING = f"postgresql://{write_user}:{postgres_password}@{postgres_write_host}:{postgres_port}/{postgres_database}"
 
-DB_WRITE_STRING = f'postgresql://{write_user}:{postgres_password}@{postgres_write_host}:{postgres_port}/{postgres_database}'
+DB_READ_STRING = f"postgresql://{read_user}:{postgres_password}@{postgres_read_host}:{postgres_port}/{postgres_database}"
 
-DB_READ_STRING = f'postgresql://{read_user}:{postgres_password}@{postgres_read_host}:{postgres_port}/{postgres_database}'
+ASYNC_DB_READ_STRING = f"postgresql+asyncpg://{read_user}:{postgres_password}@{postgres_read_host}:{postgres_port}/{postgres_database}"
 
-ASYNC_DB_READ_STRING = f'postgresql+asyncpg://{read_user}:{postgres_password}@{postgres_read_host}:{postgres_port}/{postgres_database}'
-
-ASYNC_DB_WRITE_STRING = f'postgresql+asyncpg://{write_user}:{postgres_password}@{postgres_write_host}:{postgres_port}/{postgres_database}'
+ASYNC_DB_WRITE_STRING = f"postgresql+asyncpg://{write_user}:{postgres_password}@{postgres_write_host}:{postgres_port}/{postgres_database}"
 
 # connect to database - defaulting to always use utc timezone
-connect_args = {'options': '-c timezone=utc'}
+connect_args = {"options": "-c timezone=utc"}
 
 _write_engine = create_engine(DB_WRITE_STRING, connect_args=connect_args)
 
 # use pre-ping on read, as connections are quite often stale due to how few users we have at the moment.
 _read_engine = create_engine(
-    DB_READ_STRING,
-    pool_size=int(config.get('POSTGRES_POOL_SIZE', 5)),
-    max_overflow=int(config.get('POSTGRES_MAX_OVERFLOW', 10)),
-    pool_pre_ping=True, connect_args=connect_args)
+    DB_READ_STRING, pool_size=int(config.get("POSTGRES_POOL_SIZE", 5)), max_overflow=int(config.get("POSTGRES_MAX_OVERFLOW", 10)), pool_pre_ping=True, connect_args=connect_args
+)
 
 # TODO: figure out connection pooling? pre-ping etc.?
 _async_read_engine = create_async_engine(ASYNC_DB_READ_STRING, connect_args={"timeout": 30})
@@ -46,39 +44,35 @@ _async_write_engine = create_async_engine(ASYNC_DB_WRITE_STRING)
 # avoid using these variables anywhere outside of context manager - if
 # sessions are not closed, it will result in the api running out of
 # connections and becoming non-responsive.
-_write_session = sessionmaker(
-    autocommit=False, autoflush=False, bind=_write_engine)
-_read_session = sessionmaker(
-    autocommit=False, autoflush=False, bind=_read_engine)
-_async_read_sessionmaker = sessionmaker(
-    autocommit=False, autoflush=False, bind=_async_read_engine, class_=AsyncSession)
-_async_write_sessionmaker = sessionmaker(
-    autocommit=False, autoflush=False, bind=_async_write_engine, class_=AsyncSession)
+_write_session = sessionmaker(autocommit=False, autoflush=False, bind=_write_engine)
+_read_session = sessionmaker(autocommit=False, autoflush=False, bind=_read_engine)
+_async_read_sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=_async_read_engine, class_=AsyncSession)
+_async_write_sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=_async_write_engine, class_=AsyncSession)
 
 
 def _get_write_session() -> Session:
-    """ abstraction used for mocking out a write session """
+    """abstraction used for mocking out a write session"""
     return _write_session()
 
 
 def _get_read_session() -> Session:
-    """ abstraction used for mocking out a read session """
+    """abstraction used for mocking out a read session"""
     return _read_session()
 
 
 def _get_async_read_session() -> AsyncSession:
-    """ abstraction used for mocking out a read session """
+    """abstraction used for mocking out a read session"""
     return _async_read_sessionmaker()
 
 
 def _get_async_write_session() -> AsyncSession:
-    """ abstraction used for mocking out a read session """
+    """abstraction used for mocking out a read session"""
     return _async_write_sessionmaker()
 
 
 @asynccontextmanager
 async def get_async_read_session_scope() -> AsyncGenerator[AsyncSession, None]:
-    """ Return a session scope for async read session """
+    """Return a session scope for async read session"""
     session = _get_async_read_session()
     try:
         yield session
@@ -88,7 +82,7 @@ async def get_async_read_session_scope() -> AsyncGenerator[AsyncSession, None]:
 
 @asynccontextmanager
 async def get_async_write_session_scope() -> AsyncGenerator[AsyncSession, None]:
-    """ Return a session scope for async read session """
+    """Return a session scope for async read session"""
     session = _get_async_write_session()
     try:
         yield session
@@ -109,7 +103,7 @@ def get_read_session_scope() -> Generator[Session, None, None]:
     try:
         yield session
     finally:
-        logger.info('session closed by context manager')
+        logger.info("session closed by context manager")
         session.close()
 
 
