6011b2b819bffa108e43b2fdbc9ca5803f4d69d9
Conor Brady <con.brad@gmail.com>
2024-08-12 18:20:14 -0700

    Shore up sfms run datetimes endpoint tests

--- api/app/tests/fba/test_fba_endpoint.py
+++ api/app/tests/fba/test_fba_endpoint.py
@@ -1,18 +1,23 @@
 from unittest.mock import patch
 import pytest
 from fastapi.testclient import TestClient
-
-from app.db.models.auto_spatial_advisory import AdvisoryElevationStats, AdvisoryTPIStats
+from datetime import date, datetime, timezone
+from app.db.models.auto_spatial_advisory import AdvisoryElevationStats, AdvisoryTPIStats, RunParameters
 from app.schemas.fba import FireZoneElevationStats, FireZoneElevationStatsByThreshold
 
 get_fire_centres_url = "/api/fba/fire-centers"
 get_fire_zone_areas_url = "/api/fba/fire-shape-areas/forecast/2022-09-27/2022-09-27"
 get_fire_zone_tpi_stats_url = "/api/fba/fire-zone-tpi-stats/forecast/2022-09-27/2022-09-27/1"
 get_fire_zone_elevation_info_url = "/api/fba/fire-zone-elevation-info/forecast/2022-09-27/2022-09-27/1"
+get_sfms_run_datetimes_url = "/api/fba/sfms-run-datetimes/forecast/2022-09-27"
+
 
 decode_fn = "jwt.decode"
 mock_tpi_stats = AdvisoryTPIStats(id=1, advisory_shape_id=1, valley_bottom=1, mid_slope=2, upper_slope=3, pixel_size_metres=50)
 mock_elevation_info = [AdvisoryElevationStats(id=1, advisory_shape_id=1, threshold=1, minimum=1.0, quartile_25=2.0, median=3.0, quartile_75=4.0, maximum=5.0)]
+mock_sfms_run_datetimes = [
+    RunParameters(id=1, run_type="forecast", run_datetime=datetime(year=2024, month=1, day=1, hour=1, tzinfo=timezone.utc), for_date=date(year=2024, month=1, day=2))
+]
 
 
 async def mock_get_fire_centres(*_, **__):
@@ -35,6 +40,10 @@ async def mock_get_elevation_info(*_, **__):
     return mock_elevation_info
 
 
+async def mock_get_sfms_run_datetimes(*_, **__):
+    return mock_sfms_run_datetimes
+
+
 @pytest.fixture()
 def client():
     from app.main import app as test_app
@@ -45,7 +54,7 @@ def client():
 
 @pytest.mark.parametrize(
     "endpoint",
-    [get_fire_centres_url, get_fire_zone_areas_url, get_fire_zone_tpi_stats_url, get_fire_zone_elevation_info_url],
+    [get_fire_centres_url, get_fire_zone_areas_url, get_fire_zone_tpi_stats_url, get_fire_zone_elevation_info_url, get_sfms_run_datetimes_url],
 )
 def test_get_endpoints_unauthorized(client: TestClient, endpoint: str):
     """Forbidden to get fire zone areas when unauthorized"""
@@ -76,6 +85,16 @@ def test_get_fire_zone_elevation_info_authorized(client: TestClient):
     assert response.json()["hfi_elevation_info"][0]["elevation_info"]["median"] == mock_elevation_info[0].median
     assert response.json()["hfi_elevation_info"][0]["elevation_info"]["quartile_75"] == mock_elevation_info[0].quartile_75
     assert response.json()["hfi_elevation_info"][0]["elevation_info"]["maximum"] == mock_elevation_info[0].maximum
+
+
+@patch("app.routers.fba.get_auth_header", mock_get_auth_header)
+@patch("app.routers.fba.get_run_datetimes", mock_get_sfms_run_datetimes)
+@pytest.mark.usefixtures("mock_jwt_decode")
+def test_get_sfms_run_datetimes_authorized(client: TestClient):
+    """Allowed to get fire zone tpi stats when authorized"""
+    response = client.get(get_sfms_run_datetimes_url)
+    assert response.status_code == 200
+    assert response.json()[0] == datetime(year=2024, month=1, day=1, hour=1, tzinfo=timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 @patch("app.routers.fba.get_auth_header", mock_get_auth_header)
