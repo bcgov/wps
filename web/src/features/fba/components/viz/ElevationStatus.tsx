fd5eee04f777b050ea2ad833067e4cac79cd7b0c
Darren Boss <darren.boss@gov.bc.ca>
2024-08-14 13:51:33 -0700

    Remove message

--- web/src/features/fba/components/viz/ElevationStatus.tsx
+++ web/src/features/fba/components/viz/ElevationStatus.tsx
@@ -23,49 +23,46 @@ const ElevationStatus = ({ bottom, mid, upper }: ElevationStatusProps) => {
   const theme = useTheme()
   return (
     <Box sx={{ paddingBottom: theme.spacing(2), paddingTop: theme.spacing(2) }}>
-      <Typography sx={{ fontWeight: 'bold', paddingBottom: theme.spacing(1), textAlign: 'center' }}>
-        HFI Distribution by Elevation
-      </Typography>
-    <Grid container sx={{ minHeight: theme.spacing(19) }} xs={12}>
-      <Grid container xs={4}>
-        <Grid sx={{ alignItems: 'center', display: 'flex', height: '25%', justifyContent: 'flex-end' }} xs={12}>
-          <Typography
-            sx={{
-              fontSize: '0.75em',
-              textAlign: 'right',
-              fontWeight: 'bold'
-            }}
-          >
-            Elevation:
-          </Typography>
+      <Grid container sx={{ minHeight: theme.spacing(19) }} xs={12}>
+        <Grid container xs={4}>
+          <Grid sx={{ alignItems: 'center', display: 'flex', height: '25%', justifyContent: 'flex-end' }} xs={12}>
+            <Typography
+              sx={{
+                fontSize: '0.75em',
+                textAlign: 'right',
+                fontWeight: 'bold'
+              }}
+            >
+              Elevation:
+            </Typography>
+          </Grid>
+          <ElevationLabel label={ElevationOption.Upper} />
+          <ElevationLabel label={ElevationOption.MID} />
+          <ElevationLabel label={ElevationOption.BOTTOM} />
         </Grid>
-        <ElevationLabel label={ElevationOption.Upper} />
-        <ElevationLabel label={ElevationOption.MID} />
-        <ElevationLabel label={ElevationOption.BOTTOM} />
-      </Grid>
-      <Grid container sx={{ alignItems: 'flex-end', display: 'flex' }} xs={4}>
-        <Grid sx={{ display: 'flex', alignItems: 'flex-end', height: '80%', justifyContent: 'center' }} xs={12}>
-          <TPIMountain />
+        <Grid container sx={{ alignItems: 'flex-end', display: 'flex' }} xs={4}>
+          <Grid sx={{ display: 'flex', alignItems: 'flex-end', height: '80%', justifyContent: 'center' }} xs={12}>
+            <TPIMountain />
+          </Grid>
+        </Grid>
+        <Grid container xs={4}>
+          <Grid sx={{ alignItems: 'center', display: 'flex', height: '25%', justifyContent: 'flex-start' }} xs={12}>
+            <Typography
+              sx={{
+                fontSize: '0.75em',
+                textAlign: 'left',
+                fontWeight: 'bold',
+                maxWidth: '75%'
+              }}
+            >
+              Proportion of Advisory Area:
+            </Typography>
+          </Grid>
+          <ElevationFlag percent={upper} />
+          <ElevationFlag percent={mid} />
+          <ElevationFlag percent={bottom} />
         </Grid>
       </Grid>
-      <Grid container xs={4}>
-        <Grid sx={{ alignItems: 'center', display: 'flex', height: '25%', justifyContent: 'flex-start' }} xs={12}>
-          <Typography
-            sx={{
-              fontSize: '0.75em',
-              textAlign: 'left',
-              fontWeight: 'bold',
-              maxWidth: '75%'
-            }}
-          >
-            Proportion of Advisory Area:
-          </Typography>
-        </Grid>
-        <ElevationFlag percent={upper} />
-        <ElevationFlag percent={mid} />
-        <ElevationFlag percent={bottom} />
-      </Grid>
-    </Grid>
     </Box>
   )
 }
