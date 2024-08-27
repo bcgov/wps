b25672345d924d8df1bb118294289149822e83d8
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-08-27 15:53:00 -0700

    tests

---
+++ web/src/features/fba/components/aboutDataPopover.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import AboutDataPopover from 'features/fba/components/AboutDataPopover'
+
+const ADVISORY_THRESHOLD = 20
+
+describe('AboutDataPopover', () => {
+  it('should render the About Data Popover', () => {
+    const { getByTestId } = render(<AboutDataPopover advisoryThreshold={ADVISORY_THRESHOLD}></AboutDataPopover>)
+    const aboutData = getByTestId('about-data-popover')
+    expect(aboutData).toBeInTheDocument()
+  })
+  it('should open the popover when clicked', () => {
+    render(<AboutDataPopover advisoryThreshold={ADVISORY_THRESHOLD}></AboutDataPopover>)
+
+    fireEvent.click(screen.getByTestId('about-data-trigger'))
+
+    expect(screen.getByTestId('about-data-content')).toBeVisible()
+  })
+  it('should close the popover when clicking outside of it', async () => {
+    render(<AboutDataPopover advisoryThreshold={ADVISORY_THRESHOLD}></AboutDataPopover>)
+    fireEvent.click(screen.getByTestId('about-data-trigger'))
+    expect(screen.getByTestId('about-data-content')).toBeVisible()
+
+    fireEvent.click(document.body)
+    await waitFor(() => {
+      expect(screen.queryByTestId('popover-title')).not.toBeInTheDocument()
+    })
+  })
+})
