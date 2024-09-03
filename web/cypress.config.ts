5560d7c78702e008ac0cb2d23abec8fb88fcbc09
jacques metevier <jmetevier@gmail.com>
2024-09-03 13:41:21 -0700

    skip cy

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   screenshotOnRunFailure: false,
   video: false,
   e2e: {
+    specPattern: '**/fire-behaviour-advisory-calculator-page.cy.ts',
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
