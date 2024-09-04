4c220d3a5616634aeb81793d3983f8882981a4ed
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-09-04 08:26:54 -0700

    nullish op

--- web/src/features/fba/components/infoPanel/FireZoneUnitSummary.tsx
+++ web/src/features/fba/components/infoPanel/FireZoneUnitSummary.tsx
@@ -30,7 +30,7 @@ const FireZoneUnitSummary = ({ fuelTypeInfo, fireZoneTPIStats, selectedFireZoneU
           <FuelSummary selectedFireZoneUnit={selectedFireZoneUnit} fuelTypeInfo={fuelTypeInfo} />
         </Grid>
         <Grid item sx={{ width: '95%' }}>
-          {isUndefined(fireZoneTPIStats) ||
+          {!fireZoneTPIStats ||
           fireZoneTPIStats.valley_bottom + fireZoneTPIStats.mid_slope + fireZoneTPIStats.upper_slope === 0 ? (
             <Typography>No elevation information available.</Typography>
           ) : (
