6a2c057097a689204cd684a411e253f8226f6e05
Conor Brady <con.brad@gmail.com>
2024-09-10 14:58:22 -0700

    Ran formatting with prettier

--- web/src/features/fba/components/viz/CriticalHours.tsx
+++ web/src/features/fba/components/viz/CriticalHours.tsx
@@ -8,11 +8,12 @@ interface CriticalHoursProps {
 }
 
 const CriticalHours = ({ start, end }: CriticalHoursProps) => {
-  const formattedCriticalHours = isNull(start) || isUndefined(start) || isNull(end) || isUndefined(end) ? "-" : `${start}:00 - ${end}:00`
+  const formattedCriticalHours =
+    isNull(start) || isUndefined(start) || isNull(end) || isUndefined(end) ? '-' : `${start}:00 - ${end}:00`
   return (
-      <Typography sx={{ fontSize: '0.75rem' }} data-testid="critical-hours">
-        {formattedCriticalHours}
-      </Typography>
+    <Typography sx={{ fontSize: '0.75rem' }} data-testid="critical-hours">
+      {formattedCriticalHours}
+    </Typography>
   )
 }
 
