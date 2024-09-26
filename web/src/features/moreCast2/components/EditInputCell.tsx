5545b6e4e2d50c68155ec4d0c0b8f72622bea80b
Conor Brady <con.brad@gmail.com>
2024-09-26 14:00:22 -0700

    Dispatch in useEffect

--- web/src/features/moreCast2/components/EditInputCell.tsx
+++ web/src/features/moreCast2/components/EditInputCell.tsx
@@ -14,7 +14,9 @@ export const EditInputCell = (props: GridRenderEditCellParams) => {
   const inputRef = useRef<HTMLInputElement | null>(null)
   const dispatch: AppDispatch = useDispatch()
 
-  dispatch(setInputValid(isEmpty(error)))
+  useEffect(() => {
+    dispatch(setInputValid(isEmpty(error)))
+  }, [])
 
   useEffect(() => {
     if (hasFocus && inputRef.current) {
