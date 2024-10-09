8cbbcb65d1b0761775d68604b0d7f8a5a3676544
Conor Brady <con.brad@gmail.com>
2024-10-09 12:58:27 -0700

    Set path to public in api

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -83,6 +83,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
+        context.execute("SET search_path TO public;")
         context.configure(connection=connection, target_metadata=target_metadata, include_object=include_object)
 
         with context.begin_transaction():
