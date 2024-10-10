22376592dc55af050180ad369476372dfa97b591
Conor Brady <con.brad@gmail.com>
2024-10-09 19:27:22 -0700

    remove set search path alembic

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -84,7 +84,6 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata, include_object=include_object)
-        context.execute("SET search_path TO public;")
 
         with context.begin_transaction():
             context.run_migrations()
