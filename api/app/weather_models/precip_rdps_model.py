74acf1a23fcbbd59c717e46332e01c95ef633961
dgboss <darren.boss@gov.bc.ca>
2024-10-11 12:02:43 -0700

    Update api/app/weather_models/precip_rdps_model.py

--- api/app/weather_models/precip_rdps_model.py
+++ api/app/weather_models/precip_rdps_model.py
@@ -144,7 +144,7 @@ def _diff(value_a: float, value_b: float):
     """
     Subtract value_a from value_b.
     :param value_a: The first value
-    :param value_b: _description_
+    :param value_b: The second value
     :raises ValueError: If difference is less than -0.01 (ie. negative precip not allowed)
     :return: Return value_a minus value_b if the value is >= 0. If the difference is slightly negative (-0.01 <= value < 0), due to floating
     point math for example, return 0. If the value is truly negative, raise an error.
