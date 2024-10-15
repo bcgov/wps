cd192c90c9a156f94103d4ced7f1f395dc0d9097
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-10-15 14:05:03 -0700

    dmc & dc

---
+++ api/app/sfms/bui.py
@@ -0,0 +1,123 @@
+import asyncio
+import logging
+import os
+from tempfile import TemporaryDirectory
+from time import perf_counter
+from datetime import timezone, datetime
+
+import numpy as np
+from osgeo import gdal
+
+from app.auto_spatial_advisory.sfms import vectorized_dmc, vectorized_dc
+from app.sfms.fwi_input import FWIInput
+from app.utils.geospatial import export_to_geotiff, generate_latitude_array
+from app.utils.s3 import get_client, set_s3_gdal_config
+from app.utils.time import get_utc_now
+
+gdal.UseExceptions()
+logger = logging.getLogger(__name__)
+
+
+def replace_nodata_with_zero(dataset: gdal.Dataset):
+    """Reads the first band of a dataset, replaces NoData values with 0, returns the array and the nodata value."""
+    band = dataset.GetRasterBand(1)
+    nodata_value = band.GetNoDataValue()
+    array = band.ReadAsArray()
+
+    if nodata_value is not None:
+        array[array == nodata_value] = 0
+
+    array[(array < 0) & (array > -0.1)] = 0  # TODO remove this once negative precip is fixed
+
+    return array, nodata_value
+
+
+async def calculate_and_store_dmc(
+    datetime_to_calculate_utc: datetime, dmc_ds: gdal.Dataset, temp_ds: gdal.Dataset, rh_ds: gdal.Dataset, precip_ds: gdal.Dataset, latitude: np.ndarray, month: np.ndarray
+):
+    async with get_client() as (client, bucket):
+        key = f"sfms/calculated/forecast/{datetime_to_calculate_utc.date().isoformat()}/dmc{datetime_to_calculate_utc.date().isoformat().replace('-', '')}.tif"
+
+        dmc_array, dmc_nodata_value = replace_nodata_with_zero(dmc_ds)
+        temp_array, _ = replace_nodata_with_zero(temp_ds)
+        rh_array, _ = replace_nodata_with_zero(rh_ds)
+        precip_array, _ = replace_nodata_with_zero(precip_ds)
+
+        start = perf_counter()
+        dmc_values = vectorized_dmc(dmc_array, temp_array, rh_array, precip_array, latitude, month, True)
+        print(perf_counter() - start)
+
+        if dmc_nodata_value is not None:
+            nodata_mask = dmc_ds.GetRasterBand(1).ReadAsArray() == dmc_nodata_value
+            dmc_values[nodata_mask] = dmc_nodata_value
+
+        geotransform = dmc_ds.GetGeoTransform()
+        projection = dmc_ds.GetProjection()
+
+        logger.info("Uploading dmc to s3")
+        with TemporaryDirectory() as temp_dir:
+            temp_geotiff = os.path.join(temp_dir, f"dmc{datetime_to_calculate_utc.date().isoformat()}.tif")
+            export_to_geotiff(dmc_values, temp_geotiff, geotransform, projection, dmc_nodata_value)
+
+            await client.put_object(Bucket=bucket, Key=key, Body=open(temp_geotiff, "rb"))
+
+        return key
+
+
+async def calculate_and_store_dc(
+    datetime_to_calculate_utc: datetime, dc_ds: gdal.Dataset, temp_ds: gdal.Dataset, rh_ds: gdal.Dataset, precip_ds: gdal.Dataset, latitude: np.ndarray, month: np.ndarray
+):
+    async with get_client() as (client, bucket):
+        key = f"sfms/calculated/forecast/{datetime_to_calculate_utc.date().isoformat()}/dc{datetime_to_calculate_utc.date().isoformat().replace('-', '')}.tif"
+
+        dmc_array, dmc_nodata_value = replace_nodata_with_zero(dc_ds)
+        temp_array, _ = replace_nodata_with_zero(temp_ds)
+        rh_array, _ = replace_nodata_with_zero(rh_ds)
+        precip_array, _ = replace_nodata_with_zero(precip_ds)
+
+        start = perf_counter()
+        dmc_values = vectorized_dc(dmc_array, temp_array, rh_array, precip_array, latitude, month, True)
+        print(perf_counter() - start)
+
+        if dmc_nodata_value is not None:
+            nodata_mask = dc_ds.GetRasterBand(1).ReadAsArray() == dmc_nodata_value
+            dmc_values[nodata_mask] = dmc_nodata_value
+
+        geotransform = dc_ds.GetGeoTransform()
+        projection = dc_ds.GetProjection()
+
+        logger.info("Uploading dmc to s3")
+        with TemporaryDirectory() as temp_dir:
+            temp_geotiff = os.path.join(temp_dir, f"dc{datetime_to_calculate_utc.date().isoformat()}.tif")
+            export_to_geotiff(dmc_values, temp_geotiff, geotransform, projection, dmc_nodata_value)
+
+            await client.put_object(Bucket=bucket, Key=key, Body=open(temp_geotiff, "rb"))
+
+        return key
+
+
+async def calculate_bui():
+    set_s3_gdal_config()
+    timestamp = get_utc_now()
+    # timestamp = datetime(2024, 10, 15, 23, tzinfo=timezone.utc)
+
+    fwi_input = FWIInput(timestamp)
+    await fwi_input.check_keys_exist()
+
+    # while fwi_input.s3_keys_exist:
+    dmc_ds, dc_ds = fwi_input.open_fwi_datasets()
+    transformed_temp, transformed_rh, transformed_precip = fwi_input.warp_weather_to_match_fwi(dmc_ds)
+
+    latitude_array = await generate_latitude_array(fwi_input.dmc_key)
+    month_array = np.full(latitude_array.shape, fwi_input.start_time_utc.month)
+
+    dmc_key = await calculate_and_store_dmc(fwi_input.datetime_to_calculate_utc, dmc_ds, transformed_temp, transformed_rh, transformed_precip, latitude_array, month_array)
+    dc_key = await calculate_and_store_dc(fwi_input.datetime_to_calculate_utc, dc_ds, transformed_temp, transformed_rh, transformed_precip, latitude_array, month_array)
+
+
+async def main():
+    await calculate_bui()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- api/app/sfms/fwi_input.py
+++ api/app/sfms/fwi_input.py
@@ -24,24 +24,24 @@ class FWIKeyFetcher:
         self.bucket_name = bucket_name
         self.sfms_upload_prefix = "sfms/uploads/actual/"
 
-    def determine_last_fwi_date(self, datetime: datetime, model_run_hour: int):
+    def determine_last_fwi_date(self, datetime: datetime):
         local_datetime = datetime.astimezone(ZoneInfo("America/Vancouver"))  # sfms is currently uploaded according to local time
         day_of_last_actual = (local_datetime - timedelta(days=1)).date()
 
-        if model_run_hour == 0:  # if the model run hour is 00 (17:00 PDT), we should have today's actuals
+        if local_datetime.hour > 18:  # if the local time is after 6:00pm, we should have today's actuals
             day_of_last_actual = local_datetime.date()
 
         return day_of_last_actual
 
-    def get_previous_dmc_key(self, date: datetime, model_run_hour: int):
-        day_of_last_actual = self.determine_last_fwi_date(date, model_run_hour)
+    def get_previous_dmc_key(self, date: datetime):
+        day_of_last_actual = self.determine_last_fwi_date(date)
 
         dmc_key = os.path.join(self.sfms_upload_prefix, f"{day_of_last_actual}/dmc{day_of_last_actual.isoformat().replace('-', '')}.tif")
 
         return dmc_key
 
-    def get_previous_dc_key(self, date: datetime, model_run_hour: int):
-        day_of_last_actual = self.determine_last_fwi_date(date, model_run_hour)
+    def get_previous_dc_key(self, date: datetime):
+        day_of_last_actual = self.determine_last_fwi_date(date)
 
         dc_key = os.path.join(self.sfms_upload_prefix, f"{day_of_last_actual}/dc{day_of_last_actual.isoformat().replace('-', '')}.tif")
 
@@ -92,8 +92,8 @@ class FWIInput:
 
         self.s3_key_fetcher = FWIKeyFetcher(BUCKET)
 
-        self.dmc_key = self.s3_key_fetcher.get_previous_dmc_key(self.start_time_utc, self.model_run_hour)
-        self.dc_key = self.s3_key_fetcher.get_previous_dc_key(self.start_time_utc, self.model_run_hour)
+        self.dmc_key = self.s3_key_fetcher.get_previous_dmc_key(self.start_time_utc)
+        self.dc_key = self.s3_key_fetcher.get_previous_dc_key(self.start_time_utc)
         self.temp_key, self.rh_key, self.wind_spd_key = self.s3_key_fetcher.get_model_data_keys(self.start_time_utc, self.prediction_hour)
         self.precip_key = self.s3_key_fetcher.get_calculated_precip_key(self.datetime_to_calculate_utc)
 
@@ -115,44 +115,52 @@ class FWIInput:
 
     async def check_keys_exist(self):
         """Asynchronously check if all keys exist."""
-        self.s3_keys_exist = await all_objects_exist(self.dmc_key, self.temp_key, self.rh_key, self.wind_spd_key, self.precip_key)
+        self.s3_keys_exist = await all_objects_exist(self.dmc_key, self.dc_key, self.temp_key, self.rh_key, self.wind_spd_key, self.precip_key)
         return self.s3_keys_exist
 
-    def get_gdal_s3_paths(self):
+    def _get_gdal_s3_paths(self):
         """Generate the S3 paths for all relevant weather data."""
         s3_prefix = f"/vsis3/{BUCKET}"
         dmc_s3 = os.path.join(s3_prefix, self.dmc_key)
+        dc_s3 = os.path.join(s3_prefix, self.dc_key)
         temp_s3 = os.path.join(s3_prefix, self.temp_key)
         rh_s3 = os.path.join(s3_prefix, self.rh_key)
         precip_s3 = os.path.join(s3_prefix, self.precip_key)
-        return dmc_s3, temp_s3, rh_s3, precip_s3
+        return dmc_s3, dc_s3, temp_s3, rh_s3, precip_s3
 
-    def open_gdal_datasets(self):
+    def _open_weather_gdal_datasets(self):
         """Open the GDAL datasets for all required weather data."""
-        dmc_s3, temp_s3, rh_s3, precip_s3 = self.get_gdal_s3_paths()
+        _, _, temp_s3, rh_s3, precip_s3 = self._get_gdal_s3_paths()
 
-        prev_fwi_ds = gdal.Open(dmc_s3)
         temp_ds = gdal.Open(temp_s3)
         rh_ds = gdal.Open(rh_s3)
         precip_ds = gdal.Open(precip_s3)
 
-        return prev_fwi_ds, temp_ds, rh_ds, precip_ds
+        return temp_ds, rh_ds, precip_ds
 
-    def warp_datasets_to_match_fwi(self):
+    def open_fwi_datasets(self) -> tuple[gdal.Dataset, gdal.Dataset]:
+        dmc_s3, dc_s3, *_ = self._get_gdal_s3_paths()
+
+        prev_dmc_ds = gdal.Open(dmc_s3)
+        prev_dc_ds = gdal.Open(dc_s3)
+
+        return prev_dmc_ds, prev_dc_ds
+
+    def warp_weather_to_match_fwi(self, fwi_dataset: gdal.Dataset):
         """Process and warp the datasets to match the reference dataset."""
         with TemporaryDirectory() as tempdir:
-            prev_fwi_ds, temp_ds, rh_ds, precip_ds = self.open_gdal_datasets()
+            temp_ds, rh_ds, precip_ds = self._open_weather_gdal_datasets()
 
             warped_temp_path = os.path.join(tempdir, f"warp_{os.path.basename(self.temp_key)}")
             warped_rh_path = os.path.join(tempdir, f"warp_{os.path.basename(self.rh_key)}")
             warped_precip_path = os.path.join(tempdir, f"warp_{os.path.basename(self.precip_key)}")
 
-            transformed_temp_ds = warp_to_match_raster(temp_ds, prev_fwi_ds, warped_temp_path, GDALResamplingMethod.BILINEAR)
-            transformed_rh_ds = warp_to_match_raster(rh_ds, prev_fwi_ds, warped_rh_path, GDALResamplingMethod.BILINEAR)
-            transformed_precip_ds = warp_to_match_raster(precip_ds, prev_fwi_ds, warped_precip_path, GDALResamplingMethod.BILINEAR)
+            transformed_temp_ds = warp_to_match_raster(temp_ds, fwi_dataset, warped_temp_path, GDALResamplingMethod.BILINEAR)
+            transformed_rh_ds = warp_to_match_raster(rh_ds, fwi_dataset, warped_rh_path, GDALResamplingMethod.BILINEAR)
+            transformed_precip_ds = warp_to_match_raster(precip_ds, fwi_dataset, warped_precip_path, GDALResamplingMethod.BILINEAR)
 
             temp_ds = None
             rh_ds = None
             precip_ds = None
 
-            return prev_fwi_ds, transformed_temp_ds, transformed_rh_ds, transformed_precip_ds
+            return transformed_temp_ds, transformed_rh_ds, transformed_precip_ds
