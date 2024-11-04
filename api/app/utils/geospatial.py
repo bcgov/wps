5583ce1707a229bdefbca0e97ac016d58462ab64
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-11-04 14:06:43 -0800

    geospatial imports

--- api/app/utils/geospatial.py
+++ api/app/utils/geospatial.py
@@ -1,13 +1,6 @@
 from enum import Enum
-import logging
-import os
 from typing import Tuple
 from osgeo import gdal, ogr, osr
-import numpy as np
-from tempfile import TemporaryDirectory
-
-
-logger = logging.getLogger(__name__)
 
 
 class GDALResamplingMethod(Enum):
