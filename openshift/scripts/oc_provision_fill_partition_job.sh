728d53f68e29b8065100d4b7542f794ec67411d6
Conor Brady <con.brad@gmail.com>
2024-11-05 13:53:13 -0800

    move template to templates folder, add helper script

---
+++ openshift/scripts/oc_provision_fill_partition_job.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -l
+#
+source "$(dirname ${0})/common/common"
+
+#%
+#% OpenShift Deploy Helper
+#%
+#%   Intended for use with a pull request-based pipeline.
+#%   Suffixes incl.: pr-###.
+#%
+#% Usage:
+#%
+#%    [PROJ_TARGET] [PG_DATABASE] [TABLE] ${THIS_FILE} [SUFFIX]
+#%
+#% Examples:
+#%
+#%   PROJ_TARGET=e1e498-dev PG_DATABASE=wps TABLE=table ${THIS_FILE} pr-0
+
+JOB="job/fill-partition-data-${SUFFIX}"
+
+# create the job
+oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/partition_filler_job.yaml \
+    -p SUFFIX=${SUFFIX} \
+    -p PG_DATABASE=${PG_DATABASE} \
+    -p TABLE=${TABLE} \
+    -p CRUNCHYDB_USER=wps-crunchydb-${SUFFIX}-pguser-wps-crunchydb-${SUFFIX} \
+    -p PROJ_TOOLS=${PROJ_TOOLS} | jq '.items[0]' | oc -n ${PROJ_TARGET} create -f -
+# wait for the job to finish
+oc wait --for=condition=complete ${JOB} --timeout=60s
+# output the log for debugging
+oc logs -f ${JOB}
+# we're done, so get rid of the job
+oc delete ${JOB}
+
