ee8910c7eb0417d723acede9763d0db56fab850d
dgboss <darren.boss@gov.bc.ca>
2024-11-18 10:12:08 -0800

    Update prod db to pg16 (#4100)

--- openshift/scripts/oc_provision_wfwx_hourly_actuals_cronjob.sh
+++ openshift/scripts/oc_provision_wfwx_hourly_actuals_cronjob.sh
@@ -37,7 +37,7 @@ OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/wfwx_hourly_actuals
 -p SUFFIX=${SUFFIX} \
 -p SCHEDULE=\"${SCHEDULE}\" \
 -p POSTGRES_DATABASE=${POSTGRES_DATABASE:-${APP_NAME}} \
--p CRUNCHYDB_USER=wps-crunchydb-${SUFFIX}-pguser-wps-crunchydb-${SUFFIX} \
+-p CRUNCHYDB_USER=wps-crunchydb-16-${SUFFIX}-pguser-wps-crunchydb-16-${SUFFIX} \
 ${PROJ_TOOLS:+ "-p PROJ_TOOLS=${PROJ_TOOLS}"} \
 ${IMAGE_REGISTRY:+ "-p IMAGE_REGISTRY=${IMAGE_REGISTRY}"}"
 
