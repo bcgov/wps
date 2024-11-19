95034840c4c0767f9b2433f2eb7ab23431b057fb
Conor Brady <con.brad@gmail.com>
2024-11-19 15:35:33 -0800

    No quotes

--- openshift/pgslice/docker/fill_partition_data.sh
+++ openshift/pgslice/docker/fill_partition_data.sh
@@ -49,7 +49,7 @@ then
     exit 1
 fi
 
-PGSLICE_URL="postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOSTNAME}:${PG_PORT}/${PG_DATABASE}"
+PGSLICE_URL=postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOSTNAME}:${PG_PORT}/${PG_DATABASE}
 # Fill the partitions with data from the original table
 pgslice fill $TABLE --url $PGSLICE_URL
 # Analyze for query planner
