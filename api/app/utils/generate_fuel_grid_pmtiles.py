ec86f6c788c6996c8f3437232315abfdbeb540dc
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-11-28 14:05:33 -0800

    read from s3

--- api/app/utils/generate_fuel_grid_pmtiles.py
+++ api/app/utils/generate_fuel_grid_pmtiles.py
@@ -1,7 +1,9 @@
 from tempfile import TemporaryDirectory
 
+from app import config
 from app.utils.pmtiles import tippecanoe_wrapper, write_geojson
 from app.utils.polygonize import polygonize_in_memory
+from app.utils.s3 import set_s3_gdal_config
 
 
 def generate_fuel_pmtiles(fuel_grid_path: str, output_path: str):
@@ -9,6 +11,7 @@ def generate_fuel_pmtiles(fuel_grid_path: str, output_path: str):
     Simple function for local use to regenerate fuel grid pmtiles annually when the new fuel grid raster
     is received from BCWS Geospatial. Typically stored in s3: {bucket}/psu/pmtiles/fuel
     """
+    set_s3_gdal_config()
     with polygonize_in_memory(fuel_grid_path, "fuel", "fuel") as layer, TemporaryDirectory() as temp_dir:
         temp_geojson = write_geojson(layer, temp_dir)
 
@@ -16,8 +19,9 @@ def generate_fuel_pmtiles(fuel_grid_path: str, output_path: str):
 
 
 def main():
-    fuel_grid = "/path/to/fbp2024.tif"
-    output_path = "/output/to/fbp2024.pmtiles"
+    bucket = config.get("OBJECT_STORE_BUCKET")
+    fuel_grid = f"/vsis3/{bucket}/sfms/static/fbp2024.tif"  # input fuel grid stored in s3
+    output_path = "/output/to/fbp2024.pmtiles"  # local output
 
     generate_fuel_pmtiles(fuel_grid, output_path)
 
