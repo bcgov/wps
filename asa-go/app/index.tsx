68129e337bb75f7aabf5ca9dae206d476bdca84a
Conor Brady <con.brad@gmail.com>
2025-01-14 13:23:17 -0800

    Base map app

---
+++ asa-go/app/index.tsx
@@ -0,0 +1,36 @@
+import {
+  MapView,
+  VectorSource,
+  FillLayer,
+  LineLayer,
+  SymbolLayer,
+  Camera,
+} from "@maplibre/maplibre-react-native";
+
+export default function Page() {
+  return (
+    <MapView style={{ flex: 1 }} zoomEnabled compassEnabled>
+      <Camera centerCoordinate={[-125, 54.5]} zoomLevel={3.5} />
+      <VectorSource
+        id="fireCentreSource"
+        url="pmtiles://https://nrs.objectstore.gov.bc.ca/lwzrin/psu/pmtiles/fireCentres.pmtiles"
+      >
+        <LineLayer
+          id="border-fire-centres"
+          sourceLayerID="tippecanoe_input"
+          style={{ lineColor: "red" }}
+        />
+      </VectorSource>
+      <VectorSource
+        id="fireZonesSource"
+        url="pmtiles://https://nrs.objectstore.gov.bc.ca/lwzrin/psu/pmtiles/fireZoneUnits.pmtiles"
+      >
+        <LineLayer
+          id="border-fire-ones"
+          sourceLayerID="tippecanoe_input"
+          style={{ lineColor: "black" }}
+        />
+      </VectorSource>
+    </MapView>
+  );
+}
---
+++ asa-go/components/ui/TabBarBackground.ios.tsx
@@ -0,0 +1,20 @@
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { StyleSheet } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function BlurTabBarBackground() {
+  return (
+    <View
+      // System chrome material automatically adapts to the system's theme
+      // and matches the native tab bar appearance on iOS.
+      style={StyleSheet.absoluteFill}
+    />
+  );
+}
+
+export function useBottomTabOverflow() {
+  const tabHeight = useBottomTabBarHeight();
+  const { bottom } = useSafeAreaInsets();
+  return tabHeight - bottom;
+}
