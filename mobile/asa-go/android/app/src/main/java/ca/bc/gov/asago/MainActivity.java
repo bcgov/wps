60783147f89ab0791d1412c78b3e051c421bb0b4
Conor Brady <con.brad@gmail.com>
2025-01-15 14:31:48 -0800

    capacitor app

---
+++ mobile/asa-go/android/app/src/main/java/ca/bc/gov/asago/MainActivity.java
@@ -0,0 +1,5 @@
+package ca.bc.gov.asago;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ mobile/asa-go/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ca.bc.gov.asago',
+  appName: 'asa-go',
+  webDir: 'dist'
+};
+
+export default config;
