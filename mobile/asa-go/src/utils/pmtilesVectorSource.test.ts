0fe0ab0b04c618b2e5bf97dff0f89c0ac8b74fda
Conor Brady <con.brad@gmail.com>
2025-02-03 14:40:53 -0800

    Add more tests

--- mobile/asa-go/src/utils/pmtilesVectorSource.test.ts
+++ mobile/asa-go/src/utils/pmtilesVectorSource.test.ts
@@ -2,8 +2,84 @@ import { RunType } from "@/api/fbaAPI";
 import { IPMTilesCache } from "@/utils/pmtilesCache";
 import { PMTilesFileVectorSource } from "@/utils/pmtilesVectorSource";
 import { DateTime } from "luxon";
-import { PMTiles } from "pmtiles";
+import {
+  Cache,
+  Compression,
+  DecompressFunc,
+  Header,
+  PMTiles,
+  RangeResponse,
+  Source,
+  TileType,
+} from "pmtiles";
 import sinon from "sinon";
+import { assert } from "vitest";
+
+const testPMTilesHeader: Header = {
+  specVersion: 0,
+  rootDirectoryOffset: 0,
+  rootDirectoryLength: 0,
+  jsonMetadataOffset: 0,
+  jsonMetadataLength: 0,
+  leafDirectoryOffset: 0,
+  tileDataOffset: 0,
+  numAddressedTiles: 0,
+  numTileEntries: 0,
+  numTileContents: 0,
+  clustered: false,
+  internalCompression: Compression.Unknown,
+  tileCompression: Compression.Unknown,
+  tileType: TileType.Unknown,
+  minZoom: 0,
+  maxZoom: 0,
+  minLon: 0,
+  minLat: 0,
+  maxLon: 0,
+  maxLat: 0,
+  centerZoom: 0,
+  centerLon: 0,
+  centerLat: 0,
+};
+
+class TestPMTiles implements PMTiles {
+  source!: Source;
+  cache!: Cache;
+  decompress!: DecompressFunc;
+  getHeader(): Promise<Header> {
+    console.log("getHeader called");
+    return Promise.resolve(testPMTilesHeader);
+  }
+  getZxyAttempt(
+    z: number,
+    x: number,
+    y: number,
+    signal?: AbortSignal
+  ): Promise<RangeResponse | undefined> {
+    console.log("getZxyAttempt called", z, x, y, signal);
+    return Promise.resolve(undefined);
+  }
+  getZxy(
+    z: number,
+    x: number,
+    y: number,
+    signal?: AbortSignal
+  ): Promise<RangeResponse | undefined> {
+    console.log("getZxy called", z, x, y, signal);
+    return Promise.resolve(undefined);
+  }
+  getMetadataAttempt(): Promise<unknown> {
+    console.log("getMetadataAttempt called");
+    return Promise.resolve();
+  }
+  getMetadata(): Promise<unknown> {
+    console.log("getMetadata called");
+    return Promise.resolve();
+  }
+  getTileJson(baseTilesUrl: string): Promise<unknown> {
+    console.log("getTileJSON called", baseTilesUrl);
+    return Promise.resolve();
+  }
+}
 
 describe("pmTilesVectorSource", () => {
   let sandbox: sinon.SinonSandbox;
@@ -13,14 +89,15 @@ describe("pmTilesVectorSource", () => {
   afterEach(() => {
     sandbox.restore();
   });
-  const buildPMTilesTestCache = () => {
+
+  const buildPMTilesTestCache = (pmtiles?: PMTiles) => {
     return {
       loadPMTiles: function (
         filename: string,
         fetchAndStoreCallback?: () => Promise<PMTiles | undefined>
       ): Promise<PMTiles | undefined> {
         console.log("loadPMTiles called", filename, fetchAndStoreCallback);
-        return Promise.resolve(undefined);
+        return Promise.resolve(pmtiles);
       },
       loadHFIPMTiles: function (
         for_date: DateTime,
@@ -35,12 +112,12 @@ describe("pmTilesVectorSource", () => {
           run_date,
           filename
         );
-        return Promise.resolve(undefined);
+        return Promise.resolve(pmtiles);
       },
     };
   };
   it("should attempt to load static pmtiles upon creation", async () => {
-    const testCache: IPMTilesCache = buildPMTilesTestCache();
+    const testCache: IPMTilesCache = buildPMTilesTestCache(new TestPMTiles());
     const pmTilesCacheSpy = sandbox.spy(testCache);
 
     await PMTilesFileVectorSource.createStaticLayer(testCache, {
@@ -51,7 +128,7 @@ describe("pmTilesVectorSource", () => {
   });
 
   it("should attempt to load hfi pmtiles upon creation", async () => {
-    const testCache: IPMTilesCache = buildPMTilesTestCache();
+    const testCache: IPMTilesCache = buildPMTilesTestCache(new TestPMTiles());
     const pmTilesCacheSpy = sandbox.spy(testCache);
 
     await PMTilesFileVectorSource.createHFILayer(testCache, {
@@ -62,5 +139,20 @@ describe("pmTilesVectorSource", () => {
     });
     sinon.assert.calledOnce(pmTilesCacheSpy.loadHFIPMTiles);
     sinon.assert.notCalled(pmTilesCacheSpy.loadPMTiles);
+  });
+
+  it("should set tile status ready once initialized", async () => {
+    const testCache: IPMTilesCache = buildPMTilesTestCache(new TestPMTiles());
+    const instance = await PMTilesFileVectorSource.createHFILayer(testCache, {
+      filename: "test.pmtiles",
+      for_date: DateTime.fromISO("2016-05-25T09:08:34.123"),
+      run_type: RunType.FORECAST,
+      run_date: DateTime.fromISO("2016-05-25T09:08:34.123"),
+    });
+    const tileGrid = instance.getTileGrid();
+    assert(tileGrid !== null);
+    assert(tileGrid.getMaxZoom() === testPMTilesHeader.maxZoom);
+    assert(tileGrid.getMinZoom() === testPMTilesHeader.minZoom);
+    assert(instance.getState() === "ready");
   });
 });
