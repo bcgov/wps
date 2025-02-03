f32dae8ac52288ae1aeb79d518f7c7c3d779ceaf
Darren Boss <darren.boss@gov.bc.ca>
2025-02-03 13:46:27 -0800

    Fix data migration

--- api/alembic/versions/fa4b3ecb57fe_populate_tpi_fuel_area_table.py
+++ api/alembic/versions/fa4b3ecb57fe_populate_tpi_fuel_area_table.py
@@ -5,8 +5,12 @@ Revises: 1e6932096921
 Create Date: 2025-01-21 10:04:48.838953
 
 """
+from alembic import op
+import geoalchemy2
+import sqlalchemy as sa
 
-from app.auto_spatial_advisory.process_tpi_fuel_area import process_tpi_fuel_area
+from sqlalchemy.orm.session import Session
+from app.auto_spatial_advisory.process_tpi_fuel_area import calculate_masked_tpi_areas
 
 
 # revision identifiers, used by Alembic.
@@ -15,11 +19,49 @@ down_revision = "1e6932096921"
 branch_labels = None
 depends_on = None
 
+shape_type_table = sa.Table("advisory_shape_types", sa.MetaData(), sa.Column("id", sa.Integer), sa.Column("name", sa.String))
+
+shape_table = sa.Table(
+    "advisory_shapes",
+    sa.MetaData(),
+    sa.Column("id", sa.Integer),
+    sa.Column("source_identifier", sa.String),
+    sa.Column("shape_type", sa.Integer),
+    sa.Column("geom", geoalchemy2.Geometry),
+)
+
+tpi_area_table = sa.Table(
+    "tpi_fuel_area",
+    sa.MetaData(),
+    sa.Column("advisory_shape_id", sa.Integer),
+    sa.Column("tpi_class", sa.Enum("valley_bottom", "mid_slope", "upper_slope", name="tpiclassenum")),
+    sa.Column("fuel_area", sa.Float),
+)
+
+
+def get_fire_zone_unit_shape_type_id(session: Session):
+    statement = shape_type_table.select().where(shape_type_table.c.name == "fire_zone_unit")
+    result = session.execute(statement).fetchone()
+    return result.id
+
+
+def get_fire_zone_units(session: Session, fire_zone_type_id: int):
+    statement = shape_table.select().where(shape_table.c.shape_type == fire_zone_type_id)
+    result = session.execute(statement).fetchall()
+    return result
+
 
 def upgrade():
-    process_tpi_fuel_area()
-    pass
+    session = Session(bind=op.get_bind())
+    fire_zone_shape_type_id = get_fire_zone_unit_shape_type_id(session)
+    zone_units = get_fire_zone_units(session, fire_zone_shape_type_id)
+    tpi_area = calculate_masked_tpi_areas(zone_units)
+    for tuple in tpi_area:
+        stmt = sa.insert(tpi_area_table).values(advisory_shape_id=tuple[0], tpi_class=tuple[1].name, fuel_area=tuple[2])
+        session.execute(stmt)
 
 
 def downgrade():
-    pass
+    session = Session(bind=op.get_bind())
+    stmt = sa.delete(tpi_area_table)
+    session.execute(stmt)
--- api/app/auto_spatial_advisory/process_tpi_fuel_area.py
+++ api/app/auto_spatial_advisory/process_tpi_fuel_area.py
@@ -1,19 +1,15 @@
 import numpy as np
+from geoalchemy2.shape import to_shape
 from osgeo import gdal, ogr, osr
-
-from sqlalchemy import func, select
-from sqlalchemy.orm import Session
 from app import config
-from app.db.database import get_write_session_scope
-from app.db.models.auto_spatial_advisory import Shape, TPIClassEnum, TPIFuelArea
+from app.db.models.auto_spatial_advisory import TPIClassEnum
 from app.utils.s3 import set_s3_gdal_config
 
 
-def store_tpi_area_data(session: Session, advisory_shape_id: int, data: np.ndarray, pixel_size: int):
+def calculate_tpi_area_data_for_zone(advisory_shape_id: int, data: np.ndarray, pixel_size: int):
     """
     Save TPIFuelArea records to the API database.
 
-        :param session: A synchronous sqlalchemy session object.
         :param advisory_shape_id: A fire zone id used as a foreign key to the advisory_shapes table.
         :param data: A numpy ndarray containing classified TPI values that have been masked by the fuel layer.
         :param pixel_size: The size of the cells in the TPI layer.
@@ -23,8 +19,7 @@ def store_tpi_area_data(session: Session, advisory_shape_id: int, data: np.ndarr
         if value in TPIClassEnum:
             tpi_enum = TPIClassEnum(value)
             fuel_area = count * pixel_size * pixel_size
-            tpi_fuel_area = TPIFuelArea(advisory_shape_id=advisory_shape_id, tpi_class=tpi_enum, fuel_area=fuel_area)
-            session.add(tpi_fuel_area)
+            yield (advisory_shape_id, tpi_enum, fuel_area)
 
 
 def prepare_wkt_geom_for_gdal(wkt_geom: str):
@@ -43,7 +38,7 @@ def prepare_wkt_geom_for_gdal(wkt_geom: str):
     return geometry
 
 
-def process_tpi_fuel_area():
+def calculate_masked_tpi_areas(zones):
     """
     Entry point for calculating the fuel layer masked area of each TPI class (valley bottom, mid slope and upper slope) per fire zone unit.
     """
@@ -53,20 +48,20 @@ def process_tpi_fuel_area():
     masked_tpi_ds: gdal.Dataset = gdal.Open(masked_tpi_key, gdal.GA_ReadOnly)
     masked_tpi_pixel_size = masked_tpi_ds.GetGeoTransform()[1]
 
-    with get_write_session_scope() as session:
-        # Iterate through the fire zone units from the advisory_shapes table
-        stmt = select(Shape.id, func.ST_AsText(Shape.geom))
-        result = session.execute(stmt)
-        for row in result:
-            # Convert the WKT geometry of a fire zone unit into a form that can be used by gdal
-            geometry = prepare_wkt_geom_for_gdal(row[1])
+    for zone in zones:
+        zone_wkb = zone.geom
+        shapely_zone_geom = to_shape(zone_wkb)
+        zone_wkt = shapely_zone_geom.wkt
+        zone_geom = prepare_wkt_geom_for_gdal(zone_wkt)
 
-            # Us gdal.Warp to clip out our fire zone unit from the masked tpi raster and then store areas in the tpi_fuel_area table
-            warp_options = gdal.WarpOptions(cutlineWKT=geometry, cutlineSRS=geometry.GetSpatialReference(), cropToCutline=True)
-            intersected_path = "/vsimem/intersected.tif"
-            intersected_ds: gdal.Dataset = gdal.Warp(intersected_path, masked_tpi_ds, options=warp_options)
-            intersected_band: gdal.Band = intersected_ds.GetRasterBand(1)
-            intersected_data: np.ndarray = intersected_band.ReadAsArray()
-            store_tpi_area_data(session, advisory_shape_id=row[0], data=intersected_data, pixel_size=masked_tpi_pixel_size)
-            intersected_ds = None
+        # Use gdal.Warp to clip out our fire zone unit from the masked tpi raster
+        warp_options = gdal.WarpOptions(cutlineWKT=zone_geom, cutlineSRS=zone_geom.GetSpatialReference(), cropToCutline=True)
+        intersected_path = "/vsimem/intersected.tif"
+        intersected_ds: gdal.Dataset = gdal.Warp(intersected_path, masked_tpi_ds, options=warp_options)
+        intersected_band: gdal.Band = intersected_ds.GetRasterBand(1)
+        intersected_data: np.ndarray = intersected_band.ReadAsArray()
+        intersected_ds = None
+        tpi_area_data = calculate_tpi_area_data_for_zone(advisory_shape_id=zone.id, data=intersected_data, pixel_size=masked_tpi_pixel_size)
+        for tpi_area in tpi_area_data:
+            yield tpi_area
     masked_tpi_ds = None
