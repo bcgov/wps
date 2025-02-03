17dc3af0c1fce129fe6c755361b53c91b696c5f2
Darren Boss <darren.boss@gov.bc.ca>
2025-02-03 09:31:00 -0800

    Pre generate masked tpi

--- api/app/auto_spatial_advisory/process_tpi_fuel_area.py
+++ api/app/auto_spatial_advisory/process_tpi_fuel_area.py
@@ -4,15 +4,9 @@ from osgeo import gdal, ogr, osr
 from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 from app import config
-from app.auto_spatial_advisory.process_fuel_type_area import get_fuel_type_s3_key
 from app.db.database import get_write_session_scope
 from app.db.models.auto_spatial_advisory import Shape, TPIClassEnum, TPIFuelArea
-from app.utils.geospatial import GDALResamplingMethod, warp_to_match_raster
 from app.utils.s3 import set_s3_gdal_config
-
-
-REPROJECTED_FUEL_LAYER_NAME = "warped_fuel.tif"
-MASKED_TPI_NAME = "masked_tpi.tif"
 
 
 def store_tpi_area_data(session: Session, advisory_shape_id: int, data: np.ndarray, pixel_size: int):
@@ -31,49 +25,6 @@ def store_tpi_area_data(session: Session, advisory_shape_id: int, data: np.ndarr
             fuel_area = count * pixel_size * pixel_size
             tpi_fuel_area = TPIFuelArea(advisory_shape_id=advisory_shape_id, tpi_class=tpi_enum, fuel_area=fuel_area)
             session.add(tpi_fuel_area)
-
-
-def prepare_masked_tif():
-    """
-    Creates a masked TPI raster using a classified TPI raster from S3 storage and masking it using the fuel layer
-    also from S3 storage
-    """
-    # Open up our rasters with gdal
-    set_s3_gdal_config()
-    bucket = config.get("OBJECT_STORE_BUCKET")
-    tpi_raster_name = config.get("CLASSIFIED_TPI_DEM_NAME")
-    fuel_raster_key = get_fuel_type_s3_key(bucket)
-    tpi_raster_key = f"/vsis3/{bucket}/dem/tpi/{tpi_raster_name}"
-    fuel_ds: gdal.Dataset = gdal.Open(fuel_raster_key, gdal.GA_ReadOnly)  # LCC projection
-    tpi_ds: gdal.Dataset = gdal.Open(tpi_raster_key, gdal.GA_ReadOnly)  # BC Albers 3005 projection
-
-    # Warp the fuel raster to match extent, spatial reference and cell size of the TPI raster
-    warped_fuel_path = f"/vsimem/{REPROJECTED_FUEL_LAYER_NAME}"
-    warped_fuel_ds: gdal.Dataset = warp_to_match_raster(fuel_ds, tpi_ds, warped_fuel_path, GDALResamplingMethod.NEAREST_NEIGHBOUR)
-
-    # Convert the warped fuel dataset into a binary mask by classifying fuel cells as 1 and non-fuel cells as 0.
-    warped_fuel_band: gdal.Band = warped_fuel_ds.GetRasterBand(1)
-    warped_fuel_data: np.ndarray = warped_fuel_band.ReadAsArray()
-    mask = np.where((warped_fuel_data > 0) & (warped_fuel_data < 99), 1, 0)
-
-    # Some helpful things for creating the final masked TPI raster
-    geo_transform = tpi_ds.GetGeoTransform()
-    tpi_ds_srs = tpi_ds.GetProjection()
-    tpi_band: gdal.Band = tpi_ds.GetRasterBand(1)
-    tpi_data = tpi_band.ReadAsArray()
-
-    # Apply the fuel layer mask to the classified TPI raster and store the result in an in-memory gdal dataset
-    masked_tpi_data = np.multiply(mask, tpi_data)
-    output_driver: gdal.Driver = gdal.GetDriverByName("MEM")
-    masked_tpi_dataset: gdal.Dataset = output_driver.Create(f"/vsimem/{MASKED_TPI_NAME}", xsize=tpi_band.XSize, ysize=tpi_band.YSize, bands=1, eType=gdal.GDT_Byte)
-    masked_tpi_dataset.SetGeoTransform(geo_transform)
-    masked_tpi_dataset.SetProjection(tpi_ds_srs)
-    masked_fuel_type_band: gdal.Band = masked_tpi_dataset.GetRasterBand(1)
-    masked_fuel_type_band.SetNoDataValue(0)
-    masked_fuel_type_band.WriteArray(masked_tpi_data)
-    fuel_ds = None
-    tpi_ds = None
-    return masked_tpi_dataset
 
 
 def prepare_wkt_geom_for_gdal(wkt_geom: str):
@@ -96,7 +47,10 @@ def process_tpi_fuel_area():
     """
     Entry point for calculating the fuel layer masked area of each TPI class (valley bottom, mid slope and upper slope) per fire zone unit.
     """
-    masked_tpi_ds: gdal.Dataset = prepare_masked_tif()
+    set_s3_gdal_config()
+    bucket = config.get("OBJECT_STORE_BUCKET")
+    masked_tpi_key = f"/vsis3/{bucket}/dem/tpi/{config.get("CLASSIFIED_TPI_DEM_FUEL_MASKED_NAME")}"
+    masked_tpi_ds: gdal.Dataset = gdal.Open(masked_tpi_key, gdal.GA_ReadOnly)
     masked_tpi_pixel_size = masked_tpi_ds.GetGeoTransform()[1]
 
     with get_write_session_scope() as session:
