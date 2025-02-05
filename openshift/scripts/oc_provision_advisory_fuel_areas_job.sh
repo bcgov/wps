1d22cb155867ee275dca132e964587930ce0fc9e
Darren Boss <darren.boss@gov.bc.ca>
2025-02-05 13:51:21 -0800

    Fix script

--- openshift/scripts/oc_provision_advisory_fuel_areas_job.sh
+++ openshift/scripts/oc_provision_advisory_fuel_areas_job.sh
@@ -31,3 +31,24 @@ oc logs -f ${JOB}
 # we're done, so get rid of the job
 oc delete ${JOB}
 
+# Process template
+OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/advisory_fuel_areas_job.yaml \
+-p APP_LABEL=${APP_NAME}-${SUFFIX} \
+-p NAME=${APP_NAME} \
+-p SUFFIX=${SUFFIX} \
+-p CRUNCHYDB_USER=${CRUNCHY_NAME}-${SUFFIX}-pguser-${CRUNCHY_NAME}-${SUFFIX}
+
+# Apply template (apply or use --dry-run)
+#
+OC_APPLY="oc -n ${PROJ_TARGET} apply -f -"
+[ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run"
+
+# Execute commands
+#
+eval "${OC_PROCESS}"
+eval "${OC_PROCESS} | ${OC_APPLY}"
+
+# Provide oc command instruction
+#
+display_helper "${OC_PROCESS} | ${OC_APPLY}"
+
