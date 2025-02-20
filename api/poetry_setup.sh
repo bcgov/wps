4d94a19a91c899233813b19765c9c89c4d80b393
Darren Boss <darren.boss@gov.bc.ca>
2025-02-20 10:42:45 -0800

    Revert venv in project false

--- api/poetry_setup.sh
+++ api/poetry_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-poetry config virtualenvs.in-project true --local
+poetry config virtualenvs.in-project false --local
 poetry env use 3.12.3
 poetry run python -m pip install --upgrade pip
 pip install sqlalchemy[asyncio]
