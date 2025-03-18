118cf68c9573d41b1e009317049af68719e8955f
Conor Brady <con.brad@gmail.com>
2025-03-18 11:37:02 -0700

    New column to threshold foreign key

--- api/alembic/versions/395caeeeaf77_add_column_to_min_win_speed_threshold_.py
+++ api/alembic/versions/395caeeeaf77_add_column_to_min_win_speed_threshold_.py
@@ -7,7 +7,6 @@ Create Date: 2025-03-18 10:31:43.997506
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '395caeeeaf77'
