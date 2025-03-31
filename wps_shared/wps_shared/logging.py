dfe201e02206a2cbf71605807bc9b72f202e3360
Darren Boss <darren.boss@gov.bc.ca>
2025-03-31 13:49:38 -0700

    revert logging change

--- wps_shared/wps_shared/logging.py
+++ wps_shared/wps_shared/logging.py
@@ -1,6 +1,7 @@
 import os
 import json
-from logging import config
+import logging
+import logging.config
 
 # database is used externally by alembic.
 from . import db  # noqa: F401
@@ -12,4 +13,4 @@ def configure_logging():
     if os.path.exists(logging_config):
         with open(logging_config, encoding="utf-8") as config_file:
             config_json = json.load(config_file)
-        config.dictConfig(config_json)
+        logging.config.dictConfig(config_json)
