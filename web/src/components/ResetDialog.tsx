937df689de5370ebed23ab26a3e7cdb98fc1c357
Darren Boss <darren.boss@gov.bc.ca>
2025-04-10 16:30:03 -0700

    Remove fragment

--- web/src/components/ResetDialog.tsx
+++ web/src/components/ResetDialog.tsx
@@ -19,37 +19,34 @@ const ResetDialog = ({
     setShowResetDialog(false)
   }
   return (
-    <>
-      <Dialog
-        open={showResetDialog}
-        onClose={(handleResetDialogClose)}
-        data-testid={'reset-dialog'}
-        PaperProps={{ sx: { border: 2, borderColor: '#808080' } }}
-      >
-        <DialogContent>
-          <DialogContentText>
-              {message}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="outlined" onClick={handleResetDialogClose} data-testid={'reset-dialog-cancel-button'}>
-            Cancel
-          </Button>
-          <Button
-            variant="contained"
-            onClick={handleResetButtonConfirm}
-            data-testid={'reset-dialog-confirm-button'}
-            sx={{
-              border: `1px solid ${theme.palette.primary.main}`,
-              bgcolor: 'error.main',
-              color: 'error.contrastText',
-              '&:hover': { bgcolor: 'error.dark'}}}
-          >
-            Reset
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog
+      open={showResetDialog}
+      onClose={handleResetDialogClose}
+      data-testid={'reset-dialog'}
+      PaperProps={{ sx: { border: 2, borderColor: '#808080' } }}
+    >
+      <DialogContent>
+        <DialogContentText>{message}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" onClick={handleResetDialogClose} data-testid={'reset-dialog-cancel-button'}>
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleResetButtonConfirm}
+          data-testid={'reset-dialog-confirm-button'}
+          sx={{
+            border: `1px solid ${theme.palette.primary.main}`,
+            bgcolor: 'error.main',
+            color: 'error.contrastText',
+            '&:hover': { bgcolor: 'error.dark' }
+          }}
+        >
+          Reset
+        </Button>
+      </DialogActions>
+    </Dialog>
   )
 }
 
