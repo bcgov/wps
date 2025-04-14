1cf1113a59e8a97f1d70f012cb8af8c6e60b5112
Darren Boss <darren.boss@gov.bc.ca>
2025-04-14 16:54:57 -0700

    Pin footer on landing page

--- web/src/features/landingPage/pages/LandingPage.tsx
+++ web/src/features/landingPage/pages/LandingPage.tsx
@@ -1,54 +1,15 @@
 import React, { useEffect } from 'react'
 import { useTheme, styled } from '@mui/material/styles'
-import { theme } from 'app/theme'
+import { LANDING_BACKGROUND_COLOUR } from 'app/theme'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import ContentContainer from 'app/ContentContainer'
 import Footer from 'features/landingPage/components/Footer'
 import Sidebar from 'features/landingPage/components/Sidebar'
 import ToolCards from 'features/landingPage/components/ToolCards'
 import { LANDING_PAGE_DOC_TITLE } from 'utils/constants'
-
-const PREFIX = 'LandingPage'
-
-const Root = styled('div', {
-  name: `${PREFIX}-root`
-})({
-  display: 'flex',
-  flexDirection: 'column',
-  minHeight: '100vh'
-})
-
-const Content = styled('div', {
-  name: `${PREFIX}-content`
-})({
-  display: 'flex',
-  flexGrow: 1
-})
-
-const SubContainer = styled('div', {
-  name: `${PREFIX}-subContainer`
-})({
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1
-})
-
-const SidebarContainer = styled('div', {
-  name: `${PREFIX}-sidebarContainer`
-})({
-  display: 'flex',
-  flexGrow: 1,
-  overflowY: 'auto',
-  [theme.breakpoints.up('sm')]: {
-    minWidth: '251px',
-    maxWidth: '251px',
-    width: '251px'
-  }
-})
+import { Box } from '@mui/material'
 
 const LandingPage: React.FunctionComponent = () => {
   const theme = useTheme()
-
   const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
 
   useEffect(() => {
@@ -56,24 +17,35 @@ const LandingPage: React.FunctionComponent = () => {
   }, [])
 
   return (
-    <Root>
-      <Content>
-        <SidebarContainer>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        maxHeight: '100vh',
+        minHeight: '100vh',
+        overflow: 'hidden'
+      }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'row', flexGrow: 1, overflow: 'hidden' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            overflowY: 'auto',
+            [theme.breakpoints.up('sm')]: {
+              minWidth: '265px',
+              maxWidth: '265px',
+              width: '265px'
+            }
+          }}
+        >
           <Sidebar />
-        </SidebarContainer>
-        {!isSmall && (
-          <SubContainer>
-            <ContentContainer>
-              {/* Future home of a routing component once we integrate apps into the landing page
-               *For now the landing page just displays the side bar and tool cards
-               */}
-              <ToolCards />
-            </ContentContainer>
-          </SubContainer>
-        )}
-      </Content>
+        </Box>
+        <Box sx={{ bgcolor: LANDING_BACKGROUND_COLOUR, display: 'flex', flexGrow: 1, overflowY: 'auto' }}>
+          {!isSmall && <ToolCards />}
+        </Box>
+      </Box>
       <Footer />
-    </Root>
+    </Box>
   )
 }
 
