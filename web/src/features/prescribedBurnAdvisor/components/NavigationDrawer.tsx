70e45fc85d1ccce0bed37dc3c0681934ad887845
Darren Boss <darren.boss@gov.bc.ca>
2025-04-22 15:57:40 -0700

    Unused imports

--- web/src/features/prescribedBurnAdvisor/components/NavigationDrawer.tsx
+++ web/src/features/prescribedBurnAdvisor/components/NavigationDrawer.tsx
@@ -1,53 +1,61 @@
-import { CSSObject, Drawer as MuiDrawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, styled, Theme, IconButton, Tooltip, Typography } from "@mui/material"
+import {
+  CSSObject,
+  Drawer as MuiDrawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+  Theme,
+  Tooltip
+} from '@mui/material'
 import DashboardIcon from '@mui/icons-material/Dashboard'
-import { DRAWER_WIDTH } from "@/utils/constants"
-
+import { DRAWER_WIDTH } from '@/utils/constants'
 
 const openedMixin = (theme: Theme): CSSObject => ({
-    width: DRAWER_WIDTH,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden'
-  })
-  
-  const closedMixin = (theme: Theme): CSSObject => ({
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(${theme.spacing(8)} + 1px)`,
-    }
-  })
+  width: DRAWER_WIDTH,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen
+  }),
+  overflowX: 'hidden'
+})
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme }) => ({
-      width: DRAWER_WIDTH,
-      flexShrink: 0,
-      whiteSpace: 'nowrap',
-      boxSizing: 'border-box',
-      variants: [
-        {
-          props: ({ open }) => open,
-          style: {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-          },
-        },
-        {
-          props: ({ open }) => !open,
-          style: {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-          },
-        },
-      ],
-    }),
-  )
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`
+  }
+})
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== 'open' })(({ theme }) => ({
+  width: DRAWER_WIDTH,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  variants: [
+    {
+      props: ({ open }) => open,
+      style: {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme)
+      }
+    },
+    {
+      props: ({ open }) => !open,
+      style: {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme)
+      }
+    }
+  ]
+}))
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -55,14 +63,14 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
+  ...theme.mixins.toolbar
 }))
 
 interface NavigationDrawerProps {
   open: boolean
 }
 
-const NavigationDrawer = ({open}: NavigationDrawerProps) => {
+const NavigationDrawer = ({ open }: NavigationDrawerProps) => {
   return (
     <Drawer open={open} PaperProps={{ sx: { backgroundColor: '#eaeaea' } }} variant="permanent">
       <DrawerHeader />
