888b0f329ec6803d2f130ab6cf12125ce303b30e
dgboss <darren.boss@gov.bc.ca>
2025-04-23 16:57:20 -0700

    Deploy fire watch to test (#4454)

--- openshift/scripts/oc_promote.sh
+++ openshift/scripts/oc_promote.sh
@@ -23,12 +23,12 @@ source "$(dirname ${0})/common/common"
 
 # Source and destination
 #
-IMG_DEST="${APP_NAME}-${MODULE_NAME}-${TAG_PROD}"
+IMG_DEST="${APP_NAME}-${MODULE_NAME}-${TAG_TEST}"
 
 # Tag PR image on prod imagestream, leaving the original tag in, then retag prod imagestream
 #
 OC_IMG_PROD_STREAM_TAG="oc -n ${PROJ_TOOLS} tag ${APP_NAME}-${MODULE_NAME}-${SUFFIX}:${SUFFIX} ${IMG_DEST}:${SUFFIX}"
-OC_IMG_PROD_LATEST_TAG="oc -n ${PROJ_TOOLS} tag ${IMG_DEST}:${SUFFIX} ${IMG_DEST}:${TAG_PROD}"
+OC_IMG_PROD_LATEST_TAG="oc -n ${PROJ_TOOLS} tag ${IMG_DEST}:${SUFFIX} ${IMG_DEST}:${TAG_TEST}"
 
 # Get list of images to prune.
 #
