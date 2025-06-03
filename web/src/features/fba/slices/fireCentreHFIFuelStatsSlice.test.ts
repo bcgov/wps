5ff14a77209c823ce8cbd59dbeedd64b819a6b0a
Darren Boss <darren.boss@gov.bc.ca>
2025-06-03 16:44:53 -0700

    De-dupe code in tests

--- web/src/features/fba/slices/fireCentreHFIFuelStatsSlice.test.ts
+++ web/src/features/fba/slices/fireCentreHFIFuelStatsSlice.test.ts
@@ -1,64 +1,51 @@
-import { FireCentreHFIStats } from '@/api/fbaAPI'
-import fireCentreHFIFuelStatsReducer, { getFireCentreHFIFuelStatsFailed, getFireCentreHFIFuelStatsSuccess, initialState} from '@/features/fba/slices/fireCentreHFIFuelStatsSlice'
+import { AdvisoryCriticalHours, AdvisoryMinWindStats, FireCentreHFIStats, FuelType, HfiThreshold } from '@/api/fbaAPI'
+import fireCentreHFIFuelStatsReducer, {
+  getFireCentreHFIFuelStatsFailed,
+  getFireCentreHFIFuelStatsSuccess,
+  initialState
+} from '@/features/fba/slices/fireCentreHFIFuelStatsSlice'
+
+const createThreshold = (id: number, name: string, description: string): HfiThreshold => {
+  return { id, name, description }
+}
+
+const createFuelType = (fuel_type_id: number, fuel_type_code: string, description: string): FuelType => {
+  return { fuel_type_id, fuel_type_code, description }
+}
+
+const createCriticalHours = (start_time: number, end_time: number): AdvisoryCriticalHours => {
+  return { start_time, end_time }
+}
+
+const createMinWindStats = (id: number, name: string, description: string, min_wind_speed: number): AdvisoryMinWindStats => {
+  return {
+    threshold: { id, name, description },
+    min_wind_speed
+  }
+}
 
 const fireCentreHfiStats: FireCentreHFIStats = {
   'Cariboo Fire Centre': {
     '20': {
       fuel_area_stats: [
         {
-          fuel_type: {
-            fuel_type_id: 2,
-            fuel_type_code: 'C-2',
-            description: 'Boreal Spruce'
-          },
-          threshold: {
-            id: 1,
-            name: 'advisory',
-            description: '4000 < hfi < 10000'
-          },
-          critical_hours: {
-            start_time: 9,
-            end_time: 13
-          },
+          fuel_type: createFuelType(2, 'C-2', 'Boreal Spruce'),
+          threshold: createThreshold(1, 'advisory', '4000 < hfi < 10000'),
+          critical_hours: createCriticalHours(9, 13),
           area: 4000000000,
           fuel_area: 8000000000
         },
         {
-          fuel_type: {
-            fuel_type_id: 2,
-            fuel_type_code: 'S-1',
-            description: 'Slash'
-          },
-          threshold: {
-            id: 1,
-            name: 'advisory',
-            description: '4000 < hfi < 10000'
-          },
-          critical_hours: {
-            start_time: 9,
-            end_time: 13
-          },
+          fuel_type: createFuelType(2, 'S-1', 'Slash'),
+          threshold: createThreshold(1, 'advisory', '4000 < hfi < 10000'),
+          critical_hours: createCriticalHours(9, 13),
           area: 4000000000,
           fuel_area: 8000000000
         }
       ],
       min_wind_stats: [
-        {
-          threshold: {
-            id: 1,
-            name: 'advisory',
-            description: '4000 < hfi < 10000'
-          },
-          min_wind_speed: 1
-        },
-        {
-          threshold: {
-            id: 2,
-            name: 'warning',
-            description: 'hfi > 1000'
-          },
-          min_wind_speed: 1
-        }
+        createMinWindStats(1, 'advisory', '4000 < hfi < 10000', 1),
+        createMinWindStats(2, 'warning', 'hfi > 10000', 1),
       ]
     }
   }
@@ -70,7 +57,7 @@ describe('fireCentreHFIFuelStatsSlice', () => {
       expect(fireCentreHFIFuelStatsReducer(undefined, { type: '' })).toEqual(initialState)
     })
     it('should set a value for error state when getFireCentreHFIFuelStatsFailed is called', () => {
-      expect(fireCentreHFIFuelStatsReducer(initialState, getFireCentreHFIFuelStatsFailed("error")).error).not.toBeNull()
+      expect(fireCentreHFIFuelStatsReducer(initialState, getFireCentreHFIFuelStatsFailed('error')).error).not.toBeNull()
     })
     it('should set a value for fireCentreHFIFuelStats when getFireCentreHFIFuelStatsSuccess is called', () => {
       const result = fireCentreHFIFuelStatsReducer(initialState, getFireCentreHFIFuelStatsSuccess(fireCentreHfiStats))
