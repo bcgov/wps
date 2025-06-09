b6a7a5398fd3be63b048f033c6da2a0a407fea2d
Conor Brady <con.brad@gmail.com>
2025-06-09 11:08:49 -0700

    Add CapacitorEmailComposer dependency and integrate email functionality in HamburgerMenu

--- mobile/asa-go/src/components/HamburgerMenu.tsx
+++ mobile/asa-go/src/components/HamburgerMenu.tsx
@@ -1,4 +1,11 @@
-import { Drawer, IconButton, Typography } from "@mui/material";
+import {
+  Drawer,
+  IconButton,
+  List,
+  Typography,
+  ListItemButton,
+} from "@mui/material";
+import { EmailComposer } from "capacitor-email-composer";
 import Grid from "@mui/material/Grid2";
 
 import { Menu as MenuIcon, Close as CloseIcon } from "@mui/icons-material";
@@ -6,10 +13,6 @@ import { Menu as MenuIcon, Close as CloseIcon } from "@mui/icons-material";
 import { useState } from "react";
 import { RunType, FireCenter, FireShape } from "@/api/fbaAPI";
 import { DateTime } from "luxon";
-import ActualForecastControl from "@/components/ActualForecastControl";
-import WPSDatePicker from "@/components/WPSDatePicker";
-import FireCenterDropdown from "@/components/FireCenterDropdown";
-import { theme } from "@/theme";
 
 export interface HamburgerMenuProps {
   runType: RunType;
@@ -32,19 +35,23 @@ export interface HamburgerMenuProps {
 }
 
 export const HamburgerMenu = ({
-  runType,
-  setRunType,
-  date,
-  updateDate,
-  selectedFireCenter,
-  fireCenterOptions,
-  setSelectedFireShape,
-  setSelectedFireCenter,
-  setZoomSource,
   drawerTop,
   drawerHeight,
 }: HamburgerMenuProps) => {
   const [open, setOpen] = useState(false);
+
+  const handleListButtonClick = (url: string) => {
+    if (url.startsWith("mail:to")) {
+      EmailComposer.open({
+        to: ["bcws.predictiveservices@gov.bc.ca"],
+        subject: "ASA App Contact",
+        body: "",
+        isHtml: false,
+      });
+    } else {
+      window.open(url, "_blank", "noopener,noreferrer");
+    }
+  };
 
   return (
     <>
@@ -59,6 +66,7 @@ export const HamburgerMenu = ({
           sx: {
             top: `${drawerTop}px`,
             height: `${drawerHeight}px`,
+            backgroundColor: "lightGrey",
             borderTopLeftRadius: 16,
             borderBottomLeftRadius: 16,
           },
@@ -71,21 +79,14 @@ export const HamburgerMenu = ({
           sx={{ width: 250, padding: "16px" }}
         >
           <Grid container alignItems="center" justifyContent="space-between">
-            <Typography
-              variant="h2"
-              sx={{
-                color: theme.palette.primary.contrastText,
-                fontSize: "1.7rem",
-              }}
-            >
-              ASA
-            </Typography>
             <IconButton
               onClick={() => setOpen(false)}
               sx={{
                 cursor: "pointer",
                 backgroundColor: "transparent",
                 transition: "background-color 0.2s",
+                alignSelf: "flex-end",
+                marginLeft: "auto",
                 "&:hover": {
                   backgroundColor: "#f0f0f0",
                 },
@@ -95,21 +96,74 @@ export const HamburgerMenu = ({
               <CloseIcon />
             </IconButton>
           </Grid>
-          <Grid>
-            <ActualForecastControl runType={runType} setRunType={setRunType} />
-          </Grid>
-          <Grid>
-            <WPSDatePicker date={date} updateDate={updateDate} />
-          </Grid>
-          <Grid>
-            <FireCenterDropdown
-              fireCenterOptions={fireCenterOptions}
-              selectedFireCenter={selectedFireCenter}
-              setSelectedFireCenter={setSelectedFireCenter}
-              setSelectedFireShape={setSelectedFireShape}
-              setZoomSource={setZoomSource}
-            />
-          </Grid>
+          <List
+            sx={{
+              width: "100%",
+              "& .MuiListItemButton-root": {
+                width: "100%",
+                justifyContent: "flex-end",
+              },
+            }}
+          >
+            <ListItemButton
+              divider
+              onClick={() =>
+                handleListButtonClick("https://psu.nrs.gov.bc.ca/")
+              }
+            >
+              <Typography variant="subtitle1">Home</Typography>
+            </ListItemButton>
+            <ListItemButton
+              divider
+              onClick={() =>
+                handleListButtonClick(
+                  "https://www2.gov.bc.ca/gov/content/home/disclaimer"
+                )
+              }
+            >
+              Disclaimer
+            </ListItemButton>
+            <ListItemButton
+              divider
+              onClick={() =>
+                handleListButtonClick(
+                  "https://www2.gov.bc.ca/gov/content/home/privacy"
+                )
+              }
+            >
+              Privacy
+            </ListItemButton>
+            <ListItemButton
+              divider
+              onClick={() =>
+                handleListButtonClick(
+                  "https://www2.gov.bc.ca/gov/content/home/accessible-government"
+                )
+              }
+            >
+              Accessibility
+            </ListItemButton>
+            <ListItemButton
+              divider
+              onClick={() =>
+                handleListButtonClick(
+                  "https://www2.gov.bc.ca/gov/content/home/copyright"
+                )
+              }
+            >
+              Copyright
+            </ListItemButton>
+            <ListItemButton
+              divider
+              onClick={() =>
+                handleListButtonClick(
+                  "mailto:bcws.predictiveservices@gov.bc.ca"
+                )
+              }
+            >
+              Contact Us
+            </ListItemButton>
+          </List>
         </Grid>
       </Drawer>
     </>
