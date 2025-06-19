c8bec3d0e1ac50f4c5b65f02fe31d1b90bbfea23
dgboss <darren.boss@gov.bc.ca>
2025-06-19 09:41:34 -0700

    Populate fuel type raster with migration v2 (#4610)

--- api/alembic/versions/c2cd7a585bbd_recompute_combustible_area.py
+++ api/alembic/versions/c2cd7a585bbd_recompute_combustible_area.py
@@ -5,40 +5,57 @@ Revises: 8635552697ad
 Create Date: 2024-05-15 08:32:04.942965
 
 """
-from alembic import op
-import sqlalchemy as sa
-import geoalchemy2
-from geoalchemy2.shape import to_shape
-from sqlalchemy.orm.session import Session
-from osgeo import ogr
+
 from contextlib import contextmanager
-from app.auto_spatial_advisory.calculate_combustible_land_area import calculate_combustible_area_by_fire_zone
+
+import geoalchemy2
+import sqlalchemy as sa
+from alembic import op
+from app.auto_spatial_advisory.calculate_combustible_land_area import (
+    calculate_combustible_area_by_fire_zone,
+)
+from geoalchemy2.shape import to_shape
+from osgeo import ogr
+from sqlalchemy.orm.session import Session
 
 # revision identifiers, used by Alembic.
-revision = 'c2cd7a585bbd'
-down_revision = '8635552697ad'
+revision = "c2cd7a585bbd"
+down_revision = "8635552697ad"
 branch_labels = None
 depends_on = None
 
 
-shape_type_table = sa.Table('advisory_shape_types', sa.MetaData(),
-                            sa.Column('id', sa.Integer),
-                            sa.Column('name', sa.String))
+shape_type_table = sa.Table(
+    "advisory_shape_types",
+    sa.MetaData(),
+    sa.Column("id", sa.Integer),
+    sa.Column(
+        "name",
+        sa.Enum("fire_centre", "fire_zone", "fire_zone_unit", name="shapetypeenum_2"),
+        nullable=False,
+    ),
+)
 
-shape_table = sa.Table('advisory_shapes', sa.MetaData(),
-                       sa.Column('id', sa.Integer),
-                       sa.Column('source_identifier', sa.String),
-                       sa.Column('shape_type', sa.Integer),
-                       sa.Column('geom', geoalchemy2.Geometry))
+shape_table = sa.Table(
+    "advisory_shapes",
+    sa.MetaData(),
+    sa.Column("id", sa.Integer),
+    sa.Column("source_identifier", sa.String),
+    sa.Column("shape_type", sa.Integer),
+    sa.Column("geom", geoalchemy2.Geometry),
+)
 
-fuel_type_table = sa.Table('advisory_fuel_types', sa.MetaData(),
-                        sa.Column('id', sa.Integer),
-                        sa.Column('fuel_type_id', sa.Integer),
-                        sa.Column('geom', geoalchemy2.Geometry))
+fuel_type_table = sa.Table(
+    "advisory_fuel_types",
+    sa.MetaData(),
+    sa.Column("id", sa.Integer),
+    sa.Column("fuel_type_id", sa.Integer),
+    sa.Column("geom", geoalchemy2.Geometry),
+)
 
 
 def get_fire_zone_unit_shape_type_id(session: Session):
-    statement = shape_type_table.select().where(shape_type_table.c.name == 'fire_zone_unit')
+    statement = shape_type_table.select().where(shape_type_table.c.name == "fire_zone_unit")
     result = session.execute(statement).fetchone()
     return result.id
 
@@ -50,18 +67,20 @@ def get_fire_zone_units(session: Session, fire_zone_type_id: int):
 
 
 def get_fuel_type_polygons(session: Session):
-    stmt = fuel_type_table.select().where(fuel_type_table.c.fuel_type_id < 99, fuel_type_table.c.fuel_type_id > 0)
+    stmt = fuel_type_table.select().where(
+        fuel_type_table.c.fuel_type_id < 99, fuel_type_table.c.fuel_type_id > 0
+    )
     result = session.execute(stmt).fetchall()
     return result
 
 
 @contextmanager
 def get_fuel_types_from_db(session):
-    mem_driver = ogr.GetDriverByName('Memory')
-    mem_ds = mem_driver.CreateDataSource('mem_data')
-    fuel_types_layer = mem_ds.CreateLayer('fuel_types', geom_type=ogr.wkbPolygon)
-    fuel_types_layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
-    fuel_types_layer.CreateField(ogr.FieldDefn('fuel_type_id', ogr.OFTInteger))
+    mem_driver = ogr.GetDriverByName("Memory")
+    mem_ds = mem_driver.CreateDataSource("mem_data")
+    fuel_types_layer = mem_ds.CreateLayer("fuel_types", geom_type=ogr.wkbPolygon)
+    fuel_types_layer.CreateField(ogr.FieldDefn("id", ogr.OFTInteger))
+    fuel_types_layer.CreateField(ogr.FieldDefn("fuel_type_id", ogr.OFTInteger))
 
     fuel_type_polygons = get_fuel_type_polygons(session)
     for row in fuel_type_polygons:
@@ -69,8 +88,8 @@ def get_fuel_types_from_db(session):
 
         feature = ogr.Feature(fuel_types_layer.GetLayerDefn())
         feature.SetGeometry(ogr.CreateGeometryFromWkt(shapely_obj.wkt))
-        feature.SetField('id', row.id)
-        feature.SetField('fuel_type_id', row.fuel_type_id)
+        feature.SetField("id", row.id)
+        feature.SetField("fuel_type_id", row.fuel_type_id)
         fuel_types_layer.CreateFeature(feature)
         feature = None
 
@@ -87,8 +106,10 @@ def upgrade():
 
         zone_areas = calculate_combustible_area_by_fire_zone(fuel_types, zone_units)
         for tuple in zone_areas:
-            op.execute('UPDATE advisory_shapes SET combustible_area={} WHERE source_identifier LIKE \'{}\''.format(
-                tuple[1], tuple[0])
+            op.execute(
+                "UPDATE advisory_shapes SET combustible_area={} WHERE source_identifier LIKE '{}'".format(
+                    tuple[1], tuple[0]
+                )
             )
 
 
@@ -98,6 +119,8 @@ def downgrade():
     zones = get_fire_zone_units(session, fire_zone_shape_id)
 
     for zone in zones:
-        op.execute('UPDATE advisory_shapes SET combustible_area = NULL WHERE source_identifier LIKE \'{}\''.format(
-            str(zone.source_identifier)
-        ))
+        op.execute(
+            "UPDATE advisory_shapes SET combustible_area = NULL WHERE source_identifier LIKE '{}'".format(
+                str(zone.source_identifier)
+            )
+        )
