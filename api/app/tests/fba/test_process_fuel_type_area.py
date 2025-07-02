1339fa39dc79a00b6a47d7c8e1c06fb6bcd49c45
Darren Boss <darren.boss@gov.bc.ca>
2025-07-02 08:32:37 -0700

    Remove unused code from test

--- api/app/tests/fba/test_process_fuel_type_area.py
+++ api/app/tests/fba/test_process_fuel_type_area.py
@@ -1,22 +1,27 @@
-from wps_shared import config
-from app.auto_spatial_advisory.process_fuel_type_area import get_fuel_type_s3_key, classify_by_threshold
 import numpy as np
+from app.auto_spatial_advisory.process_fuel_type_area import (
+    classify_by_threshold,
+    get_fuel_type_s3_key,
+)
 
+from wps_shared import config
 
-HFI_RASTER = np.array([[1000, 2000, 3000, 4005], [5000, 10001, 11000, 12000], [300, 500, 7006, 9000], [400, 0, 1, 300]])
+HFI_RASTER = np.array(
+    [
+        [1000, 2000, 3000, 4005],
+        [5000, 10001, 11000, 12000],
+        [300, 500, 7006, 9000],
+        [400, 0, 1, 300],
+    ]
+)
 
-mock_fuel_raster_name = "fbp2024.tif"
-
-
-def get_mock_fuel_raster_name():
-    return mock_fuel_raster_name
 
 def test_get_warped_fuel_type_s3_key():
     bucket = "abcde"
     key = get_fuel_type_s3_key(bucket)
     fuel_raster_name = config.get("FUEL_RASTER_NAME")
     assert key == f"/vsis3/{bucket}/sfms/static/{fuel_raster_name}"
-    
+
 
 def test_classify_by_threshold_1():
     result = classify_by_threshold(HFI_RASTER, 1)
