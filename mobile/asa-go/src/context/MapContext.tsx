508bf68a2b81b9086ca8591f85f05d88f471ff1a
Brett Edwards <brett.1.edwards@gov.bc.ca>
2025-07-07 08:41:41 -0700

    context

--- mobile/asa-go/src/context/MapContext.tsx
+++ mobile/asa-go/src/context/MapContext.tsx
@@ -2,8 +2,3 @@ import React from "react";
 import { Map } from "ol";
 
 export const MapContext = React.createContext<Map | null>(null);
-
-export const useMapContext = () => {
-  const context = React.useContext(MapContext);
-  return context;
-};
