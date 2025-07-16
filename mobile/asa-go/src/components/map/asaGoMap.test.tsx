c810586610708a5d05168984c26afe30607291bf
Brett Edwards <brett.1.edwards@gov.bc.ca>
2025-07-16 10:02:19 -0700

    init

--- mobile/asa-go/src/components/map/asaGoMap.test.tsx
+++ mobile/asa-go/src/components/map/asaGoMap.test.tsx
@@ -2,9 +2,10 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { DateTime } from "luxon";
 import { describe, expect, it, vi, beforeEach } from "vitest";
-import ASAGoMap from "@/components/map/ASAGoMap";
+import ASAGoMap, { ASAGoMapProps } from "@/components/map/ASAGoMap";
 import { createTestStore } from "@/testUtils";
 import { geolocationInitialState } from "@/slices/geolocationSlice";
+import { RunType } from "@/api/fbaAPI";
 
 class ResizeObserver {
   observe() {
@@ -23,16 +24,16 @@ describe("ASAGoMap", () => {
     window.ResizeObserver = ResizeObserver;
   });
 
-  const defaultProps = {
+  const defaultProps: ASAGoMapProps = {
     testId: "asa-go-map",
     selectedFireShape: undefined,
     setSelectedFireShape: vi.fn(),
-    fireShapeAreas: [],
     advisoryThreshold: 0,
     date: DateTime.now(),
     setDate: vi.fn(),
     setTab: vi.fn(),
-    startWatching: vi.fn(),
+    runType: RunType.FORECAST,
+    runDatetime: DateTime.now(),
   };
 
   const mockPosition = {
