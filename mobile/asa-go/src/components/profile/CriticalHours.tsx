f465787707ea56ae6c04e22b9d6cafc360fa1668
Conor Brady <con.brad@gmail.com>
2025-07-22 15:33:30 -0700

    feedback

--- mobile/asa-go/src/components/profile/CriticalHours.tsx
+++ mobile/asa-go/src/components/profile/CriticalHours.tsx
@@ -1,25 +1,36 @@
-import { Typography } from '@mui/material'
-import React from 'react'
-import { isNil } from 'lodash'
-import { formatCriticalHoursTimeText } from '@/utils/criticalHoursStartEndTime'
+import { Typography } from "@mui/material";
+import React from "react";
+import { isNil } from "lodash";
+import { formatCriticalHoursTimeText } from "@/utils/criticalHoursStartEndTime";
 
 interface CriticalHoursProps {
-  start?: number
-  end?: number
+  start?: number;
+  end?: number;
 }
 
 const CriticalHours = ({ start, end }: CriticalHoursProps) => {
-  let formattedCriticalHours = '-'
+  let formattedCriticalHours = "-";
 
   if (!isNil(start) && !isNil(end)) {
-    const [formattedStartTime, formattedEndTime] = formatCriticalHoursTimeText(start, end)
-    formattedCriticalHours = `${formattedStartTime} - ${formattedEndTime}`
+    const [formattedStartTime, formattedEndTime] = formatCriticalHoursTimeText(
+      start,
+      end
+    );
+    formattedCriticalHours = `${formattedStartTime} - ${formattedEndTime}`;
   }
   return (
-    <Typography sx={{ fontSize: '0.75rem' }} data-testid="critical-hours">
+    <Typography
+      sx={{
+        fontSize: "0.75rem",
+        display: "flex",
+        height: "100%",
+        alignItems: "center",
+      }}
+      data-testid="critical-hours"
+    >
       {formattedCriticalHours}
     </Typography>
-  )
-}
+  );
+};
 
-export default React.memo(CriticalHours)
+export default React.memo(CriticalHours);
