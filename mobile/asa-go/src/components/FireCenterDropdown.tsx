45eb7e7d3678d0d3978ccfc05aeb24b211e09a34
Brett Edwards <brett.1.edwards@gov.bc.ca>
2025-07-28 10:19:42 -0700

    ASA Go - Allow no zone/centre selection (#4715)
    - Don't auto select the first fire zone/centre on initial load
    - Allow user to select no zone by touching outside of BC
    - Adds default text to the `Profile` tab 'Please select a fire centre"

--- mobile/asa-go/src/components/FireCenterDropdown.tsx
+++ mobile/asa-go/src/components/FireCenterDropdown.tsx
@@ -1,6 +1,5 @@
-import { MenuItem, Select } from "@mui/material";
+import { MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
 import { FireCenter, FireShape } from "api/fbaAPI";
-import { isEqual, isNil } from "lodash";
 import React from "react";
 
 interface FireCenterDropdownProps {
@@ -20,25 +19,34 @@ const FireCenterDropdown = ({
   setSelectedFireCenter,
   setSelectedFireShape,
 }: FireCenterDropdownProps) => {
-  const handleClick = (value: FireCenter) => {
-    if (!isEqual(selectedFireCenter, value)) {
-      setSelectedFireShape(undefined);
-      setSelectedFireCenter(value);
-    }
+  const handleChange = (event: SelectChangeEvent<string>): void => {
+    const selectedName = event.target.value;
+    const selected = fireCenterOptions.find((fc) => fc.name === selectedName);
+    setSelectedFireShape(undefined);
+    setSelectedFireCenter(selected ?? undefined);
   };
 
-  if (isNil(selectedFireCenter) && fireCenterOptions.length) {
-    setSelectedFireCenter(fireCenterOptions[0]);
-  }
+  const getSelectedDisplay = (selected: FireCenter | undefined) => {
+    if (!selected) {
+      return (
+        <Typography sx={{ color: "text.disabled" }}>
+          Select Fire Center
+        </Typography>
+      );
+    }
+    return selected.name;
+  };
 
   return (
-    <Select data-testid="fire-center-dropdown" value={selectedFireCenter?.name ?? ""}>
+    <Select
+      data-testid="fire-center-dropdown"
+      value={selectedFireCenter?.name ?? ""}
+      onChange={handleChange}
+      displayEmpty
+      renderValue={() => getSelectedDisplay(selectedFireCenter)}
+    >
       {fireCenterOptions.map((option) => (
-        <MenuItem
-          key={option.name}
-          value={option.name}
-          onClick={() => handleClick(option)}
-        >
+        <MenuItem key={option.name} value={option.name}>
           {option.name}
         </MenuItem>
       ))}
