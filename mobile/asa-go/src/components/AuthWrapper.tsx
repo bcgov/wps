1c2b562d081f21d75a7542524d75b10a8f19bcf9
Darren Boss <darren.boss@gov.bc.ca>
2025-07-28 10:15:39 -0700

    Login button

--- mobile/asa-go/src/components/AuthWrapper.tsx
+++ mobile/asa-go/src/components/AuthWrapper.tsx
@@ -1,48 +1,88 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Capacitor } from "@capacitor/core";
-import { AppDispatch, selectAuthentication } from "@/store";
-import { authenticate } from "@/slices/authenticationSlice";
+import { selectAuthentication } from "@/store";
+import { Box, CircularProgress, Typography, useTheme } from "@mui/material";
+import { isNull } from "lodash";
+import LoginButton from "@/components/LoginButton";
+import AsaIcon from "@/assets/asa-go-transparent.png";
 
 interface Props {
   children: React.ReactElement;
 }
 
 const AuthWrapper = ({ children }: Props) => {
-  const dispatch: AppDispatch = useDispatch();
+  const theme = useTheme();
   const { isAuthenticated, authenticating, error } =
     useSelector(selectAuthentication);
-
-  useEffect(() => {
-    dispatch(authenticate());
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   // TODO implement for Android
   if (Capacitor.getPlatform() === "android") {
     return <React.StrictMode>{children}</React.StrictMode>;
   }
 
-  if (error) {
-    setInterval(() => {
-      dispatch(authenticate());
-    }, 3000);
-    return (
-      <div style={{ marginTop: 100 }}>
-        {error}
-        <p>Trying again in 3 seconds</p>
-      </div>
-    );
+  if (isAuthenticated) {
+    return <React.StrictMode>{children}</React.StrictMode>;
   }
 
-  if (authenticating) {
-    return <div>Signing in...</div>;
-  }
-
-  if (!isAuthenticated) {
-    return <div>You are not authenticated!</div>;
-  }
-
-  return <React.StrictMode>{children}</React.StrictMode>;
+  return (
+    <Box
+      sx={{
+        bgcolor: theme.palette.primary.dark,
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          height: "40%",
+          display: "flex",
+          alignItems: "end",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{ alignItems: "center", display: "flex", height: "200px" }}>
+          <Box
+            component="img"
+            sx={{ height: "150px", width: "150px" }}
+            src={AsaIcon}
+          />
+          <Typography sx={{ color: "white", fontWeight: "bold" }} variant="h2">
+            ASA Go
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          height: "60%",
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          alignItems: "center",
+        }}
+      >
+        {!authenticating && (
+          <>
+            <LoginButton label="Login" />
+          </>
+        )}
+        {authenticating && (
+          <Box sx={{ pt: theme.spacing(4) }}>
+            <CircularProgress color="secondary" />
+          </Box>
+        )}
+        {!isNull(error) && (
+          <Typography
+            sx={{ color: "white", mt: theme.spacing(5) }}
+            variant="h5"
+          >
+            Unable to login, please try again.
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  );
 };
 
 export default React.memo(AuthWrapper);
