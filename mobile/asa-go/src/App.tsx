09380cc6107db991716f13ebe824954473bc1207
Conor Brady <con.brad@gmail.com>
2025-08-25 16:49:37 -0700

    Add push notification support and update related configurations

--- mobile/asa-go/src/App.tsx
+++ mobile/asa-go/src/App.tsx
@@ -27,6 +27,7 @@ import { isNull, isUndefined } from "lodash";
 import { DateTime } from "luxon";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { PushNotifications } from "@capacitor/push-notifications";
 
 const ADVISORY_THRESHOLD = 20;
 
@@ -67,6 +68,33 @@ const App = () => {
       Network.removeAllListeners();
     };
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    const addListeners = async () => {
+      await PushNotifications.addListener("registration", (token) => {
+        console.info("Registration token: ", token.value);
+      });
+
+      await PushNotifications.addListener("registrationError", (err) => {
+        console.error("Registration error: ", err.error);
+      });
+    };
+    const registerNotifications = async () => {
+      let permStatus = await PushNotifications.checkPermissions();
+
+      if (permStatus.receive === "prompt") {
+        permStatus = await PushNotifications.requestPermissions();
+      }
+
+      if (permStatus.receive !== "granted") {
+        throw new Error("User denied permissions!");
+      }
+
+      await PushNotifications.register();
+    };
+    addListeners();
+    registerNotifications();
+  }, []);
 
   useEffect(() => {
     dispatch(fetchFireCenters());
