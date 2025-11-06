480b8dffd988ef37e489d81e2a34ba4ef7ddc4fe
Conor Brady <con.brad@gmail.com>
2025-11-06 15:06:23 -0800

    Move back to auth/callback

--- mobile/asa-go/src/slices/authenticationSlice.ts
+++ mobile/asa-go/src/slices/authenticationSlice.ts
@@ -87,7 +87,7 @@ export const authenticate = (): AppThunk => (dispatch) => {
     import.meta.env.VITE_KEYCLOAK_AUTH_URL
   }/realms/${realm}/protocol/openid-connect/token`;
 
-  const customRedirectUri = "ca.bc.gov.asago://auth";
+  const customRedirectUri = "ca.bc.gov.asago://auth/callback";
   Keycloak.authenticate({
     authorizationBaseUrl: authUrl,
     clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
