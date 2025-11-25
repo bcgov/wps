541dddffd9f44f450ed997e962726621cc93bf6a
Conor Brady <con.brad@gmail.com>
2025-11-24 16:08:53 -0800

    fix tests

--- backend/packages/wps-api/src/app/tests/fba_calc/test_c3_firebat.py
+++ backend/packages/wps-api/src/app/tests/fba_calc/test_c3_firebat.py
@@ -66,7 +66,7 @@ async def test_c3_request_response(
     )
     assert math.isclose(response.json()["stations"][0]["fire_weather_index"], 27.792, abs_tol=0.001)
     assert math.isclose(
-        response.json()["stations"][0]["head_fire_intensity"], 2911.250, abs_tol=0.001
+        response.json()["stations"][0]["head_fire_intensity"], 2911.248, abs_tol=0.001
     )
     assert math.isclose(response.json()["stations"][0]["rate_of_spread"], 2.754, abs_tol=0.001)
     assert math.isclose(
