48efcd013ad7aa97d8b3a7ebdd2519156ff9e382
Darren Boss <darren.boss@gov.bc.ca>
2025-12-01 14:46:25 -0800

    Mock base layer

--- web/src/features/sfmsInsights/components/map/sfmsMap.test.tsx
+++ web/src/features/sfmsInsights/components/map/sfmsMap.test.tsx
@@ -1,4 +1,14 @@
+vi.mock('@/features/sfmsInsights/components/map/layerDefinitions', async () => {
+  const actual = await import('@/features/sfmsInsights/components/map/layerDefinitions')
+
+  return {
+    ...actual,
+    createBasemapLayer: vi.fn().mockImplementation(() => Promise.resolve(baseLayerMock))
+  }
+})
+
 import SFMSMap from '@/features/sfmsInsights/components/map/SFMSMap'
+import { baseLayerMock } from '@/test/testUtils'
 import { render } from '@testing-library/react'
 
 describe('SFMSMap', () => {
