464d0b66a189e3d95721da220c0355fadbe91f76
Conor Brady <con.brad@gmail.com>
2026-01-12 13:36:40 -0800

    remove unused import

--- web/src/features/sfmsInsights/components/RasterTypeDropdown.test.tsx
+++ web/src/features/sfmsInsights/components/RasterTypeDropdown.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, within } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import RasterTypeDropdown from './RasterTypeDropdown'
 
