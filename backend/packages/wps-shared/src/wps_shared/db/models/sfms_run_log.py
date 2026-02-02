7ff5f7dcfd0d4741d99963988a948c3012e051f4
Conor Brady <con.brad@gmail.com>
2026-02-02 12:38:46 -0800

    use strings for model to make changes to python enums

--- backend/packages/wps-shared/src/wps_shared/db/models/sfms_run_log.py
+++ backend/packages/wps-shared/src/wps_shared/db/models/sfms_run_log.py
@@ -2,7 +2,7 @@
 
 import enum
 
-from sqlalchemy import Column, Date, Enum, Integer
+from sqlalchemy import Column, Date, Integer, String
 
 from wps_shared.db.models import Base
 from wps_shared.db.models.common import TZTimeStamp
@@ -30,8 +30,8 @@ class SFMSRunLog(Base):
     __table_args__ = {"comment": "Tracks SFMS job runs with execution timestamps and status."}
 
     id = Column(Integer, primary_key=True, index=True)
-    job_name = Column(Enum(SFMSRunLogJobName), nullable=False, index=True)
+    job_name = Column(String, nullable=False, index=True)
     target_date = Column(Date, nullable=False, index=True)
     started_at = Column(TZTimeStamp, nullable=False)
     completed_at = Column(TZTimeStamp, nullable=True)
-    status = Column(Enum(SFMSRunLogStatus), nullable=False)
+    status = Column(String, nullable=False)
