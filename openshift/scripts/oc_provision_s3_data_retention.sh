ce4e1984a4c8ad14c74c489dcec726c0fc10353a
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-02-05 11:13:57 -0800

    name?

--- openshift/scripts/oc_provision_s3_data_retention.sh
+++ openshift/scripts/oc_provision_s3_data_retention.sh
@@ -28,7 +28,6 @@ PROJ_TARGET="${PROJ_TARGET:-${PROJ_DEV}}"
 
 # Process template
 OC_PROCESS="oc -n ${PROJ_TARGET} process -f ${TEMPLATE_PATH}/s3_retention_cronjob.yaml \
--p JOB_NAME=s3-retention-${APP_NAME}-${SUFFIX} \
 -p SUFFIX=${SUFFIX} \
 -p SCHEDULE=${SCHEDULE}"
 
