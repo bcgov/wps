2470387cee87725f055090d77f0a0ce618c95c04
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-02-19 16:08:02 -0800

    react warning

--- mobile/asa-go/src/App.tsx
+++ mobile/asa-go/src/App.tsx
@@ -63,6 +63,12 @@ const App = () => {
   // hooks
   const runParameter = useRunParameterForDate(dateOfInterest);
 
+  const selectedFireCenterName = selectedFireShape?.mof_fire_centre_name;
+  const matchingFireCenter = selectedFireCenterName
+    ? fireCenters.find((center) => center.name === selectedFireCenterName)
+    : undefined;
+  const selectedFireCenter = matchingFireCenter ?? fireCenter;
+
   useEffect(() => {
     // Effect to manage status bar visibility
     const handleOrientationChange = async () => {
@@ -132,18 +138,6 @@ const App = () => {
       dispatch(fetchSFMSRunParameters());
     }
   }, [dateOfInterest, networkStatus.connected]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    if (selectedFireShape?.mof_fire_centre_name) {
-      const matchingFireCenter = fireCenters.find(
-        (center) => center.name === selectedFireShape.mof_fire_centre_name,
-      );
-
-      if (matchingFireCenter) {
-        setFireCenter(matchingFireCenter);
-      }
-    }
-  }, [selectedFireShape, fireCenters]);
 
   useEffect(() => {
     if (!isNil(runParameters)) {
@@ -231,7 +225,7 @@ const App = () => {
           <Profile
             date={dateOfInterest}
             setDate={setDateOfInterest}
-            selectedFireCenter={fireCenter}
+            selectedFireCenter={selectedFireCenter}
             setSelectedFireCenter={setFireCenter}
             selectedFireZoneUnit={selectedFireShape}
             setSelectedFireZoneUnit={setSelectedFireShape}
@@ -241,7 +235,7 @@ const App = () => {
           <Advisory
             date={dateOfInterest}
             setDate={setDateOfInterest}
-            selectedFireCenter={fireCenter}
+            selectedFireCenter={selectedFireCenter}
             setSelectedFireCenter={setFireCenter}
             selectedFireZoneUnit={selectedFireShape}
             setSelectedFireZoneUnit={setSelectedFireShape}
