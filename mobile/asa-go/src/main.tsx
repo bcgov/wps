f58196285cca37b915e45fb6779ccd3bee3666f8
Conor Brady <con.brad@gmail.com>
2026-04-15 16:26:47 -0700

    setup sentry

--- mobile/asa-go/src/main.tsx
+++ mobile/asa-go/src/main.tsx
@@ -7,6 +7,23 @@ import { store } from "@/store";
 import { theme } from "@/theme.ts";
 import App from "@/App.tsx";
 import AuthWrapper from "@/components/AuthWrapper";
+import * as Sentry from "@sentry/capacitor";
+import * as SentryReact from "@sentry/react";
+
+Sentry.init(
+  {
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    environment: import.meta.env.MODE,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 0.1,
+    replaysSessionSampleRate: 0.0,
+    replaysOnErrorSampleRate: 1.0,
+  },
+  SentryReact.init,
+);
 
 const render = () => {
   const container = document.getElementById("root");
--- mobile/asa-go/vite.config.ts
+++ mobile/asa-go/vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 import { resolve } from "path";
 
+const env = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "bcps-wps",
+      project: "asago",
+      authToken: env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -14,5 +25,9 @@ export default defineConfig({
       api: resolve(__dirname, "src", "api"),
       "#root": resolve(__dirname),
     },
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
