e076cbb100ac0ac4498a76eaea2df97bcd07941b
dgboss <darren.boss@gov.bc.ca>
2026-04-23 17:08:25 -0700

    Update web/apps/wps-web/playwright/fixtures.ts
    Co-authored-by: Conor Brady <con.brad@gmail.com>

--- web/apps/wps-web/playwright/fixtures.ts
+++ web/apps/wps-web/playwright/fixtures.ts
@@ -1,4 +1,4 @@
-import { test as base, expect } from '@playwright/test'
+import { test as base } from '@playwright/test'
 import fs from 'node:fs'
 import path from 'node:path'
 import crypto from 'node:crypto'
