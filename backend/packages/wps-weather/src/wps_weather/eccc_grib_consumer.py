eb9a52ad3350032263741aa1d43ee83bf84ddedf
Darren Boss <darren.boss@gov.bc.ca>
2026-04-27 16:20:23 -0700

    Download model_gem_global

--- backend/packages/wps-weather/src/wps_weather/eccc_grib_consumer.py
+++ backend/packages/wps-weather/src/wps_weather/eccc_grib_consumer.py
@@ -12,7 +12,12 @@ from wps_shared import config
 from wps_shared.utils.s3_client import S3Client
 from wps_shared.utils.time import get_utc_now
 
-from wps_weather.model_variables import GDPS_VARIABLES, HRDPS_VARIABLES, RDPS_VARIABLES
+from wps_weather.model_variables import (
+    GDPS_GEM_VARIABLES,
+    GDPS_VARIABLES,
+    HRDPS_VARIABLES,
+    RDPS_VARIABLES,
+)
 from wps_weather.utils import parse_date_and_run, s3_key_from_eccc_path
 
 logger = logging.getLogger(__name__)
@@ -43,6 +48,11 @@ MODEL_CONFIGS: dict[ModelName, ModelConfig] = {
         "routing_key": "v02.post.#.WXO-DD.model_hrdps.continental.#",
         "description": "High Resolution Deterministic Prediction System (2.5km)",
         "variables": HRDPS_VARIABLES,
+    },
+    "GDPS_GEM": {
+        "routing_key": "v02.post.#.WXO-DD.model_gem_global.15km.grib2.lat_lon.#",
+        "description": "Global Environmental Multiscale Model (15km)",
+        "variables": GDPS_GEM_VARIABLES,
     },
 }
 
