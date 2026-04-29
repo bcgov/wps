99b1108189c6444547f07f7708f8d6e12bc09a3b
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-04-29 12:46:18 -0700

    try without crypto

--- web/apps/wps-web/src/setupTests.ts
+++ web/apps/wps-web/src/setupTests.ts
@@ -5,21 +5,8 @@ import '@testing-library/jest-dom'
 import { cleanup } from '@testing-library/react'
 import { afterEach, vi } from 'vitest'
 
-let uuidCounter = 0
-
-const randomUUID = () => {
-  uuidCounter += 1
-  return `00000000-0000-4000-8000-${uuidCounter.toString().padStart(12, '0')}`
-}
-
 afterEach(() => {
   cleanup()
-})
-
-Object.defineProperty(globalThis, 'crypto', {
-  value: {
-    randomUUID
-  }
 })
 
 vi.mock('@mui/x-license', () => ({
