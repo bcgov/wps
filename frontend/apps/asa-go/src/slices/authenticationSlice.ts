fb992b0ccf6ed0030c31a3486b3bc2ca625aaebb
Conor Brady <con.brad@gmail.com>
2026-05-04 11:43:48 -0700

    fix package name

--- frontend/apps/asa-go/src/slices/authenticationSlice.ts
+++ frontend/apps/asa-go/src/slices/authenticationSlice.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AppThunk } from "@/store";
 import { jwtDecode } from "jwt-decode";
 import { isUndefined } from "lodash";
-import { Keycloak } from "@wps/keycloak";
+import { Keycloak } from "keycloak";
 
 export interface AuthState {
   authenticating: boolean;
