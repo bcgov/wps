84bdb8504cc11c4b88db4fdb74367d3ef9e1820f
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-05-04 11:40:03 -0700

    try cache clear

--- backend/packages/wps-api/src/app/auto_spatial_advisory/process_stats.py
+++ backend/packages/wps-api/src/app/auto_spatial_advisory/process_stats.py
@@ -14,6 +14,7 @@ from app.auto_spatial_advisory.process_hfi import RunType, process_hfi
 from app.auto_spatial_advisory.process_high_hfi_area import process_high_hfi_area
 from app.auto_spatial_advisory.process_zone_status import process_zone_statuses
 from app.fcm.notifications import trigger_notifications
+from wps_shared.geospatial.geospatial import clear_gdal_cache
 
 logger = logging.getLogger(__name__)
 
@@ -27,6 +28,8 @@ async def process_sfms_hfi_stats(run_type: RunType, run_datetime: datetime, for_
     await process_hfi_percent_conifer(run_type, run_datetime, for_date)
     await calculate_critical_hours(run_type, run_datetime, for_date)
     await process_zone_statuses(run_type, run_datetime, for_date)
+
+    clear_gdal_cache()
 
     async with get_async_write_session_scope() as session:
         await mark_run_parameter_complete(session, run_type, run_datetime, for_date)
