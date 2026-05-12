bba16373393a69712886972e980be1623a9fdd56
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-05-12 14:13:37 -0700

    API_PUBLIC_BASE_URL

--- mobile/asa-go/src/utils/env.ts
+++ mobile/asa-go/src/utils/env.ts
@@ -1,7 +1,6 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-export const API_PUBLIC_BASE_URL =
-  (import.meta.env.VITE_PUBLIC_API_BASE_URL as string | undefined) ??
-  API_BASE_URL;
+export const API_PUBLIC_BASE_URL = import.meta.env
+  .VITE_PUBLIC_API_BASE_URL as string;
 export const PMTILES_BUCKET = import.meta.env.VITE_PMTILES_BUCKET as string;
 export const BASEMAP_TILE_URL = import.meta.env.VITE_BASEMAP_TILE_URL as string;
 export const BASEMAP_STYLE_URL = import.meta.env
