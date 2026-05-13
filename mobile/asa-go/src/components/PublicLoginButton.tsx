d67992d98239ff0bca79cd81c90e25d5bb558b5b
Darren Boss <darren.boss@gov.bc.ca>
2026-05-13 09:36:01 -0700

    Post merge updates

--- mobile/asa-go/src/components/PublicLoginButton.tsx
+++ mobile/asa-go/src/components/PublicLoginButton.tsx
@@ -1,9 +1,16 @@
+import { continueAsGuest } from "@/slices/authenticationSlice";
+import { AppDispatch } from "@/store";
 import { Button } from "@mui/material";
+import { useDispatch } from "react-redux";
 
 const PublicLoginButton = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(continueAsGuest());
+  };
   return (
     <Button
-      onClick={() => console.log("PublicLoginButton clicked")}
+      onClick={handleClick}
       sx={{ color: "white", textDecoration: "underline" }}
     >
       Continue as guest
