144197a7944774d231e1872cb54b707c7579d975
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-06-11 15:33:21 -0700

    set_s3_gdal_config

--- backend/packages/wps-jobs/src/fuel_grid/fuel_type_layer.py
+++ backend/packages/wps-jobs/src/fuel_grid/fuel_type_layer.py
@@ -3,12 +3,12 @@
 import logging
 from typing import Generator, Tuple
 
-from osgeo import gdal, ogr, osr
+from osgeo import ogr, osr
 from shapely import wkb, wkt
 
-from wps_shared import config
 from wps_shared.geospatial.geospatial import NAD83_BC_ALBERS
 from wps_shared.utils.polygonize import polygonize_in_memory
+from wps_shared.utils.s3 import set_s3_gdal_config
 
 logger = logging.getLogger(__name__)
 
@@ -21,10 +21,7 @@ def fuel_type_iterator_by_key(fuel_type_raster_key: str) -> Generator[Tuple[int,
     NOTE: This works fine with a small FTL file, such as the SFMS one, but the high resolution
     FTL file uses a large amount of memory when polygonizing.
     """
-    gdal.SetConfigOption("AWS_SECRET_ACCESS_KEY", config.get("OBJECT_STORE_SECRET"))
-    gdal.SetConfigOption("AWS_ACCESS_KEY_ID", config.get("OBJECT_STORE_USER_ID"))
-    gdal.SetConfigOption("AWS_S3_ENDPOINT", config.get("OBJECT_STORE_SERVER"))
-    gdal.SetConfigOption("AWS_VIRTUAL_HOSTING", "FALSE")
+    set_s3_gdal_config()
     logger.info("Polygonizing %s...", fuel_type_raster_key)
     with polygonize_in_memory(fuel_type_raster_key, "fuel", "fuel") as layer:
         spatial_reference: osr.SpatialReference = layer.GetSpatialRef()
