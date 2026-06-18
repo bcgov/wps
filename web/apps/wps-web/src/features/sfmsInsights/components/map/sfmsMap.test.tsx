7b6e1e688018dcd708c3f3029e8bb05fafb68246
conor <con.brad@gmail.com>
2026-06-17 17:34:57 -0700

    unused import

--- web/apps/wps-web/src/features/sfmsInsights/components/map/sfmsMap.test.tsx
+++ web/apps/wps-web/src/features/sfmsInsights/components/map/sfmsMap.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { createVectorTileLayer, getStyleJson } from '@wps/utils/vectorLayerUtils'
 import { DateTime } from 'luxon'
 import { Provider } from 'react-redux'
