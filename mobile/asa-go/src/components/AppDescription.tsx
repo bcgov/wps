b8de6d697bbbdb66cdc8d481cbffe9002e65e66e
Brett Edwards <brett.1.edwards@gov.bc.ca>
2026-07-06 15:04:39 -0700

    ASA Go: Splash Screen wording update (#5584)
    - Updates splash screen wording to match design
    - Changes guest login button text to match design (button still not being used)
    - closes #5515

--- mobile/asa-go/src/components/AppDescription.tsx
+++ mobile/asa-go/src/components/AppDescription.tsx
@@ -11,11 +11,11 @@ const AppDescription = () => {
   return (
     <Box>
       <StyledDescription data-testid="app-description-p1" variant="body1">
-        A spatial analysis tool that automates the continuous monitoring, updating, and communication of anticipated
-        fire behaviour that will challenge direct suppression efforts and put the safety of responders at risk.
+        An app for BC Wildland Firefighters that automates the continuous monitoring, updating, and communication of
+        anticipated fire behaviour.
       </StyledDescription>
       <StyledDescription data-testid="app-description-p2" variant="body1" sx={{ pt: theme.spacing(4) }}>
-        A government of BC IDIR is required for advanced features.
+        A Government of BC IDIR is required for advanced features.
       </StyledDescription>
     </Box>
   )
