d0c44f8c0a5231d75a45a2e83cbb8d2c93c03e48
conor <con.brad@gmail.com>
2026-08-24 18:33:48 -0700

    simplify snow masking

--- backend/packages/wps-api/src/app/auto_spatial_advisory/snow.py
+++ backend/packages/wps-api/src/app/auto_spatial_advisory/snow.py
@@ -1,109 +1,55 @@
 import logging
 import os
+
 import numpy as np
 from osgeo import gdal
 from wps_shared import config
 from wps_shared.db.models.snow import ProcessedSnow
 from wps_shared.geospatial.wps_dataset import WPSDataset
+from wps_shared.utils.s3 import gdal_s3_context
 
-SNOW_COVERAGE_WARPED_NAME = 'snow_coverage_warped.tif'
-SNOW_COVERAGE_MASK_NAME = 'snow_coverage_mask.tif'
-MASKED_HFI_PATH_NAME = 'masked_hfi.tif'
+MASKED_HFI_PATH_NAME = "masked_hfi.tif"
 
 logger = logging.getLogger(__name__)
 
 
-def classify_snow_mask(snow_path: str, temp_dir: str):
+def classify_snow_mask(snow_source: WPSDataset) -> WPSDataset:
     """
-    Given a path to snow coverage data, re-classify the data to act as a mask for future HFI processing.
+    Given snow coverage data, re-classify the data to act as a mask for future HFI processing.
     A NDSI (ie. snow coverage) value between 0-100 represent snow coverage. Here we define snow coverage
     between 10-100. We need to consult the literature or data scientists on proper use of NDSI.
     """
-    snow_mask_path = os.path.join(temp_dir, SNOW_COVERAGE_MASK_NAME)
-    with WPSDataset(snow_path) as source:
-        # In the classified data 0 is assigned to snow covered pixels which will 'cancel' HFI
-        # values when the rasters are multiplied later on. QA values in the original data are
-        # assigned a value of 1 so they dont impact HFI calculations for now.
-        classified = np.where((source > 10) & (source <= 100), 0, 1)
-        source.with_array(classified, datatype=gdal.GDT_Byte, output_path=snow_mask_path)
-    return snow_mask_path
+    # In the classified data 0 is assigned to snow covered pixels which will 'cancel' HFI
+    # values when the rasters are multiplied later on. QA values in the original data are
+    # assigned a value of 1 so they dont impact HFI calculations for now.
+    classified = np.where((snow_source > 10) & (snow_source <= 100), 0, 1)
+    return snow_source.with_array(classified, datatype=gdal.GDT_Byte)
 
 
-async def prepare_snow_mask(hfi_path: str, last_processed_snow: ProcessedSnow, temp_dir: str):
+async def apply_snow_mask(hfi_path: str, last_processed_snow: ProcessedSnow, temp_dir: str) -> str:
+    with gdal_s3_context():
+        bucket = config.get("OBJECT_STORE_BUCKET")
+        for_date = last_processed_snow.for_date
+        # The filename of the snow coverage tiff in our object store, prepended with "vsis3" - which
+        # tells GDAL to use it's S3 virtual file system driver to read the file.
+        # https://gdal.org/user/virtual_file_systems.html
+        snow_key = f"/vsis3/{bucket}/snow_coverage/{for_date.strftime('%Y-%m-%d')}/clipped_snow_coverage_{for_date.strftime('%Y-%m-%d')}_epsg4326.tif"
+        masked_hfi_path = os.path.join(temp_dir, MASKED_HFI_PATH_NAME)
 
-    # Open the HFI tiff to use as a source of parameters for preparation of the snow mask
-    source = gdal.Open(hfi_path, gdal.GA_ReadOnly)
-    geo_transform = source.GetGeoTransform()
-    x_res = geo_transform[1]
-    y_res = -geo_transform[5]
-    minx = geo_transform[0]
-    maxy = geo_transform[3]
-    maxx = minx + geo_transform[1] * source.RasterXSize
-    miny = maxy + geo_transform[5] * source.RasterYSize
-    extent = [minx, miny, maxx, maxy]
-    source_projection = source.GetProjection()
-
-    bucket = config.get('OBJECT_STORE_BUCKET')
-    for_date = last_processed_snow.for_date
-    # The filename of the snow coverage tiff in our object store, prepended with "vsis3" - which tells GDAL to use
-    # it's S3 virtual file system driver to read the file.
-    # https://gdal.org/user/virtual_file_systems.html
-    snow_key = f"/vsis3/{bucket}/snow_coverage/{for_date.strftime('%Y-%m-%d')}/clipped_snow_coverage_{for_date.strftime('%Y-%m-%d')}_epsg4326.tif"
-    snow_mask_warped_output_path = os.path.join(temp_dir, SNOW_COVERAGE_WARPED_NAME)
-    # Perform reprojection to Lambert Conformal Conic to match HFI data, crop extent and resample to 2km x 2km pixels
-    gdal.Warp(snow_mask_warped_output_path, snow_key, dstSRS=source_projection,
-              outputBounds=extent, xRes=x_res, yRes=y_res, resampleAlg=gdal.GRA_NearestNeighbour)
-    source = None
-    return snow_mask_warped_output_path
-    
-def apply_snow_mask_to_hfi(hfi_path: str, snow_mask_path: str, temp_dir: str):
-    # Open the hfi data
-    source_tiff = gdal.Open(hfi_path, gdal.GA_ReadOnly)
-    source_band = source_tiff.GetRasterBand(1)
-    source_data = source_band.ReadAsArray()
-
-    # Open the snow mask
-    snow_tiff = gdal.Open(snow_mask_path, gdal.GA_ReadOnly)
-    snow_band = snow_tiff.GetRasterBand(1)
-    snow_data = snow_band.ReadAsArray()
-
-    # The snow mask tiff has values of 0 or 1 where 0 represents areas covered by snow and 1 represents
-    # snow free areas. Multiply the rasters to apply the mask.
-    masked_data = np.multiply(source_data, snow_data)
-    masked_hfi_path = os.path.join(temp_dir, MASKED_HFI_PATH_NAME)
-    output_driver = gdal.GetDriverByName("GTiff")
-    # Create an object with the same dimensions as the hfi input
-    masked_hfi_tiff = output_driver.Create(masked_hfi_path, xsize=source_band.XSize,
-                                       ysize=source_band.YSize, bands=1, eType=gdal.GDT_Byte)
-    # Set the geotransform and projection to the same as the input.
-    masked_hfi_tiff.SetGeoTransform(source_tiff.GetGeoTransform())
-    masked_hfi_tiff.SetProjection(source_tiff.GetProjection())
-
-    # Write the classified data to the band.
-    masked_hfi_tiff_band = masked_hfi_tiff.GetRasterBand(1)
-    masked_hfi_tiff_band.SetNoDataValue(0)
-    masked_hfi_tiff_band.WriteArray(masked_data)
-
-    # Explicit delete to make sure underlying resources are cleared up!
-    source_data = None
-    source_band = None
-    source_tiff = None
-    snow_data = None
-    snow_band = None
-    snow_tiff = None
-    masked_hfi_tiff_band = None
-    masked_hfi_tiff = None
+        with (
+            WPSDataset(hfi_path, output_path=masked_hfi_path) as hfi_source,
+            WPSDataset(snow_key) as snow_source,
+        ):
+            # Reproject the snow coverage data to match the HFI grid (same projection, extent and
+            # pixel size), classify it into a mask, then apply that mask to the HFI raster.
+            with (
+                snow_source.warp_to_match(hfi_source) as warped_snow,
+                classify_snow_mask(warped_snow) as snow_mask,
+            ):
+                # The snow mask has values of 0 (snow covered) or 1 (snow free); multiplying
+                # applies the mask.
+                masked = hfi_source * snow_mask
+                masked.ds.GetRasterBand(1).SetNoDataValue(0)
+                masked.ds.FlushCache()
 
     return masked_hfi_path
-
-
-async def apply_snow_mask(hfi_path: str, last_processed_snow: ProcessedSnow, temp_dir: str):
-    gdal.SetConfigOption('AWS_SECRET_ACCESS_KEY', config.get('OBJECT_STORE_SECRET'))
-    gdal.SetConfigOption('AWS_ACCESS_KEY_ID', config.get('OBJECT_STORE_USER_ID'))
-    gdal.SetConfigOption('AWS_S3_ENDPOINT', config.get('OBJECT_STORE_SERVER'))
-    gdal.SetConfigOption('AWS_VIRTUAL_HOSTING', 'FALSE')
-
-    snow_mask_path = await prepare_snow_mask(hfi_path, last_processed_snow, temp_dir)
-    snow_masked_classified_path = classify_snow_mask(snow_mask_path, temp_dir)
-    hfi_snow_masked_path = apply_snow_mask_to_hfi(hfi_path, snow_masked_classified_path, temp_dir)
-    return hfi_snow_masked_path
--- backend/packages/wps-api/src/app/tests/auto_spatial_advisory/test_snow.py
+++ backend/packages/wps-api/src/app/tests/auto_spatial_advisory/test_snow.py
@@ -5,34 +5,40 @@ import numpy as np
 from osgeo import gdal, osr
 
 from app.auto_spatial_advisory.snow import classify_snow_mask
+from wps_shared.geospatial.wps_dataset import WPSDataset
+
+
+def _make_raster(path: str, values, datatype, geotransform, projection_wkt, np_dtype=np.uint8):
+    ds = gdal.GetDriverByName("GTiff").Create(path, 2, 2, 1, datatype)
+    ds.SetGeoTransform(geotransform)
+    ds.SetProjection(projection_wkt)
+    ds.GetRasterBand(1).WriteArray(np.array(values, dtype=np_dtype))
+    ds.FlushCache()
+    return ds
 
 
 def test_classify_snow_mask():
     """0 = snow covered (10 < NDSI <= 100), 1 = snow free / QA, preserving georeferencing."""
     with tempfile.TemporaryDirectory() as temp_dir:
         source_path = os.path.join(temp_dir, "snow.tif")
-
         srs = osr.SpatialReference()
         srs.ImportFromEPSG(4326)
-        source_ds = gdal.GetDriverByName("GTiff").Create(source_path, 2, 2, 1, gdal.GDT_Float32)
-        source_ds.SetGeoTransform((0, 1, 0, 0, 0, -1))
-        source_ds.SetProjection(srs.ExportToWkt())
         # values: 5 (below threshold), 50 (snow), 100 (snow, inclusive), 101 (above range/QA)
-        source_ds.GetRasterBand(1).WriteArray(
-            np.array([[5, 50], [100, 101]], dtype=np.float32)
+        source_ds = _make_raster(
+            source_path,
+            [[5, 50], [100, 101]],
+            gdal.GDT_Float32,
+            (0, 1, 0, 0, 0, -1),
+            srs.ExportToWkt(),
+            np_dtype=np.float32,
         )
-        source_ds.FlushCache()
         geotransform = source_ds.GetGeoTransform()
         projection = source_ds.GetProjection()
         source_ds = None
 
-        result_path = classify_snow_mask(source_path, temp_dir)
-
-        result_ds = gdal.Open(result_path, gdal.GA_ReadOnly)
-        result_band = result_ds.GetRasterBand(1)
-
-        assert result_band.ReadAsArray().tolist() == [[1, 0], [0, 1]]
-        assert result_band.DataType == gdal.GDT_Byte
-        assert result_ds.GetGeoTransform() == geotransform
-        assert result_ds.GetProjection() == projection
-        result_ds = None
+        with WPSDataset(source_path) as source, classify_snow_mask(source) as result:
+            raw = result.as_gdal_ds()
+            assert raw.GetRasterBand(1).ReadAsArray().tolist() == [[1, 0], [0, 1]]
+            assert raw.GetRasterBand(1).DataType == gdal.GDT_Byte
+            assert raw.GetGeoTransform() == geotransform
+            assert raw.GetProjection() == projection
